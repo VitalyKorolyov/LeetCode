fe27f9f424c25cadf81381bdef4d272fe199e890
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-01-31 17:09:37 +0300

    add solution of Range Sum of BST

---
+++ c#_LeetCode/BST/Easy/RangeSumOfBST/Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.BST.Easy.RangeSumOfBST
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //https://leetcode.com/problems/range-sum-of-bst/
+    public class Solution
+    {
+        private static int _result = 0;
+
+        public static int RangeSumBST(TreeNode root, int low, int high)
+        {
+            CalcSum(root, low, high);
+            return _result;
+        }
+
+        private static void CalcSum(TreeNode root, int low, int high)
+        {
+            if (root == null) return;
+
+            if (root.val >= low && root.val <= high)
+            {
+                _result += root.val;
+            }
+            if (root.val > low)
+            {
+                RangeSumBST(root.left, low, high);
+            }
+            if (root.val < high)
+            {
+                RangeSumBST(root.right, low, high);
+            }
+        }
+    }
+}
---
+++ c#_LeetCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LeetCode.BST.Easy.RangeSumOfBST;
+
+namespace LeetCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = Solution.RangeSumBST(new TreeNode(10, new TreeNode(5, new TreeNode(3), new TreeNode(7)), new TreeNode(15, null, new TreeNode(18))), 7, 15);
+
+            Console.WriteLine(result);
+        }
+    }
+}
