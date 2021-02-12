ecffe4fe98b6fb5002be56b749473cdd739150dc
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-02-12 22:33:07 +0300

    add solution of Convert Sorted Array to Binary Search Tree

---
+++ LeetCodeTests/BST/Easy/ConvertSortedArrayToBinarySearchTree/SolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using LeetCode.BST.Easy.ConvertSortedArrayToBinarySearchTree;
+
+namespace LeetCodeTests.BST.Easy.ConvertSortedArrayToBinarySearchTree
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var result = Solution.SortedArrayToBST(new[] {-10, -3, 0, 5, 9});
+        }
+    }
+}
---
+++ c#_LeetCode/BST/Easy/ConvertSortedArrayToBinarySearchTree/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace LeetCode.BST.Easy.ConvertSortedArrayToBinarySearchTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+    public class Solution
+    {
+        public static TreeNode SortedArrayToBST(int[] nums)
+        {
+            return !nums.Any() ? null : Build(nums, 0, nums.Length - 1);
+        }
+
+        private static TreeNode Build(int[] nums, int low, int high)
+        {
+            if (low > high) return null;
+
+            var middle = (low + high) / 2;
+
+            var treeNode = new TreeNode(nums[middle])
+            {
+                left = Build(nums, low, middle - 1), 
+                right = Build(nums, middle + 1, high)
+            };
+
+            return treeNode;
+        }
+    }
+}
