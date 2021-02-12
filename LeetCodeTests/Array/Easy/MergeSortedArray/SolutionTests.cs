a894bc5351a845d9186dd4365d2743dd817a8417
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-02-12 21:26:34 +0300

    add solution of Merge Sorted Array; add tests proj;

---
+++ LeetCodeTests/Array/Easy/MergeSortedArray/SolutionTests.cs
@@ -0,0 +1,52 @@
+﻿using Xunit;
+using LeetCode.Array.Easy.MergeSortedArray;
+
+namespace LeetCodeTests.Array.Easy.MergeSortedArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var nums1 = new[] {1, 2, 3, 0, 0, 0};
+            var nums2 = new[] { 2, 5, 6 };
+            var expectedResult = new[] { 1, 2, 2, 3, 5, 6 };
+
+            Solution.Merge(nums1, 3, nums2, 3);
+
+            Assert.Equal(expectedResult, nums1);
+
+            nums1 = new[] { 0, 2, 3, 0, 0, 0 };
+            nums2 = new[] { 2, 5, 6 };
+            expectedResult = new[] { 0, 2, 2, 3, 5, 6 };
+
+            Solution.Merge(nums1, 3, nums2, 3);
+
+            Assert.Equal(expectedResult, nums1);
+
+            nums1 = new[] { 0, 0, 3, 0, 0, 0 };
+            nums2 = new[] { 2, 5, 6 };
+            expectedResult = new[] { 0, 0, 2, 3, 5, 6 };
+
+            Solution.Merge(nums1, 3, nums2, 3);
+
+            Assert.Equal(expectedResult, nums1);
+
+            nums1 = new[] { 1 };
+            nums2 = new int[0];
+            expectedResult = new[] { 1 };
+
+            Solution.Merge(nums1, 1, nums2, 0);
+
+            Assert.Equal(expectedResult, nums1);
+
+            nums1 = new[] { -1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0 };
+            nums2 = new[] { -1, -1, 0, 0, 1, 2 };
+            expectedResult = new[] { -1, -1, -1, 0, 0, 0, 0, 0, 1, 2, 3 };
+
+            Solution.Merge(nums1, 5, nums2, 6);
+
+            Assert.Equal(expectedResult, nums1);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/MergeSortedArray/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Array.Easy.MergeSortedArray
+{
+    //https://leetcode.com/problems/merge-sorted-array/
+    public class Solution
+    {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = m + n - 1, indexOfNums1 = m - 1, indexOfNums2 = n - 1; i > -1; i--)
+            {
+                if (indexOfNums1 < 0)
+                    nums1[i] = nums2[indexOfNums2--];
+                else if (indexOfNums2 < 0 || nums1[indexOfNums1] > nums2[indexOfNums2])
+                    nums1[i] = nums1[indexOfNums1--];
+                else
+                    nums1[i] = nums2[indexOfNums2--];
+            }
+        }
+    }
+}
