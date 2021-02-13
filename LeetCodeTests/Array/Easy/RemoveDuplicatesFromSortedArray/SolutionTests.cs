a2129d8a8250d10cbb2eb5004d6031bce6d077ad
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-02-13 21:25:44 +0300

    add solution of Remove Duplicates from Sorted Array

---
+++ LeetCodeTests/Array/Easy/RemoveDuplicatesFromSortedArray/SolutionTests.cs
@@ -0,0 +1,14 @@
+﻿using LeetCode.Array.Easy.RemoveDuplicatesFromSortedArray;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.RemoveDuplicatesFromSortedArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var result = Solution.RemoveDuplicates(new[] {1, 1, 2});
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/RemoveDuplicatesFromSortedArray/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace LeetCode.Array.Easy.RemoveDuplicatesFromSortedArray
+{
+    public class Solution
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (!nums.Any()) return 0;
+
+            var index = 0;
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                if (nums[index] != nums[i])
+                    nums[++index] = nums[i];
+            }
+
+            return index + 1;
+        }
+    }
+}
