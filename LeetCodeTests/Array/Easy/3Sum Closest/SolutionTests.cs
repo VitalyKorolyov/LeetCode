bf4bb4a1d0881152e8aaa8e69a0e78b7e722f009
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-02-13 22:54:00 +0300

    add solution of 3Sum Closest, need to improve it to O(n^2)

---
+++ LeetCodeTests/Array/Easy/3Sum Closest/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Array.Medium._3Sum_Closest;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy._3Sum_Closest
+{
+    //https://leetcode.com/problems/3sum-closest/
+    public class SolutionTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var result = Solution.ThreeSumClosest(new[] { -1, 2, 1, -4 }, 1);
+            Assert.Equal(2, result);
+
+            result = Solution.ThreeSumClosest(new[] { 3, 2, 1 }, 1);
+            Assert.Equal(6, result);
+
+            result = Solution.ThreeSumClosest(new[] { 1, 1, 1, -1, 0, 1 }, 1);
+            Assert.Equal(1, result);
+
+            result = Solution.ThreeSumClosest(new[] { 1, 1, -1, -1, 3 }, 3);
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/3Sum Closest/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Array.Medium._3Sum_Closest
+{
+    //https://leetcode.com/problems/3sum-closest
+    //O(n^3), can be improved to O(n^2)
+    public class Solution
+    {
+        public static int ThreeSumClosest(int[] nums, int target)
+        {
+            var closestSum = nums[0] + nums[1] + nums[2];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = i + 1; j < nums.Length; j++)
+                {
+                    for (var k = j + 1; k < nums.Length; k++)
+                    {
+                        var sum = nums[i] + nums[j] + nums[k];
+
+                        if (Math.Abs(target - sum) < Math.Abs(target - closestSum))
+                            closestSum = sum;
+
+                        if (closestSum == target) return closestSum;
+                    }
+                }
+            }
+
+            return closestSum;
+        }
+    }
+}
