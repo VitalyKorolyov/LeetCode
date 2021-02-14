9c74536c440743e5585e8cb79722dd240bd787ab
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-02-14 20:33:16 +0300

    add solution of Find Peak Element

---
+++ LeetCodeTests/Array/Medium/FindPeakElement/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using LeetCode.Array.Medium.FindPeakElement;
+
+namespace LeetCodeTests.Array.Medium.FindPeakElement
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Tests()
+        {
+            var result = Solution.FindPeakElement(new[] {1, 2, 3, 1});
+            Assert.Equal(2, result);
+
+            result = Solution.FindPeakElement(new[] { 1, 2, 1, 3, 5, 6, 4 });
+            Assert.Equal(5, result);
+
+            result = Solution.FindPeakElement(new[] { 10 });
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/FindPeakElement/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Array.Medium.FindPeakElement
+{
+    //https://leetcode.com/problems/find-peak-element/
+    public class Solution
+    {
+        public static int FindPeakElement(int[] nums)
+        {
+            var indexResult = 0;
+
+            for (var i = 1; i < nums.Length; i++)
+                if (nums[i] > nums[indexResult])
+                    indexResult = i;
+
+            return indexResult;
+        }
+    }
+}
