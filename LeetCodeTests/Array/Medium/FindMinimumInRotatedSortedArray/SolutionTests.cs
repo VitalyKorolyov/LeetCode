c96eff5ae94f76fd538649e50dbd4634ee4f8028
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-02-14 22:34:55 +0300

    add not completed solution;

---
+++ LeetCodeTests/Array/Medium/FindMinimumInRotatedSortedArray/SolutionTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using LeetCode.Array.Medium.FindMinimumInRotatedSortedArray;
+
+namespace LeetCodeTests.Array.Medium.FindMinimumInRotatedSortedArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Tests()
+        {
+            var result = Solution.FindMin(new[] { 3, 4, 5, 1, 2 });
+            Assert.Equal(1, result);
+
+            result = Solution.FindMin(new[] { 4, 5, 6, 7, 0, 1, 2 });
+            Assert.Equal(0, result);
+
+            result = Solution.FindMin(new[] { 11, 13, 15, 17 });
+            Assert.Equal(11, result);
+
+            result = Solution.FindMin(new[] { 15 });
+            Assert.Equal(15, result);
+
+            result = Solution.FindMin(new[] { 2, 3, 1 });
+            Assert.Equal(1, result);
+
+            result = Solution.FindMin(new[] { 1, 2 });
+            Assert.Equal(1, result);
+
+            result = Solution.FindMin(new[] { 2, 1 });
+            Assert.Equal(1, result);
+
+            result = Solution.FindMin(new[] { 2, 3, 4, 5, 1 });
+            Assert.Equal(1, result);
+
+            result = Solution.FindMin(new[] { 5, 1, 2, 3, 4 });
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/FindMinimumInRotatedSortedArray/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Array.Medium.FindMinimumInRotatedSortedArray
+{
+    //https://leetcode.com/problems/find-minimum-in-rotated-sorted-array
+    public class Solution
+    {
+        public static int FindMin(int[] nums)
+        {
+            if (nums.Length == 1) return nums[0];
+
+            return Min(nums, 0, nums.Length - 1);
+        }
+
+        private static int Min(int[] nums, int low, int high)
+        {
+            var middle = (high + low) / 2;
+
+            if (nums[middle] > nums[middle + 1]) return nums[middle + 1];
+            if (nums[middle] < nums[middle - 1]) return nums[middle];
+
+            if (nums[middle] > nums[low])
+                return Min(nums, middle + 1, high);
+
+            return 0;
+    }
+}
