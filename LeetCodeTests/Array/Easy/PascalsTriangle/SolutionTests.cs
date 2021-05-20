9b2ff1107c295ba14424524b407545d009c128a6
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-05-21 00:12:20 +0300

    add solution of Pascal's Triangle

---
+++ LeetCodeTests/Array/Easy/PascalsTriangle/SolutionTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.Array.Easy.PascalsTriangle;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.PascalsTriangle
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var exResult = new List<List<int>>()
+            {
+                new List<int> {1},
+                new List<int> {1, 1},
+                new List<int> {1, 2, 1},
+                new List<int> {1, 3, 3, 1},
+                new List<int> {1, 4, 6, 4, 1},
+            };
+            var result = Solution.Generate(5);
+
+            foreach (var (ex, res) in exResult.Zip(result))
+            {
+                Assert.Equal(ex, res);
+            }
+
+            exResult = new List<List<int>>()
+            {
+                new List<int> {1},
+            };
+            result = Solution.Generate(1);
+
+            foreach (var (ex, res) in exResult.Zip(result))
+            {
+                Assert.Equal(ex, res);
+            }
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/PascalsTriangle/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Easy.PascalsTriangle
+{
+    //https://leetcode.com/problems/pascals-triangle/
+    public static class Solution
+    {
+        public static IList<IList<int>> Generate(int numRows)
+        {
+            var resultLIst = new List<IList<int>>(numRows);
+
+            for (int i = 0; i < numRows; i++)
+            {
+                var prevIndexLayer = i == 0 ? i : i - 1;
+                var nextLayerSize = i + 1;
+                var nextLayer = new List<int>(nextLayerSize);
+
+                for (int j = 0; j < nextLayerSize; j++)
+                {
+                    if (j == 0 || j == resultLIst[prevIndexLayer].Count)
+                    {
+                        nextLayer.Add(1);
+                        continue;
+                    }
+
+                    nextLayer.Add(resultLIst[prevIndexLayer][j - 1] + resultLIst[prevIndexLayer][j]);
+                }
+
+                resultLIst.Add(nextLayer);
+            }
+
+            return resultLIst;
+        }
+    }
+}
