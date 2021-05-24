b39a0d7ddb4071118ce1a16fc9b73aeace8bf684
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-05-25 00:07:06 +0300

    add solution of Linked List Cycle

---
+++ LeetCodeTests/LinkedList/Easy/LinkedListCycleTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeetCode.LinkedList.Easy.LinkedListCycle;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Easy
+{
+    public class LinkedListCycleTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var head = new ListNode(1, new ListNode(3, null));
+
+            Assert.False(Solution.HasCycle(head));
+
+            var first = new ListNode(10, new ListNode(11, null));
+            head = new ListNode(1, first);
+            first.next.next = head;
+
+            Assert.True(Solution.HasCycle(head));
+
+            Assert.False(Solution.HasCycle(null));
+        }
+    }
+}
---
+++ c#_LeetCode/LinkedList/Easy/LinkedListCycle/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.LinkedList.Easy.LinkedListCycle
+{
+    //https://leetcode.com/problems/linked-list-cycle/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x, ListNode y)
+        {
+            val = x;
+            next = y;
+        }
+    }
+
+    public static class Solution
+    {
+        public static bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+
+            var slow = head;
+            var fast = head.next;
+
+            while (slow != fast)
+            {
+                if (slow == null || fast == null) return false;
+
+                slow = slow.next;
+                fast = fast.next?.next;
+            }
+
+            return true;
+        }
+    }
+}
