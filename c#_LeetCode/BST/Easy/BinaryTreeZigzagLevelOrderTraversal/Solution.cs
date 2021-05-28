831e7a316c26ddbe88463e9617f762189437c815
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-05-28 23:15:47 +0300

    add some solutions of trees;

---
+++ c#_LeetCode/BST/Easy/BinaryTreeZigzagLevelOrderTraversal/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.BST.Easy.BinaryTreeZigzagLevelOrderTraversal
+{
+    //https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            List<List<int>> result = new List<List<int>>();
+            if (root == null) result;
+
+
+        }
+
+        private 
+    }
+}
---
+++ c#_LeetCode/BST/Easy/MaximumDepthOfBinaryTree/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LeetCode.BST.Easy.MaximumDepthOfBinaryTree
+{
+
+    //https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Solution
+    {
+        public static int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+        }
+    }
+}
