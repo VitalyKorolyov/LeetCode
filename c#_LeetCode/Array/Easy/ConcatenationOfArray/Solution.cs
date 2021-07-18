fe3f910c23f083555743ed11af85a44df25b1201
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-18 21:43:37 +0300

    add solution of Concatenation of Array

---
+++ c#_LeetCode/Array/Easy/ConcatenationOfArray/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Array.Easy.ConcatenationOfArray
+{
+    //https://leetcode.com/problems/concatenation-of-array/submissions/
+    public class Solution
+    {
+        public static int[] GetConcatenation(int[] nums)
+        {
+            var ans = new int[nums.Length * 2];
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                ans[i] = nums[i];
+                ans[nums.Length + i] = nums[i];
+            }
+
+            return ans;
+        }
+    }
+}
