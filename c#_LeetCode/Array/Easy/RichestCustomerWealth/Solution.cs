1f2fb82de5f6298c3d6c34864950e8c6b3229653
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-19 20:31:30 +0300

    add solution of Richest Customer Wealth

---
+++ c#_LeetCode/Array/Easy/RichestCustomerWealth/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Array.Easy.RichestCustomerWealth
+{
+    //https://leetcode.com/problems/richest-customer-wealth/
+    public class Solution
+    {
+        public static int MaximumWealth(int[][] accounts)
+        {
+            var result = 0;
+
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                var sum = 0;
+                for (int j = 0; j < accounts[i].Length; j++)
+                {
+                    sum += accounts[i][j];
+                }
+
+                if (result < sum) result = sum;
+            }
+
+            return result;
+        }
+    }
+}
