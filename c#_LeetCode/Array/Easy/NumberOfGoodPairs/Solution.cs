fa12f6f8c91e2ce2356d3ea85e6fe770579bc633
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-19 21:03:35 +0300

    add solution of Number of Good Pairs;

---
+++ c#_LeetCode/Array/Easy/NumberOfGoodPairs/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Array.Easy.NumberOfGoodPairs
+{
+    //https://leetcode.com/problems/number-of-good-pairs/
+    public class Solution
+    {
+        public static int NumIdenticalPairs(int[] nums)
+        {
+            var result = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j]) result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
