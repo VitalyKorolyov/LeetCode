b48bdd0b4f886525aa67c71fbde59dbc25737454
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-19 20:52:56 +0300

    add solution of Kids With the Greatest Number of Candies

---
+++ c#_LeetCode/Array/Easy/KidsWithTheGreatestNumberOfCandies/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Easy.KidsWithTheGreatestNumberOfCandies
+{
+    //https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+    public class Solution
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            IList<bool> listResult = new List<bool>(candies.Length);
+            
+            for (int i = 0; i < candies.Length; i++)
+            {
+                var currentCandies = candies[i] + extraCandies;
+                var result = true;
+
+                for (int j = 0; j < candies.Length; j++)
+                {
+                    if (currentCandies >= candies[j]) continue;
+
+                    result = false;
+                    break;
+                }
+
+                listResult.Add(result);
+            }
+
+            return listResult;
+        }
+    }
+}
