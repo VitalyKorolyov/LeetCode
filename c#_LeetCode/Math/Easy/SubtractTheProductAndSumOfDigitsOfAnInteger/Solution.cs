2502ce0c4bb5c52640f3f3872983d2e2a082ab0a
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-21 23:17:34 +0300

    add solution of Subtract the Product and Sum of Digits of an Integer

---
+++ c#_LeetCode/Math/Easy/SubtractTheProductAndSumOfDigitsOfAnInteger/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Math.Easy.SubtractTheProductAndSumOfDigitsOfAnInteger
+{
+    //https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+    public class Solution
+    {
+        public static int SubtractProductAndSum(int n)
+        {
+            var sum = 0;
+            var product = 1;
+
+            while (n > 0)
+            {
+                var number = n % 10;
+
+                sum += number;
+                product *= number;
+
+                n /= 10;
+            }
+
+            return product - sum;
+        }
+    }
+}
