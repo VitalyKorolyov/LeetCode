40d69699adefc1235632979a6117de969948c517
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-21 22:57:07 +0300

    add solution of Shuffle String

---
+++ c#_LeetCode/Array/Easy/ShuffleString/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace LeetCode.Array.Easy.ShuffleString
+{
+    //https://leetcode.com/problems/shuffle-string/
+    public class Solution
+    {
+        public static string RestoreString(string s, int[] indices)
+        {
+            var result = new StringBuilder(s.Length);
+
+            foreach (var indicate in indices)
+                result.Append(s[indicate]);
+
+            return result.ToString();
+        }
+    }
+}
