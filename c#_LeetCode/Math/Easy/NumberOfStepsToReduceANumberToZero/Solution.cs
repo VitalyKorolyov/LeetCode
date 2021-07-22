c9d8348c73842fdfafd84affc0b81906c3feaa4e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-22 21:02:55 +0300

    add solution of Number of Steps to Reduce a Number to Zero;

---
+++ c#_LeetCode/Math/Easy/NumberOfStepsToReduceANumberToZero/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Math.Easy.NumberOfStepsToReduceANumberToZero
+{
+    //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+    public class Solution
+    {
+        public static int NumberOfSteps(int num)
+        {
+            var counter = 0;
+
+            while (num != 0)
+            {
+                num = num % 2 == 0 ? num / 2 : num - 1;
+
+                counter++;
+            }
+
+
+            return counter;
+        }
+    }
+}
