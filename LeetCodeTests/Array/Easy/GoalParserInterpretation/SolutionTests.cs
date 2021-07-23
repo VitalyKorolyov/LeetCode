6b6c3a49a8ee61110c241302fb8fd4b158404625
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-23 22:41:28 +0300

    add solution of Goal Parser Interpretation

---
+++ LeetCodeTests/Array/Easy/GoalParserInterpretation/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Easy.GoalParserInterpretation;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.GoalParserInterpretation
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Interpret_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string command = "G()(al)";
+
+            // Act
+            var result = Solution.Interpret(command);
+
+            // Assert
+            Assert.Equal("Goal", result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/GoalParserInterpretation/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Array.Easy.GoalParserInterpretation
+{
+    //https://leetcode.com/problems/goal-parser-interpretation/
+    public class Solution
+    {
+        public static string Interpret(string command)
+        {
+            var str = new Dictionary<string, string>
+            {
+                {"G", "G"}, 
+                {"()", "o"}, 
+                {"(al)", "al"}
+            };
+
+            var stringBuilder = new StringBuilder(100);
+
+            for (int i = 0; i < command.Length; i++)
+            {
+                for (int j = i + 1; j <= command.Length; j++)
+                {
+                    var key = command[i..j];
+                    if (str.ContainsKey(key))
+                    {
+                        stringBuilder.Append(str[key]);
+                        i = j - 1;
+                        break;
+                    }
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
