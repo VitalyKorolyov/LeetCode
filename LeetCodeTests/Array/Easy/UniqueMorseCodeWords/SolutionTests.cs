558be99504a0d9b17545479d017a1a3543fc652f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-23 21:47:58 +0300

    add solution of Unique Morse Code Words

---
+++ LeetCodeTests/Array/Easy/UniqueMorseCodeWords/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Easy.UniqueMorseCodeWords;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.UniqueMorseCodeWords
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void UniqueMorseRepresentations_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string[] words = { "gin", "zen", "gig", "msg" };
+
+            // Act
+            var result = Solution.UniqueMorseRepresentations(words);
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/UniqueMorseCodeWords/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Array.Easy.UniqueMorseCodeWords
+{
+    //https://leetcode.com/problems/unique-morse-code-words/
+    public class Solution
+    {
+        public static int UniqueMorseRepresentations(string[] words)
+        {
+            var morseCodes = new[]{".-", "-...", "-.-.", "-..", ".", "..-.", 
+            "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", 
+            ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", 
+            "-.--", "--.."};
+
+            var hash = new HashSet<string>();
+
+            foreach (var word in words)
+            {
+                var stringBuilder = new StringBuilder(word.Length * 4);
+
+                foreach (var c in word)
+                    stringBuilder.Append(morseCodes[c - 'a']);
+
+                var str = stringBuilder.ToString();
+                if (!hash.Contains(str))
+                    hash.Add(str);
+            }
+
+            return hash.Count;
+        }
+    }
+}
