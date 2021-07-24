3fe2c2458fb5eeca8d2d6eeae1c87f4b7d46f423
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-24 19:31:53 +0300

    add solution of Flipping an Image;

---
+++ c#_LeetCode/Array/Easy/FlippingAnImage/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Array.Easy.FlippingAnImage
+{
+    //https://leetcode.com/problems/flipping-an-image/
+    public class Solution
+    {
+        public static int[][] FlipAndInvertImage(int[][] image)
+        {
+            for (int i = 0; i < image.Length; i++)
+            {
+                for (int j = 0, z = image[i].Length - 1; j <= z; j++, z--)
+                {
+                    var valuej = image[i][j] == 0 ? 1 : 0;
+                    var valuez = image[i][z] == 0 ? 1 : 0;
+
+                    image[i][j] = valuez;
+                    image[i][z] = valuej;
+                }
+            }
+
+            return image;
+        }
+    }
+}
