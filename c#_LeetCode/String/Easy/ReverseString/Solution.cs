b354ab0e4fd8cc1c2513bfcfeca1092cd59debc4
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-24 19:17:50 +0300

    add solution of Reverse String;

---
+++ c#_LeetCode/String/Easy/ReverseString/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.String.Easy.ReverseString
+{
+    //https://leetcode.com/problems/reverse-string/
+    public class Solution
+    {
+        public static void ReverseString(char[] s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            {
+                var value = s[i];
+                s[i] = s[j];
+                s[j] = value;
+            }
+        }
+    }
+}
