598fa85df5d95f79c010815b4cdc9f26eeb74151
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-24 19:03:40 +0300

    add solution of Find Center of Star Graph

---
+++ c#_LeetCode/Graph/Easy/FindCenterOfStarGraph.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Graph.Easy
+{
+    //https://leetcode.com/problems/find-center-of-star-graph/
+    public class FindCenterOfStarGraph
+    {
+        public static int FindCenter(int[][] edges)
+        {
+            return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+        }
+    }
+}
