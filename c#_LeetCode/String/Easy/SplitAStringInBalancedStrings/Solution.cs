6074d91d704f56c19f78904be929b0fcca606187
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-24 17:27:07 +0300

    add solution of Split a String in Balanced Strings

---
+++ c#_LeetCode/String/Easy/SplitAStringInBalancedStrings/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.String.Easy.SplitAStringInBalancedStrings
+{
+    //https://leetcode.com/problems/split-a-string-in-balanced-strings/
+    public class Solution
+    {
+        public static int BalancedStringSplit(string s)
+        {
+            var max = 0;
+            var charCounter = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var item = s[i];
+
+                if (item == 'L')
+                    charCounter++;
+                else charCounter--;
+
+                if (charCounter == 0) max++;
+            }
+
+            return max;
+        }
+    }
+}
