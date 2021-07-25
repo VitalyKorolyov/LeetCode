6d0f1f1ca6705cff96a718fdd6725d0f28cb82c7
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-25 19:56:45 +0300

    add solution of XOR Operation in an Array

---
+++ c#_LeetCode/Array/Easy/XOROperationInAnArray/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Array.Easy.XOROperationInAnArray
+{
+    //https://leetcode.com/problems/xor-operation-in-an-array/
+    public class Solution
+    {
+        public int XorOperation(int n, int start)
+        {
+            var result = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                result ^= start + 2 * i;
+            }
+
+            return result;
+        }
+    }
+}
