ddd2ae4f03c67944097c78581b8a759bcd8ee817
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-28 20:49:02 +0300

    add solution of Missing Number

---
+++ c#_LeetCode/Array/Easy/MissingNumber/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode.Array.Easy.MissingNumber
+{
+    //https://leetcode.com/problems/missing-number/
+    public class Solution
+    {
+        public static int MissingNumber(int[] nums)
+        {
+            var expectedSum = nums.Length * (nums.Length + 1) / 2;
+
+            var currentSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+                currentSum += nums[i];
+
+            return expectedSum - currentSum;
+        }
+    }
+}
