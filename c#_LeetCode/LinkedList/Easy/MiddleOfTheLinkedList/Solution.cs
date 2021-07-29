b317eb5aad6d3d5d5f3655f8f16f5b3d875f5ebd
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-07-29 21:37:39 +0300

    add solution of Middle of the Linked List

---
+++ c#_LeetCode/LinkedList/Easy/MiddleOfTheLinkedList/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.LinkedList.Easy.MiddleOfTheLinkedList
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //https://leetcode.com/problems/middle-of-the-linked-list/submissions/
+    public class Solution
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            var slowPointer = head;
+            var fastPointer = head;
+
+            while (fastPointer?.next != null)
+            {
+                slowPointer = slowPointer.next;
+                fastPointer = fastPointer.next.next;
+            }
+
+            return slowPointer;
+        }
+    }
+}
