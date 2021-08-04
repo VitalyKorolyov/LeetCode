708ec5ea488208d82111ba674479f7085af87375
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-04 21:51:44 +0300

    add solution of Maximum Product Difference Between Two Pairs

---
+++ LeetCodeTests/Array/Easy/MaximumProductDifferenceBetweenTwoPairs/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Easy.MaximumProductDifferenceBetweenTwoPairs;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.MaximumProductDifferenceBetweenTwoPairs
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MaxProductDifference_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            int[] nums = { 4, 2, 5, 9, 7, 4, 8 };
+
+            // Act
+            var result = Solution.MaxProductDifference(
+                nums);
+
+            // Assert
+            Assert.Equal(64, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/MaximumProductDifferenceBetweenTwoPairs/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Array.Easy.MaximumProductDifferenceBetweenTwoPairs
+{
+    //https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+    public class Solution
+    {
+        public static int MaxProductDifference(int[] nums)
+        {
+            var firstL = int.MinValue;
+            var secondL = int.MinValue;
+            var firstS = int.MaxValue;
+            var secondS = int.MaxValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (firstL <= nums[i])
+                {
+                    secondL = firstL;
+                    firstL = nums[i];
+                }
+                else if(secondL < nums[i])
+                {
+                    secondL = nums[i];
+                }
+
+                if (firstS > nums[i])
+                {
+                    secondS = firstS;
+                    firstS = nums[i];
+                }
+                else if(secondS > nums[i])
+                {
+                    secondS = nums[i];
+                }
+            }
+
+            return firstL * secondL - firstS * secondS;
+        }
+    }
+}
