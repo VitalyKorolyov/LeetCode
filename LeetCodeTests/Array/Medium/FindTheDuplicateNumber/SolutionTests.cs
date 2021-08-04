b1b5f8ef954a5fee4289a831b6004f1de652f452
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-04 21:45:15 +0300

    add solution of Find the Duplicate Number

---
+++ LeetCodeTests/Array/Medium/FindTheDuplicateNumber/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.FindTheDuplicateNumber;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.FindTheDuplicateNumber
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FindDuplicate_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            int[] nums = {4, 2, 3, 4};
+
+            // Act
+            var result = Solution.FindDuplicate(nums);
+
+            // Assert
+            Assert.True(result == 1);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/FindTheDuplicateNumber/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Array.Medium.FindTheDuplicateNumber
+{
+    //https://leetcode.com/problems/find-the-duplicate-number/
+    public static class Solution
+    {
+        //Negative Marking
+        public static int FindDuplicate(int[] nums)
+        {
+            var dup = -1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var cur = nums[i];
+
+                if (nums[cur] < 0)
+                {
+                    dup = System.Math.Abs(nums[cur]);
+                    break;
+                }
+
+                nums[cur] *= -1;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = System.Math.Abs(nums[i]);
+            }
+
+            return dup;
+        }
+    }
+}
