c8b45cdb2585537270cca09db51167a60883d8a9
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-08 20:31:45 +0300

    add solution of Palindrome Linked List

---
+++ LeetCodeTests/LinkedList/Easy/PalindromeLinkedList/ListNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.LinkedList.Easy.PalindromeLinkedList;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Easy.PalindromeLinkedList
+{
+    public class ListNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var listNode = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+
+            // Act
+            var res = Solution.IsPalindrome(listNode);
+
+            // Assert
+            Assert.True(res);
+        }
+    }
+}
---
+++ c#_LeetCode/LinkedList/Easy/PalindromeLinkedList/Solution.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.LinkedList.Easy.PalindromeLinkedList
+{
+    //https://leetcode.com/problems/palindrome-linked-list/
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public static bool IsPalindrome(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+
+            while (fast?.next?.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            ListNode prev = null;
+            while (slow != null)
+            {
+                var next = slow.next;
+                slow.next = prev;
+                prev = slow;
+                slow = next;
+            }
+
+            while (prev != null && head != null)
+            {
+                if (prev.val != head.val) return false;
+                prev = prev.next;
+                head = head.next;
+            }
+
+            return true;
+        }
+    }
+}
