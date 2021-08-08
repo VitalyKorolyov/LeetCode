b8adfb9dfec810d5e1155f1eac76974a06e0c83b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-08 21:34:34 +0300

    add solution of Remove Linked List Elements

---
+++ c#_LeetCode/LinkedList/Easy/RemoveLinkedListElements/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.LinkedList.Easy.RemoveLinkedListElements
+{
+    //https://leetcode.com/problems/remove-linked-list-elements/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            var fake = new ListNode(0);
+            fake.next = head;
+
+            var cur = fake;
+
+            while (cur.next != null)
+            {
+                if (cur.next.val == val)
+                {
+                    cur.next = cur.next.next;
+                }
+                else
+                {
+                    cur = cur.next;
+                }
+            }
+
+            return fake.next;
+        }
+    }
+}
