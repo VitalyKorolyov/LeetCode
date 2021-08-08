30d922131b440d0157406344b2c93f7936d68bba
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-08 21:43:39 +0300

    add solution of Remove Duplicates from Sorted List

---
+++ c#_LeetCode/LinkedList/Easy/RemoveDuplicatesFromSortedList/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.LinkedList.Easy.RemoveDuplicatesFromSortedList
+{
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            var fake = new ListNode(0);
+            fake.next = head;
+
+            var cur = fake;
+
+            while (cur.next != null && cur.next.next != null)
+            {
+                if (cur.next.val == cur.next.next.val)
+                {
+                    cur.next = cur.next.next;
+                }
+                else
+                {
+                    cur = cur.next;
+                }
+            }
+
+            return fake.next;
+        }
+    }
+}
