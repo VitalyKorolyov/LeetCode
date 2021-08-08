1b87d091a6c430ef4d7ef024db332aa21750655a
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-08 22:15:07 +0300

    add solution of Reverse Words in a String III

---
+++ LeetCodeTests/String/Easy/ReverseWordsInAStringIII/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Easy.ReverseWordsInAStringIII;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.ReverseWordsInAStringIII
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void ReverseWords_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string s = "s'teL ekat edoCteeL tsetnoc";
+
+            // Act
+            var result = Solution.ReverseWords(
+                s);
+
+            // Assert
+            Assert.Equal("Let's take LeetCode contest", result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/ReverseWordsInAStringIII/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace LeetCode.String.Easy.ReverseWordsInAStringIII
+{
+    //https://leetcode.com/problems/reverse-words-in-a-string-iii/
+    public class Solution
+    {
+        public static string ReverseWords(string s)
+        {
+            var rSpaceIndex = 0;
+            var res = new StringBuilder(s.Length);
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ' || s.Length - 1 == i)
+                {
+                    var lSpaceIndex = rSpaceIndex;
+                    rSpaceIndex = i;
+
+                    for (int j = rSpaceIndex; j >= lSpaceIndex; j--)
+                    {
+                        if(s[j] == ' ') continue;
+
+                        res.Append(s[j]);
+                    }
+
+                    if (s.Length - 1 != i) res.Append(' ');
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+}
