435211f89a37e1c16d80fc451446af78cc90f3f8
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-09 21:44:05 +0300

    add solution of Sorting the Sentence

---
+++ LeetCodeTests/String/Easy/SortingTheSentence/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Easy.SortingTheSentence;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.SortingTheSentence
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SortSentence_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            string s = "is2 sentence4 This1 a3";
+
+            // Act
+            var result = Solution.SortSentence(
+                s);
+
+            // Assert
+            Assert.Equal("This is a sentence", result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/SortingTheSentence/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace LeetCode.String.Easy.SortingTheSentence
+{
+    //https://leetcode.com/problems/sorting-the-sentence/
+    public class Solution
+    {
+        public static string SortSentence(string s)
+        {
+            var words = s.Split(' ');
+            var str = new StringBuilder(s.Length - words.Length);
+
+            for (int i = 49; i <= 49 + words.Length; i++)
+            {
+                var c = (char) i;
+
+                var currentWord = string.Empty;
+
+                foreach (var word in words)
+                {
+                    if (word[word.Length - 1] == c)
+                        currentWord = word.Substring(0, word.Length - 1);
+                }
+
+                if (i == 49 + words.Length - 1)
+                {
+                    str.Append(currentWord);
+                    break;
+                }
+
+                str.Append(currentWord + " ");
+            }
+
+            return str.ToString();
+        }
+    }
+}
