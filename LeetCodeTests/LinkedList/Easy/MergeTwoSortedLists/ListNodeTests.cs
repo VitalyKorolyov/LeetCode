da25518ff55f27889b86fcbadfa23b0e30e8cf2c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-11 00:07:07 +0300

    add solution of Merge Two Sorted Lists

---
+++ LeetCodeTests/LinkedList/Easy/MergeTwoSortedLists/ListNodeTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.LinkedList.Easy.MergeTwoSortedLists;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Easy.MergeTwoSortedLists
+{
+    public class ListNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var listNode1 = new ListNode(1, new ListNode(2, new ListNode(3)));
+            var listNode2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+            // Act
+            var result = Solution.MergeTwoLists(listNode1, listNode2);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/LinkedList/Easy/MergeTwoSortedLists/Solution.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.LinkedList.Easy.MergeTwoSortedLists
+{
+    //https://leetcode.com/problems/merge-two-sorted-lists/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            var dummy = new ListNode();
+            var res = dummy;
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val < l2.val)
+                {
+                    dummy.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    dummy.next = l2;
+                    l2 = l2.next;
+                }
+
+                dummy = dummy.next;
+            }
+
+            dummy.next = l1 ?? l2;
+
+            return res.next;
+        }
+    }
+}
