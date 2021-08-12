7879b7f82b619f86380e5b6bfeec7643ceafb31d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-12 21:31:23 +0300

    add solution of Binary Search

---
+++ LeetCodeTests/Array/Easy/BinarySearch/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Array.Easy.BinarySearch;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.BinarySearch
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Search_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            int[] nums = { -1, 0, 3, 5, 9, 12 };
+            int target = -2;
+
+            // Act
+            var result = Solution.Search(
+                nums,
+                target);
+
+            // Assert
+            Assert.Equal(-1, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/BinarySearch/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Array.Easy.BinarySearch
+{
+    //https://leetcode.com/problems/binary-search/
+    public class Solution
+    {
+        public static int Search(int[] nums, int target)
+        {
+            if (nums.Length != 0 && (nums[0] > target || nums[nums.Length - 1] < target)) return -1;
+
+            var l = 0;
+            var r = nums.Length - 1;
+
+            while (r >= l)
+            {
+                var index = l + (r - l) / 2;
+
+                if (nums[index] == target) return index;
+
+                if (nums[index] < target)
+                {
+                    l = index + 1;
+                }
+                else
+                {
+                    r = index - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
