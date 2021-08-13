3b69724b6d98d65c363ba9c3ad68172d2364cf44
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-13 21:09:51 +0300

    add solution of Check if the Sentence Is Pangram

---
+++ c#_LeetCode/HashTable/Easy/CheckifTheSentenceIsPangram/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable.Easy.CheckifTheSentenceIsPangram
+{
+    //https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+    public class Solution
+    {
+        public static bool CheckIfPangram(string sentence)
+        {
+            var hashMap = new HashSet<char>();
+
+            for (var i = 0; i < sentence.Length; i++)
+            {
+                var c = sentence[i];
+                if (hashMap.Contains(c)) continue;
+
+                hashMap.Add(c);
+            }
+
+            return hashMap.Count == 26;
+        }
+    }
+}
