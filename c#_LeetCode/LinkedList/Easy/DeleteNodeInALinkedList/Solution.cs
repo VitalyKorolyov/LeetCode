48a13babaacd20ba5939c5151b9ebaccd4476b16
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-13 20:46:12 +0300

    add solution of Delete Node in a Linked List

---
+++ c#_LeetCode/LinkedList/Easy/DeleteNodeInALinkedList/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.LinkedList.Easy.DeleteNodeInALinkedList
+{
+    //https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+    public class Solution
+    {
+        public static void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
