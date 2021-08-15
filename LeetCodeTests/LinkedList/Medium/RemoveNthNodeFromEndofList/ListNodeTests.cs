9400a4661f621476cd98627cce651fec1f5315ae
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-15 18:46:55 +0300

    add solution of Remove Nth Node From End of List

---
+++ LeetCodeTests/LinkedList/Medium/RemoveNthNodeFromEndofList/ListNodeTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.LinkedList.Medium.RemoveNthNodeFromEndofList;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Medium.RemoveNthNodeFromEndofList
+{
+    public class ListNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+            // Act
+            var result = Solution.RemoveNthFromEnd(listNode, 2);
+
+            listNode = new ListNode(1, new ListNode(2));
+
+            result = Solution.RemoveNthFromEnd(listNode, 1);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/LinkedList/Medium/RemoveNthNodeFromEndofList/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.LinkedList.Medium.RemoveNthNodeFromEndofList
+{
+    //https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var f = head;
+            var s = head;
+
+            for (var i = 0; i < n; i++)
+                f = f.next;
+
+            if (f == null) return head.next;
+
+            while (f.next != null)
+            {
+                s = s.next;
+                f = f.next;
+            }
+
+            s.next = s.next.next;
+
+            return head;
+        }
+    }
+}
