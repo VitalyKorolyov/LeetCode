f0180800e623e52b3e697c7467cfd1583d4fb6bf
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-15 19:47:38 +0300

    add solution of Find Smallest Letter Greater Than Target

---
+++ LeetCodeTests/Array/Easy/FindSmallestLetterGreaterThanTarget/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Array.Easy.FindSmallestLetterGreaterThanTarget;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.FindSmallestLetterGreaterThanTarget
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void NextGreatestLetter_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            char[] letters = { 'c', 'f', 'j' };
+            char target = 'd';
+
+            // Act
+            var result = Solution.NextGreatestLetter(
+                letters,
+                target);
+
+            // Assert
+            Assert.Equal('f', result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/FindSmallestLetterGreaterThanTarget/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Array.Easy.FindSmallestLetterGreaterThanTarget
+{
+    //https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+    public class Solution
+    {
+        public static char NextGreatestLetter(char[] letters, char target)
+        {
+            var l = 0;
+            var r = letters.Length - 1;
+
+            if (letters[l] > target) return letters[l];
+
+            while (r >= l)
+            {
+                var pivot = l + (r - l) / 2;
+
+                if (letters[pivot] > target)
+                    r = pivot - 1;
+                else
+                    l = pivot + 1;
+            }
+
+            return letters[l % letters.Length];
+        }
+    }
+}
