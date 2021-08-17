47b9a44730ae265aec90a2f097f07fcbc3569c67
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-18 00:26:01 +0300

    add solution of Majority Element

---
+++ c#_LeetCode/Array/Easy/MajorityElement/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Array.Easy.MajorityElement
+{
+    //https://leetcode.com/problems/majority-element/
+    public class Solution
+    {
+        public static int MajorityElement(int[] nums)
+        {
+            var candidate = -1;
+            var count = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (count == 0)
+                {
+                    candidate = nums[i];
+                    count++;
+                }
+                else
+                {
+                    count += nums[i] == candidate ? 1 : -1;
+                }
+            }
+
+            return candidate;
+        }
+    }
+}
