825b328e9c67570b52f08a3d9d663c854f39477f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-18 21:29:45 +0300

    add solution of Average of Levels in Binary Tree

---
+++ LeetCodeTests/BST/Easy/AverageOfLevelsInBinaryTree/TreeNodeTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.BST.Easy.AverageOfLevelsInBinaryTree;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCodeTests.BST.Easy.AverageOfLevelsInBinaryTree
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+
+            // Act
+            var res = Solution.AverageOfLevels(treeNode);
+
+            var ex = new List<double> { 3.00000, 14.50000, 11.00000 };
+
+            // Assert
+            Assert.Equal(ex, res);
+        }
+    }
+}
---
+++ c#_LeetCode/BST/Easy/AverageOfLevelsInBinaryTree/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.BST.Easy.AverageOfLevelsInBinaryTree
+{
+    //https://leetcode.com/problems/average-of-levels-in-binary-tree/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public static IList<double> AverageOfLevels(TreeNode root)
+        {
+            if (root == null) return new List<double>();
+
+            var bfs = new Queue<TreeNode>();
+            bfs.Enqueue(root);
+            var res = new List<double>();
+
+            while (bfs.Count > 0)
+            {
+                var count = bfs.Count;
+                double curr = 0;
+
+                for (var i = 0; i < count; i++)
+                {
+                    var node = bfs.Dequeue();
+                    curr += node.val;
+
+                    if (node.left != null)
+                        bfs.Enqueue(node.left);
+
+                    if (node.right != null)
+                        bfs.Enqueue(node.right);
+                }
+
+                res.Add(curr / count);
+            }
+
+            return res;
+        }
+    }
+}
