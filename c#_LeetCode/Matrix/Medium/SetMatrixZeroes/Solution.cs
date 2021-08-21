509b1ee65fa9a4dde9730a4665ff4a14d2846395
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-21 22:20:46 +0300

    add first solution of Set Matrix Zeroes

---
+++ c#_LeetCode/Matrix/Medium/SetMatrixZeroes/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Matrix.Medium.SetMatrixZeroes
+{
+    //https://leetcode.com/problems/set-matrix-zeroes/
+    public class Solution
+    {
+        //O(n + m) extra space solution
+        public static void SetZeroes(int[][] matrix)
+        {
+            var rows = new HashSet<int>();
+            var cols = new HashSet<int>();
+
+            for (var row = 0; row < matrix.Length; row++)
+            {
+                for (var col = 0; col < matrix[0].Length; col++)
+                {
+                    if (matrix[row][col] != 0) continue;
+
+                    rows.Add(row);
+                    cols.Add(col);
+                }
+            }
+
+            for (var row = 0; row < matrix.Length; row++)
+            {
+                for (var col = 0; col < matrix[0].Length; col++)
+                {
+                    if (rows.Contains(row) || cols.Contains(col))
+                    {
+                        matrix[row][col] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
