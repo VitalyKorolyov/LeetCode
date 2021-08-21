5a4ce938631ee584e34392d868eb60c43d76e30d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-21 14:33:02 +0300

    add solution of Linked List Cycle II

---
+++ c#_LeetCode/LinkedList/Medium/LinkedListCycleII/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.LinkedList.Medium.LinkedListCycleII
+{
+    //https://leetcode.com/problems/linked-list-cycle-ii/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution
+    {
+        public static ListNode DetectCycle(ListNode head)
+        {
+            var cycle = FindCycle(head);
+            if (cycle == null) return null;
+
+            while (cycle != head)
+            {
+                cycle = cycle.next;
+                head = head.next;
+            }
+
+            return head;
+        }
+
+        public static ListNode FindCycle(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+
+            while (fast?.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+
+                if (slow == fast) return slow;
+            }
+
+            return null;
+        }
+    }
+}
