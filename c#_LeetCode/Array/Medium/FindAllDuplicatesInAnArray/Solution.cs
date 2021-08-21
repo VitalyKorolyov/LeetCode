3239e9b3bd119c6b7cd4bcb8eb65563fa62bbfb0
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-21 21:46:03 +0300

    add solution of Find All Duplicates in an Array

---
+++ c#_LeetCode/Array/Medium/FindAllDuplicatesInAnArray/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.FindAllDuplicatesInAnArray
+{
+    //https://leetcode.com/problems/find-all-duplicates-in-an-array/
+    public class Solution
+    {
+        public static IList<int> FindDuplicates(int[] nums)
+        {
+            var result = new List<int>();
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var index = System.Math.Abs(nums[i]) - 1;
+
+                if (nums[index] < 0)
+                {
+                    result.Add(System.Math.Abs(index+1));
+                }
+                else
+                {
+                    nums[index] *= -1;
+                }
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = System.Math.Abs(nums[i]);
+            }
+
+            return result;
+        }
+    }
+}
