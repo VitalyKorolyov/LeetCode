3b310f756e3c68779b03250bafac4877039d1b59
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-24 00:18:43 +0300

    add solution of Invert Binary Tree

---
+++ c#_LeetCode/Trees/Easy/InvertBinaryTree/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Trees.Easy.InvertBinaryTree
+{
+    //https://leetcode.com/problems/invert-binary-tree/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public static TreeNode InvertTree(TreeNode root)
+        {
+            Invert(root);
+
+            return root;
+        }
+
+        public static void Invert(TreeNode root)
+        {
+            if(root == null) return;
+
+            var leftSaved = root.left;
+            root.left = root.right;
+            root.right = leftSaved;
+
+            Invert(root.left);
+            Invert(root.right);
+        }
+    }
+}
