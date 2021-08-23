907293608264027163f8f3c247e79f904dd0fbba
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-24 00:27:35 +0300

    add solution of Binary Tree Inorder Traversal

---
+++ c#_LeetCode/Trees/Easy/BinaryTreeInorderTraversal/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees.Easy.BinaryTreeInorderTraversal
+{
+    //https://leetcode.com/problems/binary-tree-inorder-traversal/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+
+            Recursive(root, result);
+
+            return result;
+        }
+
+        public static void Recursive(TreeNode root, List<int> result)
+        {
+            if (root == null) return;
+
+            Recursive(root.left, result);
+            result.Add(root.val);
+            Recursive(root.right, result);
+        }
+    }
+}
