24660aa7c3e8d3c963a1b922abebc636a1fb60f5
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-24 00:02:18 +0300

    add solution of Search Insert Position

---
+++ LeetCodeTests/Array/Easy/SearchInsertPosition/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Array.Easy.SearchInsertPosition;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.SearchInsertPosition
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SearchInsert_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            int[] nums = { 1 };
+            int target = 7;
+
+            // Act
+            var result = Solution.SearchInsert(
+                nums,
+                target);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/SearchInsertPosition/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Array.Easy.SearchInsertPosition
+{
+    //https://leetcode.com/problems/search-insert-position/
+    public class Solution
+    {
+        public static int SearchInsert(int[] nums, int target)
+        {
+            int l = 0, r = nums.Length - 1, middle = 0;
+
+            while (r >= l)
+            {
+                middle = l + (r - l) / 2;
+
+                if (nums[middle] == target) return middle;
+
+                if (nums[middle] < target)
+                {
+                    l = middle + 1;
+                }
+                else
+                {
+                    r = middle - 1;
+                }
+            }
+
+            var resIndex = nums[middle] < target ? middle + 1 : middle;
+
+            return resIndex;
+        }
+    }
+}
