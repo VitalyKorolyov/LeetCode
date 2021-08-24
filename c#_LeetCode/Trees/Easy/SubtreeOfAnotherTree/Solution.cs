b2f97f3f68fc5fa4cde09ba370427c9ceb7d1d08
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-24 21:31:53 +0300

    add solution of Subtree of Another Tree

---
+++ c#_LeetCode/Trees/Easy/SubtreeOfAnotherTree/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Trees.Easy.SubtreeOfAnotherTree
+{
+    //https://leetcode.com/problems/subtree-of-another-tree/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsSubtree(TreeNode root, TreeNode subRoot)
+        {
+            if (root == null) return false;
+
+            if (IsEqual(root, subRoot)) return true;
+
+            return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+        }
+
+        private static bool IsEqual(TreeNode root, TreeNode subRoot)
+        {
+            if (root == null && subRoot == null) return true;
+            if (root == null || subRoot == null) return false;
+
+            if (root.val != subRoot.val) return false;
+
+            return IsEqual(root.left, subRoot.left) && IsEqual(root.right, subRoot.right);
+        } 
+    }
+}
