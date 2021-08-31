d26933aa2d7a255d0c398883c0ffbddc1786c7e4
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-31 23:56:59 +0300

    add solution of Merge Two Binary Trees

---
+++ LeetCodeTests/BST/Easy/MergeTwoBinaryTrees/TreeNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.BST.Easy.MergeTwoBinaryTrees;
+using Xunit;
+
+namespace LeetCodeTests.BST.Easy.MergeTwoBinaryTrees
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode1 = new TreeNode(1, new TreeNode(3, new TreeNode(5)), new TreeNode(2));
+            var treeNode2 = new TreeNode(2, new TreeNode(1, null, new TreeNode(4)), new TreeNode(3, null, new TreeNode(7)));
+
+            // Act
+            var res = Solution.MergeTrees(treeNode1, treeNode2);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
