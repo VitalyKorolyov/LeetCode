5add724f516d6d256e6d88f25e2731c511bd2c62
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-08-31 21:40:35 +0300

    add solution of Diameter of Binary Tree

---
+++ LeetCodeTests/Trees/Easy/DiameterOfBinaryTree/TreeNodeTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Trees.Easy.DiameterOfBinaryTree;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Easy.DiameterOfBinaryTree
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+
+            // Act
+            var result = Solution.DiameterOfBinaryTree(treeNode);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Trees/Easy/DiameterOfBinaryTree/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Trees.Easy.DiameterOfBinaryTree
+{
+    //https://leetcode.com/problems/diameter-of-binary-tree/submissions/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        private static int _max = 0;
+
+        public static int DiameterOfBinaryTree(TreeNode root)
+        {
+            Diameter(root);
+
+            return _max;
+        }
+
+        private static int Diameter(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var left = Diameter(root.left);
+            var right = Diameter(root.right);
+
+            _max = System.Math.Max(_max, left + right);
+
+            return System.Math.Max(left, right) + 1;
+        }
+    }
+}
