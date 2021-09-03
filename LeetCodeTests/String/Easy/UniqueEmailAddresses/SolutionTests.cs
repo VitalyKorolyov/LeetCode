393df26a1919f2b3dfd0a6f85b41aa8b5ab1ce81
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-03 23:40:46 +0300

    add solution of Unique Email Addresses

---
+++ LeetCodeTests/String/Easy/UniqueEmailAddresses/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Easy.UniqueEmailAddresses;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.UniqueEmailAddresses
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void NumUniqueEmails_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string[] emails =
+                {"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"};
+
+            // Act
+            var result = solution.NumUniqueEmails(emails);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/UniqueEmailAddresses/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Easy.UniqueEmailAddresses
+{
+    public class Solution
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            var hash = new HashSet<string>(emails.Length);
+
+            for (int i = 0; i < emails.Length; i++)
+            {
+                var parts = emails[i].Split('@');
+                var localName = parts[0];
+                var domainName = parts[1];
+
+                localName = localName.Split('+')[0];
+                localName = localName.Replace(".", "");
+
+                var email = localName + "@" + domainName;
+                if (!hash.Contains(email))
+                {
+                    hash.Add(email);
+                }
+            }
+
+            return hash.Count;
+        }
+    }
+}
