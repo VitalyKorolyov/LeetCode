0942f9dd97843ef02c740e8c4c17e870fd22b2c8
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-04 21:49:18 +0300

    add solution of Squares of a Sorted Array

---
+++ LeetCodeTests/Array/Easy/SquaresOfASortedArray/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Easy.SquaresOfASortedArray;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.SquaresOfASortedArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SortedSquares_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            int[] nums = { -5,-3,-2,-1};
+
+            // Act
+            var result = Solution.SortedSquares(nums);
+
+            // Assert
+            Assert.Equal(new[] {1, 4, 9, 25}, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/SquaresOfASortedArray/Solution.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.Array.Easy.SquaresOfASortedArray
+{
+    //https://leetcode.com/problems/squares-of-a-sorted-array/
+    public class Solution
+    {
+        public static int[] SortedSquares(int[] nums)
+        {
+            int k = 0;
+
+            for (k = 0; k < nums.Length; k++)
+            {
+                if (nums[k] >= 0) break;
+            }
+
+            var res = new int[nums.Length];
+
+            int tempIndex = 0;
+            int i = k - 1;
+            int j = k;
+
+            while (i >= 0 && j <= nums.Length - 1)
+            {
+                var r1 = nums[i] * nums[i];
+                var r2 = nums[j] * nums[j];
+
+                if (r1 < r2)
+                {
+                    res[tempIndex++] = r1;
+                    i--;
+                }
+                else
+                {
+                    res[tempIndex++] = r2;
+                    j++;
+                }
+            }
+
+            while (i >= 0)
+            {
+                res[tempIndex++] = nums[i] * nums[i];
+                i--;
+            }
+
+            while (j <= nums.Length - 1)
+            {
+                res[tempIndex++] = nums[j] * nums[j];
+                j++;
+            }
+
+            return res;
+        }
+    }
+}
