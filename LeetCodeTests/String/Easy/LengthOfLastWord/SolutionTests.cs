de9f6b54210e4726531f1cae2153a0029ee87fec
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-06 20:29:02 +0300

    add solution of Length of Last Word

---
+++ LeetCodeTests/String/Easy/LengthOfLastWord/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Easy.LengthOfLastWord;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.LengthOfLastWord
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void LengthOfLastWord_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "f";
+
+            // Act
+            var result = solution.LengthOfLastWord(
+                s);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/LengthOfLastWord/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.String.Easy.LengthOfLastWord
+{
+    //https://leetcode.com/problems/length-of-last-word/
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            var startedIndex = 0;
+            var isFirstIndexOfWord = true;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != ' ' && isFirstIndexOfWord)
+                {
+                    startedIndex = i;
+                    isFirstIndexOfWord = false;
+                }
+
+                if(s[i] == ' ')
+                {
+                    isFirstIndexOfWord = true;
+                }
+            }
+
+            var endIndex = startedIndex;
+            for (int i = startedIndex; i < s.Length; i++)
+            {
+                if (s[i] != ' ') endIndex = i;
+            }
+
+            return endIndex - startedIndex + 1;
+        }
+    }
+}
