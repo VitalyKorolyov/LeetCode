8ec39b8f073c61641520b0f6edf01d735d4315cc
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-06 20:04:07 +0300

    add solution of Implement strStr()

---
+++ LeetCodeTests/String/Easy/ImplementstrStr()/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.String.Easy;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.ImplementstrStr
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void StrStr_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string haystack = "mississippi";
+            string needle = "issipi";
+
+            // Act
+            var result = solution.StrStr(
+                haystack,
+                needle);
+
+            // Assert
+            Assert.Equal(-1, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/ImplementstrStr()/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.String.Easy
+{
+    //https://leetcode.com/problems/implement-strstr/
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (string.IsNullOrEmpty(needle)) return 0;
+            if(haystack.Length < needle.Length) return -1;
+
+            for (var i = 0; i < haystack.Length; i++)
+            {
+                var k = i; 
+                var j = 0;
+                var found = true;
+
+                while (j < needle.Length && k < haystack.Length)
+                {
+                    if (haystack[k++] == needle[j++]) continue;
+
+                    found = false;
+                    break;
+                }
+
+                if (found) return i;
+            }
+
+            return -1;
+        }
+    }
+}
