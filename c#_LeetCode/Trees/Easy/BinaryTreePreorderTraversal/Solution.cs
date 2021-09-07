83bc59748b3aaa10c67ec963d2a36865b399d85b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-08 00:07:30 +0300

    add some solutions of Binary Tree Preorder Traversal

--- c#_LeetCode/Trees/Easy/BinaryTreePreorderTraversal/Solution.cs
+++ c#_LeetCode/Trees/Easy/BinaryTreePreorderTraversal/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.Trees.Easy.BinaryTreePreorderTraversal
 {
@@ -30,11 +31,70 @@ namespace LeetCode.Trees.Easy.BinaryTreePreorderTraversal
 
         public void Recursive(TreeNode root, List<int> res)
         {
-            if(root == null) return;
+            if (root == null) return;
             res.Add(root.val);
 
             Recursive(root.left, res);
             Recursive(root.right, res);
+        }
+
+        public IList<int> Stack(TreeNode root)
+        {
+            var stack = new Stack<TreeNode>();
+            var output = new List<int>();
+
+            if (root == null) return output;
+
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+
+                if (node == null) continue;
+
+                output.Add(node.val);
+                stack.Push(node.right);
+                stack.Push(node.left);
+            }
+
+            return output;
+        }
+
+        public IList<int> Iterative(TreeNode root)
+        {
+            var output = new List<int>();
+
+            var node = root;
+            while (node != null)
+            {
+                if (node.left == null)
+                {
+                    output.Add(node.val);
+                    node = node.right;
+                }
+                else
+                {
+                    var predecessor = node.left;
+                    while (predecessor.right != null && predecessor.right != node)
+                    {
+                        predecessor = predecessor.right;
+                    }
+
+                    if (predecessor.right == null)
+                    {
+                        output.Add(node.val);
+                        predecessor.right = node;
+                        node = node.left;
+                    }
+                    else
+                    {
+                        predecessor.right = null;
+                        node = node.right;
+                    }
+                }
+            }
+
+            return output;
         }
     }
 }
