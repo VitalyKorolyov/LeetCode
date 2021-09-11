58f26867be9c3c3d33d56af1e9f8d7125efa7134
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-12 00:07:02 +0300

    add solution of Armstrong Number

---
+++ LeetCodeTests/Math/Easy/ArmstrongNumber/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Math.Easy.ArmstrongNumber;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Math.Easy.ArmstrongNumber
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void IsArmstrong_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int n = 153;
+
+            // Act
+            var result = solution.IsArmstrong(
+                n);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/Math/Easy/ArmstrongNumber/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Math.Easy.ArmstrongNumber
+{
+    public class Solution
+    {
+        public bool IsArmstrong(int n)
+        {
+            var sum = 0;
+            var saved = n;
+            var k = n.ToString().Length;
+
+            while (n != 0)
+            {
+                var value = n % 10;
+                n /= 10;
+
+                sum += (int)System.Math.Pow(value, k);
+            }
+
+            return saved == sum;
+        }
+    }
+}
