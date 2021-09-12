9671cfdc6e92ac8393cf7ebb0e7100424b575b0d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-12 22:52:00 +0300

    add solution of Moving Average from Data Stream

---
+++ LeetCodeTests/Array/Easy/MovingAverageFromDataStream/MovingAverageTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Easy.MovingAverageFromDataStream;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.MovingAverageFromDataStream
+{
+    public class MovingAverageTests
+    {
+        [Fact]
+        public void Next_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var movingAverage = new MovingAverage(3);
+
+            // Act
+            Assert.Equal(1.0, movingAverage.Next(1));
+            Assert.Equal(5.5, movingAverage.Next(10));
+            Assert.Equal(4.666666666666667, movingAverage.Next(3));
+            Assert.Equal(6.0, movingAverage.Next(5));
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/MovingAverageFromDataStream/MovingAverage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Easy.MovingAverageFromDataStream
+{
+    //https://leetcode.com/problems/moving-average-from-data-stream/
+    public class MovingAverage
+    {
+        private readonly Queue<int> _queue;
+        private double _currentSum;
+        private readonly int _size;
+
+        public MovingAverage(int size)
+        {
+            _queue = new Queue<int>(size);
+            _size = size;
+        }
+
+        public double Next(int val)
+        {
+            var currentValue = GetCurrentValue();
+
+            _currentSum -= currentValue;
+            _currentSum += val;
+            _queue.Enqueue(val);
+
+            return _currentSum / _queue.Count;
+        }
+
+        private int GetCurrentValue()
+        {
+            return _size != _queue.Count ? 0 : _queue.Dequeue();
+        }
+    }
+
+}
