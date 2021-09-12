5898a20995d721075ec6c9eef66e68b60d366450
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-12 23:16:41 +0300

    add solution of Single-Row Keyboard

---
+++ LeetCodeTests/String/Easy/SingleRowKeyboard/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.String.Easy.SingleRowKeyboard;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.SingleRowKeyboard
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void CalculateTime_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string keyboard = "pqrstuvwxyzabcdefghijklmno";
+            string word = "leetcode";
+
+            // Act
+            var result = solution.CalculateTime(
+                keyboard,
+                word);
+
+            // Assert
+            Assert.Equal(73, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/SingleRowKeyboard/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Easy.SingleRowKeyboard
+{
+    //https://leetcode.com/problems/single-row-keyboard/
+    public class Solution
+    {
+        public int CalculateTime(string keyboard, string word)
+        {
+            var dic = new Dictionary<char, int>(keyboard.Length);
+
+            for (var i = 0; i < keyboard.Length; i++)
+                dic.Add(keyboard[i], i);
+
+            var currentIndex = 0;
+            var time = 0;
+
+            for (var i = 0; i < word.Length; i++)
+            {
+                 time += System.Math.Abs(currentIndex - dic[word[i]]);
+                 currentIndex = dic[word[i]];
+            }
+
+            return time;
+        }
+    }
+}
