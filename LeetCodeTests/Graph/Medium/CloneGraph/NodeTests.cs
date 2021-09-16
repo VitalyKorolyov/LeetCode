9aff546e970f33041dece137bd505ac1ed29e7b6
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-16 21:32:18 +0300

    add solution of Clone Graph

---
+++ LeetCodeTests/Graph/Medium/CloneGraph/NodeTests.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Graph.Medium.CloneGraph;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCodeTests.Graph.Medium.CloneGraph
+{
+    public class NodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var node1 = new Node(1);
+            var node2 = new Node(2);
+            var node3 = new Node(3);
+            var node4 = new Node(4);
+
+            node1.neighbors.Add(node2);
+            node1.neighbors.Add(node4);
+
+            node2.neighbors.Add(node1);
+            node2.neighbors.Add(node3);
+
+            node3.neighbors.Add(node2);
+            node3.neighbors.Add(node4);
+
+            node4.neighbors.Add(node1);
+            node4.neighbors.Add(node3);
+
+
+            var solution = new Solution();
+
+            // Act
+            solution.CloneGraph(node1);
+
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ c#_LeetCode/Graph/Medium/CloneGraph/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Graph.Medium.CloneGraph
+{
+    //https://leetcode.com/problems/clone-graph/
+    public class Node
+    {
+        public int val;
+        public IList<Node> neighbors;
+
+        public Node()
+        {
+            val = 0;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val)
+        {
+            val = _val;
+            neighbors = new List<Node>();
+        }
+
+        public Node(int _val, List<Node> _neighbors)
+        {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+
+    public class Solution
+    {
+        public Node CloneGraph(Node node)
+        {
+            if (node == null) return null;
+
+            var visited = new Dictionary<Node, Node>();
+
+            return Clone(node, visited);
+        }
+
+        private Node Clone(Node node, Dictionary<Node, Node> visited)
+        {
+            if(visited.ContainsKey(node)) return visited[node];
+
+            var copy = new Node(node.val);
+            visited.Add(node, copy);
+
+            foreach (var neighbor in node.neighbors)
+                copy.neighbors.Add(Clone(neighbor, visited));
+
+            return copy;
+        }
+    }
+}
