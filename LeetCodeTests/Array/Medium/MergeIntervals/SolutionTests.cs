97c60ab99e6c91cb515ec755fdb47b6251b639a1
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-17 21:52:53 +0300

    add solution of Merge Intervals

---
+++ LeetCodeTests/Array/Medium/MergeIntervals/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Array.Medium.MergeIntervals;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.MergeIntervals
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Merge_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] intervals = new[] {new int[] {1, 4}, new int[] {0, 1} };
+
+            // Act
+            var result = solution.Merge(
+                intervals);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/MergeIntervals/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.MergeIntervals
+{
+    //https://leetcode.com/problems/merge-intervals/
+    public class Solution
+    {
+        public int[][] Merge(int[][] intervals)
+        {
+            System.Array.Sort(intervals, (i1, i2) => i1[0].CompareTo(i2[0]));
+
+            var merged = new List<int[]>();
+
+            for (var i = 0; i < intervals.Length; i++)
+            {
+                var nextI = i;
+                var merge = new[] { intervals[i][0], intervals[i][1] };
+
+                for (var j = i + 1; j < intervals.Length; j++)
+                {
+                    if (IsInside(merge, intervals[j]))
+                    {
+                        merge[1] = merge[1] > intervals[j][1] ? merge[1] : intervals[j][1];
+
+                        nextI = j;
+                        continue;
+                    }
+
+                    break;
+                }
+
+                merged.Add(merge);
+
+                i = nextI;
+            }
+
+            return merged.ToArray();
+        }
+
+        private bool IsInside(int[] first, int[] second)
+        {
+            return first[0] <= second[0] && second[0] <= first[1];
+        }
+    }
+}
