7ec3bf0c00fe606ec795e3fdde722e6724f41b1e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-18 00:17:50 +0300

    add solution of Fibonacci Number

---
+++ LeetCodeTests/Math/Easy/FibonacciNumber/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Math.Easy.FibonacciNumber;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Math.Easy.FibonacciNumber
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Fib_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int n = 4;
+
+            // Act
+            var result = solution.Fib(n);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Math/Easy/FibonacciNumber/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Math.Easy.FibonacciNumber
+{
+    //https://leetcode.com/problems/fibonacci-number/
+    public class Solution
+    {
+        public int Fib(int n)
+        {
+            if (n <= 1) return n;
+
+            var priv1 = 0;
+            var priv2 = 1;
+            var current = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                current = priv1 + priv2;
+                priv1 = priv2;
+                priv2 = current;
+            }
+
+            return current;
+        }
+
+        public int FibArraySolution(int n)
+        {
+            if (n <= 1) return n;
+
+            var ar = new int[n + 1];
+            ar[0] = 0;
+            ar[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+                ar[i] = ar[i - 1] + ar[i - 2];
+
+            return ar[n];
+        }
+
+        public int Recursive(int n)
+        {
+            if (n <= 1) return n;
+
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
