db999649329bd78147e89b8377cd191e30fcab4f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-18 20:14:39 +0300

    add solution of Climbing Stairs

---
+++ LeetCodeTests/DynamicProgramming/Easy/ClimbingStairs/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.DynamicProgramming.Easy.ClimbingStairs;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Easy.ClimbingStairs
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void ClimbStairs_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int n = 5;
+
+            // Act
+            var result = solution.ClimbStairs(
+                n);
+
+            // Assert
+            Assert.Equal(8, result);
+        }
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Easy/ClimbingStairs/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.DynamicProgramming.Easy.ClimbingStairs
+{
+    //https://leetcode.com/problems/climbing-stairs/
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 1) return 1;
+
+            int prev1 = 1;
+            int prev2 = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                int third = prev1 + prev2;
+                prev1 = prev2;
+                prev2 = third;
+            }
+
+            return prev2;
+        }
+
+        public int ClimbStairsRecursive(int n)
+        {
+            return Calc(n, 0);
+        }
+
+        private int Calc(int n, int i)
+        {
+            if (i > n) return 0;
+            if (i == n) return 1;
+
+            return Calc(n, i + 1) + Calc(n, i + 2);
+        }
+    }
+}
