b4080ea95cc0edc6e151a317eb68df33655c2501
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-18 22:20:52 +0300

    add solution of Range Sum Query - Immutable

---
+++ LeetCodeTests/Array/Easy/RangeSumQueryImmutable/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Easy.RangeSumQueryImmutable;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.RangeSumQueryImmutable
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var numArray = new Solution.NumArray(new []{ -2, 0, 3, -5, 2, -1 });
+            // Act
+            var res = numArray.SumRange(2, 5);
+            res = numArray.SumRange(2, 5);
+
+            // Assert
+            Assert.False(false);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/RangeSumQueryImmutable/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Array.Easy.RangeSumQueryImmutable
+{
+    //https://leetcode.com/problems/range-sum-query-immutable/
+    public class Solution
+    {
+        public class NumArray
+        {
+            private readonly int[] _nums;
+            private readonly Dictionary<string, int> _cache = new();
+
+            public NumArray(int[] nums)
+            {
+                _nums = nums;
+            }
+
+            public int SumRange(int left, int right)
+            {
+                var key = $"{left}{right}";
+
+                if (_cache.ContainsKey(key))
+                {
+                    return _cache[key];
+                }
+
+                var res = Calc(left, right);
+                _cache.Add(key, res);
+                return res;
+            }
+
+            private int Calc(int left, int right)
+            {
+                var sum = 0;
+
+                for (int i = left; i <= right; i++)
+                    sum += _nums[i];
+
+                return sum;
+            }
+        }
+    }
+}
