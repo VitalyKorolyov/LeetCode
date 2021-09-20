24034cdcbf9c930f57ec11ead742ae5c80c1909f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-21 00:28:46 +0300

    add solution of Valid Parentheses

---
+++ LeetCodeTests/String/Easy/ValidParentheses/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Easy.ValidParentheses;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.ValidParentheses
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void IsValidStack_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "()";
+
+            // Act
+            var result = solution.IsValidStack(
+                s);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/ValidParentheses/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Easy.ValidParentheses
+{
+    //https://leetcode.com/problems/valid-parentheses/
+    public class Solution
+    {
+        private Dictionary<char, char> pairs = new Dictionary<char, char>()
+        {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        public bool IsValidStack(string s)
+        {
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var @char = s[i];
+
+                if (pairs.ContainsKey(@char))
+                {
+                    var top = stack.Count > 0 ? stack.Pop() : '#';
+
+                    if (top != pairs[@char]) return false;
+                }
+                else
+                {
+                    stack.Push(s[i]);
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
