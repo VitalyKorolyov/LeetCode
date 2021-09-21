39f26f6906298637b10f85b1a64b83d76dce9a09
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-21 20:45:00 +0300

    add solution of Minimum Remove to Make Valid Parentheses

---
+++ LeetCodeTests/String/Medium/MinimumRemoveToMakeValidParentheses/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Medium.MinimumRemoveToMakeValidParentheses;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.MinimumRemoveToMakeValidParentheses
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinRemoveToMakeValid_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "(a(b(c)d)";
+
+            // Act
+            var result = solution.MinRemoveToMakeValid(
+                s);
+
+            // Assert
+            Assert.Equal("a(b(c)d)", result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/MinimumRemoveToMakeValidParentheses/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.String.Medium.MinimumRemoveToMakeValidParentheses
+{
+    //https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+    public class Solution
+    {
+        public class @CharPosition
+        {
+            public readonly char CharType;
+            public readonly int Index;
+
+            public @CharPosition(char charType, int index)
+            {
+                CharType = charType;
+                Index = index;
+            }
+        }
+
+        public string MinRemoveToMakeValid(string s)
+        {
+            var stack = new Stack<@CharPosition>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var @char = s[i];
+
+                if (@char == ')')
+                {
+                    var top = stack.Count > 0 ? stack.Peek() : new CharPosition('#', 0);
+                    if (top.CharType == '(')
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(new CharPosition(@char, i));
+                    }
+                }
+                if(@char == '(')
+                {
+                    stack.Push(new CharPosition(@char, i));
+                }
+            }
+
+            if (stack.Count == 0) return s;
+
+            var res = new StringBuilder(s);
+
+            while (stack.Count > 0)
+            {
+                var itemToRemove = stack.Pop();
+                res.Remove(itemToRemove.Index, 1);
+            }
+
+            return res.ToString();
+        }
+    }
+}
