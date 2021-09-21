97c81dd62a06997b0f2e35c33deb343bd3a33bf5
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-21 20:04:41 +0300

    add solution of Count Good Nodes in Binary Tree

---
+++ LeetCodeTests/BST/Medium/CountGoodNodesInBinaryTree/TreeNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.BST.Medium.CountGoodNodesInBinaryTree;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.BST.Medium.CountGoodNodesInBinaryTree
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(9, null, new TreeNode(3, new TreeNode(6)));
+
+            // Act
+            var res = new Solution().GoodNodes(treeNode);
+
+            // Assert
+            Assert.Equal(1, res);
+        }
+    }
+}
---
+++ c#_LeetCode/BST/Medium/CountGoodNodesInBinaryTree/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.BST.Medium.CountGoodNodesInBinaryTree
+{
+    //https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        private int counter = 0;
+        public int GoodNodes(TreeNode root)
+        {
+            Count(root, root.val);
+            return counter;
+        }
+
+        private void Count(TreeNode root, int prevValue)
+        {
+            if (root == null) return;
+            if (root.val >= prevValue)
+            {
+                counter++;
+            }
+
+            Count(root.left, System.Math.Max(root.val, prevValue));
+            Count(root.right, System.Math.Max(root.val, prevValue));
+        }
+    }
+}
