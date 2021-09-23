f40b0cb60fcdd0bb7bca4b490937ea66e8012c2e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-24 00:29:13 +0300

    add solution of Cousins in Binary Tree

---
+++ LeetCodeTests/BST/Easy/CousinsInBinaryTree/TreeNodeTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.BST.Easy.CousinsInBinaryTree;
+using Xunit;
+
+namespace LeetCodeTests.BST.Easy.CousinsInBinaryTree
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(1, new TreeNode(2, null, new TreeNode(4)), new TreeNode(3));
+
+            // Act
+            var res = new Solution().IsCousins(treeNode, 2, 3);
+
+            // Assert
+            Assert.False(res);
+        }
+    }
+}
---
+++ c#_LeetCode/BST/Easy/CousinsInBinaryTree/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.BST.Easy.CousinsInBinaryTree
+{
+    //https://leetcode.com/problems/cousins-in-binary-tree/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        private int DepthX;
+        private TreeNode NodeX;
+        private int DepthY;
+        private TreeNode NodeY;
+
+        public bool IsCousins(TreeNode root, int x, int y)
+        {
+            InitDepth(root, root, 0, x, y);
+
+            return DepthX == DepthY && NodeX != NodeY;
+        }
+
+        private void InitDepth(TreeNode root, TreeNode parent, int depth, int x, int y)
+        {
+            if(root == null) return;
+
+            if (x == root.val)
+            {
+                DepthX = depth;
+                NodeX = parent;
+            }
+            if (y == root.val)
+            {
+                DepthY = depth;
+                NodeY = parent;
+            }
+
+            InitDepth(root.left, root, depth + 1, x, y);
+            InitDepth(root.right, root, depth + 1, x, y);
+        }
+    }
+}
