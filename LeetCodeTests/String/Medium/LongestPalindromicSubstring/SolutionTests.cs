a247a8c83cb0d3a9866e56187fbc0f3943a7b72d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-25 22:24:12 +0300

    add solution of Longest Palindromic Substring

---
+++ LeetCodeTests/String/Medium/LongestPalindromicSubstring/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Medium.LongestPalindromicSubstring;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.LongestPalindromicSubstring
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void LongestPalindrome_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "babad";
+
+            // Act
+            var result = solution.LongestPalindrome(s);
+
+            // Assert
+            Assert.Equal("aba", result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/LongestPalindromicSubstring/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.String.Medium.LongestPalindromicSubstring
+{
+    //https://leetcode.com/problems/longest-palindromic-substring/
+    public class Solution
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return string.Empty;
+
+            int start = 0, end = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var len1 = GetLen(i, i, s);
+                var len2 = GetLen(i, i + 1, s);
+                var len = System.Math.Max(len1, len2);
+
+                if (len > end - start)
+                {
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+
+            return s[start..(end + 1)];
+        }
+
+        private int GetLen(int l, int r, string s)
+        {
+            while (l >= 0 && r < s.Length && s[l] == s[r])
+            {
+                l--; r++;
+            }
+
+            return r - l - 1;
+        }
+    }
+}
