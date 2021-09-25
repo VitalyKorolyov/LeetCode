731f617aca8da89edb579669decde92412ec3632
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-25 20:41:50 +0300

    add solution of Meeting Rooms II

---
+++ LeetCodeTests/Array/Medium/MeetingRoomsII/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Array.Medium.MeetingRoomsII;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.MeetingRoomsII
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinMeetingRooms_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            // Arrange
+            var solution = new Solution();
+            int[][] intervals = { new[] { 1, 10 }, 
+                new[] { 2, 7 }, new[] { 3, 19 }, 
+                new[] { 8, 12 }, new[] { 10, 20 },
+                new[] { 11, 30 }
+            };
+
+            // Act
+            var result = solution.MinMeetingRooms(
+                intervals);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/MeetingRoomsII/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Array.Medium.MeetingRoomsII
+{
+    //https://leetcode.com/problems/meeting-rooms-ii/
+    public class Solution
+    {
+        public int MinMeetingRooms(int[][] intervals)
+        {
+            if (intervals == null || intervals.Length == 0) return 0;
+
+            var starts = new int[intervals.Length];
+            var ends = new int[intervals.Length];
+
+            for (var i = 0; i < intervals.Length; i++)
+            {
+                starts[i] = intervals[i][0];
+                ends[i] = intervals[i][1];
+            }
+
+            System.Array.Sort(starts);
+            System.Array.Sort(ends);
+
+            var result = 0;
+
+            for (int i = 0, j = 0; i < starts.Length; i++)
+            {
+                if (starts[i] < ends[j]) result++;
+                else j++;
+            }
+
+            return result;
+        }
+    }
+}
