371cdc857f9c0ac77432c3a1f45cb99b04c688bf
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-25 23:24:00 +0300

    add solution of Maximum Nesting Depth of the Parentheses

---
+++ LeetCodeTests/String/Easy/MaximumNestingDepthOfTheParentheses/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Easy.MaximumNestingDepthOfTheParentheses;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.MaximumNestingDepthOfTheParentheses
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MaxDepth_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "(1)+((2))+(((3)))";
+
+            // Act
+            var result = solution.MaxDepth(
+                s);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/MaximumNestingDepthOfTheParentheses/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.String.Easy.MaximumNestingDepthOfTheParentheses
+{
+    //https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
+    public class Solution
+    {
+        public int MaxDepth(string s)
+        {
+            var max = 0;
+            var current = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                    current++;
+
+                if (s[i] == ')')
+                {
+                    max = System.Math.Max(current, max);
+                    current--;
+                }
+            }
+
+            return max;
+        }
+    }
+}
