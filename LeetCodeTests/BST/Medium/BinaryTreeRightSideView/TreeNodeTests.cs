e84c567ef1c1949845c97b132de0170de5d794c7
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-26 21:59:01 +0300

    add solution of Binary Tree Right Side View

---
+++ LeetCodeTests/BST/Medium/BinaryTreeRightSideView/TreeNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.BST.Medium.BinaryTreeRightSideView;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.BST.Medium.BinaryTreeRightSideView
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3, null, new TreeNode(4)));
+
+            // Act
+            var res = new Solution().RightSideView(treeNode);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/BST/Medium/BinaryTreeRightSideView/Solution.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.BST.Medium.BinaryTreeRightSideView
+{
+    //https://leetcode.com/problems/binary-tree-right-side-view/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public IList<int> RightSideView(TreeNode root)
+        {
+            var res = new List<int>();
+            if (root == null) return res;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            queue.Enqueue(null);
+
+            TreeNode prev, curr = root;
+            while (queue.Count != 0)
+            {
+                prev = curr;
+                curr = queue.Dequeue();
+
+                while (curr != null)
+                {
+                    if (curr.left != null)
+                    {
+                        queue.Enqueue(curr.left);
+                    }
+                    if (curr.right != null)
+                    {
+                        queue.Enqueue(curr.right);
+                    }
+
+                    prev = curr;
+                    curr = queue.Dequeue();
+                }
+
+                res.Add(prev.val);
+
+                if (queue.Count != 0)
+                    queue.Enqueue(null);
+            }
+            return res;
+        }
+
+        public IList<int> DFS(TreeNode root)
+        {
+            var res = new List<int>();
+
+            Right(root, res, 0);
+
+            return res;
+        }
+
+        public void Right(TreeNode root, IList<int> res, int level)
+        {
+            if(root == null) return;
+
+            if(level == res.Count)
+                res.Add(root.val);
+
+            Right(root.right, res, level + 1);
+            Right(root.left, res, level + 1);
+        }
+    }
+}
