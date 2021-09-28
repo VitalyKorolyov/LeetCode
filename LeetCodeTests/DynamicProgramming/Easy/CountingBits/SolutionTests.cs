094205b0e91e550ddabbdf98a4bc6ae1819c7d51
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-28 23:33:19 +0300

    add solution of Counting Bits

---
+++ LeetCodeTests/DynamicProgramming/Easy/CountingBits/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.DynamicProgramming.Easy.CountingBits;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Easy.CountingBits
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void CountBits_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int n = 3;
+
+            // Act
+            var result = solution.CountBits(
+                n);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Easy/CountingBits/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.DynamicProgramming.Easy.CountingBits
+{
+    //https://leetcode.com/problems/counting-bits/
+    public class Solution
+    {
+        private int PopCount(int x)
+        {
+            int count;
+            for (count = 0; x != 0; ++count)
+            {
+                var a = x & (x - 1);
+                x &= x - 1; // zeroing out the least significant nonzero bit
+            }
+            return count;
+        }
+
+        public int[] CountBits(int n)
+        {
+            int[] ans = new int[n + 1];
+            for (int x = 0; x <= n; ++x)
+            {
+                ans[x] = PopCount(x);
+            }
+            return ans;
+        }
+    }
+}
