3579eca63f474d16dc95d1786f6f0c72f82229b8
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-29 23:46:43 +0300

    add solution of Convert Binary Number in a Linked List to Integer

---
+++ LeetCodeTests/LinkedList/Easy/ConvertBinaryNumberInALinkedListToInteger/ListNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.LinkedList.Easy.ConvertBinaryNumberInALinkedListToInteger;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Easy.ConvertBinaryNumberInALinkedListToInteger
+{
+    public class ListNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var listNode = new ListNode(1, new ListNode(0, new ListNode(1)));
+
+            // Act
+            var res = new Solution().GetDecimalValue(listNode);
+
+            // Assert
+            Assert.Equal(5, res);
+        }
+    }
+}
---
+++ c#_LeetCode/LinkedList/Easy/ConvertBinaryNumberInALinkedListToInteger/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace LeetCode.LinkedList.Easy.ConvertBinaryNumberInALinkedListToInteger
+{
+    //https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            var sb = new StringBuilder();
+
+            while (head != null)
+            {
+                sb.Append(head.val);
+                head = head.next;
+            }
+
+            return Convert.ToInt32(sb.ToString(), 2);
+        }
+    }
+}
