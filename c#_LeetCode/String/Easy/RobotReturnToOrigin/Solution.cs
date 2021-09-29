dc71cda29d8062dce56492c540e83f42c9657506
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-09-29 23:23:18 +0300

    add solution of Robot Return to Origin

---
+++ c#_LeetCode/String/Easy/RobotReturnToOrigin/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.String.Easy.RobotReturnToOrigin
+{
+    //https://leetcode.com/problems/robot-return-to-origin/
+    public class Solution
+    {
+        public bool JudgeCircle(string moves)
+        {
+            int x = 0;
+            int y = 0;
+
+            for (var i = 0; i < moves.Length; i++)
+            {
+                if (moves[i] == 'U')
+                {
+                    y++;
+                    continue;
+                }
+
+                if (moves[i] == 'D')
+                {
+                    y--;
+                    continue;
+                }
+
+                if (moves[i] == 'R')
+                {
+                    x++;
+                    continue;
+                }
+
+                if (moves[i] == 'L')
+                {
+                    x--;
+                }
+            }
+
+            return x == 0 && y == 0;
+        }
+    }
+}
