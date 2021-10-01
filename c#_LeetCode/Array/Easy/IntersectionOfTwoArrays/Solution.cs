368ff28e76bbcc1ff832174dae8790294fa0c88b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-01 22:11:19 +0300

    add solution of Intersection of Two Arrays;

---
+++ c#_LeetCode/Array/Easy/IntersectionOfTwoArrays/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array.Easy.IntersectionOfTwoArrays
+{
+    //https://leetcode.com/problems/intersection-of-two-arrays/
+    public class Solution
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            var hash = new HashSet<int>();
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                if (!hash.Contains(nums1[i]))
+                {
+                    hash.Add(nums1[i]);
+                }
+            }
+
+            var res = new HashSet<int>();
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                if (hash.Contains(nums2[i]) && !res.Contains(nums2[i]))
+                {
+                    res.Add(nums2[i]);
+                }
+            }
+
+            return res.ToArray();
+        }
+    }
+}
