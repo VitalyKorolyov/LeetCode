03aad250053caf0d19616569c0435d826cb0ebae
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-03 21:04:55 +0300

    add solution of Move Zeroes;

---
+++ LeetCodeTests/Array/Easy/MoveZeroes/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Easy.MoveZeroes;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.MoveZeroes
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MoveZeroes_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = {0,1,0,3,12};
+
+            // Act
+            solution.MoveZeroes(nums);
+
+            // Assert
+            Assert.Equal(new int[] { 1, 3, 12, 0, 0 }, nums);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/MoveZeroes/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Array.Easy.MoveZeroes
+{
+    //https://leetcode.com/problems/move-zeroes/
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            var j = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0) nums[j++] = nums[i];
+            }
+
+            for (int i = j; i < nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
