695d0fd75cd978aecf445094ed63d4de44d24f52
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-04 21:45:20 +0300

    add solution of Crawler Log Folder;

---
+++ c#_LeetCode/Stack/Easy/CrawlerLogFolder/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Stack.Easy.CrawlerLogFolder
+{
+    //https://leetcode.com/problems/crawler-log-folder/
+    public class Solution
+    {
+        public int MinOperations(string[] logs)
+        {
+            var slack = new Stack<string>();
+
+            for (int i = 0; i < logs.Length; i++)
+            {
+                if (logs[i] == "../")
+                {
+                    if (slack.Count > 0)
+                    {
+                        slack.Pop();
+                    }
+                    continue;
+                }
+
+                if (logs[i] == "./") continue;
+
+                slack.Push(logs[i]);
+            }
+
+            return slack.Count;
+        }
+    }
+}
