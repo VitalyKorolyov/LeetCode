d694a191489619b750d4d2592d7d57830e629430
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-04 22:02:05 +0300

    add solution of Find a Corresponding Node of a Binary Tree in a Clone of That Tree;

---
+++ c#_LeetCode/Trees/Medium/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Trees.Medium.FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree
+{
+    //https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+
+    public class Solution
+    {
+        public TreeNode GetTargetCopy(TreeNode original, TreeNode cloned, TreeNode target)
+        {
+            return GetNode(cloned, target);
+        }
+
+        private TreeNode GetNode(TreeNode root, TreeNode target)
+        {
+            if (root == null) return null;
+            if (root.val == target.val) return root;
+
+            var res = GetNode(root.left, target);
+            if (res != null) return res;
+            return GetNode(root.right, target);
+        }
+    }
+}
