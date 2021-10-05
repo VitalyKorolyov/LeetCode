d3aaf35b9648bb8346bb3bd3d750963b970ee18b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-05 21:18:30 +0300

    add solution of Count the Number of Consistent Strings

---
+++ c#_LeetCode/String/Easy/CountTheNumberOfConsistentStrings/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.String.Easy.CountTheNumberOfConsistentStrings
+{
+    //https://leetcode.com/problems/count-the-number-of-consistent-strings/
+    public class Solution
+    {
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            var res = 0;
+
+            var hash = new HashSet<char>(26);
+            for (int i = 0; i < allowed.Length; i++)
+                hash.Add(allowed[i]);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    if (!hash.Contains(words[i][j])) break;
+
+                    if (j == words[i].Length - 1) res++;
+                }
+            }
+
+            return res;
+        }
+    }
+}
