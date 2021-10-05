48130aa2d9032a06625e4e5c45f6ef3c0a72ae0f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-05 20:55:00 +0300

    add solution of Reverse Linked List II

---
+++ LeetCodeTests/LinkedList/Medium/ReverseLinkedListII/ListNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.LinkedList.Medium.ReverseLinkedListII;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Medium.ReverseLinkedListII
+{
+    public class ListNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+            // Act
+            var res = new Solution().ReverseBetween(listNode, 2, 4);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/LinkedList/Medium/ReverseLinkedListII/Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace LeetCode.LinkedList.Medium.ReverseLinkedListII
+{
+    //https://leetcode.com/problems/reverse-linked-list-ii/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            var res = new ListNode(0, head);
+
+            ListNode prLeft = null;
+            ListNode leftN = null;
+            ListNode rightN = null;
+            ListNode rightNextN = null;
+
+            var current = res.next;
+            var pr = res;
+
+            var index = 1;
+            while (current != null)
+            {
+                if (index == left)
+                {
+                    prLeft = pr;
+                    leftN = current;
+                }
+
+                if (index == right)
+                {
+                    rightN = current;
+                    rightNextN = rightN.next;
+                    break;
+                }
+
+                pr = current;
+                current = current.next;
+                index++;
+            }
+
+            if (leftN == null || rightN == null) return res.next;
+
+            var (headR, end) = Reverse(leftN, rightNextN);
+
+            prLeft.next = headR;
+            end.next = rightNextN;
+
+            return res.next;
+        }
+
+        private Tuple<ListNode, ListNode> Reverse(ListNode left, ListNode right)
+        {
+            var root = left;
+            ListNode prev = null;
+
+            while (root != right)
+            {
+                var next = root.next;
+                root.next = prev;
+                prev = root;
+                root = next;
+            }
+
+            return Tuple.Create(prev, left);
+        }
+    }
+}
