6c2682563c62c91bf562dc200981826e1b32a47a
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-09 22:09:06 +0300

    add solution of Minimum Deletion Cost to Avoid Repeating Letters

---
+++ LeetCodeTests/String/Medium/MinimumDeletionCostToAvoidRepeatingLetters/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.String.Medium.MinimumDeletionCostToAvoidRepeatingLetters;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.MinimumDeletionCostToAvoidRepeatingLetters
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinCost_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "aaabbbabbbb";
+            int[] cost = { 3, 5, 10, 7, 5, 3, 5, 5, 4, 8, 1 };
+
+            // Act
+            var result = solution.MinCost(
+                s,
+                cost);
+
+            // Assert
+            Assert.Equal(26, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/MinimumDeletionCostToAvoidRepeatingLetters/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Medium.MinimumDeletionCostToAvoidRepeatingLetters
+{
+    //https://leetcode.com/problems/minimum-deletion-cost-to-avoid-repeating-letters/
+    public class Solution
+    {
+        public int MinCost(string s, int[] cost)
+        {
+            var prevIndex = 0;
+            var i = 1;
+            int minConst = 0;
+
+            while (i < s.Length)
+            {
+                while (i < s.Length && s[i] == s[prevIndex])
+                {
+                    var costCurrent = cost[i];
+                    var costPrev = cost[prevIndex];
+
+                    if (costPrev >= costCurrent)
+                    {
+                        minConst += costCurrent;
+                        i++;
+                    }
+                    else
+                    {
+                        minConst += costPrev;
+                        prevIndex = i;
+                        i++;
+                    }
+                }
+
+                prevIndex = i;
+                i++;
+            }
+
+            return minConst;
+        }
+    }
+}
