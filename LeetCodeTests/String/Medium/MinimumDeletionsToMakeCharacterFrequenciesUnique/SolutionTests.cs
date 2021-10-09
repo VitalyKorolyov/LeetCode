eabd7a7be3a06b8e58a06fb8bab2ff62c699d2e1
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-09 20:29:12 +0300

    add solution of Minimum Deletions to Make Character Frequencies Unique

---
+++ LeetCodeTests/String/Medium/MinimumDeletionsToMakeCharacterFrequenciesUnique/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Medium.MinimumDeletionsToMakeCharacterFrequenciesUnique;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.MinimumDeletionsToMakeCharacterFrequenciesUnique
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinDeletions_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "bbcebab";
+
+            // Act
+            var result = solution.MinDeletions(s);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/MinimumDeletionsToMakeCharacterFrequenciesUnique/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Medium.MinimumDeletionsToMakeCharacterFrequenciesUnique
+{
+    //https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
+    public class Solution
+    {
+        public int MinDeletions(string s)
+        {
+            var array = new int[26];
+
+            for (var i = 0; i < s.Length; i++)
+                array[s[i] - 'a']++;
+
+            var hash = new HashSet<int>(26);
+            var res = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                while (array[i] > 0 && hash.Contains(array[i]))
+                {
+                    --array[i];
+                    ++res;
+                }
+
+                hash.Add(array[i]);
+            }
+
+            return res;
+        }
+    }
+}
