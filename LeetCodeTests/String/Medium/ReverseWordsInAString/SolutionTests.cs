2e4bfd5e057f52082fa5e9aed2a0608649a64752
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-09 22:48:08 +0300

    add solution of Reverse Words in a String

---
+++ LeetCodeTests/String/Medium/ReverseWordsInAString/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.String.Medium.ReverseWordsInAString;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.ReverseWordsInAString
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void ReverseWords_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s =
+                "F R  I   E    N     D      S      ";
+
+            // Act
+            var result = solution.ReverseWords(
+                s);
+
+            // Assert
+            Assert.Equal(
+                "S D N E I R F", result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/ReverseWordsInAString/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.String.Medium.ReverseWordsInAString
+{
+    //https://leetcode.com/problems/reverse-words-in-a-string/
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            var res = new StringBuilder(s.Length);
+
+            for (int i = s.Length - 1; i >= 0 ; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    var end = i;
+
+                    var start = 0;
+                    for (int j = end; j >= 0; j--)
+                    {
+                        if (s[j] != ' ')
+                            start = j;
+                        else break;
+                    }
+
+                    for (int j = start; j <= end; j++)
+                        res.Append(s[j]);
+
+                    if (start != 0) res.Append(' ');
+                    i = start;
+                }
+            }
+
+            if (res[res.Length - 1] == ' ') res.Remove(res.Length - 1, 1);
+
+            return res.ToString();
+        }
+
+    }
+}
