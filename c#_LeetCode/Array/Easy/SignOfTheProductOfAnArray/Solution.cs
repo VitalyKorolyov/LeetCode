93da58afc25f7d7f5184c94ddc35e7b20a6bdee7
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-09 19:40:51 +0300

    add solution of Sign of the Product of an Array

---
+++ c#_LeetCode/Array/Easy/SignOfTheProductOfAnArray/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Array.Easy.SignOfTheProductOfAnArray
+{
+    //https://leetcode.com/problems/sign-of-the-product-of-an-array/
+    public class Solution
+    {
+        public int ArraySign(int[] nums)
+        {
+            var value = 1;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0) return 0;
+                if (nums[i] < 0) value *= -1;
+            }
+
+            return value;
+        }
+    }
+}
