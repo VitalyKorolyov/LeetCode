be3085866e558dcfb9ac8edaf895d8cb278c5515
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-11 20:40:17 +0300

    add solution of Partition Labels

---
+++ LeetCodeTests/String/Medium/PartitionLabels/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.String.Medium.PartitionLabels;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.PartitionLabels
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void PartitionLabels_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "ababcbacadefegdehijhklij";
+
+            // Act
+            var result = solution.PartitionLabels(s);
+
+            // Assert
+            Assert.Equal(new int[]{ 9, 7, 8 }, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/PartitionLabels/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Medium.PartitionLabels
+{
+    //https://leetcode.com/problems/partition-labels/
+    public class Solution
+    {
+        public IList<int> PartitionLabels(string s)
+        {
+            var res = new List<int>();
+            var hashMap = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (hashMap.ContainsKey(s[i]))
+                {
+                    hashMap[s[i]] = i;
+                }
+                else
+                {
+                    hashMap.Add(s[i], i);
+                }
+            }
+
+            int left = 0, right = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var rightPos = hashMap[s[i]];
+
+                if (rightPos > right)
+                    right = rightPos;
+
+                if (i == right)
+                {
+                    res.Add(right - left + 1);
+                    right = i + 1;
+                    left = right;
+                }
+            }
+
+            return res;
+        }
+    }
+}
