4a0f225ce959684e81c52faab63ce6374ad9e287
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-15 21:48:47 +0300

    add solution of Reorder List;

---
+++ LeetCodeTests/LinkedList/Medium/ReorderList/ListNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.LinkedList.Medium.ReorderList;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Medium.ReorderList
+{
+    public class ListNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var listNode = new ListNode(1);
+
+            // Act
+            new Solution().ReorderList(listNode);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/LinkedList/Medium/ReorderList/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.LinkedList.Medium.ReorderList
+{
+    //https://leetcode.com/problems/reorder-list/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public void ReorderList(ListNode head)
+        {
+            var slow = head;
+            var fast = head;
+
+            while (fast?.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            var middle = slow;
+            ListNode prev = null;
+
+            while (middle != null)
+            {
+                var next = middle.next;
+                middle.next = prev;
+
+                prev = middle;
+                middle = next;
+            }
+
+            middle = prev;
+            var start = head;
+
+            while (middle?.next != null)
+            {
+                var next = start.next;
+                var mNext = middle.next;
+
+                start.next = middle;
+                middle.next = next;
+
+                middle = mNext;
+                start = next;
+            }
+        }
+    }
+}
