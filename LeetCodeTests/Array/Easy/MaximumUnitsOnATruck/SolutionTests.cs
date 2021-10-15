a34d3601c22846cc164248a7abb01c4be5c1e598
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-16 00:39:54 +0300

    add solution of Maximum Units on a Truck

---
+++ LeetCodeTests/Array/Easy/MaximumUnitsOnATruck/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Array.Easy.MaximumUnitsOnATruck;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.MaximumUnitsOnATruck
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MaximumUnits_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] boxTypes = new[] {new[] {1, 3}, new Int32[] {2, 2}, new Int32[] {3, 1}};
+            int truckSize = 4;
+
+            // Act
+            var result = solution.MaximumUnits(
+                boxTypes,
+                truckSize);
+
+            // Assert
+            Assert.Equal(8, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/MaximumUnitsOnATruck/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Array.Easy.MaximumUnitsOnATruck
+{
+    //https://leetcode.com/problems/maximum-units-on-a-truck/
+    public class Solution
+    {
+        public int MaximumUnits(int[][] boxTypes, int truckSize)
+        {
+            System.Array.Sort(boxTypes, (x, y) => y[1].CompareTo(x[1]));
+
+            var res = 0;
+            for (int i = 0; i < boxTypes.Length && truckSize != 0; i++)
+            {
+                if (boxTypes[i][0] <= truckSize)
+                {
+                    truckSize -= boxTypes[i][0];
+                    res += boxTypes[i][1] * boxTypes[i][0];
+                }
+                else
+                {
+                    res += truckSize * boxTypes[i][1];
+                    truckSize -= truckSize;
+                }
+            }
+
+            return res;
+        }
+    }
+}
