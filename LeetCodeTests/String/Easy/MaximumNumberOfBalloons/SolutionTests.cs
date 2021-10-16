a408bf53b0d057efdabc713062ac6b5c2a4b9a86
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-16 21:44:07 +0300

    add solution of Maximum Number of Balloons

---
+++ LeetCodeTests/String/Easy/MaximumNumberOfBalloons/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Easy.MaximumNumberOfBalloons;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.MaximumNumberOfBalloons
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MaxNumberOfBalloons_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string text = "balllllllllllloooooooooon";
+
+            // Act
+            var result = solution.MaxNumberOfBalloons(
+                text);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/MaximumNumberOfBalloons/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.String.Easy.MaximumNumberOfBalloons
+{
+    //https://leetcode.com/problems/maximum-number-of-balloons/
+    public class Solution
+    {
+        public int MaxNumberOfBalloons(string text)
+        {
+            var dic = new Dictionary<char, int>()
+            {
+                {'a', 0},
+                {'b', 0},
+                {'n', 0},
+                {'l', 0},
+                {'o', 0}
+            };
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (dic.ContainsKey(text[i]))
+                {
+                    dic[text[i]]++;
+                }
+            }
+
+            return dic.Min(x => x.Key == 'l' || x.Key == 'o' ? x.Value/2 : x.Value);
+        }
+    }
+}
