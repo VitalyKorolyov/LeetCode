1434eb9302d36fc4e98a7ade3421585fc671fabe
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-16 20:13:58 +0300

    add solution of Find Leaves of Binary Tree

---
+++ LeetCodeTests/BST/Medium/FindLeavesOfBinaryTree/TreeNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.BST.Medium.FindLeavesOfBinaryTree;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.BST.Medium.FindLeavesOfBinaryTree
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+
+            // Act
+            var res = new Solution().FindLeaves(treeNode);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/BST/Medium/FindLeavesOfBinaryTree/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.BST.Medium.FindLeavesOfBinaryTree
+{
+    //https://leetcode.com/problems/find-leaves-of-binary-tree/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> FindLeaves(TreeNode root)
+        {
+            var res = new List<IList<int>>();
+
+            GetHeight(root, res);
+
+            return res;
+        }
+
+        private int GetHeight(TreeNode root, IList<IList<int>> res)
+        {
+            if (root == null) return -1;
+
+            var leftHeight = GetHeight(root.left, res);
+            var rightHeight = GetHeight(root.right, res);
+
+            var currHeight = System.Math.Max(leftHeight, rightHeight) + 1;
+
+            if (res.Count == currHeight)
+                res.Add(new List<int>());
+
+            res[currHeight].Add(root.val);
+
+            return currHeight;
+        }
+    }
+}
