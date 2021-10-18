265edaa411d42aa5a46e587bca6dd7167d5bd86c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-18 21:00:28 +0300

    add solution of  High Five

---
+++ LeetCodeTests/Array/Easy/HighFive/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Array.Easy.HighFive;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.HighFive
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void HighFive_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] items =
+            {
+                new int[] {1, 91}, new int[] {1, 92}, new int[] {2, 93}, new int[] {2, 97}, new int[] {1, 60},
+                new int[] {2, 77}, new int[] {1, 65},
+                new int[] {1, 87}, new int[] {1, 100}, new int[] {2, 100}, new int[] {2, 76}
+            };
+
+            // Act
+            var result = solution.HighFive(
+                items);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/HighFive/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array.Easy.HighFive
+{
+    //https://leetcode.com/problems/high-five/
+    public class Solution
+    {
+        public int[][] HighFive(int[][] items)
+        {
+            System.Array.Sort(items, (x, y) =>
+            {
+                if (x[0] != y[0])
+                    return x[0].CompareTo(y[0]);
+
+                return y[1].CompareTo(x[1]);
+            });
+
+            var hashMap = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (!hashMap.ContainsKey(items[i][0]))
+                    hashMap.Add(items[i][0], new List<int>(5));
+
+                if(hashMap[items[i][0]].Count == 5) continue;
+
+                hashMap[items[i][0]].Add(items[i][1]);
+            }
+
+            var res = new int[hashMap.Count][];
+            var j = 0;
+            foreach (var key in hashMap.Keys)
+            {
+                var avS = hashMap[key].Sum() / 5;
+                res[j++] = new[] { key, avS};
+            }
+
+            return res;
+        }
+    }
+}
