997132c708bb65907d67299cf016659694d44e47
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-10-18 21:21:53 +0300

    add solution of Find the Middle Index in Array

---
+++ LeetCodeTests/Array/Easy/FindPivotIndex/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Easy.FindPivotIndex;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.FindPivotIndex
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void PivotIndex_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new []{1, 7, 3, 6, 5, 6};
+
+            // Act
+            var result = solution.PivotIndex(nums);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/FindPivotIndex/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace LeetCode.Array.Easy.FindPivotIndex
+{
+    //https://leetcode.com/problems/find-the-middle-index-in-array/
+    //https://leetcode.com/problems/find-the-middle-index-in-array/
+    public class Solution
+    {
+        public int PivotIndex(int[] nums)
+        {
+            var lSum = 0;
+            var rSum = nums.Sum();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                rSum -= nums[i];
+
+                if (lSum == rSum) return i;
+
+                lSum += nums[i];
+            }
+
+            return -1;
+        }
+    }
+}
