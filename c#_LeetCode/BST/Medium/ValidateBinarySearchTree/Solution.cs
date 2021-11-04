4b114e9bff2cbddd3c098d7fef1df8858952cafb
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-11-04 23:14:38 +0200

    add solution of Validate Binary Search Tree

---
+++ c#_LeetCode/BST/Medium/ValidateBinarySearchTree/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.BST.Medium.ValidateBinarySearchTree
+{
+    //https://leetcode.com/problems/validate-binary-search-tree/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return IsValid(root, null, null);
+        }
+
+        private bool IsValid(TreeNode root, int? low, int? high)
+        {
+            if (root == null) return true;
+
+            if ((low != null && root.val <= low) || (high != null && root.val >= high))
+            {
+                return false;
+            }
+
+            return IsValid(root.left, low, root.val) && IsValid(root.right, root.val, high);
+        }
+    }
+}
