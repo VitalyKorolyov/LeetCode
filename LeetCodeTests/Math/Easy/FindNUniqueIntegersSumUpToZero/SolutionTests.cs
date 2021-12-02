a11f378adc6f06709ea83862341cf20ed982281f
Vitali_Karaliou <vitali.karaliou@linnworks.com>
2021-12-02 21:08:36 +0200

    add solution of Find N Unique Integers Sum up to Zero

---
+++ LeetCodeTests/Math/Easy/FindNUniqueIntegersSumUpToZero/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Math.Easy.FindNUniqueIntegersSumUpToZero;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace LeetCodeTests.Math.Easy.FindNUniqueIntegersSumUpToZero
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SumZero_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int n = 1;
+
+            // Act
+            var result = solution.SumZero(n);
+
+            // Assert
+            Assert.True(result.Sum() == 0);
+        }
+    }
+}
---
+++ c#_LeetCode/Math/Easy/FindNUniqueIntegersSumUpToZero/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Math.Easy.FindNUniqueIntegersSumUpToZero
+{
+    //https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+    public class Solution
+    {
+        public int[] SumZero(int n)
+        {
+            var mid = n / 2;
+
+            var res = new int[n];
+            for (int i = 0; i < mid; i++)
+                res[i] = i + 1;
+
+            if (n % 2 != 0)
+                res[mid] = 0;
+            else
+                res[mid] = -res[mid - 1];
+
+            for (int i = mid + 1; i < n; i++)
+                res[i] = -res[i - (mid + 1)];
+
+            return res;
+        }
+    }
+}
