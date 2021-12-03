b01d6226fd62505da21456f373e313250d595e8b
Vitali_Karaliou <vitali.karaliou@linnworks.com>
2021-12-04 00:46:02 +0200

    add solution of Reverse Vowels of a String

---
+++ LeetCodeTests/String/Easy/ReverseVowelsOfAString/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Easy.ReverseVowelsOfAString;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.ReverseVowelsOfAString
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void ReverseVowels_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "ai";
+
+            // Act
+            var result = solution.ReverseVowels(s);
+
+            // Assert
+            Assert.Equal("ia", result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/ReverseVowelsOfAString/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LeetCode.String.Easy.ReverseVowelsOfAString
+{
+    //https://leetcode.com/problems/reverse-vowels-of-a-string/
+    public class Solution
+    {
+        public string ReverseVowels(string s)
+        {
+            Span<char> vowels = stackalloc char[]
+            {
+                'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'
+            };
+
+            Span<char> res = stackalloc char[s.Length];
+            for (int k = 0; k < s.Length; k++)
+                res[k] = s[k];
+
+            int i = 0, j = s.Length - 1;
+
+            while (i < j)
+            {
+                if (vowels.IndexOf(s[i]) >= 0)
+                {
+                    while (j > i)
+                    {
+                        if (vowels.IndexOf(s[j]) >= 0)
+                        {
+                            res[i] = s[j];
+                            res[j] = s[i];
+                            j--;
+                            i++;
+                            break;
+                        }
+
+                        j--;
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+}
