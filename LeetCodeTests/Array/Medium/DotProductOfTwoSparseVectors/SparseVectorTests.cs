831653959199b67b130379344c80071ff9eaa39e
Vitali_Karaliou <vitali.karaliou@linnworks.com>
2021-12-05 02:00:03 +0200

    add solution of Dot Product of Two Sparse Vectors;

---
+++ LeetCodeTests/Array/Medium/DotProductOfTwoSparseVectors/SparseVectorTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Array.Medium.DotProductOfTwoSparseVectors;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.DotProductOfTwoSparseVectors
+{
+    public class SparseVectorTests
+    {
+        [Fact]
+        public void DotProduct_StateUnderTest_ExpectedBehavior()
+        {
+            var b = new int[] { 0, 1, 0, 0, 2, 0, 0 };
+            // Arrange
+            var v1 = new SparseVector(b);
+            var v2 = new SparseVector(new int[] { 1, 0, 0, 0, 3, 0, 4 });
+
+            // Act
+            var result = v1.DotProduct(v2);
+
+            // Assert
+            Assert.Equal(6, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/DotProductOfTwoSparseVectors/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array.Medium.DotProductOfTwoSparseVectors
+{
+    //https://leetcode.com/problems/dot-product-of-two-sparse-vectors/
+    public class SparseVector
+    {
+        public Dictionary<int, int> Vector { get; }
+
+        public SparseVector(int[] nums)
+        {
+            Vector = new Dictionary<int, int>(nums.Count(x => x != 0));
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] != 0)
+                    Vector.Add(i, nums[i]);
+            }
+        }
+
+        // Return the dotProduct of two sparse vectors
+        public int DotProduct(SparseVector vec)
+        {
+            var res = 0;
+
+            foreach (var v in vec.Vector)
+            {
+                if (Vector.ContainsKey(v.Key))
+                    res += v.Value * Vector[v.Key];
+            }
+
+            return res;
+        }
+    }
+}
