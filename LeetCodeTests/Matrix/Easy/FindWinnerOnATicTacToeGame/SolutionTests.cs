4bae87bf59bbfea938dbd43d5b68cb48b3ae11cc
Vitali_Karaliou <vitali.karaliou@linnworks.com>
2021-12-07 00:47:13 +0200

    add Solution of Find Winner on a Tic Tac Toe Game

---
+++ LeetCodeTests/Matrix/Easy/FindWinnerOnATicTacToeGame/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Matrix.Easy;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Easy.FindWinnerOnATicTacToeGame
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Tictactoe_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] moves = new int[2][];
+            moves[0] = new[] { 0, 0 };
+            moves[1] = new[] { 1, 1 };
+            //moves[2] = new[] { 1, 1 };
+            //moves[3] = new[] { 2, 1 };
+            //moves[4] = new[] { 2, 2 };
+
+            // Act
+            var result = solution.Tictactoe(moves);
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Easy/FindWinnerOnATicTacToeGame/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+namespace LeetCode.Matrix.Easy
+{
+    public class Solution
+    {
+        public string Tictactoe(int[][] moves)
+        {
+            var matrix = new int[3][];
+            matrix[0] = new int[3];
+            matrix[1] = new int[3];
+            matrix[2] = new int[3];
+
+            for (int i = 0; i < moves.Length; i++)
+            {
+                matrix[moves[i][0]][moves[i][1]] = i % 2 == 0 ? 1 : 2;
+            }
+
+            return GetResult(matrix, moves.Length);
+        }
+
+        private string GetResult(int[][] matrix, int steps)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                if (matrix[i][0] != 0 && matrix[i][0] == matrix[i][1] && matrix[i][1] == matrix[i][2]) 
+                    return matrix[i][0] == 1 ? "A" : "B";
+            }
+
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                if (matrix[0][i] != 0 && matrix[0][i] == matrix[1][i] && matrix[0][i] == matrix[2][i]) 
+                    return matrix[0][i] == 1 ? "A" : "B";
+            }
+
+            if(matrix[0][0] != 0 && matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2])
+                return matrix[0][0] == 1 ? "A" : "B";
+
+            if (matrix[0][2] != 0 && matrix[0][2] == matrix[1][1] && matrix[0][2] == matrix[2][0])
+                return matrix[0][2] == 1 ? "A" : "B";
+
+            if (steps != 9) return "Pending";
+
+            return "Draw";
+        }
+    }
+}
