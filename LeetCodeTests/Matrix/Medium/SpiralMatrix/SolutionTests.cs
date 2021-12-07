3fc16dcb6a0aaba4846f509aabcdb3cfca0317ca
Vitali_Karaliou <vitali.karaliou@linnworks.com>
2021-12-07 22:14:55 +0200

    add solution of Spiral Matrix

---
+++ LeetCodeTests/Matrix/Medium/SpiralMatrix/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Matrix.Medium.SpiralMatrix;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Medium.SpiralMatrix
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SpiralOrder_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+
+            var matrix = new int[3][];
+            matrix[0] = new int[] { 1, 2, 3 };
+            matrix[1] = new int[] { 4, 5, 6 };
+            matrix[2] = new int[] { 7, 8, 9 };
+
+
+            // Act
+            var result = solution.SpiralOrder(matrix);
+
+            // Assert
+            Assert.Equal(new[]{1, 2, 3, 6, 9, 8, 7, 4, 5}, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Medium/SpiralMatrix/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Matrix.Medium.SpiralMatrix
+{
+    //https://leetcode.com/problems/spiral-matrix/
+    public class Solution
+    {
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            var res = new List<int>(matrix.Length * matrix[0].Length);
+
+            int rowSize = matrix.Length;
+            int colSize = matrix[0].Length;
+            int up = 0;
+            int left = 0;
+            int right = colSize - 1;
+            int down = rowSize - 1;
+
+            while (res.Count < rowSize * colSize)
+            {
+                // Traverse from left to right.
+                for (int col = left; col <= right; col++)
+                {
+                    res.Add(matrix[up][col]);
+                }
+                // Traverse downwards.
+                for (int row = up + 1; row <= down; row++)
+                {
+                    res.Add(matrix[row][right]);
+                }
+                // Make sure we are now on a different row.
+                if (up != down)
+                {
+                    // Traverse from right to left.
+                    for (int col = right - 1; col >= left; col--)
+                    {
+                        res.Add(matrix[down][col]);
+                    }
+                }
+                // Make sure we are now on a different column.
+                if (left != right)
+                {
+                    // Traverse upwards.
+                    for (int row = down - 1; row > up; row--)
+                    {
+                        res.Add(matrix[row][left]);
+                    }
+                }
+
+                left++;
+                right--;
+                up++;
+                down--;
+            }
+
+            return res;
+        }
+    }
+}
