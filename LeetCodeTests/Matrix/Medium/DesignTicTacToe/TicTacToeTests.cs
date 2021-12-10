5471bbcc8129f02c1c941afe62fd14127cbbe769
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-12-10 23:17:57 +0200

    add solution of Design Tic-Tac-Toe

---
+++ LeetCodeTests/Matrix/Medium/DesignTicTacToe/TicTacToeTests.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Matrix.Medium.DesignTicTacToe;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Medium.DesignTicTacToe
+{
+    public class TicTacToeTests
+    {
+        [Fact]
+        public void Move_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var ticTacToe = new TicTacToe(4);
+
+            // Act
+            var result = ticTacToe.Move(
+                2,
+                0,
+                1);
+
+            result = ticTacToe.Move(
+                2,
+                1,
+                1);
+
+
+            result = ticTacToe.Move(
+                2,
+                2,
+                1);
+
+
+            result = ticTacToe.Move(
+                2,
+                3,
+                1);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Medium/DesignTicTacToe/Solution.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCode.Matrix.Medium.DesignTicTacToe
+{
+    //https://leetcode.com/problems/design-tic-tac-toe/
+    public class TicTacToe
+    {
+        private readonly int[][] _matrix;
+
+        public TicTacToe(int n)
+        {
+            _matrix = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                _matrix[i] = new int[n];
+            }
+        }
+
+        public int Move(int row, int col, int player)
+        {
+            _matrix[row][col] = player;
+
+            for (int i = 0; i < _matrix.Length; i++)
+            {
+                if (i == _matrix.Length - 1 && _matrix[row][i] == player) return player;
+                if (_matrix[row][i] != player) break;
+            }
+
+            for (int i = 0; i < _matrix.Length; i++)
+            {
+                if (i == _matrix.Length - 1 && _matrix[i][col] == player) return player;
+                if (_matrix[i][col] != player) break;
+            }
+
+            if (col == row)
+            {
+                for (int i = 0; i < _matrix.Length; i++)
+                {
+                    if (i == _matrix.Length - 1 && _matrix[i][i] == player) return player;
+                    if (_matrix[i][i] != player) break;
+                }
+            }
+
+            if (col + row == _matrix.Length - 1)
+            {
+                for (int i = 0; i < _matrix.Length; i++)
+                {
+                    if (i == _matrix.Length - 1 && _matrix[_matrix.Length - i - 1][i] == player) return player;
+                    if (_matrix[_matrix.Length - i - 1][i] != player) break;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
