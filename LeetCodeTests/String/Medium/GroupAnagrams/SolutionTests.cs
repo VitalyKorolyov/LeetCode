eb2edc78980429bbce050174244078bc0f71de65
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-12-13 00:41:56 +0200

    add solution of Group Anagrams

---
+++ LeetCodeTests/String/Medium/GroupAnagrams/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Medium.GroupAnagrams;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.GroupAnagrams
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void GroupAnagrams_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string[] strs = {"ddddddddddg","dgggggggggg"};
+
+            // Act
+            var result = solution.GroupAnagrams(
+                strs);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/GroupAnagrams/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.String.Medium.GroupAnagrams
+{
+    //https://leetcode.com/problems/group-anagrams/
+    public class Solution
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var res = new List<IList<string>>();
+
+            if (strs == null || strs.Length == 0)
+                return res;
+
+            var dict = new Dictionary<string, List<string>>();
+
+            foreach (var str in strs)
+            {
+                var cur = new string(str.OrderBy(x => x).ToArray());
+
+                if (!dict.ContainsKey(cur))
+                    dict.Add(cur, new List<string>());
+
+                dict[cur].Add(str);
+            }
+
+            foreach (var item in dict.Values)
+                res.Add(item);
+
+            return res;
+        }
+    }
+}
