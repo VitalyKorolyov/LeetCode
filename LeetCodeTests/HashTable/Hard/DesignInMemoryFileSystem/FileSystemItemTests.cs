9f86aaf9fe1e2c4be6e6b92e298a1df998467d8e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-12-14 01:39:42 +0200

    add solution of Design In-Memory File System

---
+++ LeetCodeTests/HashTable/Hard/DesignInMemoryFileSystem/FileSystemItemTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.HashTable.Hard.DesignInMemoryFileSystem;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.HashTable.Hard.DesignInMemoryFileSystem
+{
+    public class FileSystemItemTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var fileSystemItem = new FileSystem();
+
+            // Act
+            fileSystemItem.Mkdir("/goowmfn");
+            fileSystemItem.Ls("/goowmfn");
+            fileSystemItem.Ls("/");
+            fileSystemItem.Mkdir("/z");
+            fileSystemItem.AddContentToFile("/goowmfn/c", "shetopcy");
+            fileSystemItem.Ls("/");
+            fileSystemItem.Ls("/goowmfn/c");
+            fileSystemItem.Ls("/goowmfn");
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/HashTable/Hard/DesignInMemoryFileSystem/FileSystem.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.HashTable.Hard.DesignInMemoryFileSystem
+{
+    //https://leetcode.com/problems/design-in-memory-file-system/
+    internal class FileSystemItem
+    {
+        public string Content;
+        public bool IsFile = false;
+        public Dictionary<string, FileSystemItem> Items = new();
+    }
+
+    public class FileSystem
+    {
+        private FileSystemItem _fileSystemItem;
+
+        public FileSystem()
+        {
+            _fileSystemItem = new FileSystemItem();
+        }
+
+        public IList<string> Ls(string path)
+        {
+            if (!_fileSystemItem.Items.Any()) return new List<string>();
+
+            var pathArray = path == "/" ? new []{""} : path.Split("/");
+
+            var head = _fileSystemItem;
+            foreach (var pathItem in pathArray)
+            {
+                head = head.Items[pathItem];
+            }
+
+            if (head.IsFile) return new List<string> {pathArray[pathArray.Length - 1]};
+
+            return head.Items.Keys.OrderBy(x => x).ToList();
+        }
+
+        public void Mkdir(string path)
+        {
+            var pathArray = path.Split('/');
+
+            var head = _fileSystemItem;
+
+            foreach (var pathItem in pathArray)
+            {
+                if (!head.Items.ContainsKey(pathItem))
+                {
+                    head.Items.Add(pathItem, new FileSystemItem());
+                }
+
+                head = head.Items[pathItem];
+            }
+        }
+
+        public void AddContentToFile(string filePath, string content)
+        {
+            var file = GetItem(filePath);
+            file.Content += content;
+            file.IsFile = true;
+        }
+
+        public string ReadContentFromFile(string filePath)
+        {
+            var file = GetItem(filePath);
+            return file.Content;
+        }
+
+        private FileSystemItem GetItem(string path)
+        {
+            var pathArray = path.Split('/');
+
+            var head = _fileSystemItem;
+
+            foreach (var pathItem in pathArray)
+            {
+                if (!head.Items.ContainsKey(pathItem))
+                {
+                    head.Items.Add(pathItem, new FileSystemItem());
+                }
+
+                head = head.Items[pathItem];
+            }
+
+            return head;
+        }
+    }
+}
