415f4b52fd2a21070f9061e38192b3d9733ad484
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-12-15 01:17:55 +0200

    add solution of Design HashMap

---
+++ LeetCodeTests/HashTable/Easy/DesignHashMap/PairTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.HashTable.Easy.DesignHashMap;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.HashTable.Easy.DesignHashMap
+{
+    public class PairTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var hashMap = new MyHashMap();
+            int res;
+
+            // Act
+            hashMap.Put(1, 1);
+            hashMap.Put(2, 2);
+            hashMap.Put(1, 10);
+            hashMap.Put(10002, 3);
+            res = hashMap.Get(1);
+            Assert.Equal(10, res);
+            res = hashMap.Get(10002); 
+            Assert.Equal(3, res);
+            hashMap.Remove(10002);
+        }
+    }
+}
---
+++ c#_LeetCode/HashTable/Easy/DesignHashMap/Solution.cs
@@ -0,0 +1,101 @@
+﻿namespace LeetCode.HashTable.Easy.DesignHashMap
+{
+    //https://leetcode.com/problems/design-hashmap/
+    public class Pair
+    {
+        public int Key;
+        public int Value;
+        public Pair Next;
+    }
+
+    public class MyHashMap
+    {
+        private const int DefSize = 10000;
+        private readonly Pair[] _hashMap;
+
+        public MyHashMap()
+        {
+            _hashMap = new Pair[DefSize];
+        }
+
+        public void Put(int key, int value)
+        {
+            var hash = CalcHash(key);
+
+            var pair = _hashMap[hash];
+            if (pair == null)
+            {
+                _hashMap[hash] = new Pair {Key = key, Value = value};
+            }
+            else
+            {
+                while (pair.Next != null)
+                {
+                    if(pair.Key == key) break;
+
+                    pair = pair.Next;
+                }
+
+                if (pair.Key == key)
+                {
+                    pair.Value = value;
+                }
+                else
+                {
+                    pair.Next = new Pair {Key = key, Value = value};
+                }
+            }
+        }
+
+        public int Get(int key)
+        {
+            var hash = CalcHash(key);
+            var pair = _hashMap[hash];
+
+            if (pair == null) return -1;
+
+            while (pair != null)
+            {
+                if (pair.Key == key) return pair.Value;
+
+                pair = pair.Next;
+            }
+
+            return -1;
+        }
+
+        public void Remove(int key)
+        {
+            var hash = CalcHash(key);
+            var pair = _hashMap[hash];
+
+            if(pair == null) return;
+
+            if (pair.Key == key)
+            {
+                _hashMap[hash] = pair.Next;
+                return;
+            }
+
+            var prev = pair;
+            pair = pair.Next;
+
+            while (pair != null)
+            {
+                if (pair.Key == key)
+                {
+                    prev.Next = pair.Next;
+                    return;
+                }
+
+                prev = prev.Next;
+                pair = pair.Next;
+            }
+        }
+
+        private static int CalcHash(int key)
+        {
+            return key % DefSize;
+        }
+    }
+}
