bb16cb46906f812e6120d971e985e4c050628ac7
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-12-24 01:12:14 +0200

    add solution of Logger Rate Limiter

---
+++ LeetCodeTests/HashTable/Easy/LoggerRateLimiter/LoggerTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.HashTable.Easy.LoggerRateLimiter;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.HashTable.Easy.LoggerRateLimiter
+{
+    public class LoggerTests
+    {
+        [Fact]
+        public void ShouldPrintMessage_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var logger = new Logger();
+
+            // Asserts
+            Assert.True(logger.ShouldPrintMessage(1, "foo"));
+            Assert.True(logger.ShouldPrintMessage(2, "bar"));
+            Assert.False(logger.ShouldPrintMessage(3, "foo"));
+            Assert.False(logger.ShouldPrintMessage(8, "bar"));
+            Assert.False(logger.ShouldPrintMessage(10, "foo"));
+            Assert.True(logger.ShouldPrintMessage(11, "foo"));
+        }
+    }
+}
---
+++ c#_LeetCode/HashTable/Easy/LoggerRateLimiter/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable.Easy.LoggerRateLimiter
+{
+    //https://leetcode.com/problems/logger-rate-limiter/
+    public class Logger
+    {
+        private readonly Dictionary<string, int> _logger;
+
+        public Logger()
+        {
+            _logger = new Dictionary<string, int>();
+        }
+
+        public bool ShouldPrintMessage(int timestamp, string message)
+        {
+            if (!_logger.ContainsKey(message))
+            {
+                _logger.Add(message, timestamp + 10);
+                return true;
+            }
+
+            var time = _logger[message];
+
+            if (time <= timestamp)
+            {
+                _logger[message] = timestamp + 10;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
