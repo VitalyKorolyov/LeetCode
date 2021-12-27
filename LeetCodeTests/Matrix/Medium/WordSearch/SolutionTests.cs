78fe9cd79c1434dc525a747448313574a3a0f387
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-12-28 00:27:11 +0200

    add solution of Word Search

---
+++ LeetCodeTests/Matrix/Medium/WordSearch/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Matrix.Medium.WordSearch;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Medium.WordSearch
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Exist_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            char[][] board = new char[4][];
+            board[0] = new char[]{'E', 'C', 'B', 'F'}; 
+            board[1] = new char[]{'D', 'M', 'K', 'Y'}; 
+            board[2] = new char[]{'K', 'C', 'B', 'D'}; 
+            board[3] = new char[]{'O', 'Y', 'K', 'M'}; 
+
+            string word = "MKBK";
+
+            // Act
+            var result = solution.Exist(board, word);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Medium/WordSearch/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Matrix.Medium.WordSearch
+{
+    //https://leetcode.com/problems/word-search/
+    public class Solution
+    {
+        public bool Exist(char[][] board, string word)
+        {
+            for (int col = 0; col < board.Length; col++)
+            {
+                for (int row = 0; row < board[0].Length; row++)
+                {
+                    if (board[col][row] == word[0])
+                    {
+                        var isExist = IsExist(board, col, row, word, 0);
+                        if (isExist) return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsExist(char[][] board, int col, int row, string word, 
+            int indexWord)
+        {
+            if (col < 0 || row < 0 || col > board.Length - 1 
+                || row > board[0].Length - 1 || word.Length - 1 < indexWord) return false;
+
+            if (board[col][row] != word[indexWord]) return false;
+
+            if (word.Length - 1 == indexWord) return true;
+
+            board[col][row] = '#';
+
+            var res = IsExist(board, col - 1, row, word, indexWord + 1)
+                   || IsExist(board, col, row - 1, word, indexWord + 1)
+                   || IsExist(board, col + 1, row, word, indexWord + 1)
+                   || IsExist(board, col, row + 1, word, indexWord + 1);
+
+            board[col][row] = word[indexWord];
+
+            return res;
+        }
+    }
+}
