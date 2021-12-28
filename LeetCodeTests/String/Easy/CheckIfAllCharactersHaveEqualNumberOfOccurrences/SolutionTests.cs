4fc61460fbbdff8ac1d1a2d3d3c0bdbfac0c1228
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-12-29 00:23:41 +0200

    add solution of Check if All Characters Have Equal Number of Occurrences

---
+++ LeetCodeTests/String/Easy/CheckIfAllCharactersHaveEqualNumberOfOccurrences/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Easy.CheckIfAllCharactersHaveEqualNumberOfOccurrences;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.CheckIfAllCharactersHaveEqualNumberOfOccurrences
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void AreOccurrencesEqual_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "abacbc";
+
+            // Act
+            var result = solution.AreOccurrencesEqual(s);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/CheckIfAllCharactersHaveEqualNumberOfOccurrences/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.String.Easy.CheckIfAllCharactersHaveEqualNumberOfOccurrences
+{
+    //https://leetcode.com/problems/check-if-all-characters-have-equal-number-of-occurrences/
+    public class Solution
+    {
+        public bool AreOccurrencesEqual(string s)
+        {
+            Span<int> c = stackalloc int[26];
+
+            for (int i = 0; i < s.Length; i++) c[s[i] - 'a']++;
+
+            int c1 = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                if (c[i] > 0)
+                {
+                    if (c1 == 0) { c1 = c[i]; continue; }
+                    if (c1 != c[i]) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
