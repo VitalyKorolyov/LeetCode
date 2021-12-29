0acb41a57680f4f79416b6710edba8e164a99476
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-12-30 00:13:59 +0200

    add solution of Design Hit Counter

---
+++ LeetCodeTests/HashTable/Medium/DesignHitCounter/HitCounterTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using LeetCode.Queue.Medium.DesignHitCounter;
+using Xunit;
+
+namespace LeetCodeTests.HashTable.Medium.DesignHitCounter
+{
+    public class HitCounterTests
+    {
+        [Fact]
+        public void Hit_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var hitCounter = new HitCounter();
+
+            // Act
+            hitCounter.Hit(1);
+            hitCounter.Hit(1);
+            hitCounter.Hit(2);
+            hitCounter.Hit(3);
+            hitCounter.Hit(3);
+            hitCounter.Hit(300);
+
+            // Assert
+            Assert.Equal(6, hitCounter.GetHits(300));
+            Assert.Equal(4, hitCounter.GetHits(301));
+        }
+
+        [Fact]
+        public void GetHits_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var hitCounter = new HitCounter();
+            int timestamp = 0;
+
+            // Act
+            var result = hitCounter.GetHits(
+                timestamp);
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ c#_LeetCode/Queue/Medium/DesignHitCounter/HitCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Queue.Medium.DesignHitCounter
+{
+    //https://leetcode.com/problems/design-hit-counter/
+    public class HitCounter
+    {
+        private readonly Queue<int> queue = new();
+
+        public void Hit(int timestamp)
+        {
+            queue.Enqueue(timestamp);
+        }
+
+        public int GetHits(int timestamp)
+        {
+            while (queue.Count > 0)
+            {
+                int diff = timestamp - queue.Peek();
+                if (diff >= 300) queue.Dequeue();
+                else break;
+            }
+
+            return queue.Count;
+        }
+    }
+}
