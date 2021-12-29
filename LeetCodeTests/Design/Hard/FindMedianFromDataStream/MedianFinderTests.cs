d0b63b6d32040b7c16a87eac48dc1430ea83cd55
Vitaly Korolev <vitaly.korolyov@gmail.com>
2021-12-29 22:55:57 +0200

    add solution of Find Median from Data Stream

---
+++ LeetCodeTests/Design/Hard/FindMedianFromDataStream/MedianFinderTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Design.Hard.FindMedianFromDataStream;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Design.Hard.FindMedianFromDataStream
+{
+    public class MedianFinderTests
+    {
+        [Fact]
+        public void AddNum_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var medianFinder = new MedianFinder();
+
+            // Act
+            medianFinder.AddNum(6);
+            medianFinder.AddNum(10);
+            medianFinder.AddNum(2);
+
+            // Assert
+            Assert.Equal(6, medianFinder.FindMedian());
+        }
+    }
+}
---
+++ c#_LeetCode/Design/Hard/FindMedianFromDataStream/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Design.Hard.FindMedianFromDataStream
+{
+    //https://leetcode.com/problems/find-median-from-data-stream/
+    public class MedianFinder
+    {
+        private List<int> array;
+
+        public MedianFinder()
+        {
+            array = new List<int>();
+        }
+
+        public void AddNum(int num)
+        {
+            var pos = array.BinarySearch(num);
+            if (pos < 0)
+            {
+                pos = ~pos;
+            }
+
+            array.Insert(pos, num);
+        }
+
+        public double FindMedian()
+        {
+            var middle = array.Count / 2;
+
+            if (array.Count % 2 == 0)
+            {
+                return (array[middle - 1] + array[middle]) / 2.0;
+            }
+
+            return array[middle];
+        }
+    }
+}
