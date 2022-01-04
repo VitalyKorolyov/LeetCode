711f482c516327c2d6082128b8c578402589e265
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-04 23:11:15 +0200

    add solution of String Compression

---
+++ LeetCodeTests/String/Medium/StringCompression/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Medium.StringCompression;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.StringCompression
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Compress_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            char[] chars = {'c', 'c', 'c' , 'c', 'c', 'b' };
+
+            // Act
+            var result = solution.Compress(
+                chars);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/StringCompression/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace LeetCode.String.Medium.StringCompression
+{
+    //https://leetcode.com/problems/string-compression/
+    public class Solution
+    {
+        public int Compress(char[] chars)
+        {
+            if (!chars.Any()) return 0;
+            if (chars.Length == 1) return 1;
+
+            var indexToInsert = 0;
+            for (int i = 0; i < chars.Length; i++)
+            {
+                var index = i;
+
+                while (index < chars.Length && chars[i] == chars[index])
+                {
+                    index++;
+                }
+
+                chars[indexToInsert++] = chars[i];
+                var count = index - i;
+
+                if (count > 1)
+                {
+                    foreach (var @char in count.ToString().ToCharArray())
+                        chars[indexToInsert++] = @char;
+                }
+
+                i = index - 1;
+            }
+
+            return indexToInsert;
+        }
+    }
+}
