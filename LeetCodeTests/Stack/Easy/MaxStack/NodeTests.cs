686021a55c0fd3e04db556293a9789a075106cea
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-05 22:17:29 +0200

    add solution of Max Stack

---
+++ LeetCodeTests/Stack/Easy/MaxStack/NodeTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Stack.Easy.MaxStack;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Stack.Easy.MaxStack
+{
+    public class NodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            var stack = new LeetCode.Stack.Easy.MaxStack.MaxStack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(1);
+
+            Assert.Equal(3, stack.PeekMax());
+            Assert.Equal(3, stack.PopMax());
+            Assert.Equal(1, stack.Top());
+            Assert.Equal(1, stack.Pop());
+            Assert.Equal(2, stack.PeekMax());
+            Assert.Equal(2, stack.PopMax());
+        }
+    }
+}
---
+++ c#_LeetCode/Stack/Easy/MaxStack/MaxStack.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Stack.Easy.MaxStack
+{
+    //https://leetcode.com/problems/max-stack/
+    public class Node
+    {
+        public int Value { get; set; }
+    }
+
+    public class MaxStack
+    {
+        private readonly SortedDictionary<int, List<Node>> _dictionary;
+        private readonly LinkedList<Node> _stack;
+
+        public MaxStack()
+        {
+            _stack = new LinkedList<Node>();
+            _dictionary = new SortedDictionary<int, List<Node>>();
+        }
+
+        //Log(n)
+        public void Push(int x)
+        {
+            var node = new Node {Value = x};
+            _stack.AddFirst(node);
+
+            if (!_dictionary.ContainsKey(x))
+            {
+                _dictionary.Add(x, new List<Node> {node});
+            }
+            else
+            {
+                var head = _dictionary[x];
+                head.Add(node);
+            }
+        }
+
+        //O(1)
+        public int Pop()
+        {
+            var node = _stack.First.Value;
+
+            var list = _dictionary[node.Value];
+            if (list.Count == 1)
+            {
+                _dictionary.Remove(node.Value);
+            }
+            else
+            {
+                list.RemoveAt(list.Count - 1);
+            }
+
+            _stack.RemoveFirst();
+
+            return node.Value;
+        }
+
+        //O(1)
+        public int Top()
+        {
+            return _stack.First.Value.Value;
+        }
+
+        //O(1)
+        public int PeekMax()
+        {
+            return _dictionary.Last().Key;
+        }
+
+        //O(1)
+        public int PopMax()
+        {
+            var list = _dictionary.Last().Value;
+
+            var last = list[list.Count - 1];
+
+            _stack.Remove(last);
+            list.RemoveAt(list.Count - 1);
+
+            if (!list.Any()) _dictionary.Remove(last.Value);
+
+            return last.Value;
+        }
+    }
+}
