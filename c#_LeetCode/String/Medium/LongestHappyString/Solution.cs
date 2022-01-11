8b142f0cc2dad7986fb6663964a818b60406efeb
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-11 23:00:48 +0200

    add solution of Longest Happy String

---
+++ c#_LeetCode/String/Medium/LongestHappyString/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace LeetCode.String.Medium.LongestHappyString
+{
+    //https://leetcode.com/problems/longest-happy-string/
+    public class Solution
+    {
+        public string LongestDiverseString(int a, int b, int c)
+        {
+            var size = a + b + c;
+            var res = new StringBuilder(size);
+
+            int A = 0, B = 0, C = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (a >= b && a >= c && A != 2 || B == 2 && a > 0 || C == 2 && a > 0)
+                {
+                    res.Append("a");
+                    A++;
+                    a--;
+                    B = 0;
+                    C = 0;
+                }
+                else if (b >= a && b >= c && B != 2 || A == 2 && b > 0 || C == 2 && b > 0)
+                {
+                    res.Append("b");
+                    b--;
+                    B++;
+                    C = 0;
+                    A = 0;
+                }
+                else if (c >= a && c >= b && C != 2 || A == 2 && c > 0 || B == 2 && c > 0)
+                {
+                    res.Append("c");
+                    C++;
+                    c--;
+                    A = 0;
+                    B = 0;
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+}
