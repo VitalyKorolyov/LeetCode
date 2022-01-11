9e7b073b14fc557ddaac6ca21f5ac48422ed72a7
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-12 00:03:05 +0200

    add solution of Angle Between Hands of a Clock

---
+++ LeetCodeTests/Math/Medium/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Math.Medium;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Math.Medium
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void AngleClock_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int hour = 3;
+            int minutes = 30;
+
+            // Act
+            var result = solution.AngleClock(
+                hour,
+                minutes);
+
+            // Assert
+            Assert.Equal(75, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Math/Medium/Solution.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCode.Math.Medium
+{
+    //https://leetcode.com/problems/angle-between-hands-of-a-clock/
+    public class Solution
+    {
+        public double AngleClock(int hour, int minutes)
+        {
+            var minDegrees = 6 * minutes;
+            double partsAfterHour = minutes / 60.0;
+            double degreesAfterHour = 30.0 * partsAfterHour;
+            var degreesFromHours = (HoursToMinutes(hour) * 6) + degreesAfterHour;
+
+            var firstAngle = System.Math.Abs(minDegrees - degreesFromHours);
+            var secondAngle = 360 - firstAngle;
+
+            return firstAngle < secondAngle ? firstAngle : secondAngle;
+        }
+
+        private int HoursToMinutes(int hours)
+        {
+            if (hours == 1)
+                return 5;
+            if (hours == 2)
+                return 10;
+            if (hours == 3)
+                return 15;
+            if (hours == 4)
+                return 20;
+            if (hours == 5)
+                return 25;
+            if (hours == 6)
+                return 30;
+            if (hours == 7)
+                return 35;
+            if (hours == 8)
+                return 40;
+            if (hours == 9)
+                return 45;
+            if (hours == 10)
+                return 50;
+            if (hours == 11)
+                return 55;
+            return 0;
+        }
+    }
+}
