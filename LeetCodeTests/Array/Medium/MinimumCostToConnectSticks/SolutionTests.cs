30cd35733bd94aeb3ad83a1bb8fc20da8ea0b011
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-14 23:04:30 +0200

    add solution of Minimum Cost to Connect Sticks

---
+++ LeetCodeTests/Array/Medium/MinimumCostToConnectSticks/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Array.Medium.MinimumCostToConnectSticks;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.MinimumCostToConnectSticks
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void ConnectSticks_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] sticks = {3, 7, 1, 10, 8};
+
+            // Act
+            var result = solution.ConnectSticks(
+                sticks);
+
+            // Assert
+            Assert.Equal(62, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/MinimumCostToConnectSticks/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.MinimumCostToConnectSticks
+{
+    //https://leetcode.com/problems/minimum-cost-to-connect-sticks/
+    public class Solution
+    {
+        public int ConnectSticks(int[] sticks)
+        {
+            var totalCost = 0;
+
+            if (sticks.Length <= 1) return totalCost;
+
+            var queue = new PriorityQueue<int, int>(sticks.Length);
+
+            for (int i = 0; i < sticks.Length; i++)
+                queue.Enqueue(sticks[i], sticks[i]);
+
+            while (queue.Count > 1)
+            {
+                var stickF = queue.Dequeue();
+                var stickS = queue.Dequeue();
+
+                var cost = stickF + stickS;
+                totalCost += cost;
+                queue.Enqueue(cost, cost);
+            }
+
+            return totalCost;
+        }
+    }
+}
