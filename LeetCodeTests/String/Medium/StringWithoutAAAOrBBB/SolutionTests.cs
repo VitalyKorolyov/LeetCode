9b84e20ed6002982caee205a48b4f813b2fd41e4
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-16 23:05:30 +0200

    add solution of String Without AAA or BBB

---
+++ LeetCodeTests/String/Medium/StringWithoutAAAOrBBB/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Medium.StringWithoutAAAOrBBB;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.StringWithoutAAAOrBBB
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void StrWithout3a3b_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int a = 1;
+            int b = 2;
+
+            // Act
+            var result = solution.StrWithout3a3b(a,b);
+
+            // Assert
+            Assert.Equal("bab", result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/StringWithoutAAAOrBBB/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace LeetCode.String.Medium.StringWithoutAAAOrBBB
+{
+    //https://leetcode.com/problems/string-without-aaa-or-bbb/
+    public class Solution
+    {
+        public string StrWithout3a3b(int a, int b)
+        {
+            var size = a + b;
+            var res = new StringBuilder(size);
+
+            var countA = 0; 
+            var countB = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                if(a >= b && countA <= 1 || countB == 2)
+                {
+                    res.Append("a");
+                    countA++;
+                    countB = 0;
+                    a--;
+                }
+                else
+                {
+                    res.Append("b");
+                    countB++;
+                    countA = 0;
+                    b--;
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+}
