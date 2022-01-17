a7dddc76bb72032449926ae44c43ffd3be981d89
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-18 01:16:16 +0200

    add solution of Cinema Seat Allocation

---
+++ LeetCodeTests/Array/Medium/CinemaSeatAllocation/SolutionTests.cs
@@ -0,0 +1,61 @@
+﻿using LeetCode.Array.Medium.CinemaSeatAllocation;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.CinemaSeatAllocation
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MaxNumberOfFamilies_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int n = 5;
+            int[][] reservedSeats = new int[15][];
+            for (int i = 0; i < 15; i++)
+            {
+                reservedSeats[i] = new int[2];
+            }
+            reservedSeats[0][0] = 4;
+            reservedSeats[0][1] = 7;
+            reservedSeats[1][0] = 4;
+            reservedSeats[1][1] = 1;
+            reservedSeats[2][0] = 3;
+            reservedSeats[2][1] = 1;
+            reservedSeats[3][0] = 5;
+            reservedSeats[3][1] = 9;
+            reservedSeats[4][0] = 4;
+            reservedSeats[4][1] = 4;
+            reservedSeats[5][0] = 3;
+            reservedSeats[5][1] = 7;
+            reservedSeats[6][0] = 1;
+            reservedSeats[6][1] = 3;
+            reservedSeats[7][0] = 5;
+            reservedSeats[7][1] = 5;
+            reservedSeats[8][0] = 1;
+            reservedSeats[8][1] = 6;
+            reservedSeats[9][0] = 1;
+            reservedSeats[9][1] = 8;
+            reservedSeats[10][0] = 3;
+            reservedSeats[10][1] = 9;
+            reservedSeats[11][0] = 2;
+            reservedSeats[11][1] = 9;
+            reservedSeats[12][0] = 1;
+            reservedSeats[12][1] = 4;
+            reservedSeats[13][0] = 1;
+            reservedSeats[13][1] = 9;
+            reservedSeats[14][0] = 1;
+            reservedSeats[14][1] = 10;
+
+
+            // Act
+            var result = solution.MaxNumberOfFamilies(
+                n,
+                reservedSeats);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/CinemaSeatAllocation/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.CinemaSeatAllocation
+{
+    //https://leetcode.com/problems/cinema-seat-allocation/
+    public class Solution
+    {
+        public int MaxNumberOfFamilies(int n, int[][] reservedSeats)
+        {
+            int result = 0;
+            var rows = new Dictionary<int, bool[]>();
+
+            foreach (var r in reservedSeats)
+            {
+                if (!rows.ContainsKey(r[0] - 1))
+                    rows[r[0] - 1] = new bool[10];
+
+                rows[r[0] - 1][r[1] - 1] = true;
+            }
+
+            foreach (var row in rows)
+                result += Count(row.Value);
+
+            return result + 2 * (n - rows.Count);
+        }
+
+        private int Count(bool[] row)
+        {
+            int count = 0;
+            if (!row[1] && !row[2] && !row[3] && !row[4])
+                count++;
+
+            if (!row[5] && !row[6] && !row[7] && !row[8])
+                count++;
+
+            if (count == 0 && !row[3] && !row[4] && !row[5] && !row[6])
+                count++;
+
+            return count;
+        }
+    }
+}
