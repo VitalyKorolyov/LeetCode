8bc3c9c073a166650069d76a07df8699c747de35
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-19 00:16:59 +0200

    add solution of Maximum Frequency Stack

---
+++ LeetCodeTests/Stack/Hard/FreqTests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Stack.Hard;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Stack.Hard
+{
+    public class FreqTests
+    {
+        [Fact]
+        public void Compare_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var freq = new FreqStack();
+
+            // Act
+            freq.Push(5); 
+            freq.Push(5);
+            freq.Push(5);
+            freq.Push(7);
+            freq.Push(5);
+            freq.Push(7);
+            freq.Push(1);
+            freq.Push(1);
+            freq.Push(1);
+            freq.Push(1);
+            freq.Push(1);
+            freq.Push(5);
+            freq.Push(1);
+
+            var value = freq.Pop();
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Stack/Hard/FreqStack.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Stack.Hard
+{
+    //https://leetcode.com/problems/maximum-frequency-stack/
+    public class FreqStack
+    {
+        Dictionary<int, Stack<int>> groups;
+        Dictionary<int, int> freqs;
+        int maxFreq;
+
+        public FreqStack()
+        {
+            groups = new Dictionary<int, Stack<int>>();
+            freqs = new Dictionary<int, int>();
+            maxFreq = 0;
+        }
+
+        public void Push(int x)
+        {
+            if (!freqs.TryAdd(x, 1))
+                freqs[x]++;
+
+            if (freqs[x] > maxFreq)
+                maxFreq = freqs[x];
+
+            Stack<int> group = null;
+            if (!groups.TryGetValue(freqs[x], out group))
+            {
+                group = new Stack<int>();
+                groups.Add(freqs[x], group);
+            }
+            group.Push(x);
+        }
+
+        public int Pop()
+        {
+            int x = groups[maxFreq].Pop();
+            freqs[x]--;
+
+            if (groups[maxFreq].Count == 0)
+            {
+                maxFreq--;
+                groups.Remove(maxFreq);
+            }
+            if(freqs[x] == 0)
+            {
+                freqs.Remove(x);
+            }
+
+            return x;
+        }
+    }
+}
