64eabe48f38214ec5833a6d82972f907a4fe5873
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-19 00:51:50 +0200

    add solution of Minimum Moves to Equal Array Elements II

---
+++ c#_LeetCode/Array/Medium/MinimumMovesToEqualArrayElementsII/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Array.Medium.MinimumMovesToEqualArrayElementsII
+{
+    //https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+    public class Solution
+    {
+        public int MinMoves2(int[] nums)
+        {
+            System.Array.Sort(nums);
+
+            var median = nums[nums.Length/2];
+
+            var moves = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                moves += System.Math.Abs(nums[i] - median);
+            }
+
+            return moves;
+        }
+    }
+}
