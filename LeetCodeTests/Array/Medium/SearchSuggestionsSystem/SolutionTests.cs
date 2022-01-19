04b44693ac8e19a570bf79397db21461bc1e1e48
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-20 00:14:02 +0200

    add solution of Search Suggestions System

---
+++ LeetCodeTests/Array/Medium/SearchSuggestionsSystem/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Array.Medium.SearchSuggestionsSystem;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.SearchSuggestionsSystem
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SuggestedProducts_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string[] products = { "mobile", "mouse", "moneypot", "monitor", "mousepad" };
+            string searchWord = "mouse";
+
+            // Act
+            var result = solution.SuggestedProducts(
+                products,
+                searchWord);
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/SearchSuggestionsSystem/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array.Medium.SearchSuggestionsSystem
+{
+    //https://leetcode.com/problems/search-suggestions-system/
+    public class Solution
+    {
+        public IList<IList<string>> SuggestedProducts(string[] products, string searchWord)
+        {
+            System.Array.Sort(products); //nlogn
+
+            var res = new List<IList<string>>();
+
+            for (int i = 0; i < searchWord.Length; i++) //k
+            {
+                var search = new List<string>();
+
+                foreach (var product in products) //n
+                {
+                    if (search.Count == 3) break;
+
+                    for (int j = 0; j <= i && j < product.Length; j++)
+                    {
+                        if(product[j] == searchWord[j] && j == i)
+                        {
+                            search.Add(product);
+                            break;
+                        }
+
+                        if (product[j] != searchWord[j]) break;
+                    }
+                }
+
+                res.Add(search);
+            }
+
+            return res;
+        }
+    }
+}
