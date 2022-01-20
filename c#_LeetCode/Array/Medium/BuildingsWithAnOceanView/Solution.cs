fa01b11f9b248964e0ac0b7c7f930347edce8725
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-21 00:15:48 +0200

    add Solution of Buildings With an Ocean View

---
+++ c#_LeetCode/Array/Medium/BuildingsWithAnOceanView/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array.Medium.BuildingsWithAnOceanView
+{
+    //https://leetcode.com/problems/buildings-with-an-ocean-view/
+    public class Solution
+    {
+        public int[] FindBuildings(int[] heights)
+        {
+            var res = new List<int>();
+
+            var highestIndex = heights.Length - 1;
+            res.Add(highestIndex);
+
+            for (int i = heights.Length - 2; i >= 0; i--)
+            {
+                if(heights[i] > heights[highestIndex])
+                {
+                    highestIndex = i;
+                    res.Add(i);
+                }
+            }
+
+            return res.OrderBy(x => x).ToArray();
+        }
+    }
+}
