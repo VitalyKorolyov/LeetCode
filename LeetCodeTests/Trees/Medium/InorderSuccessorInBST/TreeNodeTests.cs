4b581ace150a592a5d6ccf50f6186a83599f797f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-20 23:41:02 +0200

    add solution of Inorder Successor in BST

---
+++ LeetCodeTests/Trees/Medium/InorderSuccessorInBST/TreeNodeTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Trees.Medium.InorderSuccessorInBST;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.InorderSuccessorInBST
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(5);
+            treeNode.left = new TreeNode(3);
+            treeNode.right = new TreeNode(6);   
+            treeNode.left.left = new TreeNode(2);
+            treeNode.left.right = new TreeNode(4);
+            treeNode.left.left.left = new TreeNode(1);
+
+            // Act
+            var res = new Solution().InorderSuccessor(treeNode, new TreeNode(3));
+
+            // Assert
+            Assert.Equal(treeNode.left.right, res);
+        }
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/InorderSuccessorInBST/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Trees.Medium.InorderSuccessorInBST
+{
+    //https://leetcode.com/problems/inorder-successor-in-bst/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        private TreeNode sueccessor;
+        private int diff = int.MaxValue;
+
+        public TreeNode InorderSuccessor(TreeNode root, TreeNode p)
+        {
+            Dfs(root, p);
+
+            return sueccessor;
+        }
+
+        private void Dfs(TreeNode root, TreeNode p)
+        {
+            if(root == null) return;
+
+            if(root.val > p.val && root.val - p.val < diff)
+            {
+                sueccessor = root;
+                diff = root.val - p.val;
+            }
+
+            Dfs(root.left, p);
+            Dfs(root.right, p);
+        }
+    }
+}
