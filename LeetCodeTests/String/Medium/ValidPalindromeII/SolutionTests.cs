93d29c70468793909d2e7675597e73400ef07eff
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-23 00:53:38 +0200

    add solution of Valid Palindrome II

---
+++ LeetCodeTests/String/Medium/ValidPalindromeII/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Medium.ValidPalindromeII;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.ValidPalindromeII
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void ValidPalindrome_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "abckmkdcba";
+
+            // Act
+            var result = solution.ValidPalindrome(
+                s);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/ValidPalindromeII/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.String.Medium.ValidPalindromeII
+{
+    public class Solution
+    {
+        public bool ValidPalindrome(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            {
+                if (s[i] == s[j]) continue;
+                if (s[i] != s[j])
+                {
+                    return Compare(i + 1, j, s) || Compare(i, j - 1, s);
+                }
+            }
+
+            return true;
+        }
+
+        private bool Compare(int i, int j, string s)
+        {
+            for (; i < j; i++, j--)
+            {
+                if (s[i] == s[j]) continue;
+                else return false;
+            }
+
+            return true;
+        }
+    }
+}
