5ce10c35409443adc79087a9987f5e47cc71257c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-23 02:12:33 +0200

    add solution of Kth Largest Element in an Array

---
+++ LeetCodeTests/Array/Medium/KthLargestElementInAnArray/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Array.Medium.KthLargestElementInAnArray;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.KthLargestElementInAnArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FindKthLargest_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
+            int k = 4;
+
+            // Act
+            var result = solution.FindKthLargest(
+                nums,
+                k);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/KthLargestElementInAnArray/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.KthLargestElementInAnArray
+{
+    //https://leetcode.com/problems/kth-largest-element-in-an-array/
+    public class Solution
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            var list = new PriorityQueue<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                list.Enqueue(nums[i], nums[i]);
+            }
+
+            for (int i = 0; i < nums.Length - k; i++)
+            {
+                list.Dequeue();
+            }
+
+            return list.Dequeue();  
+        }
+    }
+}
