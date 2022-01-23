6bfa31e581ad75943fa94ce496756d09a4127f04
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-23 23:42:37 +0200

    add solution of Data Stream as Disjoint Intervals

---
+++ LeetCodeTests/HashTable/Hard/DataStreamAsDisjointIntervals/SummaryRangesTests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.HashTable.Hard.DataStreamAsDisjointIntervals;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.HashTable.Hard.DataStreamAsDisjointIntervals
+{
+    public class SummaryRangesTests
+    {
+        [Fact]
+        public void AddNum_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var summaryRanges = new SummaryRanges();
+            int val = 1;
+
+            // Act
+            summaryRanges.AddNum(val);
+            var res =  summaryRanges.GetIntervals();
+            summaryRanges.AddNum(3);
+            res = summaryRanges.GetIntervals();
+            summaryRanges.AddNum(7);
+            res = summaryRanges.GetIntervals();
+            summaryRanges.AddNum(2);
+            res = summaryRanges.GetIntervals();
+            summaryRanges.AddNum(6);
+            res = summaryRanges.GetIntervals();
+
+            // Assert
+            //Assert.True(false);
+        }
+    }
+}
---
+++ c#_LeetCode/HashTable/Hard/DataStreamAsDisjointIntervals/SummaryRanges.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable.Hard.DataStreamAsDisjointIntervals
+{
+    //https://leetcode.com/problems/data-stream-as-disjoint-intervals/
+    public class SummaryRanges
+    {
+        private readonly List<int> numbers;
+
+        public SummaryRanges()
+        {
+            numbers = new List<int>();
+        }
+
+        public void AddNum(int val)
+        {
+            var pos = numbers.BinarySearch(val);
+            if(pos < 0)
+                numbers.Insert(~pos, val);
+        }
+
+        public int[][] GetIntervals()
+        {
+            var result = new List<int[]>();
+
+            if (numbers.Count == 0)
+                return result.ToArray();
+
+            int start = numbers[0];
+            int end = numbers[0];
+            bool added = false;
+
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] - numbers[i - 1] == 1)
+                {
+                    end = numbers[i];
+                }
+                else
+                {
+                    result.Add(new int[2] { start, end });
+                    start = numbers[i];
+                    end = numbers[i];
+                }
+            }
+
+            if (!added)
+            {
+                result.Add(new int[2] { start, end });
+            }
+
+            return result.ToArray();
+        }
+    }
+}
