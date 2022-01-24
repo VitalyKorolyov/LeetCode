88e381a56d8680db1cc4e976581493cdc9e38b17
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-24 23:16:15 +0200

    add solution of Binary Tree Zigzag Level Order Traversal

---
+++ LeetCodeTests/Trees/Medium/BinaryTreeZigzagLevelOrderTraversal/TreeNodeTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Trees.Medium.BinaryTreeZigzagLevelOrderTraversal;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.BinaryTreeZigzagLevelOrderTraversal
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(0, new TreeNode(2, new TreeNode(1, new TreeNode(5), new TreeNode(1))), 
+                new TreeNode(4, new TreeNode(3, null, new TreeNode(6)), new TreeNode(-1, null, new TreeNode(8))));
+
+            // Act
+            var res = new Solution().ZigzagLevelOrder(treeNode);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/BinaryTreeZigzagLevelOrderTraversal/Solution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Trees.Medium.BinaryTreeZigzagLevelOrderTraversal
+{
+    //https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+     
+    public class Solution
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            var res = new Dictionary<int, IList<int>>();
+            if(root == null) return res.Values.ToList();
+
+            Dfs(root, res, 0);
+
+            foreach(var (key, item) in res)
+            {
+                if(key % 2 != 0) res[key] = res[key].Reverse().ToList();
+            }
+
+            return res.Values.ToList();
+        }
+
+        private void Dfs(TreeNode node, Dictionary<int, IList<int>> res, int level)
+        {
+            if (node == null) return;
+
+            Add(res, node.val, level);
+
+            Dfs(node.left, res, level + 1);
+            Dfs(node.right, res, level + 1);
+        }
+
+        private void Add(Dictionary<int, IList<int>> res, int value, int level)
+        {
+            if (res.ContainsKey(level))
+            {
+                res[level].Add(value);
+            }
+            else
+            {
+                res.Add(level, new List<int> { value });
+            }
+        }
+    }
+}
