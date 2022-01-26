05ac43ce8b9593562503f8d9f9e107cb8e24f647
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-26 21:37:24 +0200

    add solution of The kth Factor of n

---
+++ c#_LeetCode/Math/Medium/TheKthFactorOfN/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Math.Medium.TheKthFactorOfN
+{
+    //https://leetcode.com/problems/the-kth-factor-of-n/submissions/
+    public class Solution
+    {
+        public int KthFactor(int n, int k)
+        {
+            var kFact = -1;
+
+            for (int i = 1; i <= n/2 + 1; i++)
+            {
+                if(n % i == 0) k--;
+                if (0 == k) 
+                {
+                    kFact = i;
+                    break;
+                }
+            }
+
+            return k == 1 ? n : kFact;
+        }
+    }
+}
