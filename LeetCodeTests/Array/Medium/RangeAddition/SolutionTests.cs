e79640a4c2a561a7ab0560ffa9e9fbcb3819997f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-29 01:00:35 +0200

    add solution of Range Addition

---
+++ LeetCodeTests/Array/Medium/RangeAddition/SolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Array.Medium.RangeAddition;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.RangeAddition
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void GetModifiedArray_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int length = 5;
+            int[][] updates = new int[3][];
+            updates[0] = new int[] { 1, 3, 2 };
+            updates[1] = new int[] { 2, 4, 3 };
+            updates[2] = new int[] { 0, 2, -2 };
+
+            // Act
+            var result = solution.GetModifiedArray(length, updates);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/RangeAddition/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Array.Medium.RangeAddition
+{
+    //https://leetcode.com/problems/range-addition/
+    public class Solution
+    {
+        public int[] GetModifiedArray(int length, int[][] updates)
+        {
+            int[] arr = new int[length];
+            if (length == 0) return arr;
+
+            Span<int> difference = stackalloc int[length + 1];
+            foreach (int[] update in updates)
+            {
+                difference[update[0]] += update[2];
+                difference[update[1] + 1] -= update[2];
+            }
+
+            arr[0] = difference[0];
+
+            for (int i = 1; i < length; i++)
+            {
+                difference[i] = difference[i] + difference[i - 1];//Prefix sum of difference array
+                arr[i] = difference[i];
+            }
+
+            return arr;
+        }
+    }
+}
