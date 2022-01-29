fdc417d71197da9f8a5c71f6bf2e536c99126b20
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-01-29 23:57:17 +0200

    add solution of Min Cost Climbing Stairs

---
+++ LeetCodeTests/Array/Easy/MinCostClimbingStairs/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Easy.MinCostClimbingStairs;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.MinCostClimbingStairs
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinCostClimbingStairs_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] cost = { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 };
+
+            // Act
+            var result = solution.MinCostClimbingStairs(cost);
+
+            // Assert
+            Assert.Equal(15, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/MinCostClimbingStairs/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetCode.Array.Easy.MinCostClimbingStairs
+{
+    //https://leetcode.com/problems/min-cost-climbing-stairs/
+    public class Solution
+    {
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            Span<int> route = stackalloc int[cost.Length + 1];
+
+            for (int i = 2; i <= cost.Length; i++)
+            {
+                int takeOneStep = route[i - 1] + cost[i - 1];
+                int takeTwoSteps = route[i - 2] + cost[i - 2];
+                route[i] = System.Math.Min(takeOneStep, takeTwoSteps);
+            }
+
+            return route[route.Length - 1];
+        }
+    }
+}
