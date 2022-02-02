403cfa7c515607054843b1f4c65b22eb22996ccd
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-02-03 00:01:21 +0200

    add solution of Container With Most Water

---
+++ LeetCodeTests/Array/Medium/ContainerWithMostWater/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Array.Medium.ContainerWithMostWater;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.ContainerWithMostWater
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MaxArea_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+            // Act
+            var result = solution.MaxArea(
+                height);
+
+            // Assert
+            Assert.Equal(49, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/ContainerWithMostWater/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array.Medium.ContainerWithMostWater
+{
+    //https://leetcode.com/problems/container-with-most-water/
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            var left = 0;
+            var right = height.Length - 1;
+            var maxArea = int.MinValue;
+
+            while (left != right)
+            {
+                var area = System.Math.Min(height[right], height[left]) * (right - left);
+                if(area > maxArea)
+                {
+                    maxArea = area;
+                }
+
+                if (height[left] > height[right]) right--;
+                else left++;
+            }
+
+            return maxArea;
+        }
+    }
+}
