cc2ae23bd49c1993edab3cd80035967ae016cd40
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-06 21:38:05 +0200

    add solution of First Unique Character in a String

---
+++ LeetCodeTests/String/Easy/FirstUniqueCharacterInaString/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Easy.FirstUniqueCharacterInaString;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.FirstUniqueCharacterInaString
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FirstUniqChar_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "leetcodez";
+
+            // Act
+            var result = solution.FirstUniqChar(s);
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/FirstUniqueCharacterInaString/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCode.String.Easy.FirstUniqueCharacterInaString
+{
+    //https://leetcode.com/problems/first-unique-character-in-a-string/
+    public class Solution
+    {
+        public int FirstUniqChar(string s)
+        {
+            Span<int> letters = stackalloc int[26];
+
+            for (int i = 0; i < s.Length; i++)
+                letters[s[i] - 'a'] += 1;
+
+            for (int i = 0; i < s.Length; i++)
+                if (letters[s[i] - 'a'] == 1) return i;
+
+            return -1;
+        }
+    }
+}
