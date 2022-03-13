ac6226a07a504e8fd02f84fd2f6616e9f53d5815
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-13 23:21:20 +0200

    add solution of Closest Binary Search Tree Value

---
+++ c#_LeetCode/BST/Easy/ClosestBinarySearchTreeValue/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.BST.Easy.ClosestBinarySearchTreeValue
+{
+    //https://leetcode.com/problems/closest-binary-search-tree-value/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public int ClosestValue(TreeNode root, double target)
+        {
+            int closest = root.val;
+
+            while (root != null)
+            {
+                closest = System.Math.Abs(root.val - target) < System.Math.Abs(closest - target) 
+                    ? root.val
+                    : closest;
+
+                root = target < root.val ? root.left : root.right;
+            }
+
+            return closest;
+        }
+    }
+}
