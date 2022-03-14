8ea15984dd625f3499b352df2da527f5c22a9052
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-14 23:28:43 +0200

    add solution of Top K Frequent Elements

---
+++ LeetCodeTests/Array/Medium/TopKFrequentElements/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Array.Medium.TopKFrequentElements;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.TopKFrequentElements
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void TopKFrequent_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = { 1, 1, 1, 2, 2, 3 };
+            int k = 2;
+
+            // Act
+            var result = solution.TopKFrequent(
+                nums,
+                k);
+
+            // Assert
+            Assert.Equal(new[] {2, 1}, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/TopKFrequentElements/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.TopKFrequentElements
+{
+    //https://leetcode.com/problems/top-k-frequent-elements/
+    public class Solution
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> hash = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hash.ContainsKey(nums[i]))
+                    hash[nums[i]]++;
+                else
+                    hash.Add(nums[i], 1);
+            }
+
+            PriorityQueue<int, int> queue = new();
+
+            foreach (var (key, v) in hash)
+            {
+                queue.Enqueue(key, v);
+                if (queue.Count > k) queue.Dequeue();
+            }
+
+            var res = new int[k];
+            for (int i = 0; i < k; i++)
+                res[i] = queue.Dequeue();
+
+            return res;
+        }
+    }
+}
