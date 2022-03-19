574bc8270d6f584b4a53429a2ab93c3bf537818e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-19 23:58:54 +0200

    add solution of Minimum Swaps to Group All 1's Together

---
+++ LeetCodeTests/Array/Medium/MinimumSwapstoGroupAll1'sTogether/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Array.Medium.MinimumSwapstoGroupAll1_sTogether;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.MinimumSwapstoGroupAll1
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinSwaps_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] data = { 1,0,0,1,1,1 };
+
+            // Act
+            var result = solution.MinSwaps(
+                data);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/MinimumSwapstoGroupAll1'sTogether/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace LeetCode.Array.Medium.MinimumSwapstoGroupAll1_sTogether
+{
+    //https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together/
+    public class Solution
+    {
+        public int MinSwaps(int[] data)
+        {
+            int numberOfOne = data.Count(x => x == 1);
+            int currentOne = 0, maxOne = 0;
+            int left = 0, right = 0;
+
+            while (right < data.Length)
+            {
+                currentOne += data[right++];
+
+                if (right - left > numberOfOne)
+                    currentOne -= data[left++];
+
+                maxOne = System.Math.Max(maxOne, currentOne);
+            }
+
+            return numberOfOne - maxOne;
+        }
+    }
+}
