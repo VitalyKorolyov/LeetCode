354d16fba31a8c0bb5b41855fff71bc41446ef1a
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-21 00:46:02 +0200

    add solution of Daily Temperatures

---
+++ c#_LeetCode/Stack/Medium/DailyTemperatures/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Stack.Medium.DailyTemperatures
+{
+    //https://leetcode.com/problems/daily-temperatures/
+    public class TemperatureInfo
+    {
+        public int Value { get; set; }
+        public int Index { get; set; }
+    }
+
+    public class Solution
+    {
+        public int[] DailyTemperatures(int[] temperatures)
+        {
+            var res = new int[temperatures.Length];
+            var stack = new Stack<TemperatureInfo>();
+
+            for (int i = temperatures.Length - 1; i >= 0; i--)
+            {
+                while (stack.Count > 0)
+                {
+                    var info = stack.Peek();
+                    if(info.Value <= temperatures[i])
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+                    else
+                    {
+                        res[i] = info.Index - i;
+                        stack.Push(new TemperatureInfo { Index = i, Value = temperatures[i] });
+                        break;
+                    }
+                }
+
+                if (stack.Count == 0)
+                {
+                    stack.Push(new TemperatureInfo { Index = i, Value = temperatures[i] });
+                    res[i] = 0;
+                }
+            }
+
+            return res;
+        }
+    }
+}
