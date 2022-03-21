a02b95aaf366549bd1de6fff51f4d6717c4031ee
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-21 22:28:44 +0200

    add solution of Maximum Length of Subarray With Positive Product

---
+++ LeetCodeTests/Array/Medium/MaximumLengthOfSubarrayWithPositiveProduct/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Medium.MaximumLengthOfSubarrayWithPositiveProduct;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.MaximumLengthOfSubarrayWithPositiveProduct
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void GetMaxLen_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = {1000000000,1000000000};
+
+            // Act
+            var result = solution.GetMaxLen(nums);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/MaximumLengthOfSubarrayWithPositiveProduct/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+
+namespace LeetCode.Array.Medium.MaximumLengthOfSubarrayWithPositiveProduct
+{
+    //https://leetcode.com/problems/maximum-length-of-subarray-with-positive-product/
+    public class Solution
+    {
+        public int GetMaxLen(int[] nums)
+        {
+            var currentResult = 1;
+            var currentLength = 0;
+            var maxLength = 0;
+
+            int[] newNums = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0) newNums[i] = nums[i];
+                if (nums[i] > 0) newNums[i] = 1;
+                if (nums[i] < 0) newNums[i] = -1;
+            }
+
+            nums = newNums;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums[i] == 0)
+                {
+                    currentResult = 1;
+                    currentLength = 0;
+                    continue;
+                }
+
+                currentLength++;
+                currentResult *= nums[i];
+
+                if(currentResult > 0)
+                    maxLength = System.Math.Max(maxLength, currentLength);
+            }
+
+            currentResult = 1;
+            currentLength = 0;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] == 0)
+                {
+                    currentResult = 1;
+                    currentLength = 0;
+                    continue;
+                }
+
+                currentLength++;
+                currentResult *= nums[i];
+
+                if (currentResult > 0)
+                    maxLength = System.Math.Max(maxLength, currentLength);
+            }
+
+            return maxLength;
+        }
+    }
+}
