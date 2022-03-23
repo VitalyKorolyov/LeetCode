217c7b7e9be966afc55cff22720300fd63420831
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-23 23:59:55 +0200

    add solution of Flip String to Monotone Increasing

---
+++ LeetCodeTests/String/Medium/FlipStringToMonotoneIncreasing/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Medium.FlipStringToMonotoneIncreasing;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.FlipStringToMonotoneIncreasing
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinFlipsMonoIncr_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "00110";
+
+            // Act
+            var result = solution.MinFlipsMonoIncr(s);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/FlipStringToMonotoneIncreasing/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.String.Medium.FlipStringToMonotoneIncreasing
+{
+    //https://leetcode.com/problems/flip-string-to-monotone-increasing/
+    public class Solution
+    {
+        public int MinFlipsMonoIncr(string s)
+        {
+            int oneCount = 0;
+            int flipCount = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '1')
+                    oneCount++;
+                else
+                {
+                    if (oneCount >= 1)
+                        flipCount++;
+                }
+
+                flipCount = System.Math.Min(flipCount, oneCount);
+            }
+
+            return flipCount;
+        }
+    }
+}
