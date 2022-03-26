e88cce1f52543fc9a7a97e8a02ce45f3c7c62e70
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-27 00:09:36 +0200

    add solution of Minimum Deletions to Make String Balanced

---
+++ c#_LeetCode/String/Medium/MinimumDeletionsToMakeStringBalanced/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.String.Medium.MinimumDeletionsToMakeStringBalanced
+{
+    //https://leetcode.com/problems/minimum-deletions-to-make-string-balanced/
+    public class Solution
+    {
+        public int MinimumDeletions(string s)
+        {
+            int countB = 0;
+            var res = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'b') countB++;
+
+                if(s[i] == 'a')
+                    res = System.Math.Min(++res, countB);
+            }
+
+            return res;
+        }
+    }
+}
