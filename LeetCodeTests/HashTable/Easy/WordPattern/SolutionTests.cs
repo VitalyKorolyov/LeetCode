4de93548e69390676ea216a555a322e0e3b55d92
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-27 22:34:55 +0300

    add solution of Word Pattern

---
+++ LeetCodeTests/HashTable/Easy/WordPattern/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.HashTable.Easy.WordPattern;
+using Xunit;
+
+namespace LeetCodeTests.HashTable.Easy.WordPattern
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void WordPattern_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string pattern = "abba";
+            string s = "dog cat cat fish";
+
+            // Act
+            var result = solution.WordPattern(
+                pattern,
+                s);
+
+            // Assert
+            Assert.Equal(false, result);
+        }
+    }
+}
---
+++ c#_LeetCode/HashTable/Easy/WordPattern/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable.Easy.WordPattern
+{
+    //https://leetcode.com/problems/word-pattern/
+    public class Solution
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            Dictionary<char, string> mapChar = new();
+            Dictionary<string, char> mapWord = new();
+            var words = s.Split(" ");
+
+            if (words.Length != pattern.Length) return false;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!mapChar.ContainsKey(pattern[i]))
+                {
+                    if (mapWord.ContainsKey(words[i]))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        mapChar.Add(pattern[i], words[i]);
+                        mapWord.Add(words[i], pattern[i]);
+                    }
+                }
+                else
+                {
+                    if (mapChar[pattern[i]] != words[i]) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
