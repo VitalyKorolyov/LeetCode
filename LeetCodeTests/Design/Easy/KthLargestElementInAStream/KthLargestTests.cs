1980fefc4d59a0fa089867244bbdeb2284e0b346
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-27 22:13:06 +0300

    add solution of Kth Largest Element in a Stream

---
+++ LeetCodeTests/Design/Easy/KthLargestElementInAStream/KthLargestTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Design.Easy.KthLargestElementInAStream;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Design.Easy.KthLargestElementInAStream
+{
+    public class KthLargestTests
+    {
+        [Fact]
+        public void Add_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var kthLargest = new KthLargest(3, new[] { 4, 5, 8, 2 });
+
+            // Act
+            var result = kthLargest.Add(3);
+            Assert.Equal(4, result);
+            result = kthLargest.Add(5);
+            Assert.Equal(5, result);
+            result = kthLargest.Add(10);
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Design/Easy/KthLargestElementInAStream/KthLargest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Design.Easy.KthLargestElementInAStream
+{
+    //https://leetcode.com/problems/kth-largest-element-in-a-stream/
+    public class KthLargest
+    {
+        private readonly List<int> _list;
+        private readonly int _k;
+
+        public KthLargest(int k, int[] nums)
+        {
+            _k = k;
+            _list = nums.OrderBy(x => x).ToList();
+        }
+
+        public int Add(int val)
+        {
+            var index = _list.BinarySearch(val);
+
+            if (index < 0)
+                _list.Insert(~index, val);
+            else 
+                _list.Insert(index, val);
+
+            return _list[_list.Count - _k];
+        }
+    }
+}
