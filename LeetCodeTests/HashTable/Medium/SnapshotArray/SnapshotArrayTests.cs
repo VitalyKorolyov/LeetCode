d48d2fd8ecfe27ee33b48fa5388a33b5c2858505
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-29 00:36:15 +0300

    add solution of Snapshot Array

---
+++ LeetCodeTests/HashTable/Medium/SnapshotArray/SnapshotArrayTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.HashTable.Medium.SnapshotArray;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.HashTable.Medium.SnapshotArray
+{
+    public class SnapshotArrayTests
+    {
+        [Fact]
+        public void Set_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var snapshotArray = new LeetCode.HashTable.Medium.SnapshotArray.SnapshotArray(3);
+
+            // Act
+            snapshotArray.Set(0, 15);
+            snapshotArray.Snap();
+            snapshotArray.Snap();
+            snapshotArray.Snap();
+
+            var res = snapshotArray.Get(0, 2);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/HashTable/Medium/SnapshotArray/SnapshotArray.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable.Medium.SnapshotArray
+{
+    //https://leetcode.com/problems/snapshot-array/
+    public class SnapshotArray
+    {
+        private int snapshotId = 0;
+        private Dictionary<int, int>[] snapshots;
+
+        public SnapshotArray(int length)
+        {
+            snapshots = new Dictionary<int, int>[length];
+        }
+
+        public void Set(int index, int val)
+        {
+            if (snapshots[index] == null)
+                snapshots[index] = new Dictionary<int, int>();
+
+            if (snapshots[index].ContainsKey(snapshotId))
+                snapshots[index][snapshotId] = val;
+            else snapshots[index].Add(snapshotId, val);
+        }
+
+        public int Snap()
+        {
+            return snapshotId++;
+        }
+
+        public int Get(int index, int snap_id)
+        {
+            if (snapshots[index] == null) return 0;
+
+            if (snapshots[index].ContainsKey(snap_id)) return snapshots[index][snap_id];
+
+            while (!snapshots[index].ContainsKey(snap_id) && snap_id != -1)
+                snap_id--;
+
+            return snap_id == -1 ? 0 : snapshots[index][snap_id];
+        }
+    }
+}
