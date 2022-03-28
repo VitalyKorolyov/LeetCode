80e25b6ecf273c592b364ac652be98fd3c07c730
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-28 23:22:22 +0300

    add solution of Lowest Common Ancestor of a Binary Tree III

---
+++ c#_LeetCode/BST/Medium/LowestCommonAncestorOfABinaryTreeIII/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.BST.Medium.LowestCommonAncestorOfABinaryTreeIII
+{
+
+    // Definition for a Node.
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/
+    public class Solution
+    {
+        public Node LowestCommonAncestor(Node p, Node q)
+        {
+            var hash = new HashSet<int>();
+
+            while (p != null)
+            {
+                hash.Add(p.val);
+                p = p.parent;
+            }
+
+            while (q != null)
+            {
+                if (hash.Contains(q.val)) return q;
+
+                q = q.parent;
+            }
+
+            return null;
+        }
+    }
+}
