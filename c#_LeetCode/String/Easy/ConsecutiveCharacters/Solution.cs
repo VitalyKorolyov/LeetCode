6b2c91b5c5587868fb22c00f262604440545cfe0
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-30 00:20:01 +0300

    add solution of Consecutive Characters

---
+++ c#_LeetCode/String/Easy/ConsecutiveCharacters/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.String.Easy.ConsecutiveCharacters
+{
+    //https://leetcode.com/problems/consecutive-characters/
+    public class Solution
+    {
+        public int MaxPower(string s)
+        {
+            var maxCount = 0;
+            var count = 0;
+            var prevChar = ' ';
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(prevChar == s[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    prevChar = s[i];
+                    count = 1;
+                }
+
+                maxCount = System.Math.Max(maxCount, count);
+            }
+
+            return maxCount;
+        }
+    }
+}
