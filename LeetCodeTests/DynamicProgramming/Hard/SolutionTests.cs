295c324bf78555ea64dc2badf026138b0e371625
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-03-31 01:23:50 +0300

    add solution of Trapping Rain Water

---
+++ LeetCodeTests/DynamicProgramming/Hard/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.DynamicProgramming.Hard;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Hard
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Trap_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] height = new int[] {4, 2, 0, 3, 2, 5};
+
+            // Act
+            var result = solution.Trap(height);
+
+            // Assert
+            Assert.Equal(9, result);
+        }
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Hard/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace LeetCode.DynamicProgramming.Hard
+{
+    //https://leetcode.com/problems/trapping-rain-water/
+    public class Solution
+    {
+        public int Trap(int[] height)
+        {
+            if(height == null || !height.Any()) return 0;
+
+            int[] leftMax = new int[height.Length];
+            int[] rightMax = new int[height.Length];
+
+            leftMax[0] = height[0];
+            for (int i = 1; i < height.Length; i++)
+            {
+                leftMax[i] = System.Math.Max(height[i], leftMax[i - 1]);
+            }
+
+            rightMax[height.Length - 1] = height[height.Length - 1];
+            for (int i = height.Length - 2; i >= 0; i--)
+            {
+                rightMax[i] = System.Math.Max(height[i], rightMax[i + 1]);
+            }
+
+            int res = 0;
+            for (int i = 1; i < height.Length - 1; i++)
+            {
+                res += System.Math.Min(leftMax[i], rightMax[i]) - height[i];
+            }
+
+            return res;
+        }
+    }
+}
