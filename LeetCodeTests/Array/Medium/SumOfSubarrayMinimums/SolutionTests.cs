f1c09599cae590e390fdfef5ce4a2be45d2391d7
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-04-07 00:47:09 +0300

    add solution of Sum of Subarray Minimums

---
+++ LeetCodeTests/Array/Medium/SumOfSubarrayMinimums/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Medium.SumOfSubarrayMinimums;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.SumOfSubarrayMinimums
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SumSubarrayMins_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] arr = {11, 81, 94, 43, 3};
+
+            // Act
+            var result = solution.SumSubarrayMins(arr);
+
+            // Assert
+            Assert.Equal(444, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/SumOfSubarrayMinimums/Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array.Medium.SumOfSubarrayMinimums
+{
+    public class Solution
+    {
+        //O(n^2)
+        public int SumSubarrayMins(int[] arr)
+        {
+            int mod = (int)1e9 + 7;
+
+            var res = 0;
+
+            for (int start = 0; start < arr.Length; start++)
+            {
+                var min = arr[start];
+
+                for (int end = start; end < arr.Length; end++)
+                {
+                    min = System.Math.Min(min, arr[end]);
+
+                    res = (res + min) % mod;
+                }
+            }
+
+            return res;
+        }
+
+        ////O(n^2)
+        //public int SumSubarrayMins(int[] arr)
+        //{
+        //    int mod = (int)1e9 + 7;
+
+        //    var res = 0;
+
+        //    for (int start = 0; start < arr.Length; start++)
+        //    {
+        //        var min = arr[start];
+
+        //        for (int end = start; end < arr.Length; end++)
+        //        {
+        //            min = System.Math.Min(min, arr[end]);
+
+        //            res = (res + min) % mod;
+        //        }
+        //    }
+
+        //    return res;
+        //}
+
+        ////O(n^3)
+        //public int SumSubarrayMins(int[] arr)
+        //{
+        //    int mod = (int)1e9 + 7;
+
+        //    var res = 0;
+
+        //    for (int start = 0; start < arr.Length; start++)
+        //    {
+        //        for (int end = start; end < arr.Length; end++)
+        //        {
+        //            var min = int.MaxValue;
+        //            for (int i = start; i <= end; i++)
+        //            {
+        //                min = System.Math.Min(min, arr[i]);
+        //            }
+
+        //            res = (res + min) % mod; ;
+        //        }
+        //    }
+
+        //    return res;
+        //}
+    }
+}
