2ae1c9ce9c9f8ecacabdbaa4fcf953d4ca8c4ace
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-04-11 22:13:38 +0300

    add solution of Sum of Subarray Ranges

---
+++ c#_LeetCode/Array/Medium/SumOfSubarrayRanges/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Array.Medium.SumOfSubarrayRanges
+{
+    //https://leetcode.com/problems/sum-of-subarray-ranges/
+    public class Solution
+    {
+        //O(n^2)
+        public long SubArrayRanges(int[] nums)
+        {
+            long res = 0;
+
+            for (int start = 0; start < nums.Length; start++)
+            {
+                var min = int.MaxValue;
+                var max = int.MinValue;
+
+                for (int end = start; end < nums.Length; end++)
+                {
+                    min = System.Math.Min(min, nums[end]);
+                    max = System.Math.Max(max, nums[end]);
+
+                    res += max - min;
+                }
+            }
+
+            return res;
+        }
+    }
+}
