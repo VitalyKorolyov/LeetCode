1a956b0d5d9df5cee666c545ec38e6470b0f306f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-04-13 00:18:49 +0300

    add solution of Maximum Twin Sum of a Linked List

---
+++ c#_LeetCode/LinkedList/Medium/MaximumTwinSumOfALinkedList/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.LinkedList.Medium.MaximumTwinSumOfALinkedList
+{
+    //https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public int PairSum(ListNode head)
+        {
+            var stack = new Stack<ListNode>();
+
+            var savedHead = head;
+
+            while (head != null)
+            {
+                stack.Push(head);
+                head = head.next;
+            }
+
+            head = savedHead;
+
+            var maxSum = int.MinValue;
+            var length = stack.Count;
+
+            for (var i = 0; i < length / 2; i++)
+            {
+                maxSum = System.Math.Max(maxSum, head.val + stack.Pop().val);
+                head = head.next;
+            }
+
+            return maxSum;
+        }
+    }
+}
