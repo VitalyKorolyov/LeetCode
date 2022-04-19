57f8010f7d0e1f876662332e5d85eed3989b6d88
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-04-20 00:44:51 +0300

    add solution of Minimum Add to Make Parentheses Valid

---
+++ LeetCodeTests/String/Medium/MinimumAddToMakeParenthesesValid/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Medium.MinimumAddToMakeParenthesesValid;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.MinimumAddToMakeParenthesesValid
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinAddToMakeValid_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "()))((";
+
+            // Act
+            var result = solution.MinAddToMakeValid(
+                s);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/MinimumAddToMakeParenthesesValid/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.String.Medium.MinimumAddToMakeParenthesesValid
+{
+    //https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+    public class Solution
+    {
+        public int MinAddToMakeValid(string s)
+        {
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(!stack.Any())
+                {
+                    stack.Push(s[i]);
+                    continue;
+                }
+
+                if(s[i] == ')' && stack.Peek() == '(')
+                {
+                    stack.Pop();
+                    continue;
+                }
+
+                stack.Push(s[i]);
+            }
+
+            return stack.Count;
+        }
+    }
+}
