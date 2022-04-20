d9bddd970c4ed66d8dbeff981877a7c87e79cf7d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-04-21 00:39:09 +0300

    add solution of Count Unique Characters of All Substrings of a Given String

---
+++ LeetCodeTests/String/Hard/CountUniqueCharactersOfAllSubstringsOfGivenString/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.String.Hard.CountUniqueCharactersOfAllSubstringsOfGivenString;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Hard.CountUniqueCharactersOfAllSubstringsOfGivenString
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void UniqueLetterString_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "LEETCODE";
+
+            // Act
+            var result = solution.UniqueLetterString(
+                s);
+
+            // Assert
+            Assert.Equal(92, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Hard/CountUniqueCharactersOfAllSubstringsOfGivenString/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Hard.CountUniqueCharactersOfAllSubstringsOfGivenString
+{
+    //https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/
+    public class Solution
+    {
+        public int UniqueLetterString(string s)
+        {
+            var left = new int[s.Length];
+            var right = new int[s.Length];
+
+            var dic = new Dictionary<char, int>();
+            for (int i = 0, position = 1; i < s.Length; i++, position++) 
+            {
+                if (dic.ContainsKey(s[i]))
+                {
+                    var charPosition = dic[s[i]];
+                    left[i] = position - charPosition;
+                    dic[s[i]] = position;
+                }
+                else
+                {
+                    left[i] = position;
+                    dic.Add(s[i], position);
+                }
+            }
+
+            dic.Clear();
+
+            for (int i = s.Length - 1, position = 1; i >= 0; i--, position++)
+            {
+                if (dic.ContainsKey(s[i]))
+                {
+                    var charPosition = dic[s[i]];
+                    right[i] = position - charPosition;
+                    dic[s[i]] = position;
+                }
+                else
+                {
+                    right[i] = position;
+                    dic.Add(s[i], position);
+                }
+            }
+
+            var res = 0;
+            for (int i = 0; i < left.Length; i++)
+                res += left[i] * right[i];
+
+            return res;
+        }
+    }
+}
