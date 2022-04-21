f7840f3221820d4cb8e4bbd0c17ee0650fe96a98
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-04-21 22:36:11 +0300

    add solution of Reorder Data in Log Files

---
+++ LeetCodeTests/String/Easy/ReorderDataInLogFiles/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace LeetCodeTests.String.Easy.ReorderDataInLogFiles
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void ReorderLogFiles_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new LeetCode.String.Easy.ReorderDataInLogFiles.Solution();
+            string[] logs = { "dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero" };
+
+            // Act
+            var result = solution.ReorderLogFiles(
+                logs);
+
+            // Assert
+            Assert.Equal(new string[]{ "let1 art can","let3 art zero",
+                "let2 own kit dig","dig1 8 1 5 1","dig2 3 6" }, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/ReorderDataInLogFiles/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Easy.ReorderDataInLogFiles
+{
+    //https://leetcode.com/problems/reorder-data-in-log-files/
+    public class Solution
+    {
+        public string[] ReorderLogFiles(string[] logs)
+        {
+            var digitLog = new List<string>();
+            var aplhabetLog = new List<string>();
+
+            foreach (var log in logs)
+            {
+                if (char.IsDigit(log.Split(' ')[1][0]))
+                {
+                    digitLog.Add(log);
+                }
+                else
+                {
+                    aplhabetLog.Add(log);
+                }
+            }
+
+            aplhabetLog.Sort(new LogComparetorWord());
+            aplhabetLog.AddRange(digitLog);
+
+            return aplhabetLog.ToArray();
+        }
+
+        public class LogComparetorWord : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                string[] log1 = x.Split(new char[] { ' ' }, 2);
+                string[] log2 = y.Split(new char[] { ' ' }, 2);
+
+                var cmp = log1[1].CompareTo(log2[1]);
+                if (cmp != 0) return cmp;
+                else return log1[0].CompareTo(log2[0]);
+            }
+        }
+    }
+}
