c9d666e7bedf337f9972d4dcebb4d6b4c70f9af2
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-04-24 00:57:37 +0300

    add solution of Integer to Roman

---
+++ c#_LeetCode/Math/Medium/IntegerToRoman/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Math.Medium.IntegerToRoman
+{
+    //https://leetcode.com/problems/integer-to-roman/
+    public class Solution
+    {
+        private static Dictionary<int, string> romans = new Dictionary<int, string>()
+        {
+            {1000, "M" }, {900, "CM" },{500, "D" },{400, "CD" }, {100, "C" },{90, "XC" },
+            {50, "L" },{40, "XL" },{10, "X" },{9, "IX" },{5, "V" },{4, "IV" },{1, "I" }
+        };
+
+        public string IntToRoman(int num)
+        {
+            var res = new StringBuilder();
+
+            foreach (var (value, sym) in romans)
+            {
+                while(value <= num)
+                {
+                    res.Append(sym);
+                    num -= value;
+                }
+            }
+
+            return res.ToString();
+        }
+    }
+}
