5f134c9fd9bc92605151c44b976d9a18696dc77e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-04-28 22:35:34 +0300

    add solution of Lowest Common Ancestor of a Binary Tree

---
+++ LeetCodeTests/Trees/Medium/LowestCommonAncestorOfABinaryTree/TreeNodeTests.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Trees.Medium.LowestCommonAncestorOfABinaryTree;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.LowestCommonAncestorOfABinaryTree
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var exRes = new TreeNode()
+            {
+                val = 5,
+                left = new TreeNode { val = 6 },
+                right = new TreeNode()
+                {
+                    val = 2,
+                    left = new TreeNode() { val = 7 },
+                    right = new TreeNode() { val = 4 }
+                }
+            };
+            var treeNode = new TreeNode()
+            {
+                val = 3,
+                left = exRes,
+                right = new TreeNode() 
+                {
+                    val = 1,
+                    left = new TreeNode() { val = 0 },
+                    right = new TreeNode() { val = 8 }
+                }
+            };
+
+            // Act
+            var res = new Solution().LowestCommonAncestor(treeNode, new TreeNode { val = 5 }, new TreeNode { val = 4 });
+
+            // Assert
+            Assert.Equal(exRes, res);
+        }
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/LowestCommonAncestorOfABinaryTree/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.Trees.Medium.LowestCommonAncestorOfABinaryTree
+{
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+    }
+
+    public class Solution
+    {
+        private TreeNode lowerCommonAnsestor = null;
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            FindLowerCommonAncestor(root, p, q);
+            return lowerCommonAnsestor;
+        }
+
+        private bool FindLowerCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            // If reached the end of a branch, return false.
+            if (root == null) return false;
+
+            // Left Recursion. If left recursion returns true, set left = 1 else 0
+            int left = FindLowerCommonAncestor(root.left, p, q) ? 1 : 0;
+
+            // Right Recursion
+            int right = FindLowerCommonAncestor(root.right, p, q) ? 1 : 0;
+
+            // If the current node is one of p or q
+            int mid = (root.val == p.val || root.val == q.val) ? 1 : 0;
+
+            // If any two of the flags left, right or mid become True
+            if (mid + left + right >= 2)
+                lowerCommonAnsestor = root;
+
+            // Return true if any one of the three bool values is True.
+            return (mid + left + right > 0);
+        }
+    }
+}
