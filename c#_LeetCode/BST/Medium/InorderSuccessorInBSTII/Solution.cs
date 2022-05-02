dbccf6c8b0c93ef938832fdeae41d4f9f77bfdfa
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-05-02 21:35:40 +0300

    add solution of Inorder Successor in BST II

---
+++ c#_LeetCode/BST/Medium/InorderSuccessorInBSTII/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace LeetCode.BST.Medium.InorderSuccessorInBSTII
+{
+
+    // Definition for a Node.
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    }
+
+    //https://leetcode.com/problems/inorder-successor-in-bst-ii/
+    public class Solution
+    {
+        public Node InorderSuccessor(Node x)
+        {
+            Node successor = null;
+            var value = x.val;
+
+            var node = x.right;
+            while(node != null)
+            {
+                successor = node;
+                node = node.left;
+            }
+
+            if(successor != null) return successor;
+
+            node = x.parent;
+            while (node != null)
+            {
+                if(node.val > value)
+                {
+                    if(successor == null)
+                        successor = node;
+                    else
+                    {
+                        if (successor.val > node.val)
+                            successor = node;
+                    }
+                }
+
+                node = node.parent;
+            }
+
+
+            return successor;
+        }
+    }
+}
