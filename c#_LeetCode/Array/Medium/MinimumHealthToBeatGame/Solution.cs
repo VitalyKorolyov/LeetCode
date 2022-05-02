cbe2ecb446721c6a2466422a4669232786461308
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-05-02 22:43:11 +0300

    add solution of Minimum Health to Beat Game

---
+++ c#_LeetCode/Array/Medium/MinimumHealthToBeatGame/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Array.Medium.MinimumHealthToBeatGame
+{
+    //https://leetcode.com/problems/minimum-health-to-beat-game/
+    public class Solution
+    {
+        public long MinimumHealth(int[] damage, int armor)
+        {
+            long totalHealth = 1;
+            var maxAppliedArmor = 0;
+
+            for(int i = 0; i < damage.Length; i++)
+            {
+                totalHealth += damage[i];
+                maxAppliedArmor = System.Math.Max(maxAppliedArmor, System.Math.Min(armor, damage[i]));
+            }
+
+            return totalHealth - maxAppliedArmor;
+        }
+    }
+}
