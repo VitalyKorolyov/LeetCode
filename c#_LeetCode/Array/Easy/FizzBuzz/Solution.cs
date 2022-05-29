7da2312ec78f642cf0dd0b9b86513faada93e384
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-05-30 00:11:17 +0300

    add solution of Fizz Buzz

---
+++ c#_LeetCode/Array/Easy/FizzBuzz/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Easy.FizzBuzz
+{
+    //https://leetcode.com/problems/fizz-buzz/
+    public class Solution
+    {
+        public IList<string> FizzBuzz(int n)
+        {
+            var res = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    res.Add("FizzBuzz");
+                    continue;
+                }
+                if(i % 3 == 0)
+                {
+                    res.Add("Fizz");
+                    continue;
+                }
+                if(i % 5 == 0)
+                {
+                    res.Add("Buzz");
+                    continue;
+                }
+
+                res.Add(i.ToString());
+            }
+
+            return res;
+        }
+    }
+}
