bb13a7f2ce44db68bbc019e27c712f44e4ca91c3
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-06-03 01:04:47 +0300

    add solution of Basic Calculator II

---
+++ LeetCodeTests/Stack/Medium/BasicCalculatorII/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Stack.Medium.BasicCalculatorII;
+using Xunit;
+
+namespace LeetCodeTests.Stack.Medium.BasicCalculatorII
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Calculate_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = " 3+5 / 2 ";
+
+            // Act
+            var result = solution.Calculate(
+                s);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Stack/Medium/BasicCalculatorII/Solution.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Stack.Medium.BasicCalculatorII
+{
+    public class Solution
+    {
+        public int Calculate(string s)
+        {
+            var stack = new Stack<int>();
+            int number = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    (number, i) = GetNumber(s, i);
+                    stack.Push(number);
+
+                    continue;
+                }
+
+                if (s[i] == '*')
+                {
+                    (number, i) = GetNumber(s, ++i);
+                    var prev = stack.Pop();
+                    stack.Push(prev * number);
+
+                    continue;
+                }
+                if (s[i] == '/')
+                {
+                    (number, i) = GetNumber(s, ++i);
+                    var prev = stack.Pop();
+                    stack.Push(prev / number);
+
+                    continue;
+                }
+                if (s[i] == '+')
+                {
+                    (number, i) = GetNumber(s, ++i);
+                    stack.Push(number);
+
+                    continue;
+                }
+                if (s[i] == '-')
+                {
+                    (number, i) = GetNumber(s, ++i);
+                    stack.Push(-number);
+
+                    continue;
+                }
+            }
+
+            var res = 0;
+            while(stack.Count > 0)
+            {
+                res += stack.Pop();
+            }
+
+            return res;
+        }
+
+        private Tuple<int, int> GetNumber(string s, int i)
+        {
+            var res = new StringBuilder();
+
+            if (i < s.Length && s[i] == ' ')
+            {
+                while (s[i] == ' ')
+                    i++;
+            }
+
+            while (i < s.Length && char.IsDigit(s[i]))
+                res.Append(s[i++]);
+
+            return Tuple.Create(int.Parse(res.ToString()), --i);
+        }
+    }
+}
