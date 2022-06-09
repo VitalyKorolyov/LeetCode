255b8d7eabe7fdb284cb6040a9d046bcc31f84ca
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-06-09 23:22:37 +0300

    add solution of Maximum Number of Non-Overlapping Subarrays With Sum Equals Target

---
+++ c#_LeetCode/Array/Medium/MaximumNumberOfNonOverlappingSubarraysWithSumEqualsTarget/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.MaximumNumberOfNonOverlappingSubarraysWithSumEqualsTarget
+{
+    //https://leetcode.com/problems/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/
+    public class Solution
+    {
+        public int MaxNonOverlapping(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+
+            var prevFoundIndex = -1;
+            int prefixSum = 0;
+            var count = 0;
+
+            map.Add(prefixSum, -1);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                prefixSum += nums[i] + prefixSum;
+                if (map.ContainsKey(prefixSum - target) && map[prefixSum - target] >= prevFoundIndex)
+                {
+                    count++;
+                    prevFoundIndex = i;
+                }
+
+                if(map.ContainsKey(prefixSum))
+                    map[prefixSum] = i;
+                else
+                    map.Add(prefixSum, i);
+            }
+
+            return count;
+        }
+    }
+}
