0389aa8d1c95bae9b52ce1218d0756444e5f779f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-06-14 00:11:36 +0300

    add solution of Minimum Size Subarray Sum

---
+++ LeetCodeTests/Array/Medium/MinimumSizeSubarraySum/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Medium.MinimumSizeSubarraySum;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.MinimumSizeSubarraySum
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinSubArrayLen_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int target = 4;
+            int[] nums = new int[] { 1, 4, 4 };
+
+            // Act
+            var result = solution.MinSubArrayLen(target, nums);
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/MinimumSizeSubarraySum/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Array.Medium.MinimumSizeSubarraySum
+{
+    //https://leetcode.com/problems/minimum-size-subarray-sum/
+    public class Solution
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            int ans = int.MaxValue;
+            int left = 0;
+            int sum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                while (sum >= target)
+                {
+                    ans = System.Math.Min(ans, i + 1 - left);
+                    sum -= nums[left++];
+                }
+            }
+
+            return (ans != int.MaxValue) ? ans : 0;
+        }
+    }
+}
