fde2931dccd0e213ee8a9263aba3c6562e421271
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-06-16 22:57:37 +0300

    add solutions of  Leaf-Similar Trees and Flipping an Image

---
+++ c#_LeetCode/Matrix/Easy/LeafSimilarTrees/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Matrix.Easy.FlippingAnImage
+{
+    public class Solution
+    {
+        public int[][] FlipAndInvertImage(int[][] image)
+        {
+            for (int k = 0; k < image.Length; k++)
+            {
+                for (int i = 0, j = image[k].Length - 1; i <= j; i++, j--)
+                {
+                    if (image[k][i] == image[k][j])
+                        Invert(image[k], i, j);
+                    else 
+                    {
+                        Flip(image[k], i, j);
+                        Invert(image[k], i, j);
+                    }
+                }
+            }
+
+            return image;
+        }
+
+        private void Invert(int[] image, int i, int j)
+        {
+            if(i == j)
+            {
+                image[i] = image[i] == 1 ? 0 : 1;
+                return;
+            }
+
+            image[i] = image[i] == 1 ? 0 : 1;
+            image[j] = image[j] == 1 ? 0 : 1;
+        }
+
+        private void Flip(int[] image, int i, int j)
+        {
+            if (i == j) return;
+
+            var temp = image[i];
+            image[i] = image[j];
+            image[j] = temp;
+        }
+    }
+}
---
+++ c#_LeetCode/Trees/Easy/FlippingAnImage/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Trees.Easy.LeafSimilarTrees
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public bool LeafSimilar(TreeNode root1, TreeNode root2)
+        {
+            var leaves1 = new List<TreeNode>();
+            var leaves2 = new List<TreeNode>();
+
+            GetLeaves(root1, leaves1);
+            GetLeaves(root2, leaves2);
+
+            foreach (var (leave1, leave2) in leaves1.Zip(leaves2))
+                if (leave1.val != leave2.val) return false;
+
+            return leaves1.Count == leaves2.Count;
+        }
+
+        private void GetLeaves(TreeNode root, List<TreeNode> leaves)
+        {
+            if (root == null) return;
+
+            if(root.left == null && root.right == null)
+                leaves.Add(root);
+
+            GetLeaves(root.left, leaves);
+            GetLeaves(root.right, leaves);
+        }
+    }
+}
