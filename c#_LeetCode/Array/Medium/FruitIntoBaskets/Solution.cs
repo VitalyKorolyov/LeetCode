8d7db1fd18307627e88913d4788715f2c41b1f17
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-06-20 21:30:30 +0300

    add solution of Fruit Into Baskets

---
+++ c#_LeetCode/Array/Medium/FruitIntoBaskets/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.FruitIntoBaskets
+{
+    //https://leetcode.com/problems/fruit-into-baskets/
+    public class Solution
+    {
+        public int TotalFruit(int[] fruits)
+        {
+            var backet = new Dictionary<int, int>(2);
+
+            var total = 0;
+            int start = 0; int end = 0;
+
+            while (start < fruits.Length)
+            {
+                if (backet.Count < 2 || (backet.Count == 2 && backet.ContainsKey(fruits[start])))
+                {
+                    var type = fruits[start];
+
+                    if (backet.ContainsKey(type)) backet[type]++;
+                    else backet.Add(type, 1);
+
+                    start++;
+                }
+                else
+                {
+                    var type = fruits[end];
+                    backet[type]--;
+                    if (backet[type] == 0) backet.Remove(type);
+                    end++;
+                }
+
+                total = System.Math.Max(total, start - end);
+            }
+
+            return total;
+        }
+    }
+}
