d89ec25e060f2616dcbf54456b68f67d0721becf
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-06-20 20:54:14 +0300

    add solution of Longest Substring with At Most K Distinct Characters

---
+++ LeetCodeTests/String/Medium/LongestSubstringWithAtMostKDistinctCharacters/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.String.Medium.LongestSubstringWithAtMostKDistinctCharacters;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.LongestSubstringWithAtMostKDistinctCharacters
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void LengthOfLongestSubstringKDistinct_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "a";
+            int k = 0;
+
+            // Act
+            var result = solution.LengthOfLongestSubstringKDistinct(
+                s,
+                k);
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/LongestSubstringWithAtMostKDistinctCharacters/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Medium.LongestSubstringWithAtMostKDistinctCharacters
+{
+    //https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
+    public class Solution
+    {
+        public int LengthOfLongestSubstringKDistinct(string s, int k)
+        {
+            var maxLength = 0;
+            if(k == 0) return maxLength;
+
+            var hash = new Dictionary<char, int>(k);
+
+            int start = 0; int end = 0;
+
+            while(start < s.Length)
+            {
+                if (hash.Count < k || (hash.Count == k && hash.ContainsKey(s[start])))
+                {
+                    if(!hash.ContainsKey(s[start]))
+                        hash.Add(s[start], 1);
+                    else
+                        hash[s[start]]++;
+
+                    start++;
+                }
+                else
+                {
+                    var endCharacter = s[end];
+                    hash[endCharacter]--;
+                    if (hash[endCharacter] == 0) hash.Remove(endCharacter);
+                    end++;
+                }
+
+                maxLength = System.Math.Max(maxLength, start - end);
+            }
+
+            return maxLength;
+        }
+    }
+}
