f5714104b7b66d6380af613a73689808f8dc9878
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-06-23 00:31:09 +0300

    add solution of Happy Number

---
+++ LeetCodeTests/Math/Easy/HappyNumber/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Math.Easy.HappyNumber;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Math.Easy.HappyNumber
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void IsHappy_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int n = 19;
+
+            // Act
+            var result = solution.IsHappy(n);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/Math/Easy/HappyNumber/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Math.Easy.HappyNumber
+{
+    //https://leetcode.com/problems/happy-number/
+    public class Solution
+    {
+        public bool IsHappy(int n)
+        {
+            var hash = new HashSet<int>();
+
+            while (!hash.Contains(n))
+            {
+                hash.Add(n);
+
+                var sum = 0;
+                while (n != 0)
+                {
+                    var value = n % 10;
+                    sum += value * value;
+                    n /= 10;
+                }
+                if (sum == 1) return true;
+                n = sum;
+            }
+
+            return false;
+        }
+    }
+}
