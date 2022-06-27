eb0c70e2152489e15d4aa4a9f4961b354b1e9ae9
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-06-27 23:58:40 +0300

    add solution of Two Sum II - Input Array Is Sorted

---
+++ c#_LeetCode/Array/Medium/TwoSumIIInputArrayIsSorted/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Array.Medium.TwoSumIIInputArrayIsSorted
+{
+    //https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+    public class Solution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int[] result = new int[2];
+
+            for (int i = 0, j = numbers.Length - 1; j > i;)
+            {
+                var sum = numbers[i] + numbers[j];
+
+                if (sum == target)
+                {
+                    result[0] = i + 1;
+                    result[1] = j + 1;
+                    break;
+                }
+                if (sum > target) j--;
+                else i++;
+            }
+
+            return result;
+        }
+    }
+}
