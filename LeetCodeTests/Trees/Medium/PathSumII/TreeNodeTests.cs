4717e0c273e6e2cd5d195b38492e32c64de0a2ce
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-06 01:12:04 +0300

    add solution of Path Sum II

---
+++ LeetCodeTests/Trees/Medium/PathSumII/TreeNodeTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Trees.Medium.PathSumII;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.PathSumII
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(5, 
+                new TreeNode(4, new TreeNode(11, new TreeNode(7), new TreeNode(2))),
+                new TreeNode(8, new TreeNode(13), new TreeNode(4, new TreeNode(5), new TreeNode(1))));
+
+            // Act
+            var res = new Solution().PathSum(treeNode, 22);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/PathSumII/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Trees.Medium.PathSumII
+{
+    //https://leetcode.com/problems/path-sum-ii/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> PathSum(TreeNode root, int targetSum)
+        {
+            var result = new List<IList<int>>();
+
+            Dfs(root, 0, targetSum, result, new LinkedList<int>());
+
+            return result;
+        }
+
+        public void Dfs(TreeNode node, int sum, int targetSum, 
+            IList<IList<int>> result, LinkedList<int> list)
+        {
+            if (node == null) return;
+
+            var currentSum = sum + node.val;
+            list.AddLast(node.val);
+
+            if(node.left == null && node.right == null)
+            {
+                if (currentSum == targetSum)
+                    result.Add(list.ToList());
+            }
+            else
+            {
+                Dfs(node.left, currentSum, targetSum, result, list);
+                Dfs(node.right, currentSum, targetSum, result, list);
+            }
+
+            list.RemoveLast();
+        }
+    }
+}
