5c010256eb141a1f2337612b462a5a30fbbaa552
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-05 23:07:28 +0300

    add solution of Insert Interval

---
+++ LeetCodeTests/Array/Medium/InsertInterval/SolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Array.Medium.InsertInterval;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.InsertInterval
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Insert_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] intervals = new int[1][];
+            intervals[0] = new int[] { 1, 5};
+            int[] newInterval = new int[] {6, 8};
+
+            // Act
+            var result = solution.Insert(
+                intervals,
+                newInterval);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/InsertInterval/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.InsertInterval
+{
+    //https://leetcode.com/problems/insert-interval/
+    public class Solution
+    {
+        public int[][] Insert(int[][] intervals, int[] newInterval)
+        {
+            var merged = new List<int[]>();
+
+            int i = 0;
+            for (; i < intervals.Length && intervals[i][1] < newInterval[0]; i++)
+                merged.Add(intervals[i]);
+
+            for (; i < intervals.Length && intervals[i][0] <= newInterval[1]; i++)
+            {
+                newInterval[0] = System.Math.Min(intervals[i][0], newInterval[0]);
+                newInterval[1] = System.Math.Max(intervals[i][1], newInterval[1]);
+            }
+
+            merged.Add(newInterval);
+
+            for (; i < intervals.Length; i++)
+                merged.Add(intervals[i]);
+
+            return merged.ToArray();
+        }
+    }
+}
