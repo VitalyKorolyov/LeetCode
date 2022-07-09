8d8ae2071a426e423de37ebb79668b77d36f78d0
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-09 19:51:16 +0300

    add solution of Sort Array by Increasing Frequency

---
+++ c#_LeetCode/Array/Easy/SortArrayByIncreasingFrequency/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Easy.SortArrayByIncreasingFrequency
+{
+    //https://leetcode.com/problems/sort-array-by-increasing-frequency/
+    public class Solution
+    {
+        public int[] FrequencySort(int[] nums)
+        {
+            var hashMap = new Dictionary<int, int>(nums.Length);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (hashMap.ContainsKey(nums[i]))
+                    hashMap[nums[i]]++;
+                else hashMap.Add(nums[i], 1);
+            }
+
+            System.Array.Sort(nums, (a, b) => hashMap[a] != hashMap[b] 
+                ? hashMap[a] - hashMap[b]
+                : b - a);
+
+            return nums;
+        }
+    }
+}
