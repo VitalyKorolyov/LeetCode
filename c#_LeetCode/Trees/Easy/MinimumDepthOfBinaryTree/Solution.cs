083b4c7172679293aad6eea145b04e388969d437
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-11 23:41:27 +0300

    update Minimum Depth of Binary Tree

--- c#_LeetCode/Trees/Easy/MinimumDepthOfBinaryTree/Solution.cs
+++ c#_LeetCode/Trees/Easy/MinimumDepthOfBinaryTree/Solution.cs
@@ -1,4 +1,6 @@
-﻿namespace LeetCode.Trees.Easy.MinimumDepthOfBinaryTree
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees.Easy.MinimumDepthOfBinaryTree
 {
     //https://leetcode.com/problems/minimum-depth-of-binary-tree
     public class TreeNode
@@ -20,9 +22,41 @@
         public int MinDepth(TreeNode root)
         {
             if (root == null) return 0;
-            int left = MinDepth(root.left);
-            int right = MinDepth(root.right);
-            return (left == 0 || right == 0) ? left + right + 1 : System.Math.Min(left, right) + 1;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            var level = 1;
+            while(queue.Count > 0)
+            {
+                var size = queue.Count;
+
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+
+                    if(node.left != null)
+                        queue.Enqueue(node.left);
+
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+
+                    if (node.left == null && node.right == null)
+                        return level;
+                }
+
+                level++;
+            }
+
+            return 0;
         }
+
+        //public int MinDepth(TreeNode root)
+        //{
+        //    if (root == null) return 0;
+        //    int left = MinDepth(root.left);
+        //    int right = MinDepth(root.right);
+        //    return (left == 0 || right == 0) ? left + right + 1 : System.Math.Min(left, right) + 1;
+        //}
     }
 }
