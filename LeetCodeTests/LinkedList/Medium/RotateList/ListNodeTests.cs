4f44c5ab8ae0dd6a43c91581b82055a90aa78166
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-18 00:51:17 +0300

    add solution of Rotate List

---
+++ LeetCodeTests/LinkedList/Medium/RotateList/ListNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.LinkedList.Medium.RotateList;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Medium.RotateList
+{
+    public class ListNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var listNode = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+            // Act
+            var res = new Solution().RotateRight(listNode, 10);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/LinkedList/Medium/RotateList/Solution.cs
@@ -0,0 +1,60 @@
+﻿
+namespace LeetCode.LinkedList.Medium.RotateList
+{
+    //https://leetcode.com/problems/rotate-list/
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+            var length = GetLength(head);
+            if(length == 0 || length == 1 || k == 0 || k % length == 0) return head;
+
+            k = k > length ? k % length : k;
+
+            var n = length - k - 1;
+
+            var newHead = head;
+            while (n > 0)
+            {
+                newHead = newHead.next;
+                n--;
+            }
+
+            var next = newHead.next;
+            newHead.next = null;
+            newHead = next;
+
+            next = newHead;
+            while (next.next != null)
+                next = next.next;
+
+            next.next = head;
+
+            return newHead;
+        }
+
+        private int GetLength(ListNode head)
+        {
+            int length = 0;
+
+            while (head != null)
+            {
+                head = head.next;
+                length++;
+            }
+
+            return length;
+        }
+    }
+}
