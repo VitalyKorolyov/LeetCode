54d3ecb625d799806b75f5f1db55e284dadfa82c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-19 00:37:19 +0300

    add solution of Interval List Intersections

---
+++ c#_LeetCode/Array/Medium/IntervalListIntersections/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.IntervalListIntersections
+{
+    //https://leetcode.com/problems/interval-list-intersections/
+    public class Solution
+    {
+        public int[][] IntervalIntersection(int[][] firstList, int[][] secondList)
+        {
+            List<int[]> res = new();
+            int i = 0, j = 0;
+
+            while(i < firstList.Length && j < secondList.Length)
+            {
+                if ((firstList[i][0] >= secondList[j][0] && firstList[i][0] <= secondList[j][1])
+                    || (secondList[j][0] >= firstList[i][0] && secondList[j][0] <= firstList[i][1]))
+                {
+                    var left = System.Math.Max(firstList[i][0], secondList[j][0]);
+                    var right = System.Math.Min(firstList[i][1], secondList[j][1]);
+
+                    res.Add(new[] { left, right });
+                }
+
+                if (firstList[i][1] < secondList[j][1]) i++;
+                else j++;
+            }
+
+            return res.ToArray();
+        }
+    }
+}
