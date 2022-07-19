75e12573463aa9fdddb32c54c337594d9964a379
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-19 20:57:24 +0300

    add solution of Subsets

---
+++ LeetCodeTests/Array/Medium/Subsets/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Medium.Subsets;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.Subsets
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Subsets_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] { 1, 5, 3 };
+
+            // Act
+            var result = solution.Subsets(nums);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/Subsets/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.Subsets
+{
+    //https://leetcode.com/problems/subsets/
+    public class Solution
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            List<IList<int>> result = new() { new List<int>() };
+
+            foreach (int currentNumber in nums)
+            {
+                int n = result.Count;
+
+                for (int i = 0; i < n; i++)
+                {
+                    List<int> set = new(result[i]);
+                    set.Add(currentNumber);
+                    result.Add(set);
+                }
+            }
+
+            return result;
+        }
+    }
+}
