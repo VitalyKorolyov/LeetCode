8083ba6ab3be7a12dd95de8d60fdc77f7c3c957e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-19 21:43:47 +0300

    add solution of Longest Repeating Character Replacement

---
+++ LeetCodeTests/Array/Medium/LongestRepeatingCharacterReplacement/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Array.Medium.LongestRepeatingCharacterReplacement;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.LongestRepeatingCharacterReplacement
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void CharacterReplacement_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "AABABBA";
+            int k = 1;
+
+            // Act
+            var result = solution.CharacterReplacement(
+                s,
+                k);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/LongestRepeatingCharacterReplacement/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array.Medium.LongestRepeatingCharacterReplacement
+{
+    //https://leetcode.com/problems/longest-repeating-character-replacement/
+    public class Solution
+    {
+        public int CharacterReplacement(string s, int k)
+        {
+            int maxLength = int.MinValue, maxRepeatLetter = int.MinValue;
+            Dictionary<char, int> letterFrequencyMap = new();
+            int left = 0, right = 0;
+
+            while (right < s.Length)
+            {
+                if (letterFrequencyMap.ContainsKey(s[right]))
+                    letterFrequencyMap[s[right]]++;
+                else
+                    letterFrequencyMap.Add(s[right], 1);
+
+                maxRepeatLetter = System.Math.Max(maxRepeatLetter, letterFrequencyMap[s[right]]);
+
+                if (right - left + 1 - maxRepeatLetter > k)
+                {
+                    letterFrequencyMap[s[left]]--;
+                    left++;
+                }
+
+                maxLength = System.Math.Max(maxLength, right - left + 1);
+
+                right++;
+            }
+
+            return maxLength;
+        }
+    }
+}
