d8162958e2a9db65f1aba6358b4c98b9efebcd5e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-21 23:12:58 +0300

    add solution Permutation in String

---
+++ LeetCodeTests/String/Medium/PermutationInString/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.String.Medium.PermutationInString;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.PermutationInString
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void CheckInclusion_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s1 = "adc";
+            string s2 = "dcda";
+
+            // Act
+            var result = solution.CheckInclusion(
+                s1,
+                s2);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/PermutationInString/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.String.Medium.PermutationInString
+{
+    //https://leetcode.com/problems/permutation-in-string/
+    public class Solution
+    {
+        public bool CheckInclusion(string s1, string s2)
+        {
+            var freqS1 = new int[26];
+            var freqS2 = new int[26];
+
+            for (int i = 0; i < s1.Length; i++)
+                freqS1[s1[i] - 'a']++;
+
+            for (int start = 0, end = 0; end < s2.Length; end++)
+            {
+                if (end >= s1.Length)
+                {
+                    if (ContainsPermutation(freqS1, freqS2)) return true;
+
+                    freqS2[s2[start] - 'a']--;
+                    start++;
+                }
+
+                freqS2[s2[end] - 'a']++;
+            }
+
+            return ContainsPermutation(freqS1, freqS2);
+        }
+
+        private bool ContainsPermutation(int[] freqS1, int[] freqS2)
+        {
+            for (int i = 0; i < freqS2.Length; i++)
+                if (freqS1[i] != freqS2[i]) return false;
+
+            return true;
+        }
+    }
+}
