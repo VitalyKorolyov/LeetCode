7ab6bd0d40ed5925213c41ef76f5034b9d984172
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-24 22:29:07 +0300

    add solution of Employee Free Time

---
+++ LeetCodeTests/Array/Hard/EmployeeFreeTime/IntervalTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Array.Hard.EmployeeFreeTime;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCodeTests.Array.Hard.EmployeeFreeTime
+{
+    public class IntervalTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var solution = new Solution();
+
+            var schedule = new List<IList<Interval>>();
+            schedule.Add(new List<Interval> { new Interval { start = 1, end = 2 }, new Interval { start = 5, end = 6 } });
+            schedule.Add(new List<Interval> { new Interval { start = 1, end = 3 } });
+            schedule.Add(new List<Interval> { new Interval { start = 4, end = 10 } });
+
+            // Act
+            solution.EmployeeFreeTime(schedule);
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Hard/EmployeeFreeTime/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array.Hard.EmployeeFreeTime
+{
+    //https://leetcode.com/problems/employee-free-time/
+    public class Interval
+    {
+        public int start;
+        public int end;
+
+        public Interval() { }
+        public Interval(int _start, int _end)
+        {
+            start = _start;
+            end = _end;
+        }
+    }
+
+
+    public class Solution
+    {
+        public IList<Interval> EmployeeFreeTime(IList<IList<Interval>> schedule)
+        {
+            List<Interval> merged = new();
+            var priorityQueue = new PriorityQueue<Interval, int>();
+
+            foreach (var scheduleItem in schedule)
+                foreach (var interval in scheduleItem)
+                    priorityQueue.Enqueue(interval, interval.start);
+
+            Interval prev = priorityQueue.Peek();
+            while(priorityQueue.Count > 0)
+            {
+                var interval = priorityQueue.Dequeue();
+
+                if (prev.end < interval.start)
+                {
+                    merged.Add(new Interval(prev.end, interval.start));
+                    prev = interval;
+                }
+                else
+                {
+                    if (prev.end < interval.end)
+                        prev = interval;
+                }
+            }
+
+            return merged;
+        }
+    }
+}
