8e141f93443b3ed8c12301dd2b503c8d098eb9bd
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-26 00:56:20 +0300

    add solution of Find All Numbers Disappeared in an Array

---
+++ LeetCodeTests/Array/Easy/FindAllNumbersDisappearedInAnArray/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Array.Easy.FindAllNumbersDisappearedInAnArray;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.FindAllNumbersDisappearedInAnArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FindDisappearedNumbers_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] { 4, 3, 2, 7, 8, 2, 3, 4 };
+
+            // Act
+            var result = solution.FindDisappearedNumbers(
+                nums);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Easy/FindAllNumbersDisappearedInAnArray/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Array.Easy.FindAllNumbersDisappearedInAnArray
+{
+    //https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+    public class Solution
+    {
+        public IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            var res = new List<int>();
+
+            int i = 0;
+
+            while (i < nums.Length)
+            {
+                if (nums[i] != nums[nums[i] - 1])
+                    Swap(nums, i, nums[i] - 1);
+                else
+                    i++;
+            }
+
+            for (int k = 0; k < nums.Length; k++)
+            {
+                if (k + 1 != nums[k]) res.Add(k + 1);
+            }
+
+            return res;
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            var saved = nums[i];
+            nums[i] = nums[j];
+            nums[j] = saved;
+        }
+    }
+}
