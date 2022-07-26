62349bc3de9464832a71aa1250c0e62efa3db012
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-26 22:34:43 +0300

    add solution of First Missing Positive

---
+++ LeetCodeTests/Array/Hard/FirstMissingPositive/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Hard.FirstMissingPositive;
+using Xunit;
+
+namespace LeetCodeTests.Array.Hard.FirstMissingPositive
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FirstMissingPositive_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] {3, 4, -1, 1};
+
+            // Act
+            var result = solution.FirstMissingPositive(
+                nums);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Hard/FirstMissingPositive/Solution.cs
@@ -0,0 +1,37 @@
+﻿
+namespace LeetCode.Array.Hard.FirstMissingPositive
+{
+    //https://leetcode.com/problems/first-missing-positive/
+    public class Solution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            int i = 0;
+            while (i < nums.Length)
+            {
+                int j = nums[i] - 1;
+
+                if (nums[i] > nums.Length || nums[i] <= 0 || nums[i] == nums[j])
+                    i++;
+                else
+                    Swap(nums, i, j);
+            }
+
+            int first = 1;
+            for (int k = 0; k < nums.Length; k++)
+            {
+                if (nums[k] != first) break;
+                first++;
+            }
+
+            return first;
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            var saved = nums[i];
+            nums[i] = nums[j];
+            nums[j] = saved;
+        }
+    }
+}
