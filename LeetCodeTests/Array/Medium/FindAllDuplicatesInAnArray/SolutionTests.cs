efe4d50b770d0da331daf627b90baa62fd2b0e68
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-07-26 21:46:43 +0300

    update solution of Find all Duplicate Numbers

---
+++ LeetCodeTests/Array/Medium/FindAllDuplicatesInAnArray/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.FindAllDuplicatesInAnArray;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.FindAllDuplicatesInAnArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FindDuplicates_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] {4, 3, 2, 7, 8, 2, 3, 1};
+
+            // Act
+            var result = solution.FindDuplicates(nums);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
--- c#_LeetCode/Array/Medium/FindAllDuplicatesInAnArray/Solution.cs
+++ c#_LeetCode/Array/Medium/FindAllDuplicatesInAnArray/Solution.cs
@@ -5,30 +5,60 @@ namespace LeetCode.Array.Medium.FindAllDuplicatesInAnArray
     //https://leetcode.com/problems/find-all-duplicates-in-an-array/
     public class Solution
     {
-        public static IList<int> FindDuplicates(int[] nums)
+        public IList<int> FindDuplicates(int[] nums)
         {
-            var result = new List<int>();
-
-            for (var i = 0; i < nums.Length; i++)
+            int i = 0;
+            while (i < nums.Length)
             {
-                var index = System.Math.Abs(nums[i]) - 1;
-
-                if (nums[index] < 0)
-                {
-                    result.Add(System.Math.Abs(index+1));
-                }
-                else
-                {
-                    nums[index] *= -1;
-                }
+                int j = nums[i] - 1;
+                if (nums[i] != nums[j])
+                    Swap(nums, i, j);
+                else i++;
             }
 
-            for (int i = 0; i < nums.Length; i++)
+
+            List<int> result = new();
+            for (int k = 0; k < nums.Length; k++)
             {
-                nums[i] = System.Math.Abs(nums[i]);
+                if(nums[k] != k + 1)
+                    result.Add(nums[k]);
             }
 
             return result;
         }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            var saved = nums[i];
+            nums[i] = nums[j];
+            nums[j] = saved;
+        }
+
+        //negative approuch
+        //public static IList<int> FindDuplicates(int[] nums)
+        //{
+        //    var result = new List<int>();
+
+        //    for (var i = 0; i < nums.Length; i++)
+        //    {
+        //        var index = System.Math.Abs(nums[i]) - 1;
+
+        //        if (nums[index] < 0)
+        //        {
+        //            result.Add(System.Math.Abs(index+1));
+        //        }
+        //        else
+        //        {
+        //            nums[index] *= -1;
+        //        }
+        //    }
+
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+        //        nums[i] = System.Math.Abs(nums[i]);
+        //    }
+
+        //    return result;
+        //}
     }
 }
