9bf80458aa75d407d6300e4ef414f7e1cfa57c66
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-08-04 00:49:55 +0300

    add solution of Path Sum III

---
+++ LeetCodeTests/Trees/Medium/PathSumIII/TreeNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Trees.Medium.PathSumIII;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.PathSumIII
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(-2, null, new TreeNode(-3));
+
+            // Act
+            var solution = new Solution().PathSum(treeNode, -3);
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/PathSumIII/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees.Medium.PathSumIII
+{
+    //https://leetcode.com/problems/path-sum-iii/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        private int pathSum;
+
+        public int PathSum(TreeNode root, int targetSum)
+        {
+            Dfs(root, 0, targetSum, new Dictionary<long, int>());
+
+            return pathSum;
+        }
+
+        private void Dfs(TreeNode root, long sum, int target, Dictionary<long, int> hashMap)
+        {
+            if (root == null) return;
+
+            sum += root.val;
+
+            if (sum == target)
+                pathSum++;
+
+            if (hashMap.ContainsKey(sum - target))
+                pathSum += hashMap[sum - target];
+
+            if (hashMap.ContainsKey(sum))
+                hashMap[sum]++;
+            else hashMap.Add(sum, 1);
+
+            Dfs(root.left, sum, target, hashMap);
+            Dfs(root.right, sum, target, hashMap);
+
+            hashMap[sum]--;
+        }
+    }
+}
