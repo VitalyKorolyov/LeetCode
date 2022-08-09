4db7ca3247bdd193a4cd705cbeb5c3753577611c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-08-09 23:49:44 +0300

    update Find Median from Data Stream solution

---
+++ LeetCodeTests/DynamicProgramming/Medium/HouseRobber/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.DynamicProgramming.Medium.HouseRobber;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Medium.HouseRobber
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Rob_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] {1, 2, 3, 1};
+
+            // Act
+            var result = solution.Rob(nums);
+
+            // Assert
+            Assert.Equal(4, result);
+        }
+    }
+}
--- c#_LeetCode/Design/Hard/FindMedianFromDataStream/Solution.cs
+++ c#_LeetCode/Design/Hard/FindMedianFromDataStream/Solution.cs
@@ -3,36 +3,79 @@
 namespace LeetCode.Design.Hard.FindMedianFromDataStream
 {
     //https://leetcode.com/problems/find-median-from-data-stream/
+
+    //O(logN)
     public class MedianFinder
     {
-        private List<int> array;
-
-        public MedianFinder()
-        {
-            array = new List<int>();
-        }
+        private PriorityQueue<int, int> max = new PriorityQueue<int, int>(new MaxComp());
+        private PriorityQueue<int, int> min = new PriorityQueue<int, int>();
 
         public void AddNum(int num)
         {
-            var pos = array.BinarySearch(num);
-            if (pos < 0)
-            {
-                pos = ~pos;
-            }
+            if (max.Count == 0 || max.Peek() >= num)
+                max.Enqueue(num, num);
+            else
+                min.Enqueue(num, num);
 
-            array.Insert(pos, num);
+            if (max.Count > min.Count + 1)
+            {
+                var maxValue = max.Dequeue();
+                min.Enqueue(maxValue, maxValue);
+            }
+            else if (max.Count < min.Count)
+            {
+                var minValue = min.Dequeue();
+                max.Enqueue(minValue, minValue);
+            }
         }
 
         public double FindMedian()
         {
-            var middle = array.Count / 2;
+            var isSizeOdd = (max.Count + min.Count) % 2 != 0;
 
-            if (array.Count % 2 == 0)
-            {
-                return (array[middle - 1] + array[middle]) / 2.0;
-            }
-
-            return array[middle];
+            if (isSizeOdd)
+                return max.Peek();
+            else
+                return (max.Peek() + min.Peek()) / 2.0;
         }
     }
+
+    public class MaxComp : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            return y.CompareTo(x);
+        }
+    }
+
+    //O(n)
+    //private List<int> array;
+
+    //public MedianFinder()
+    //{
+    //    array = new List<int>();
+    //}
+
+    //public void AddNum(int num)
+    //{
+    //    var pos = array.BinarySearch(num);
+    //    if (pos < 0)
+    //    {
+    //        pos = ~pos;
+    //    }
+
+    //    array.Insert(pos, num);
+    //}
+
+    //public double FindMedian()
+    //{
+    //    var middle = array.Count / 2;
+
+    //    if (array.Count % 2 == 0)
+    //    {
+    //        return (array[middle - 1] + array[middle]) / 2.0;
+    //    }
+
+    //    return array[middle];
+    //}
 }
