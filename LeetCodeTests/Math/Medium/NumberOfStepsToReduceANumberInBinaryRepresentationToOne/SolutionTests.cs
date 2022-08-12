670430888068c9378c379b2b8f1ea4553e70f0af
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-08-12 23:34:28 +0300

    add solution of Number of Steps to Reduce a Number in Binary Representation to One

---
+++ LeetCodeTests/Math/Medium/NumberOfStepsToReduceANumberInBinaryRepresentationToOne/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Math.Medium.NumberOfStepsToReduceANumberInBinaryRepresentationToOne;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Math.Medium.NumberOfStepsToReduceANumberInBinaryRepresentationToOne
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void NumSteps_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "10101";
+
+            // Act
+            var result = solution.NumSteps(
+                s);
+
+            // Assert
+            Assert.True(result == 8);
+        }
+    }
+}
---
+++ c#_LeetCode/Math/Medium/NumberOfStepsToReduceANumberInBinaryRepresentationToOne/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Math.Medium.NumberOfStepsToReduceANumberInBinaryRepresentationToOne
+{
+    //https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
+    public class Solution
+    {
+        public int NumSteps(string s)
+        {
+            var res = 0;
+            var carry = 0;
+
+            for (int i = s.Length - 1; i > 0; i--)
+            {
+                res++;
+
+                if(s[i] - '0' + carry == 1)
+                {
+                    res++;
+                    carry = 1;
+                }
+            }
+
+            return res + carry;
+        }
+    }
+}
