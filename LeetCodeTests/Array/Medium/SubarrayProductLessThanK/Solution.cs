63954ca500eadcf4d872d763393bb8819523a100
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-08-14 02:04:57 +0300

    add solution of Subarray Product Less Than K

---
+++ LeetCodeTests/Array/Medium/SubarrayProductLessThanK/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeTests.Array.Medium.SubarrayProductLessThanK
+{
+    //https://leetcode.com/problems/subarray-product-less-than-k/
+    public class Solution
+    {
+        public int NumSubarrayProductLessThanK(int[] nums, int k)
+        {
+            if (k <= 1) return 0;
+
+            int prod = 1, ans = 0, left = 0;
+
+            for (int right = 0; right < nums.Length; right++)
+            {
+                prod *= nums[right];
+                while (prod >= k) prod /= nums[left++];
+                ans += right - left + 1;
+            }
+
+            return ans;
+        }
+    }
+}
