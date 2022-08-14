360fe8aa8f9fbfc8a25425c36eb30c45196b473f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-08-14 23:27:36 +0300

    add solution of Palindrome Permutation

---
+++ c#_LeetCode/String/Easy/PalindromePermutation/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Easy.PalindromePermutation
+{
+    //https://leetcode.com/problems/palindrome-permutation/
+    public class Solution
+    {
+        public bool CanPermutePalindrome(string s)
+        {
+            var hash = new HashSet<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if(hash.Contains(s[i]))
+                    hash.Remove(s[i]);
+                else
+                    hash.Add(s[i]);
+            }
+
+            return s.Length % 2 == 0 ? hash.Count == 0 : hash.Count == 1;
+        }
+    }
+}
