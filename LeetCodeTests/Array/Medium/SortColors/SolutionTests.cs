e624a14d233447f978ea6c1f1eee016d2915e7fd
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-08-15 01:02:20 +0300

    add solution of Sort Colors

---
+++ LeetCodeTests/Array/Medium/SortColors/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.SortColors;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.SortColors
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SortColors_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] { 1, 2, 0 };
+
+            // Act
+            solution.SortColors(nums);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/SortColors/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Array.Medium.SortColors
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+
+            for (int i = 0; i <= right;)
+            {
+                if(nums[i] == 0)
+                {
+                    Swap(nums, i, left);
+                    left++;
+                    i++;
+                }
+                else if (nums[i] == 1)
+                {
+                    i++;
+                }
+                else if(nums[i] == 2)
+                {
+                    Swap(nums, i, right);
+                    right--;
+                }
+            }
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            var saved = nums[j];
+            nums[j] = nums[i];
+            nums[i] = saved;
+        }
+    }
+}
