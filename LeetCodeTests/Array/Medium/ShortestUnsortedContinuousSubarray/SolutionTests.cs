ddca2dcef60128edac00cc8656cb6eed8bd1a850
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-08-16 01:54:53 +0300

    add solution of Shortest Unsorted Continuous Subarray

---
+++ LeetCodeTests/Array/Medium/ShortestUnsortedContinuousSubarray/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.ShortestUnsortedContinuousSubarray;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.ShortestUnsortedContinuousSubarray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FindUnsortedSubarray_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] { 2, 1 };
+
+            // Act
+            var result = solution.FindUnsortedSubarray(nums);
+
+            // Assert
+            Assert.True(result == 2);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/ShortestUnsortedContinuousSubarray/Solution.cs
@@ -0,0 +1,45 @@
+﻿namespace LeetCode.Array.Medium.ShortestUnsortedContinuousSubarray
+{
+    //https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+    public class Solution
+    {
+        public int FindUnsortedSubarray(int[] nums)
+        {
+            int min = int.MaxValue, max = int.MinValue;
+
+            bool flag = false;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] < nums[i - 1])
+                    flag = true;
+                if (flag)
+                    min = System.Math.Min(min, nums[i]);
+            }
+
+            flag = false;
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                if (nums[i] > nums[i + 1])
+                    flag = true;
+                if (flag)
+                    max = System.Math.Max(max, nums[i]);
+            }
+
+            int minIndex;
+            for (minIndex = 0; minIndex < nums.Length; minIndex++)
+            {
+                if (nums[minIndex] > min)
+                    break;
+            }
+
+            int maxIndex;
+            for (maxIndex = nums.Length - 1; maxIndex >= 0; maxIndex--)
+            {
+                if (nums[maxIndex] < max)
+                    break;
+            }
+
+            return maxIndex - minIndex < 0 ? 0 : maxIndex - minIndex + 1;
+        }
+    }
+}
