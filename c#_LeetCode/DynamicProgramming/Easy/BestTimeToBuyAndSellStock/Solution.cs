e6b560591931d3267949ddde1b2133675b27ea57
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-08-27 00:46:17 +0300

    update Best Time to Buy and Sell Stock solution

--- c#_LeetCode/DynamicProgramming/Easy/BestTimeToBuyAndSellStock/Solution.cs
+++ c#_LeetCode/DynamicProgramming/Easy/BestTimeToBuyAndSellStock/Solution.cs
@@ -1,29 +1,19 @@
-﻿using System.Linq;
-
-namespace LeetCode.DynamicProgramming.Easy.BestTimeToBuyAndSellStock
+﻿namespace LeetCode.DynamicProgramming.Easy.BestTimeToBuyAndSellStock
 {
     //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
     public static class Solution
     {
         public static int MaxProfit(int[] prices)
         {
-            if (!prices.Any() || prices.Length == 1) return 0;
+            if (prices == null || prices.Length <= 1) return 0;
 
-            int min = prices[0];
+            int minPrice = prices[0];
             int maxProfit = 0;
+
             for (int i = 1; i < prices.Length; i++)
             {
-                if (min > prices[i])
-                {
-                    min = prices[i];
-                    continue;
-                }
-
-                var newMaxProfit = prices[i] - min;
-                if (newMaxProfit > maxProfit)
-                {
-                    maxProfit = newMaxProfit;
-                }
+                maxProfit = System.Math.Max(maxProfit, prices[i] - minPrice);
+                minPrice = System.Math.Min(minPrice, prices[i]);
             }
 
             return maxProfit;
