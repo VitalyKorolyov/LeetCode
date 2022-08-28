8e4400908bf2c22fd9e3be965f0b613c45be0dd5
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-08-28 23:09:45 +0300

    add solution of Best Time to Buy and Sell Stock III

---
+++ LeetCodeTests/DynamicProgramming/Hard/BestTimeToBuyAndSellStockIII/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.DynamicProgramming.Hard.BestTimeToBuyAndSellStockIII;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Hard.BestTimeToBuyAndSellStockIII
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MaxProfit_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] prices = new int[] {3, 3, 5, 0, 0, 3, 1, 4};
+
+            // Act
+            var result = solution.MaxProfit(prices);
+
+            // Assert
+            Assert.True(result == 6);
+        }
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Hard/BestTimeToBuyAndSellStockIII/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.DynamicProgramming.Hard.BestTimeToBuyAndSellStockIII
+{
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if(prices == null || prices.Length < 1) return 0;
+
+            var oneBuyOneSell = 0;
+            var twoBuyTwoSell = 0;
+            var oneBuy = int.MaxValue;
+            var twoBuy = int.MaxValue;
+
+            for (var i = 0; i < prices.Length; i++)
+            {
+                oneBuy = System.Math.Min(oneBuy, prices[i]);
+                oneBuyOneSell = System.Math.Max(oneBuyOneSell, prices[i] - oneBuy);
+                twoBuy = System.Math.Min(twoBuy, prices[i] - oneBuyOneSell);
+                twoBuyTwoSell = System.Math.Max(twoBuyTwoSell, prices[i] - twoBuy);
+            }
+
+            return twoBuyTwoSell;
+        }
+    }
+}
