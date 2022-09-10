ee73bc20d08e08f75c8c126ee4e6833c65d0160d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-10 23:39:30 +0300

    add solution of Plates Between Candles

---
+++ LeetCodeTests/String/Medium/PlatesBetweenCandles/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.String.Medium.PlatesBetweenCandles;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.PlatesBetweenCandles
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void PlatesBetweenCandles_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "**|**|***|";
+            int[][] queries = new int[2][];
+            queries[0] = new int[] {2, 5};
+            queries[1] = new int[] { 5, 9 };
+
+            // Act
+            var result = solution.PlatesBetweenCandles(s, queries);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/PlatesBetweenCandles/Solution.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Medium.PlatesBetweenCandles
+{
+    //https://leetcode.com/problems/plates-between-candles/
+    public class Solution
+    {
+        public int[] PlatesBetweenCandles(string s, int[][] queries)
+        {
+            List<int> candles = new();
+
+            for (int i = 0; i < s.Length; ++i) //O(n)
+                if (s[i] == '|') candles.Add(i);
+
+            int[] result = new int[queries.Length];
+
+            for (int i = 0; i < queries.Length; ++i) 
+            {
+                int[] query = queries[i];
+                int lm = LBinarySearch(candles, query[0]);
+                int rm = RBinarySearch(candles, query[1]) - 1;
+
+                if (lm < rm)
+                    result[i] = candles[rm] - candles[lm] - (rm - lm);
+                else
+                    result[i] = 0;
+            }
+
+            return result;
+        }
+
+        public int LBinarySearch(List<int> ci, int lb)
+        {
+            int l = 0, r = ci.Count;
+
+            while (l < r)
+            {
+                int m = l + (r - l) / 2;
+
+                if (ci[m] >= lb)
+                    r = m;
+                else
+                    l = m + 1;
+            }
+
+            return l;
+        }
+
+        public int RBinarySearch(List<int> ci, int rb)
+        {
+            int l = 0, r = ci.Count;
+
+            while (l < r)
+            {
+                int m = l + (r - l) / 2;
+                if (ci[m] <= rb)
+                    l = m + 1;
+                else
+                    r = m;
+            }
+
+            return l;
+        }
+    }
+}
