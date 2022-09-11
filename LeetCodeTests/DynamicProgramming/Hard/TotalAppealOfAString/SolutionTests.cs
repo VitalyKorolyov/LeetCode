cf23eb9f0d011853309a95fb59fdfa07beca1e52
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-11 19:45:09 +0300

    add solution of Total Appeal of A String;

---
+++ LeetCodeTests/DynamicProgramming/Hard/TotalAppealOfAString/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.DynamicProgramming.Hard.TotalAppealOfAString;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Hard.TotalAppealOfAString
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void AppealSum_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "fxfz";
+
+            // Act
+            var result = solution.AppealSum(s);
+
+            // Assert
+            Assert.True(18 == result);
+        }
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Hard/TotalAppealOfAString/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace LeetCode.DynamicProgramming.Hard.TotalAppealOfAString
+{
+    //https://leetcode.com/problems/total-appeal-of-a-string/
+    public class Solution
+    {
+        public long AppealSum(string s)
+        {
+            var seen = new int[26];
+
+            long res = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var index = s[i] - 'a';
+
+                seen[index] = i + 1;
+                res += seen.Sum();
+            }
+
+            return res;
+        }
+    }
+}
