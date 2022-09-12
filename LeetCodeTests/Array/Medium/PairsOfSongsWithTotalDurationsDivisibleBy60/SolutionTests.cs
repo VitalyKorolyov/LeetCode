897d6b84dae872b239210d9a9d8b50154f68912e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-12 23:12:00 +0300

    add solution of Pairs of Songs With Total Durations Divisible by 60

---
+++ LeetCodeTests/Array/Medium/PairsOfSongsWithTotalDurationsDivisibleBy60/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.PairsOfSongsWithTotalDurationsDivisibleBy60;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.PairsOfSongsWithTotalDurationsDivisibleBy60
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void NumPairsDivisibleBy60_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] time = new int[] { 60, 60, 60 };
+
+            // Act
+            var result = solution.NumPairsDivisibleBy60(time);
+
+            // Assert
+            Assert.True(3 == result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/PairsOfSongsWithTotalDurationsDivisibleBy60/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Array.Medium.PairsOfSongsWithTotalDurationsDivisibleBy60
+{
+    //https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+    public class Solution
+    {
+        public int NumPairsDivisibleBy60(int[] time)
+        {
+            var remainders = new int[60];
+
+            int count = 0;
+            for (int i = 0; i < time.Length; i++)
+            {
+                var remainder = time[i] % 60;
+
+                if (remainder == 0)
+                    count += remainders[0];
+                else
+                    count += remainders[60 - remainder];
+
+                remainders[remainder]++;
+            }
+
+            return count;
+        }
+    }
+}
