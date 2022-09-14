9dc294074e1a89e9370991185d588413de406e2d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-14 23:49:43 +0300

    add solution of Slowest Key;

---
+++ c#_LeetCode/Array/Easy/SlowestKey/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Array.Easy.SlowestKey
+{
+    //https://leetcode.com/problems/slowest-key/
+    public class Solution
+    {
+        public char SlowestKey(int[] releaseTimes, string keysPressed)
+        {
+            char slowestKey = 'a';
+            var keyTime = int.MinValue;
+
+            var time = 0;
+
+            for (int i = 0; i < keysPressed.Length; i++)
+            {
+                var currentTime = releaseTimes[i] - time;
+                if (currentTime >= keyTime)
+                {
+                    if (currentTime == keyTime)
+                        slowestKey = slowestKey > keysPressed[i] ? slowestKey : keysPressed[i];
+                    else
+                        slowestKey = keysPressed[i];
+
+                    keyTime = currentTime;
+                }
+
+                time = releaseTimes[i];
+            }
+
+            return slowestKey;
+        }
+    }
+}
