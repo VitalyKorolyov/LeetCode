f9cd0dbe60888c17053ef205a8986a8452a9d534
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-18 01:36:34 +0300

    add solution of Sliding Window Maximum

---
+++ LeetCodeTests/Array/Hard/SlidingWindowMaximum/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Hard.SlidingWindowMaximum;
+using Xunit;
+
+namespace LeetCodeTests.Array.Hard.SlidingWindowMaximum
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MaxSlidingWindow_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] { 1, 3, -1, -3, 5, 3, 6, 7 };
+            int k = 3;
+
+            // Act
+            var result = solution.MaxSlidingWindow(nums, k);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Hard/SlidingWindowMaximum/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array.Hard.SlidingWindowMaximum
+{
+    //https://leetcode.com/problems/sliding-window-maximum/
+    public class Solution
+    {
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            var res = new List<int>();
+            var sortedHash = new SortedDictionary<int, int>(new Comp());
+
+            for (int i = 0, j = 0; i < nums.Length; i++)
+            {
+                if(i - j >= k)
+                {
+                    res.Add(sortedHash.Keys.ElementAt(0));
+
+                    Decrease(sortedHash, nums[j]);
+                    j++;
+                }
+
+                Add(sortedHash, nums[i]);
+            }
+
+            res.Add(sortedHash.Keys.ElementAt(0));
+            return res.ToArray();
+        }
+
+        private void Add(SortedDictionary<int, int> sortedHash, int value)
+        {
+            if (sortedHash.ContainsKey(value))
+                sortedHash[value]++;
+            else sortedHash.Add(value, 1);
+        }
+
+        private void Decrease(SortedDictionary<int, int> sortedHash, int value)
+        {
+            sortedHash[value]--;
+            if (sortedHash[value] == 0) sortedHash.Remove(value);
+        }
+
+        public class Comp : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return y.CompareTo(x);
+            }
+        }
+    }
+}
