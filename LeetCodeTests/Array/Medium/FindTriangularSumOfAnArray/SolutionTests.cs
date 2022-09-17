ed778459b2297288b3e33435301595ab716fe2db
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-18 00:05:23 +0300

    add solution of Find Triangular Sum of an Array

---
+++ LeetCodeTests/Array/Medium/FindTriangularSumOfAnArray/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.FindTriangularSumOfAnArray;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.FindTriangularSumOfAnArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void TriangularSum_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] { 1, 2, 3, 4, 5 };
+
+            // Act
+            var result = solution.TriangularSum(nums);
+
+            // Assert
+            Assert.True(result == 8);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/FindTriangularSumOfAnArray/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Array.Medium.FindTriangularSumOfAnArray
+{
+    //https://leetcode.com/problems/find-triangular-sum-of-an-array/
+    public class Solution
+    {
+        public int TriangularSum(int[] nums)
+        {
+            if(nums == null || nums.Length == 0) return 0;
+            if(nums.Length == 1) return nums[0];
+
+            var size = nums.Length;
+
+            while(size > 1)
+            {
+                for (int i = 1; i < size; i++)
+                {
+                    nums[i - 1] = (nums[i - 1] + nums[i]) % 10;
+                }
+
+                size--;
+            }
+
+            return nums[0];
+        }
+    }
+}
