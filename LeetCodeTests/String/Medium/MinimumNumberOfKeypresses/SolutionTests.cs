d1967d8c5b6d6349c153e8a8ec21f49826b49964
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-19 00:16:57 +0300

    add solution of Minimum Number of Keypresses

---
+++ LeetCodeTests/String/Medium/MinimumNumberOfKeypresses/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.String.Medium.MinimumNumberOfKeypresses;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.MinimumNumberOfKeypresses
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinimumKeypresses_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "abcdefghijkl";
+
+            // Act
+            var result = solution.MinimumKeypresses(s);
+
+            // Assert
+            Assert.True(result == 15);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/MinimumNumberOfKeypresses/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.String.Medium.MinimumNumberOfKeypresses
+{
+    public class Solution
+    {
+        public int MinimumKeypresses(string s)
+        {
+            var frequency = new int[26];
+
+            for (int i = 0; i < s.Length; i++)
+                frequency[s[i] - 'a'] += 1;
+
+            System.Array.Sort(frequency, new Comparison<int>(
+                  (i1, i2) => i2.CompareTo(i1)));
+
+            var res = 0;
+            var pressTime = 1;
+
+            for (int i = 0; i < frequency.Length; i++)
+            {
+                res += pressTime * frequency[i];
+
+                if ((i + 1) % 9 == 0)
+                    pressTime++;
+            }
+
+            return res;
+        }
+    }
+}
