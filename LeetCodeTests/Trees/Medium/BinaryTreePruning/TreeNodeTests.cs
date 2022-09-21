19389a378cf56e66c78cbbfb757c58cfcea4799c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-21 22:49:48 +0300

    add solution of Binary Tree Pruning

---
+++ LeetCodeTests/Trees/Medium/BinaryTreePruning/TreeNodeTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Trees.Medium.BinaryTreePruning;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.BinaryTreePruning
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(1, null, new TreeNode(0, new TreeNode(0), new TreeNode(1)));
+
+            // Act
+            var res = new Solution().PruneTree(treeNode);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/BinaryTreePruning/Solution.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Trees.Medium.BinaryTreePruning
+{
+    //https://leetcode.com/problems/binary-tree-pruning/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public TreeNode PruneTree(TreeNode root)
+        {
+            var res = Dfs(root);
+            return res ? root : null;
+        }
+
+        bool Dfs(TreeNode node)
+        {
+            if (node == null) return false;
+
+            bool seen = false;
+            bool res = Dfs(node.left); // false
+            if (res == false)
+                node.left = null;
+            seen = res;
+
+            res = Dfs(node.right); //true
+            if (res == false)
+                node.right = null;
+
+            seen = res || seen;
+
+            return seen || node.val == 1;
+        }
+    }
+}
