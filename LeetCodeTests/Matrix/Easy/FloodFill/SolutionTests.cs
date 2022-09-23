2249d936598e6539082cf05b2bfa203b31935fc2
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-24 00:13:13 +0300

    add solution of Flood Fill;

---
+++ LeetCodeTests/Matrix/Easy/FloodFill/SolutionTests.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Matrix.Easy.FloodFill;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Easy.FloodFill
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FloodFill_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] image = new int[3][];
+            image[0] = new int[] { 1, 1, 1 };
+            image[1] = new int[] { 1, 1, 0 };
+            image[2] = new int[] { 1, 0, 1 };
+            int sr = 1;
+            int sc = 1;
+            int color = 2;
+
+            // Act
+            var result = solution.FloodFill(
+                image,
+                sr,
+                sc,
+                color);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Easy/FloodFill/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Matrix.Easy.FloodFill
+{
+    //https://leetcode.com/problems/flood-fill/
+    public class Solution
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+        {
+            if (image[sr][sc] == color) return image;
+
+            PerformFloodFill(image, sr, sc, image[sr][sc], color);
+
+            return image;
+        }
+
+        private void PerformFloodFill(int[][] image, int i, int j, int originalColor, int newColor)
+        {
+            if (i < 0 || i >= image.Length) return;
+            if (j < 0 || j >= image[i].Length) return;
+            if (image[i][j] != originalColor) return;
+
+            image[i][j] = newColor;
+
+            PerformFloodFill(image, i, j + 1, originalColor, newColor);
+            PerformFloodFill(image, i, j - 1, originalColor, newColor);
+            PerformFloodFill(image, i + 1, j, originalColor, newColor);
+            PerformFloodFill(image, i - 1, j, originalColor, newColor);
+        }
+    }
+}
