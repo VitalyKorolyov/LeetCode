5197d423cf4acde26cd5b5b5d2137303d7987991
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-25 01:24:56 +0300

    add solution of Make Array Zero by Subtracting Equal Amounts

---
+++ c#_LeetCode/Array/Easy/MakeArrayZeroBySubtractingEqualAmounts/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Easy.MakeArrayZeroBySubtractingEqualAmounts
+{
+    //https://leetcode.com/problems/make-array-zero-by-subtracting-equal-amounts/
+    public class Solution
+    {
+        public int MinimumOperations(int[] nums)
+        {
+            HashSet<int> hash = new();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0 || hash.Contains(nums[i]))
+                    continue;
+                else hash.Add(nums[i]);
+            }
+
+            return hash.Count;
+        }
+    }
+}
