6f28beee8fbca389a8bf54eff191cadb116c67bc
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-28 23:25:12 +0300

    add solution of Copy List with Random Pointer

---
+++ c#_LeetCode/LinkedList/Medium/CopyListWithRandomPointer/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.LinkedList.Medium.CopyListWithRandomPointer
+{
+    //https://leetcode.com/problems/copy-list-with-random-pointer/
+    public class Node
+    {
+        public int val;
+        public Node next;
+        public Node random;
+
+        public Node(int _val)
+        {
+            val = _val;
+            next = null;
+            random = null;
+        }
+    }
+
+    public class Solution
+    {
+        public Node CopyRandomList(Node head)
+        {
+            if (head == null) return head;
+
+            Dictionary<Node, Node> oldToNew = new();
+
+            var current = head;
+            while(current != null)
+            {
+                oldToNew.Add(current, new Node(current.val));
+                current = current.next;
+            }
+
+            foreach (var item in oldToNew)
+            {
+                if(item.Key.next != null)
+                    item.Value.next = oldToNew[item.Key.next];
+
+                if(item.Key.random != null)
+                    item.Value.random = oldToNew[item.Key.random];
+            }
+
+            return oldToNew[head];
+        }
+    }
+}
