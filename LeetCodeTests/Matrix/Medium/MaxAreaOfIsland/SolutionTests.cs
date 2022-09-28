ddc3cb711e7a0b901dc546be20b6dc577664721e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-28 21:33:34 +0300

    add solution of Max Area of Island

---
+++ LeetCodeTests/Matrix/Medium/MaxAreaOfIsland/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Matrix.Medium.MaxAreaOfIsland;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Medium.MaxAreaOfIsland
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MaxAreaOfIsland_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] grid = new int[3][];
+            grid[0] = new int[] { 0, 0, 1 };
+            grid[1] = new int[] { 0, 1, 0 };
+            grid[2] = new int[] { 0, 1, 1 };
+
+            // Act
+            var result = solution.MaxAreaOfIsland(grid);
+
+            // Assert
+            Assert.True(result == 3);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Medium/MaxAreaOfIsland/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Matrix.Medium.MaxAreaOfIsland
+{
+    //https://leetcode.com/problems/max-area-of-island/
+    public class Solution
+    {
+        public int MaxAreaOfIsland(int[][] grid)
+        {
+            var maxArea = 0;
+
+            for (int i = 0; i < grid.Length; i++)
+                for (int j = 0; j < grid[i].Length; j++)
+                    if (grid[i][j] == 1)
+                        maxArea = System.Math.Max(Dfs(grid, i, j), maxArea);
+
+            return maxArea;
+        }
+
+        private int Dfs(int[][] grid, int i, int j)
+        {
+            if (i < 0 || j < 0) return 0;
+            if(grid.Length <= i || grid[i].Length <= j) return 0;
+            if (grid[i][j] == 0) return 0;
+
+            grid[i][j] = 0;
+
+            return 1 + 
+                Dfs(grid, i + 1, j) + 
+                Dfs(grid, i - 1, j) + 
+                Dfs(grid, i, j + 1) + 
+                Dfs(grid, i, j - 1);
+        }
+    }
+}
