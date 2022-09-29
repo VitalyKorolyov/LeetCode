1662b450566bc10442231a90ff0c29f986013599
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-09-29 21:45:29 +0300

    add solution of Minimum Adjacent Swaps to Make a Valid Array

---
+++ LeetCodeTests/Array/Medium/MinimumAdjacentSwapsToMakeAValidArray/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.MinimumAdjacentSwapsToMakeAValidArray;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.MinimumAdjacentSwapsToMakeAValidArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinimumSwaps_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] {3, 4, 5, 5, 3, 1};
+
+            // Act
+            var result = solution.MinimumSwaps(nums);
+
+            // Assert
+            Assert.True(result == 6);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/MinimumAdjacentSwapsToMakeAValidArray/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Array.Medium.MinimumAdjacentSwapsToMakeAValidArray
+{
+    //https://leetcode.com/problems/minimum-adjacent-swaps-to-make-a-valid-array/
+    public class Solution
+    {
+        public int MinimumSwaps(int[] nums)
+        {
+            if(nums == null || nums.Length <= 1) return 0;
+
+            var minIndex = 0;
+            var maxIndex = 0;
+            var max = nums[0];
+            var min = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if(min > nums[i])
+                {
+                    minIndex = i;
+                    min = nums[i];
+                }
+                if(max <= nums[i])
+                {
+                    maxIndex = i;
+                    max = nums[i];
+                }
+            }
+
+            if (max == min) return 0;
+
+            var result = minIndex + (nums.Length - 1) - maxIndex;
+
+            return minIndex > maxIndex ? result - 1 : result;
+        }
+    }
+}
