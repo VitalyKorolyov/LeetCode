f6967205aa8aaa13e7df3154415a233c18a862c7
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-01 18:41:41 +0300

    add solution of Valid Palindrome

---
+++ LeetCodeTests/String/Easy/ValidPalindrome/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.String.Easy.ValidPalindrome;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.ValidPalindrome
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void IsPalindrome_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string s = "A man, a plan, a canal: Panama";
+
+            // Act
+            var result = solution.IsPalindrome(s);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/ValidPalindrome/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.String.Easy.ValidPalindrome
+{
+    //https://leetcode.com/problems/valid-palindrome/
+    public class Solution
+    {
+        public bool IsPalindrome(string s)
+        {
+            if (s == null || s.Length <= 1) return true;
+
+            int i = 0, j = s.Length - 1;
+
+            while (i < j)
+            {
+                if (!char.IsLetterOrDigit(s[i]))
+                {
+                    i++;
+                    continue;
+                }
+                if (!char.IsLetterOrDigit(s[j]))
+                {
+                    j--;
+                    continue;
+                }
+
+                if (char.ToLower(s[i++]) != char.ToLower(s[j--])) return false;
+            }
+
+            return true;
+        }
+    }
+}
