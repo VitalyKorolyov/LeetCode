c3e3e8bbc740ff6f9e0932642a29998d0b3605f9
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-10 23:26:31 +0300

    add solution of Transpose Matrix

---
+++ LeetCodeTests/Matrix/Easy/TransposeMatrix/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Matrix.Easy.TransposeMatrix;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Easy.TransposeMatrix
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Transpose_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] matrix = new int[2][];
+            matrix[0] = new int[] { 1, 2, 3 };
+            matrix[1] = new int[] { 4, 5, 6 };
+
+            // Act
+            var result = solution.Transpose(matrix);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Easy/TransposeMatrix/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Matrix.Easy.TransposeMatrix
+{
+    //https://leetcode.com/problems/transpose-matrix/
+    public class Solution
+    {
+        public int[][] Transpose(int[][] matrix)
+        {
+            if (matrix == null || matrix.Length == 0) return null;
+
+            var transposedMatrix = new int[matrix[0].Length][];
+
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                transposedMatrix[i] = new int[matrix.Length];
+
+                for (int j = 0; j < matrix.Length; j++)
+                    transposedMatrix[i][j] = matrix[j][i];
+            }
+
+            return transposedMatrix;
+        }
+    }
+}
