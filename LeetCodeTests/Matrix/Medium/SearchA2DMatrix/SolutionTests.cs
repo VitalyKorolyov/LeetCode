68987741d77b9cb334f294ccaf67171df3bcd350
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-10 22:32:37 +0300

    add solution of Search a 2D Matrix;

---
+++ LeetCodeTests/Matrix/Medium/SearchA2DMatrix/SolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Matrix.Medium.SearchA2DMatrix;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Medium.SearchA2DMatrix
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SearchMatrix_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] matrix = new int[3][];
+            matrix[0] = new int[] { 2, 5, 6 };
+            matrix[1] = new int[] { 7, 10, 12 };
+            matrix[2] = new int[] { 15, 16, 20 };
+
+            int target = 11;
+
+            // Act
+            var result = solution.SearchMatrix(matrix, target);
+
+            // Assert
+            Assert.False(result);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Medium/SearchA2DMatrix/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Matrix.Medium.SearchA2DMatrix
+{
+    //https://leetcode.com/problems/search-a-2d-matrix/
+    public class Solution
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            var m = matrix.Length;
+            if(m == 0) return false;
+
+            var n = matrix[0].Length;
+            var left = 0;
+            var right = (n * m) - 1;
+
+            while (left <= right)
+            {
+                var middleIndex = (left + right) / 2;
+                var middleElement = matrix[middleIndex / n][middleIndex % n];
+
+                if(target == middleElement) return true;
+
+                if (target < middleElement)
+                    right = middleIndex - 1;
+                else
+                    left = middleIndex + 1;
+            }
+
+            return false;
+        }
+    }
+}
