6fde979646f835232fc8f8b18cb7ea501106e012
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-11 00:35:25 +0300

    add solution of Search a 2D Matrix II

---
+++ LeetCodeTests/Matrix/Medium/SearchA2DMatrixII/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Matrix.Medium.SearchA2DMatrixII;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Medium.SearchA2DMatrixII
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void SearchMatrix_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] matrix = new int[1][];
+            matrix[0] = new int[] {-1, 3};
+            int target = 3;
+
+            // Act
+            var result = solution.SearchMatrix(matrix, target);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Medium/SearchA2DMatrixII/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Matrix.Medium.SearchA2DMatrixII
+{
+    //https://leetcode.com/problems/search-a-2d-matrix-ii/
+    public class Solution
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            if(matrix == null || matrix[0].Length == 0) return false;
+
+            var row = matrix.Length - 1;
+            var col = 0;
+
+            while (col < matrix[0].Length && row >= 0)
+            {
+                if (matrix[row][col] == target) return true;
+
+                if (matrix[row][col] < target)
+                    col++;
+                else
+                    row--;
+            }
+
+            return false;
+        }
+    }
+}
