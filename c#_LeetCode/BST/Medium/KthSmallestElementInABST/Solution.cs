b0d664ec2c6c576bd28d454721021adab491e7c7
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-12 01:56:02 +0300

    add solution of Kth Smallest Element in a BST

---
+++ c#_LeetCode/BST/Medium/KthSmallestElementInABST/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.BST.Medium.KthSmallestElementInABST
+{
+    //https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        private int _k;
+        private int res = 0;
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+            _k = k;
+            Dfs(root);
+
+            return res;
+        }
+
+        void Dfs(TreeNode node)
+        {
+            if(node == null) return;
+
+            Dfs(node.left);
+
+            if(_k == 1)
+                res = node.val;
+            _k--;
+
+            Dfs(node.right);
+        }
+    }
+}
