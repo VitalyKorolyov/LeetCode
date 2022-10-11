d2a92867929893e5c6737166e15095367ddc800e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-12 00:29:11 +0300

    add solution of Longest Common Prefix

---
+++ LeetCodeTests/String/Easy/LongestCommonPrefix/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.String.Easy.LongestCommonPrefix;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.LongestCommonPrefix
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void LongestCommonPrefix_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string[] strs = new string[] { "flower", "flow", "flight" };
+
+            // Act
+            var result = solution.LongestCommonPrefix(strs);
+
+            // Assert
+            Assert.True("fl" == result);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/LongestCommonPrefix/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.String.Easy.LongestCommonPrefix
+{
+    //https://leetcode.com/problems/longest-common-prefix/
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0) return "";
+
+            var commonPrefix = strs[0];
+            for (int i = 1; i < strs.Length; i++)
+            {
+                while (!strs[i].StartsWith(commonPrefix))
+                    commonPrefix = commonPrefix.Remove(commonPrefix.Length - 1);
+            }
+
+            return commonPrefix;
+        }
+    }
+}
