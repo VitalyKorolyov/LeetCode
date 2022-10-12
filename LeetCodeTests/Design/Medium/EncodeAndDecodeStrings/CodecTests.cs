39e0813de36b274b5f160e46bb94ebfc173d6042
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-13 00:12:16 +0300

    add solution of Encode and Decode Strings

---
+++ LeetCodeTests/Design/Medium/EncodeAndDecodeStrings/CodecTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Design.Medium.EncodeAndDecodeStrings;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCodeTests.Design.Medium.EncodeAndDecodeStrings
+{
+    public class CodecTests
+    {
+        [Fact]
+        public void encode_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var codec = new Codec();
+
+            // Act
+            var result = codec.encode(new List<string> { "Hello", "World"});
+            var str = codec.decode(result);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Design/Medium/EncodeAndDecodeStrings/Codec.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Design.Medium.EncodeAndDecodeStrings
+{
+    //https://leetcode.com/problems/encode-and-decode-strings/
+    public class Codec
+    {
+        private string IntToString(int value)
+        {
+            var str = value.ToString();
+
+            if (str.Length < 4)
+            {
+                var strB = new StringBuilder(4);
+
+                for (int i = 0; i < 4 - str.Length; i++)
+                    strB.Append("0");
+
+                strB.Append(str);
+
+                return strB.ToString();
+            }
+            return str;
+        }
+
+        private int StringToInt(string value)
+        {
+            return Convert.ToInt32(value);
+        }
+
+        // Encodes a list of strings to a single string.
+        public string encode(IList<string> strs)
+        {
+            var res = new StringBuilder();
+
+            foreach (var str in strs)
+            {
+                var length = str.Length;
+                res.Append(IntToString(length));
+                res.Append(str);
+            }
+
+            return res.ToString();
+        }
+
+        // Decodes a single string to a list of strings.
+        public IList<string> decode(string s)
+        {
+            int i = 0, n = s.Length;
+            List<string> output = new();
+
+            while (i < n)
+            {
+                int length = StringToInt(s.Substring(i, 4));
+                i += 4;
+                output.Add(s.Substring(i, length));
+                i += length;
+            }
+
+            return output;
+        }
+    }
+}
