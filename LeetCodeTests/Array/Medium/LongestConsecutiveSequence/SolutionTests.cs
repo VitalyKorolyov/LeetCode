6d00af8905014efbbc4bb9d4b70dde5747de3291
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-22 22:17:18 +0300

    add solution of Longest Consecutive Sequence

---
+++ LeetCodeTests/Array/Medium/LongestConsecutiveSequence/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.LongestConsecutiveSequence;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.LongestConsecutiveSequence
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void LongestConsecutive_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] { 100, 4, 200, 1, 3, 2 };
+
+            // Act
+            var result = solution.LongestConsecutive(nums);
+
+            // Assert
+            Assert.True(result == 4);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/LongestConsecutiveSequence/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.LongestConsecutiveSequence
+{
+    //https://leetcode.com/problems/longest-consecutive-sequence/
+    public class Solution
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            HashSet<int> hash = new(nums.Length);
+            HashSet<int> visited = new(nums.Length);
+
+            for (int i = 0; i < nums.Length; i++)
+                hash.Add(nums[i]);
+
+            var longest = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!visited.Contains(nums[i]))
+                {
+                    visited.Add(nums[i]);
+
+                    var value = nums[i];
+                    var current = 1;
+
+                    while (hash.Contains(value - 1))
+                    {
+                        value--;
+                        current++;
+                        visited.Add(value);
+                    }
+
+                    value = nums[i];
+
+                    while (hash.Contains(value + 1))
+                    {
+                        value++;
+                        current++;
+                        visited.Add(value);
+                    }
+
+                    longest = System.Math.Max(current, longest);
+                }
+            }
+
+            return longest;
+        }
+    }
+}
