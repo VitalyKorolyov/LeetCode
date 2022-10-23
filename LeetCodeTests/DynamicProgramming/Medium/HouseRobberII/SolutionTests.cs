8533fd427cf2962fae958fd28c150dd662a6e9d6
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-24 00:47:54 +0300

    add solution of House Robber II

---
+++ LeetCodeTests/DynamicProgramming/Medium/HouseRobberII/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.DynamicProgramming.Medium.HouseRobberII;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Medium.HouseRobberII
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Rob_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] {2, 3, 2 };
+
+            // Act
+            var result = solution.Rob(
+                nums);
+
+            // Assert
+            Assert.True(result == 3);
+        }
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Medium/HouseRobberII/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.DynamicProgramming.Medium.HouseRobberII
+{
+    //https://leetcode.com/problems/house-robber-ii/
+    public class Solution
+    {
+        public int Rob(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+            if (nums.Length == 1) return nums[0];
+            if (nums.Length == 2) return System.Math.Max(nums[0], nums[1]);
+
+            var firstProfit = GetProfit(nums, 0, nums.Length - 1);
+            var secondProfit = GetProfit(nums, 1, nums.Length);
+
+            return System.Math.Max(firstProfit, secondProfit);
+        }
+
+        private int GetProfit(int[] nums, int start, int end)
+        {
+            var memorization = new int[end - start];
+
+            memorization[0] = nums[start];
+            memorization[1] = System.Math.Max(nums[start], nums[start + 1]);
+
+            for (int i = start + 2; i < end; i++)
+            {
+                memorization[i - start] = System.Math.Max(memorization[i - start - 1], memorization[i - start - 2] + nums[i]);
+            }
+
+            return memorization[memorization.Length - 1];
+        }
+    }
+}
