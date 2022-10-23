791c9bf52133ad44829c387cc9f83dde4e89f13c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-23 22:53:29 +0300

    add solution of Min Stack

---
+++ LeetCodeTests/Stack/Medium/MinStack/StackValueTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace LeetCodeTests.Stack.Medium.MinStack
+{
+    public class StackValueTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var minStack = new LeetCode.Stack.Medium.MinStack.MinStack();
+            minStack.Push(-2);
+            minStack.Push(0);
+            minStack.Push(-3);
+
+            var min = minStack.GetMin();
+            minStack.Pop();
+            minStack.Top();
+            min = minStack.GetMin();
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Stack/Medium/MinStack/MinStack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Stack.Medium.MinStack
+{
+    //https://leetcode.com/problems/min-stack/
+    public class StackValue
+    {
+        public int Value { get; set; }
+    }
+
+    public class MinStack
+    {
+        private Stack<StackValue> _stack = new();
+        private Stack<StackValue> _min = new();
+
+        public void Push(int val)
+        {
+            var value = new StackValue() { Value = val };
+
+            if(_stack.Count == 0)
+                _min.Push(value);
+            
+            if(_min.Peek().Value > val)
+                _min.Push(value);
+
+            _stack.Push(value);
+        }
+
+        public void Pop()
+        {
+            var value = _stack.Pop();
+
+            if(value == _min.Peek())
+                _min.Pop();
+        }
+
+        public int Top()
+        {
+            return _stack.Peek().Value;
+        }
+
+        public int GetMin()
+        {
+            return _min.Peek().Value;
+        }
+    }
+}
