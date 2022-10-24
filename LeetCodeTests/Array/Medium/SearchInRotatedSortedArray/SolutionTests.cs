4b34309b5b580d16e39af98838013c24c76371e7
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-25 00:40:09 +0300

    add solution of Search in Rotated Sorted Array

---
+++ LeetCodeTests/Array/Medium/SearchInRotatedSortedArray/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Array.Medium.SearchInRotatedSortedArray;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.SearchInRotatedSortedArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Search_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+            int target = 5;
+
+            // Act
+            var result = solution.Search(
+                nums,
+                target);
+
+            // Assert
+            Assert.True(result == 1);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/SearchInRotatedSortedArray/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode.Array.Medium.SearchInRotatedSortedArray
+{
+    //https://leetcode.com/problems/search-in-rotated-sorted-array/
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length == 0) return -1;
+
+            var pivot = FindPivotIndex(nums);
+
+            return FindTarget(nums, target, pivot);
+        }
+
+        private int FindTarget(int[] nums, int target, int pivot)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            if (nums[pivot] <= target && target <= nums[nums.Length - 1])
+                left = pivot;
+            else
+                right = pivot;
+
+            while (left <= right)
+            {
+                var mid = left + (right - left) / 2;
+
+                if (nums[mid] == target) return mid;
+
+                if (nums[mid] < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            return -1;
+        }
+
+        private int FindPivotIndex(int[] nums)
+        {
+            int left = 0, right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] < nums[right])
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+
+            return left;
+        }
+    }
+}
