8c52527a7a488433d629a5ff69b1c378256ed836
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-10-29 15:13:22 +0300

    add solution of Plus One

---
+++ c#_LeetCode/Array/Easy/PlusOne/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Array.Easy.PlusOne
+{
+    //https://leetcode.com/problems/plus-one/
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            bool isOne = true;
+
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                if (isOne)
+                {
+                    digits[i] += 1;
+                    if (digits[i] > 9)
+                    {
+                        digits[i] = 0;
+                        continue;
+                    }
+                    else isOne = false;
+                }
+                else break;
+            }
+
+            digits = isOne ? new int[digits.Length + 1] : digits;
+            digits[0] = isOne ? 1 : digits[0];
+
+            return digits;
+        }
+    }
+}
