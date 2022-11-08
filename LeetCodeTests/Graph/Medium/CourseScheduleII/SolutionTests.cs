c98731ee7d7a36802f4be2118b1effd5d13fc854
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-11-09 00:44:13 +0200

    add solution of Course Schedule II

---
+++ LeetCodeTests/Graph/Medium/CourseScheduleII/SolutionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Graph.Medium.CourseScheduleII;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Graph.Medium.CourseScheduleII
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FindOrder_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int numCourses = 2;
+            int[][] prerequisites = new int[][] { new int[] { 1, 0 }, new int[] { 0, 1 } };
+
+            // Act
+            var result = solution.FindOrder(
+                numCourses,
+                prerequisites);
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Graph/Medium/CourseScheduleII/Solution.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Graph.Medium.CourseScheduleII
+{
+    //https://leetcode.com/problems/course-schedule-ii/description/
+    public class Solution
+    {
+        public int[] FindOrder(int numCourses, int[][] prerequisites)
+        {
+            Dictionary<int, List<int>> graph = new();
+
+            for (int i = 0; i < prerequisites.Length; i++)
+            {
+                if (graph.ContainsKey(prerequisites[i][0]))
+                    graph[prerequisites[i][0]].Add(prerequisites[i][1]);
+                else graph.Add(prerequisites[i][0], new List<int> { prerequisites[i][1] });
+            }
+
+            HashSet<int> visited = new();
+            HashSet<int> @check = new();
+            List<int> topologicalResult = new();
+            for (int i = 0; i < numCourses; i++)
+            {
+                var res = IsCycle(i, graph, visited, @check, topologicalResult);
+                if (res) return new int[0];
+            }
+
+            return topologicalResult.ToArray();
+        }
+
+        private bool IsCycle(int current, Dictionary<int, List<int>> graph,
+            HashSet<int> visited, HashSet<int> @checked, List<int> topologicalRes)
+        {
+            if (@checked.Contains(current)) return true;
+            if (visited.Contains(current)) return false;
+
+            visited.Add(current);
+            @checked.Add(current);
+
+            if(graph.ContainsKey(current))
+            {
+                foreach (var item in graph[current])
+                {
+                    var res = IsCycle(item, graph, visited, @checked, topologicalRes);
+                    if (res) return true;
+                }
+            }
+
+            topologicalRes.Add(current);
+            @checked.Remove(current);
+            return false;
+        }
+    }
+}
