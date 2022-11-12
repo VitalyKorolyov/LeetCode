9737055ce315892fc815b97a14ef8a9bebeb2673
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-11-13 00:50:02 +0200

    add solution of Minimum Path Sum

---
+++ LeetCodeTests/Matrix/Medium/MinimumPathSum/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Matrix.Medium.MinimumPathSum;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Medium.MinimumPathSum
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void MinPathSum_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[][] grid = new int[3][];
+            grid[0] = new int[] { 1, 3, 1 };
+            grid[1] = new int[] { 1, 5, 1 };
+            grid[2] = new int[] { 4, 2, 1 };
+
+            // Act
+            var result = solution.MinPathSum(grid);
+
+            // Assert
+            Assert.True(result == 7);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Medium/MinimumPathSum/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Matrix.Medium.MinimumPathSum
+{
+    //https://leetcode.com/problems/minimum-path-sum/description/
+    public class Solution
+    {
+        public int MinPathSum(int[][] grid)
+        {
+            var matrix = new int[grid.Length][];
+            for (int i = 0; i < grid.Length; i++)
+                matrix[i] = new int[grid[0].Length];
+
+            matrix[0][0] = grid[0][0];
+
+            for (int i = 1; i < grid.Length; i++)
+                matrix[i][0] = matrix[i - 1][0] + grid[i][0];
+
+            for (int i = 1; i < grid[0].Length; i++)
+                matrix[0][i] = matrix[0][i - 1] + grid[0][i];
+
+            for (int i = 1; i < grid.Length; i++)
+                for (int j = 1; j < grid[0].Length; j++)
+                    matrix[i][j] = grid[i][j] + System.Math.Min(matrix[i][j - 1], matrix[i - 1][j]);
+
+            return matrix[matrix.Length - 1][matrix[0].Length - 1];
+        }
+
+
+        //2^(m+n)
+        //private int MinSum(int[][] grid, int i, int j)
+        //{
+        //    if (i == grid.Length || j == grid[0].Length) return int.MaxValue;
+        //    if (i == grid.Length - 1 && j == grid[0].Length - 1) return grid[i][j];
+
+        //    return grid[i][j] + System.Math.Min(MinSum(grid, i + 1, j), MinSum(grid, i, j + 1));
+        //}
+    }
+}
