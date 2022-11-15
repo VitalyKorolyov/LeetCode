e131e92c2fe1bd9aaecbb7eb948a810ec1d81245
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-11-15 23:31:47 +0200

    add solution of Lowest Common Ancestor of a Binary Search Tree

---
+++ c#_LeetCode/BST/Medium/LowestCommonAncestorOfABinarySearchTree/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.BST.Medium.LowestCommonAncestorOfABinarySearchTree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+    public class Solution
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if(root == null) return null;
+
+            return Dfs(root, p, q);
+        }
+
+        private TreeNode Dfs(TreeNode node, TreeNode p, TreeNode q)
+        {
+            if(node == null) return null;
+
+            if (node.val >= p.val && node.val <= q.val)
+                return node;
+            if (node.val <= p.val && node.val >= q.val)
+                return node;
+
+            return node.val > q.val && node.val > p.val 
+                ? Dfs(node.left, p, q) 
+                : Dfs(node.right, p, q);
+        }
+    }
+}
