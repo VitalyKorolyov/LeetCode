aaab8e7ed7e7543c73b26702ab30aa100cf79863
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-11-16 00:43:30 +0200

    add solution of Graph Valid Tree

---
+++ LeetCodeTests/Graph/Medium/GraphValidTree/SolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Graph.Medium.GraphValidTree;
+using System;
+using Xunit;
+
+namespace LeetCodeTests.Graph.Medium.GraphValidTree
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void ValidTree_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int n = 5;
+            int[][] edges = new int[4][];
+            edges[0] = new int[] {0, 1};
+            edges[1] = new int[] { 0, 2 };
+            edges[2] = new int[] { 0, 3 };
+            edges[3] = new int[] { 1, 4 };
+
+            // Act
+            var result = solution.ValidTree(n, edges);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/Graph/Medium/GraphValidTree/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Graph.Medium.GraphValidTree
+{
+    //https://leetcode.com/problems/graph-valid-tree/description/
+    public class Solution
+    {
+        public bool ValidTree(int n, int[][] edges)
+        {
+            Dictionary<int, List<int>> graph = new();
+
+            for (int i = 0; i < edges.Length; i++)
+            {
+                var connection = edges[i];
+
+                if (!graph.ContainsKey(connection[0])) 
+                    graph[connection[0]] = new List<int>();
+                if (!graph.ContainsKey(connection[1])) 
+                    graph[connection[1]] = new List<int>();
+
+                graph[connection[0]].Add(connection[1]);
+                graph[connection[1]].Add(connection[0]);
+            }
+
+            var seen = new HashSet<int>();
+            var isNoCycle = Dfs(graph, seen, 0, -1);
+
+            return isNoCycle && seen.Count == n;
+        }
+
+        private bool Dfs(Dictionary<int, List<int>> graph, HashSet<int> visited, int node, int parent)
+        {
+            if(visited.Contains(node)) return false;
+
+            visited.Add(node);
+
+            if (graph.ContainsKey(node))
+            {
+                foreach (var v in graph[node])
+                {
+                    if(v != parent)
+                    {
+                        var res = Dfs(graph, visited, v, node);
+                        if (res == false) return res;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
