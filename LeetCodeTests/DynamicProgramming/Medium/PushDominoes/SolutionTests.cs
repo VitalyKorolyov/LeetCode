f77cf86cf36eff7ef1d06fc95c804db73feb3d83
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-11-18 22:06:33 +0200

    add solution of Push Dominoes

---
+++ LeetCodeTests/DynamicProgramming/Medium/PushDominoes/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.DynamicProgramming.Medium.PushDominoes;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Medium.PushDominoes
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void PushDominoes_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string dominoes = ".L.R...LR..L..";
+
+            // Act
+            var result = solution.PushDominoes(dominoes);
+
+            // Assert
+            Assert.True(result == "LL.RR.LLRRLL..");
+        }
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Medium/PushDominoes/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace LeetCode.DynamicProgramming.Medium.PushDominoes
+{
+    //https://leetcode.com/problems/push-dominoes/description/
+    public class Solution
+    {
+        public string PushDominoes(string dominoes)
+        {
+            var r = new int[dominoes.Length];
+            var l = new int[dominoes.Length];
+
+            var counter = 0;
+            for (int i = 0; i < dominoes.Length; i++)
+            {
+                if (dominoes[i] == 'R')
+                {
+                    r[i] = 0;
+                    counter = 1;
+                }
+                else 
+                {
+                    if (dominoes[i] == 'L') counter = 0;
+                    else r[i] = counter > 0 ? counter++ : 0;
+                }
+            }
+
+            counter = 0;
+            for (int i = dominoes.Length - 1; i >= 0; i--)
+            {
+                if (dominoes[i] == 'L')
+                {
+                    l[i] = 0;
+                    counter = 1;
+                }
+                else
+                {
+                    if (dominoes[i] == 'R') counter = 0;
+                    else l[i] = counter > 0 ? counter++ : 0;
+                }
+            }
+
+            StringBuilder result = new(dominoes.Length);
+            for (int i = 0; i < dominoes.Length; i++)
+            {
+                if (r[i] == l[i]) result.Append(dominoes[i]);
+                else
+                {
+                    if (r[i] != 0 && l[i] != 0) result.Append(r[i] > l[i] ? 'L' : 'R');
+                    else result.Append(r[i] > l[i] ? 'R' : 'L');
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
