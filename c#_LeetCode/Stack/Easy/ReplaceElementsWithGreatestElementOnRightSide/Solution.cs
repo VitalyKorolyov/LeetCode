526a040d386685c679377f46cab31ab2730288d5
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-11-22 00:34:39 +0200

    add solution of Replace Elements with Greatest Element on Right Side

---
+++ c#_LeetCode/Stack/Easy/ReplaceElementsWithGreatestElementOnRightSide/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Stack.Easy.ReplaceElementsWithGreatestElementOnRightSide
+{
+    //https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/description/
+    public class Solution
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            var max = arr[arr.Length - 1];
+            arr[arr.Length - 1] = -1;
+
+            for (int i = arr.Length - 2; i >= 0; i--)
+            {
+                var current = arr[i];
+                arr[i] = max;
+
+                max = System.Math.Max(current, max);
+            }
+
+            return arr;
+        }
+    }
+}
