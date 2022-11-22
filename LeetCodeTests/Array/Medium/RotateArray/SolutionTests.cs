1935408db03d33844179c9275b2424775e37f833
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-11-23 00:02:29 +0200

    add solution of Rotate Array

---
+++ LeetCodeTests/Array/Medium/RotateArray/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Medium.RotateArray;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.RotateArray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void Rotate_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] {-1, -100, 3, 99};
+            int k = 2;
+
+            // Act
+            solution.Rotate(nums, k);
+
+            // Assert
+            Assert.Equal(new int[] { 3, 99, -1, -100 }, nums);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/RotateArray/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Array.Medium.RotateArray
+{
+    //https://leetcode.com/problems/rotate-array/description/
+    public class Solution
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+
+            Swap(nums, 0, nums.Length - 1);
+            Swap(nums, 0, k - 1);
+            Swap(nums, k, nums.Length - 1);
+        }
+
+        private void Swap(int[] arr, int start, int end)
+        {
+            while (start < end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
