5d8169290798022d8a9ad92233dfdb34eaf4d768
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-12-06 00:05:23 +0200

    add solution for Step-By-Step Directions From a Binary Tree Node to Another

---
+++ LeetCodeTests/Trees/Medium/StepByStepDirectionsFromABinaryTreeNodeToAnother/TreeNodeTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Trees.Medium.StepByStepDirectionsFromABinaryTreeNodeToAnother;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.StepByStepDirectionsFromABinaryTreeNodeToAnother
+{
+    public class TreeNodeTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var treeNode = new TreeNode(2, new TreeNode(1));
+
+            // Act
+            var res = new Solution().GetDirections(treeNode, 2, 1);
+
+            // Assert
+            Assert.Equal("L", res);
+        }
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/StepByStepDirectionsFromABinaryTreeNodeToAnother/Solution.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Trees.Medium.StepByStepDirectionsFromABinaryTreeNodeToAnother
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //https://leetcode.com/problems/step-by-step-directions-from-a-binary-tree-node-to-another/description/
+    public class Solution
+    {
+        public string GetDirections(TreeNode root, int startValue, int destValue)
+        {
+            StringBuilder s = new StringBuilder();
+            StringBuilder d = new StringBuilder();
+
+            Find(root, startValue, s);
+            Find(root, destValue, d);
+
+            int i = 0, min = System.Math.Min(d.Length, s.Length);
+            while (i < min && s[s.Length - i - 1] == d[d.Length - i - 1])
+                i++;
+
+            StringBuilder lR = new();
+            foreach (var item in Enumerable.Repeat("U", s.Length - i))
+                lR.Append(item);
+
+            StringBuilder rR = new();
+            foreach (var item in d.ToString().Reverse())
+                rR.Append(item);
+
+            return lR.ToString() + rR.ToString().Substring(i);
+        }
+
+        private bool Find(TreeNode n, int val, StringBuilder sb)
+        {
+            if (n.val == val) return true;
+
+            if (n.left != null && Find(n.left, val, sb))
+                sb.Append("L");
+            else if (n.right != null && Find(n.right, val, sb))
+                sb.Append("R");
+
+            return sb.Length > 0;
+        }
+    }
+}
