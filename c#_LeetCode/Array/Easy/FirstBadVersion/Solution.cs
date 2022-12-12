e4be6e66965b86e5a5abecf6ee89d1c44b3ad193
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-12-13 00:02:17 +0200

    add solution of First Bad Version

---
+++ c#_LeetCode/Array/Easy/FirstBadVersion/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Array.Easy.FirstBadVersion
+{
+    /* The isBadVersion API is defined in the parent class VersionControl.
+          bool IsBadVersion(int version); */
+
+    public class VersionControl
+    {
+        public bool IsBadVersion(int version)
+        {
+            return true;
+        }
+    }
+
+    //https://leetcode.com/problems/first-bad-version/description/
+    public class Solution : VersionControl
+    {
+        public int FirstBadVersion(int n)
+        {
+            var left = 1;
+            var right = n;
+
+            while(left < right)
+            {
+                var version = left + (right - left)/2;
+                if(IsBadVersion(version))
+                    right = version;
+                else
+                    left = version + 1;
+            }
+
+            return left;
+        }
+    }
+}
