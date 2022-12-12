24815561fe40af3eae6f4d25e75be55712b1f71f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-12-12 23:38:49 +0200

    add solution of Jump Game;

---
+++ LeetCodeTests/Array/Medium/JumpGame/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.JumpGame;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.JumpGame
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void CanJump_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            int[] nums = new int[] { 2, 5, 0, 0 };
+
+            // Act
+            var result = solution.CanJump(nums);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/JumpGame/Solution.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode.Array.Medium.JumpGame
+{
+    //https://leetcode.com/problems/jump-game/description/
+    public class Solution
+    {
+        //O(N^2)
+        public bool CanJump(int[] nums)
+        {
+            var visited = new bool?[nums.Length];
+            visited[nums.Length - 1] = true;
+
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                int furthestJump = System.Math.Min(i + nums[i], nums.Length - 1);
+
+                for (int j = i + 1; j <= furthestJump; j++)
+                {
+                    if (visited[j] == true)
+                    {
+                        visited[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return visited[0] == true;
+        }
+
+        //O(N^2)
+        //private bool CanJimp(int[] nums, int position, bool?[] visited)
+        //{
+        //    if (position == nums.Length - 1) return true;
+
+        //    int furthestJump = System.Math.Min(position + nums[position], nums.Length - 1);
+        //    for (int jump = 1 + position; jump <= furthestJump; jump++)
+        //    {
+        //        if (visited[jump] == false) continue;
+
+        //        if (CanJimp(nums, jump, visited))
+        //        {
+        //            visited[jump] = true;
+        //            return true;
+        //        }
+        //    }
+
+        //    visited[position] = false;
+        //    return visited[position].Value;
+        //}
+    }
+}
