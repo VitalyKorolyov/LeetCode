ffda98589133101a5be003129d18c369ceae73bc
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-12-24 23:59:11 +0200

    add solution of The Number of Full Rounds You Have Played

---
+++ c#_LeetCode/Math/Medium/TheNumberOfFullRoundsYouHavePlayed/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Math.Medium.TheNumberOfFullRoundsYouHavePlayed
+{
+    //https://leetcode.com/problems/the-number-of-full-rounds-you-have-played/description/
+    public class Solution
+    {
+        public int NumberOfRounds(string loginTime, string logoutTime)
+        {
+            (int hoursLogin, int minutesLogin) = GetTime(loginTime);
+            (int hoursLogout, int minutesLogout) = GetTime(logoutTime);
+
+            var start = hoursLogin * 60 + minutesLogin;
+            var finish = hoursLogout * 60 + minutesLogout;
+
+            if (start > finish) finish += 60 * 24;
+
+            return System.Math.Max(0, 
+                (int)System.Math.Floor(finish / 15.00) - (int)System.Math.Ceiling(start / 15.00));
+        }
+
+        private (int, int) GetTime(string time)
+        {
+            var parts = time.Split(':');
+
+            return (int.Parse(parts[0]), int.Parse(parts[1]));
+        }
+    }
+}
