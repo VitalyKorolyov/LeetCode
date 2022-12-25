0bf55718c83a8538cfef98eeec2a4c012a367607
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-12-25 23:08:25 +0200

    add solution of Find Words That Can Be Formed by Characters

---
+++ LeetCodeTests/String/Easy/FindWordsThatCanBeFormedByCharacters/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Easy.FindWordsThatCanBeFormedByCharacters;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.FindWordsThatCanBeFormedByCharacters
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void CountCharacters_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string[] words = new string[] { "cat", "bt", "hat", "tree" };
+            string chars = "atach";
+
+            // Act
+            var result = solution.CountCharacters(words, chars);
+
+            // Assert
+            Assert.True(result == 6);
+        }
+    }
+}
---
+++ c#_LeetCode/String/Easy/FindWordsThatCanBeFormedByCharacters/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.String.Easy.FindWordsThatCanBeFormedByCharacters
+{
+    //https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/description/
+    public class Solution
+    {
+        public int CountCharacters(string[] words, string chars)
+        {
+            int[] charCounter = new int[26];
+            int[] temp = new int[26];
+            int length = 0;
+
+            for (int i = 0; i < chars.Length; i++)
+                charCounter[chars[i] - 'a']++;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < 26; j++)
+                    temp[j] = charCounter[j];
+
+                bool canBeFormed = true;
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    int index = words[i][j] - 'a';
+                    if (temp[index] == 0)
+                    {
+                        canBeFormed = false;
+                        break;
+                    }
+
+                    temp[index]--;
+                }
+
+                if (canBeFormed)
+                    length += words[i].Length;
+            }
+
+            return length;
+        }
+    }
+}
