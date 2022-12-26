c86d32c46f024e00189ce8288bebd369affbac9c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-12-27 00:13:06 +0200

    add solution of Check if Every Row and Column Contains All Numbers

---
+++ LeetCodeTests/Matrix/Easy/CheckifEveryRowAndColumnContainsAllNumbers/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Matrix.Easy.CheckifEveryRowAndColumnContainsAllNumbers;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Easy.CheckifEveryRowAndColumnContainsAllNumbers
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void CheckValid_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange [[1,2,3],[3,1,2],[2,3,1]]
+            var solution = new Solution();
+            int[][] matrix = new int[3][];
+            matrix[0] = new int[3] { 1, 2, 3 };
+            matrix[1] = new int[3] { 3, 1, 2 };
+            matrix[2] = new int[3] { 2, 3, 1 };
+
+            // Act
+            var result = solution.CheckValid(matrix);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
---
+++ c#_LeetCode/Matrix/Easy/CheckifEveryRowAndColumnContainsAllNumbers/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.Matrix.Easy.CheckifEveryRowAndColumnContainsAllNumbers
+{
+    //https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/
+    public class Solution
+    {
+        public bool CheckValid(int[][] matrix)
+        {
+            int n = matrix.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    var index = System.Math.Abs(matrix[i][j]) - 1;
+                    if (matrix[i][index] < 0) return false;
+
+                    matrix[i][index] *= -1; 
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    var index = System.Math.Abs(matrix[j][i]) - 1;
+                    if (matrix[index][i] > 0) return false;
+
+                    matrix[index][i] *= -1;
+                }
+            }
+
+            return true;
+        }
+    }
+}
