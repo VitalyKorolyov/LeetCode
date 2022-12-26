04871f451ea6619810aa201f903a5ca3a81f997b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2022-12-26 21:27:42 +0200

    add solution of Alert Using Same Key-Card Three or More Times in a One Hour Period

---
+++ LeetCodeTests/String/Medium/AlertUsingSameKeyCardThreeOrMoreTimesInAOneHourPeriod/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.String.Medium.AlertUsingSameKeyCardThreeOrMoreTimesInAOneHourPeriod;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.AlertUsingSameKeyCardThreeOrMoreTimesInAOneHourPeriod
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void AlertNames_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            string[] keyName = { "a", "a", "a", "a", "a", "b", "b", "b", "b", "b", "b" };
+            string[] keyTime = { "04:48", "23:53", "06:36", "07:45", "12:16", "00:52", "10:59", "17:16", "00:36", "01:26", "22:42" };
+
+            // Act
+            var result = solution.AlertNames(keyName, keyTime);
+
+            // Assert
+            Assert.True(!result.Any());
+        }
+    }
+}
---
+++ c#_LeetCode/String/Medium/AlertUsingSameKeyCardThreeOrMoreTimesInAOneHourPeriod/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.String.Medium.AlertUsingSameKeyCardThreeOrMoreTimesInAOneHourPeriod
+{
+    //https://leetcode.com/problems/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/
+    public class Solution
+    {
+        public IList<string> AlertNames(string[] keyName, string[] keyTime)
+        {
+            Dictionary<string, SortedList<int, int>> namesToTime = new();
+
+            for (int i = 0; i < keyName.Length; i++)
+            {
+                string name = keyName[i];
+                int minutes = GetMinutes(keyTime[i]);
+
+                namesToTime.TryAdd(name, new SortedList<int, int>());
+                namesToTime[name].Add(minutes, minutes);
+            }
+
+            SortedSet<string> result = new();
+
+            foreach (var nameToTime in namesToTime)
+            {
+                for (var i = 2; i < nameToTime.Value.Count; i++)
+                {
+                    if (nameToTime.Value.Values[i] - nameToTime.Value.Values[i - 2] <= 60)
+                    {
+                        result.Add(nameToTime.Key);
+                        break;
+                    }
+                }
+            }
+
+            return result.ToList();
+        }
+
+        private int GetMinutes(string time)
+        {
+            var parts = time.Split(':');
+            return int.Parse(parts[0]) * 60 + int.Parse(parts[1]);
+        }
+    }
+}
