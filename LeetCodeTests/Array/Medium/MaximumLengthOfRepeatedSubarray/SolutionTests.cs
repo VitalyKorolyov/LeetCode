0d99bd27cff1132c3e4ba07c3f154b8aa0e77a28
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-01-01 22:31:50 +0200

    add solution of Maximum Length of Repeated Subarray

---
+++ LeetCodeTests/Array/Medium/MaximumLengthOfRepeatedSubarray/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Medium.MaximumLengthOfRepeatedSubarray;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.MaximumLengthOfRepeatedSubarray
+{
+    public class SolutionTests
+    {
+        [Fact]
+        public void FindLength_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var solution = new Solution();
+            var nums1 = new[] { 1, 2, 3, 2, 1 };
+            var nums2 = new[] { 3, 2, 1, 4, 7 };
+
+            // Act
+            var result = solution.FindLength(nums1, nums2);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
---
+++ c#_LeetCode/Array/Medium/MaximumLengthOfRepeatedSubarray/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCode.Array.Medium.MaximumLengthOfRepeatedSubarray
+{
+    //https://leetcode.com/problems/maximum-length-of-repeated-subarray/description/
+    public class Solution
+    {
+        //bottom-up dynamic programming time complexity: O(mn)
+        public int FindLength(int[] nums1, int[] nums2)
+        {
+            int result = 0;
+
+            int[,] memo = new int[nums1.Length + 1, nums2.Length + 1];
+            for (int i = nums1.Length - 1; i >= 0; --i)
+            {
+                for (int j = nums2.Length - 1; j >= 0; --j)
+                {
+                    if (nums1[i] == nums2[j])
+                    {
+                        memo[i, j] = memo[i + 1, j + 1] + 1;
+                        result = System.Math.Max(result, memo[i, j]);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        //Sliding window time complexity: O(mn)
+        //public int FindLength(int[] nums1, int[] nums2)
+        //{
+        //    int result = 0;
+
+        //    for (int i = 0; i < nums1.Length + nums2.Length - 1; i++)
+        //    {
+        //        int startA = System.Math.Max(0, nums1.Length - i - 1);
+        //        int startB = System.Math.Max(0,  i - nums1.Length - 1);
+
+        //        var currentLength = 0;
+        //        for(; startA < nums1.Length && startB < nums2.Length; startA++, startB++)
+        //        {
+        //            currentLength = nums1[startA] == nums2[startB] ? currentLength + 1 : 0;
+        //            result = System.Math.Max(result, currentLength);
+        //        }
+        //    }
+
+        //    return result;
+        //}
+    }
+}
