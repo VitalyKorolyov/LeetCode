8aa6c9ad0423f37de8686f295243b9872ccbefda
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-01-11 00:23:08 +0200

    add solution of Subsets II

---
+++ c#_LeetCode/Array/Medium/SubsetsII/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.SubsetsII
+{
+    //https://leetcode.com/problems/subsets-ii/description/
+    public class Solution
+    {
+        public IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            List<IList<int>> result = new();
+
+            System.Array.Sort(nums);
+            BuildSubSets(result, new List<int>(), nums, 0);
+
+            return result;
+        }
+
+        private void BuildSubSets(IList<IList<int>> res, IList<int> temp, int[] nums, int start)
+        {
+            res.Add(new List<int>(temp));
+
+            for (int i = start; i < nums.Length; i++)
+            {
+                if (i > start && nums[i] == nums[i - 1]) continue;
+
+                temp.Add(nums[i]);
+                BuildSubSets(res, temp, nums, i + 1);
+                temp.RemoveAt(temp.Count - 1);
+            }
+        }
+    }
+}
