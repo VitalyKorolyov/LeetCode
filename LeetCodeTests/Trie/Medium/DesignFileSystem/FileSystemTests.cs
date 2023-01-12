dd72731d102c95256bb0bb9e0580a9df52be625d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-01-13 00:24:03 +0200

    add solution of Design File System

---
+++ LeetCodeTests/Trie/Medium/DesignFileSystem/FileSystemTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Trie.Medium.DesignFileSystem;
+using Xunit;
+
+namespace LeetCodeTests.Trie.Medium.DesignFileSystem
+{
+    public class FileSystemTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var fileSystem = new FileSystem();
+
+            // Act
+            var res = fileSystem.CreatePath("/c/d", 1);
+            var getRes = fileSystem.Get("/a/b");
+
+            getRes = fileSystem.Get("/a");
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
---
+++ c#_LeetCode/Trie/Medium/DesignFileSystem/FileSystem.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Trie.Medium.DesignFileSystem;
+
+//https://leetcode.com/problems/design-file-system/description/
+
+public class Node
+{
+    public int? Value { get; set; }
+    public Dictionary<string, Node> Nodes { get; set; } = new();
+}
+
+public class Trie
+{
+    public Node Head { get; set; } = new();
+}
+
+public class FileSystem
+{
+    private readonly Trie Trie;
+
+    public FileSystem()
+    {
+        Trie = new Trie();
+    }
+
+    public bool CreatePath(string path, int value)
+    {
+        var paths = path.Split('/').Skip(1).ToArray();
+
+        var node = Trie.Head;
+        for (int i = 0; i < paths.Length && node != null; i++)
+        {
+            if(i == paths.Length - 1)
+            {
+                if (!node.Nodes.ContainsKey(paths[i]))
+                {
+                    Node newNode = new();
+
+                    node.Nodes.Add(paths[i], newNode);
+                    newNode.Value = value;
+
+                    return true;
+                }
+
+                return false;
+            }
+            else
+            {
+                if (node.Nodes.ContainsKey(paths[i]))
+                    node = node.Nodes[paths[i]];
+                else
+                    return false;
+            }
+        }
+
+        return false;
+    }
+
+    public int Get(string path)
+    {
+        var paths = path.Split('/').Skip(1).ToArray();
+
+        var node = Trie.Head;
+        for (int i = 0; i < paths.Length && node != null; i++)
+        {
+            if (node.Nodes.ContainsKey(paths[i]))
+            {
+                if (i == paths.Length - 1)
+                {
+                    var nodeToGet = node.Nodes[paths[i]];
+                    return nodeToGet.Value ?? -1;
+                }
+                else if (node.Nodes.ContainsKey(paths[i]))
+                    node = node.Nodes[paths[i]];
+            }
+            else
+                return -1;
+        }
+
+        return -1;
+    }
+}
