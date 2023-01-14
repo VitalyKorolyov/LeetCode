05b8e52316563c47088869ab27815b1f76cf073a
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-01-14 19:30:54 +0200

    add solution of Last Stone Weight

---
+++ LeetCodeTests/PriorityQueue/Easy/LastStoneWeight/SolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.PriorityQueue.Easy.LastStoneWeight;
+using Xunit;
+
+namespace LeetCodeTests.PriorityQueue.Easy.LastStoneWeight;
+public class SolutionTests
+{
+    [Fact]
+    public void LastStoneWeight_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] stones = new int[] { 7, 6, 7, 6, 9 };
+
+        // Act
+        var result = solution.LastStoneWeight(stones);
+
+        // Assert
+        Assert.Equal(3, result);
+    }
+}
---
+++ c#_LeetCode/PriorityQueue/Easy/LastStoneWeight/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.PriorityQueue.Easy.LastStoneWeight;
+public class Solution
+{
+    //https://leetcode.com/problems/last-stone-weight/description/
+
+    public int LastStoneWeight(int[] stones)
+    {
+        PriorityQueue<int, int> queue = new(Comparer<int>.Create((x, y) => y.CompareTo(x)));
+
+        for (int i = 0; i < stones.Length; i++)
+            queue.Enqueue(stones[i], stones[i]);
+
+        int weight = queue.Dequeue();
+        while(queue.Count > 0)
+        {
+            var secondWeight = queue.Dequeue();
+            weight = System.Math.Abs(weight - secondWeight);
+
+            queue.Enqueue(weight, weight);
+            weight = queue.Dequeue();
+        }
+
+        return weight;
+    }
+}
