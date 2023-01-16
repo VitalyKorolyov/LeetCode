6be4c2d04427be8f2db348e3a6ce329c2642c030
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-01-16 21:24:25 +0200

    add solution of My Calendar I

---
+++ LeetCodeTests/Design/Medium/MyCalendarI/MyCalendarTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Design.Medium.MyCalendarI;
+using Xunit;
+
+namespace LeetCodeTests.Design.Medium.MyCalendarI;
+
+public class MyCalendarTests
+{
+    [Fact]
+    public void Book_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var myCalendar = new MyCalendar();
+
+        // Act
+        var result = myCalendar.Book(47, 50);
+        Assert.True(result);
+
+        result = myCalendar.Book(33, 41);
+        Assert.True(result);
+
+        result = myCalendar.Book(39, 45);
+        Assert.False(result);
+
+        result = myCalendar.Book(33, 42);
+        Assert.False(result);
+    }
+}
---
+++ c#_LeetCode/Design/Medium/MyCalendarI/MyCalendar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Design.Medium.MyCalendarI;
+
+//https://leetcode.com/problems/my-calendar-i/description/
+
+public class MyCalendar
+{
+    private readonly List<Tuple<int, int>> events = new();
+    private readonly IComparer<Tuple<int, int>> comparer = 
+        Comparer<Tuple<int, int>>.Create((x, y) => x.Item1.CompareTo(y.Item1));
+
+    public bool Book(int start, int end)
+    {
+        int result = BinarySearch(start, end, events);
+        if(result != -1) return false;
+
+        events.Add(Tuple.Create(start, end));
+        events.Sort(comparer);
+
+        return true;
+    }
+
+    private int BinarySearch(int startNew, int endNew, List<Tuple<int, int>> events)
+    {
+        int left = 0;
+        int right = events.Count - 1;
+
+        while (left <= right)
+        {
+            int pivot = left + (right - left) / 2;
+            var (sEvent, eEvent) = events[pivot];
+
+            if ((startNew >= sEvent && startNew < eEvent) || (startNew < sEvent && endNew > sEvent))
+                return pivot;
+
+            if (sEvent > startNew)
+                right = pivot - 1;
+            else
+                left = pivot + 1;
+        }
+
+        return -1;
+    }
+}
