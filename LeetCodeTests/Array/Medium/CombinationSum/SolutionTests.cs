cf7a49a1484947e986e1141e0127c4d5e18ed766
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-01-19 22:59:02 +0200

    add solution of Combination Sum

---
+++ LeetCodeTests/Array/Medium/CombinationSum/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.CombinationSum;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.CombinationSum;
+
+public class SolutionTests
+{
+    [Fact]
+    public void CombinationSum_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] candidates = new int[] { 2, 3, 6, 7 };
+        int target = 7;
+
+        // Act
+        var result = solution.CombinationSum(candidates, target);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/CombinationSum/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.CombinationSum;
+
+//https://leetcode.com/problems/combination-sum/description/
+public class Solution
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        List<IList<int>> result = new();
+
+        GenerateCombSum(result, new List<int>(), 0, target, candidates, 0);
+
+        return result;
+    }
+
+    private void GenerateCombSum(IList<IList<int>> result, IList<int> temp,
+        int currentSum, int targetSum, int[] candidates, int start)
+    {
+        temp = new List<int>(temp);
+
+        if (currentSum > targetSum) return;
+        if (currentSum == targetSum)
+        {
+            result.Add(temp);
+            return;
+        }
+
+        for (int i = start; i < candidates.Length; i++)
+        {
+            temp.Add(candidates[i]);
+            GenerateCombSum(result, temp, currentSum + candidates[i], targetSum, candidates, i);
+            temp.RemoveAt(temp.Count - 1);
+        }
+    }
+}
