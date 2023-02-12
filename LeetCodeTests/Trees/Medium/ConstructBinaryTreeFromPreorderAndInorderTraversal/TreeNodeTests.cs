8df4988e8a0e81d1c7d9db984c84d69e7167db93
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-02-12 16:40:07 +0200

    add solution of Construct Binary Tree from Preorder and Inorder Traversal

---
+++ LeetCodeTests/Trees/Medium/ConstructBinaryTreeFromPreorderAndInorderTraversal/TreeNodeTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Trees.Medium.ConstructBinaryTreeFromPreorderAndInorderTraversal;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.ConstructBinaryTreeFromPreorderAndInorderTraversal;
+public class TreeNodeTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] preOrder = new int[] { 3, 9, 20, 15, 7 };
+        int[] inOrder = new int[] { 9, 3, 15, 20, 7 };
+
+        // Act
+        var result = solution.BuildTree(preOrder, inOrder);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/ConstructBinaryTreeFromPreorderAndInorderTraversal/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees.Medium.ConstructBinaryTreeFromPreorderAndInorderTraversal;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+//https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+public class Solution
+{
+    public TreeNode BuildTree(int[] preorder, int[] inorder)
+    {
+        Dictionary<int, int> inorderValueToIndex = new(inorder.Length);
+
+        for (int i = 0; i < inorder.Length; i++)
+            inorderValueToIndex.Add(inorder[i], i);
+
+        return Helper(0, 0, inorder.Length - 1, preorder, inorderValueToIndex);
+    }
+
+    public TreeNode Helper(int preStart, int inStart, int inEnd, int[] preorder, 
+        Dictionary<int, int> inorderValueToIndex)
+    {
+        if (preStart > preorder.Length - 1 || inStart > inEnd) return null;
+
+        TreeNode root = new(preorder[preStart]);
+
+        int inIndex = inorderValueToIndex[root.val];
+
+        root.left = Helper(preStart + 1, inStart, inIndex - 1, preorder, inorderValueToIndex);
+        root.right = Helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorderValueToIndex);
+
+        return root;
+    }
+}
