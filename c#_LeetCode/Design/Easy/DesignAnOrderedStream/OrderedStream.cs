8cbeadf2a5635fd297f0c0ef58fe90fd683930a1
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-02-16 00:16:55 +0200

    add solution of Design an Ordered Stream

---
+++ c#_LeetCode/Design/Easy/DesignAnOrderedStream/OrderedStream.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Design.Easy.DesignAnOrderedStream;
+
+//https://leetcode.com/problems/design-an-ordered-stream/description/
+public class OrderedStream
+{
+    private readonly string[] stream;
+    private int pointer = 0;
+
+    public OrderedStream(int n)
+    {
+        stream = new string[n];
+    }
+
+    public IList<string> Insert(int idKey, string value)
+    {
+        List<string> res = new();
+
+        stream[idKey - 1] = value;
+
+        while (pointer < stream.Length && stream[pointer] != null)
+        {
+            res.Add(stream[pointer]);
+            pointer++;
+        }
+
+        return res;
+    }
+}
