fe0594dab8bdcbfa9018b15b969cf95d1fada0dc
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-02-15 23:48:27 +0200

    add solution of  Design Underground System

---
+++ LeetCodeTests/Design/Medium/DesignUndergroundSystem/TravelInfoTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Design.Medium.DesignUndergroundSystem;
+using Xunit;
+
+namespace LeetCodeTests.Design.Medium.DesignUndergroundSystem;
+public class TravelInfoTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var system = new UndergroundSystem();
+
+        // Act
+        system.CheckIn(10, "L", 3);
+        system.CheckOut(10, "P", 8);
+        var res = system.GetAverageTime("L", "P");
+
+        system.CheckIn(5, "L", 10);
+        system.CheckIn(5, "P", 16);
+
+        res = system.GetAverageTime("L", "P");
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Design/Medium/DesignUndergroundSystem/UndergroundSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Design.Medium.DesignUndergroundSystem;
+
+//https://leetcode.com/problems/design-underground-system/description/
+
+public class TravelInfo
+{
+    public string StationName { get; set; }
+    public int Time { get; set; }
+}
+
+public class AverageTime
+{
+    public double TotalTime { get; set; }
+    public double TotalTravels { get; set; }
+}
+
+public class UndergroundSystem
+{
+    private readonly Dictionary<int, TravelInfo> checkIn = new();
+    private readonly Dictionary<string, AverageTime> pathToTime = new();
+
+    public void CheckIn(int id, string stationName, int t)
+    {
+        checkIn.Add(id, new TravelInfo { StationName = stationName,Time = t });
+    }
+
+    public void CheckOut(int id, string stationName, int t)
+    {
+        TravelInfo currentTravel = checkIn[id];
+        
+        string key = $"{currentTravel.StationName}-{stationName}";
+        int takeTime = t - currentTravel.Time;
+
+        if (pathToTime.ContainsKey(key))
+        {
+            AverageTime averageTime = pathToTime[key];
+            averageTime.TotalTime += takeTime;
+            averageTime.TotalTravels++;
+        }
+        else
+        {
+            pathToTime.Add(key, new AverageTime { TotalTravels = 1, TotalTime = takeTime });
+        }
+
+        checkIn.Remove(id);
+    }
+
+    public double GetAverageTime(string startStation, string endStation)
+    {
+        string key = $"{startStation}-{endStation}";
+        return pathToTime[key].TotalTime / pathToTime[key].TotalTravels;
+    }
+}
