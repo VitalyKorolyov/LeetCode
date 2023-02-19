54dd999c3ea310e04ed6ab8167829ee9dd09edb5
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-02-19 23:07:27 +0200

    add solution of Design A Leaderboard

---
+++ c#_LeetCode/Design/Medium/DesignALeaderboard/Leaderboard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Design.Medium.DesignALeaderboard;
+
+//https://leetcode.com/problems/design-a-leaderboard/description/
+public class Leaderboard
+{
+    private readonly Dictionary<int, int> dict = new();
+
+    public void AddScore(int playerId, int score)
+    {
+        if (!dict.ContainsKey(playerId)) dict.Add(playerId, score);
+        else dict[playerId] += score;
+    }
+
+    public int Top(int K)
+    {
+        return dict.Values.OrderByDescending(x => x).Take(K).Sum();
+    }
+
+    public void Reset(int playerId)
+    {
+        dict[playerId] = 0;
+    }
+}
