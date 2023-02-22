4b71c8ebc43980d74977876c769171c6c2091aaa
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-02-22 22:44:41 +0200

    add solution of Minimum Knight Moves;

---
+++ LeetCodeTests/Matrix/Medium/MinimumKnightMoves/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Matrix.Medium.MinimumKnightMoves;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Medium.MinimumKnightMoves;
+
+public class SolutionTests
+{
+    [Fact]
+    public void MinKnightMoves_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int x = 2;
+        int y = 112;
+
+        // Act
+        var result = solution.MinKnightMoves(x, y);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Matrix/Medium/MinimumKnightMoves/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Matrix.Medium.MinimumKnightMoves;
+
+//https://leetcode.com/problems/minimum-knight-moves/description/
+public class Solution
+{
+    private static readonly int[][] directions = new[]
+    {
+        new int[] { 2,1 },
+        new int[] { 2,-1 },
+        new int[] { 1,2 },
+        new int[] { -1,2 },
+        new int[] { -2,1 },
+        new int[] { -2,-1 },
+        new int[] { 1,-2 },
+        new int[] { -1,-2 }
+    };
+
+    public int MinKnightMoves(int x, int y)
+    {
+        x = System.Math.Abs(x);
+        y = System.Math.Abs(y);
+
+        Queue<(int, int)> queue = new();
+        queue.Enqueue((0, 0));
+        int step = -1;
+        Dictionary<int, HashSet<int>> visited = new();
+
+        while (queue.Count > 0)
+        {
+            var size = queue.Count;
+            step++;
+
+            for (int i = 0; i < size; i++)
+            {
+                var (currentX, currentY) = queue.Dequeue();
+
+                if (currentX == x && currentY == y) return step;
+
+                for (int k = 0; k < directions.Length; k++)
+                {
+                    var newX = directions[k][0] + currentX;
+                    var newY = directions[k][1] + currentY;
+
+                    if (!IsVisited(newX, newY, visited) && newX >= -1 && newY >= -1)
+                    {
+                        MarkVisited(newX, newY, visited);
+                        queue.Enqueue((newX, newY));
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private void MarkVisited(int x, int y, Dictionary<int, HashSet<int>> visited)
+    {
+        if(!visited.ContainsKey(x)) visited.Add(x, new HashSet<int> { y });
+        else visited[x].Add(y);
+    }
+
+    private bool IsVisited(int x, int y, Dictionary<int, HashSet<int>> visited)
+    {
+        if (visited.ContainsKey(x) && visited[x].Contains(y)) return true;
+
+        return false;
+    }
+}
