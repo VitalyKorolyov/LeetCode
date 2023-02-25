99a3b0d5afc0445a5f3c93a042a47853a985e59f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-02-25 20:08:47 +0200

    add solution of string rotating

---
+++ LeetCodeTests/String/Easy/RotateString/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.String.Easy.RotateString;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.RotateString;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Rotate_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string str = "GeeksforGeeks";
+        int leftShift = 4;
+        int rightShift = 3;
+
+        // Act
+        var result = solution.Rotate(str, leftShift, rightShift);
+
+        // Assert
+        Assert.True(result == "eeksforGeeksG");
+    }
+}
---
+++ c#_LeetCode/String/Easy/RotateString/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.String.Easy.RotateString;
+
+public class Solution
+{
+    public string Rotate(string str, int leftShift, int rightShift)
+    {
+        if (leftShift == rightShift) return str;
+
+        leftShift = leftShift % str.Length;
+        rightShift = rightShift % str.Length;
+
+        if (rightShift > leftShift)
+        {
+            rightShift = rightShift - leftShift;
+            return LeftRotate(str, str.Length - rightShift);
+        }
+        else
+        {
+            leftShift = leftShift - rightShift;
+            return LeftRotate(str, leftShift);
+        }
+    }
+
+    static string LeftRotate(string str, int leftShift)
+    {
+        return str.Substring(leftShift, str.Length - leftShift) + str.Substring(0, leftShift);
+    }
+}
