7289e5d8f27cc1c81393250fa091152817d4c5ac
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-02-26 15:23:21 +0200

    add solution of Two Out of Three

---
+++ LeetCodeTests/Array/Easy/TwoOutOfThree/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Array.Easy.TwoOutOfThree;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.TwoOutOfThree;
+
+public class SolutionTests
+{
+    [Fact]
+    public void TwoOutOfThree_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] nums1 = new int[] { 1, 1, 3, 2 };
+        int[] nums2 = new int[] { 2, 3 };
+        int[] nums3 = new int[] { 3 };
+
+        // Act
+        var result = solution.TwoOutOfThree(nums1, nums2, nums3);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Array/Easy/TwoOutOfThree/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Easy.TwoOutOfThree;
+
+public class Solution
+{
+    public IList<int> TwoOutOfThree(int[] nums1, int[] nums2, int[] nums3)
+    {
+        HashSet<int> allNumbers = new();
+
+        for (int i = 0; i < nums1.Length; i++)
+            allNumbers.Add(nums1[i]);
+
+        for (int i = 0; i < nums2.Length; i++)
+            allNumbers.Add(nums2[i]);
+
+        for (int i = 0; i < nums3.Length; i++)
+            allNumbers.Add(nums3[i]);
+
+        HashSet<int> set1 = new (nums1);
+        HashSet<int> set2 = new (nums2);
+        HashSet<int> set3 = new (nums3);
+
+        List<int> result = new();
+        foreach (var num in allNumbers)
+        {
+            if(set1.Contains(num) && set2.Contains(num) || set1.Contains(num) && set3.Contains(num) ||
+                set2.Contains(num) && set3.Contains(num))
+                result.Add(num);
+        }
+
+        return result;
+    }
+}
