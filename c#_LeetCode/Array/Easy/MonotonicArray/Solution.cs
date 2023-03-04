c90aba1ff083924752153ac1c69578d324f45afc
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-04 21:52:32 +0200

    add solution of Monotonic Array

---
+++ c#_LeetCode/Array/Easy/MonotonicArray/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Array.Easy.MonotonicArray;
+
+//https://leetcode.com/problems/monotonic-array/description/
+public class Solution
+{
+    public bool IsMonotonic(int[] nums)
+    {
+        bool isInc = true, isDec = true;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            isInc = nums[i] >= nums[i - 1] && isInc;
+            isDec = nums[i] <= nums[i - 1] && isDec;
+        }
+
+        return isInc || isDec;
+    }
+}
