e28a250968f8998626e0dc619572a13e21d7f15f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-05 23:09:52 +0200

    add solution of Closest Leaf in a Binary Tree

---
+++ LeetCodeTests/Trees/Medium/ClosestLeafInABinaryTree/TreeNodeTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Trees.Medium.ClosestLeafInABinaryTree;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.ClosestLeafInABinaryTree;
+
+public class TreeNodeTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var treeNode = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+
+        // Act
+        var result = new Solution().FindClosestLeaf(treeNode, 5);
+
+        // Assert
+        Assert.True(result == 5);
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/ClosestLeafInABinaryTree/Solution.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees.Medium.ClosestLeafInABinaryTree;
+
+//https://leetcode.com/problems/closest-leaf-in-a-binary-tree/description/
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public int FindClosestLeaf(TreeNode root, int k)
+    {
+        Dictionary<TreeNode, List<TreeNode>> graph = new();
+        Dfs(root, graph);
+
+        HashSet<TreeNode> visited = new();
+        Queue<TreeNode> queue = new();
+
+        foreach (TreeNode node in graph.Keys)
+        {
+            if (node != null && node.val == k)
+            {
+                queue.Enqueue(node);
+                visited.Add(node);
+            }
+        }
+
+        while (queue.Count > 0)
+        {
+            int size = queue.Count;
+            var val = queue.Dequeue();
+            if (val.left == null && val.right == null) return val.val;
+
+            for (int i = 0; i < size; i++)
+            {
+                var list = graph[val];
+
+                foreach (var child in list)
+                {
+                    if(child.right == null && child.left == null) return child.val;
+
+                    if (visited.Contains(child)) continue;
+                    queue.Enqueue(child);
+                    visited.Add(child);
+                }
+            }
+        }
+
+        return 1;
+    }
+
+    private void Dfs(TreeNode node, Dictionary<TreeNode, List<TreeNode>> graph)
+    {
+        if(node == null) return;
+
+        if(!graph.ContainsKey(node))
+            graph.Add(node, new List<TreeNode>());
+
+        if (node.left != null)
+        {
+            if (!graph.ContainsKey(node.left))
+                graph.Add(node.left, new List<TreeNode>());
+
+            graph[node.left].Add(node);
+            graph[node].Add(node.left);
+        }
+        if (node.right != null)
+        {
+            if (!graph.ContainsKey(node.right))
+                graph.Add(node.right, new List<TreeNode>());
+
+            graph[node.right].Add(node);
+            graph[node].Add(node.right);
+        }
+
+        Dfs(node.left, graph);
+        Dfs(node.right, graph);
+    }
+}
