3a5befbebfc61ca6c95290d73995c124dd5d324a
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-08 23:13:22 +0200

    add solution of Find Duplicate Subtrees

---
+++ LeetCodeTests/Trees/Medium/FindDuplicateSubtrees/TreeNodeTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Trees.Medium.FindDuplicateSubtrees;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.FindDuplicateSubtrees;
+public class TreeNodeTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var treeNode = new TreeNode(1, new TreeNode(2, 
+            new TreeNode(4)), new TreeNode(3, new TreeNode(2, new TreeNode(4)), new TreeNode(4)));
+
+        // Act
+        var solution = new Solution();
+        solution.FindDuplicateSubtrees(treeNode);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/FindDuplicateSubtrees/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees.Medium.FindDuplicateSubtrees;
+
+//https://leetcode.com/problems/find-duplicate-subtrees/description/
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public IList<TreeNode> FindDuplicateSubtrees(TreeNode root)
+    {
+        List<TreeNode> res = new();
+        Dfs(root, new Dictionary<string, int>(), res);
+
+        return res;
+    }
+
+    public string Dfs(TreeNode node, Dictionary<string, int> hashMap, List<TreeNode> res)
+    {
+        if (node == null) return string.Empty;
+
+        var subTree = $"({Dfs(node.left, hashMap, res)}){node.val}({Dfs(node.right, hashMap, res)})";
+
+        hashMap[subTree] = hashMap.GetValueOrDefault(subTree, 0) + 1;
+
+        if (hashMap[subTree] == 2)
+            res.Add(node);
+
+        return subTree;
+    }
+}
