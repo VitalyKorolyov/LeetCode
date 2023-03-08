bd43c2a12edfcad844c366d1c8cc3b4bdffd814d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-09 00:48:36 +0200

    add solution of Bus Routes

---
+++ LeetCodeTests/Graph/Hard/BusRoutes/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Graph.Hard.BusRoutes;
+using Xunit;
+
+namespace LeetCodeTests.Graph.Hard.BusRoutes;
+
+public class SolutionTests
+{
+    [Fact]
+    public void NumBusesToDestination_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[][] routes = new int[][] { new int[] { 1, 2, 7 }, new int[] { 3, 6, 7 } };
+        int source = 1;
+        int target = 6;
+
+        // Act
+        var result = solution.NumBusesToDestination(routes, source, target);
+
+        // Assert
+        Assert.True(result == 2);
+    }
+}
---
+++ c#_LeetCode/Graph/Hard/BusRoutes/Solution.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Graph.Hard.BusRoutes;
+
+//https://leetcode.com/problems/bus-routes/description/
+public class Solution
+{
+    public int NumBusesToDestination(int[][] routes, int source, int target)
+    {
+        int numBuses = 0;
+        if (source == target) return numBuses;
+
+        Dictionary<int, List<int>> graph = new();
+
+        for (int bus = 0; bus < routes.Length; bus++)
+            for (int j = 0; j < routes[bus].Length; j++)
+            {
+                var route = routes[bus][j];
+
+                if (!graph.ContainsKey(route))
+                    graph[route] = new List<int>();
+
+                graph[route].Add(bus);
+            }
+
+        Queue<int> queue = new();
+        queue.Enqueue(source);
+        HashSet<int> visited = new();
+
+        while (queue.Count > 0)
+        {
+            var size = queue.Count;
+            numBuses++;
+
+            for (int i = 0; i < size; i++)
+            {
+                var route = queue.Dequeue();
+
+                foreach (int bus in graph[route].Where(bus => !visited.Contains(bus)))
+                {
+                    visited.Add(bus);
+                    for (int j = 0; j < routes[bus].Length; j++)
+                    {
+                        if (routes[bus][j] == target) return numBuses;
+                        queue.Enqueue(routes[bus][j]);
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
