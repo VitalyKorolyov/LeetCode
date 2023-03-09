b7bd94935da716f6d82db1ee91b565b43b62d6e1
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-09 22:46:39 +0200

    add solution of Shortest Word Distance III

---
+++ LeetCodeTests/Array/Medium/ShortestWordDistanceIII/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Array.Medium.ShortestWordDistanceIII;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.ShortestWordDistanceIII;
+public class SolutionTests
+{
+    [Fact]
+    public void ShortestWordDistance_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string[] wordsDict = new string[] { "practice", "makes", "perfect", "coding", "makes" };
+        string word1 = "makes";
+        string word2 = "coding";
+
+        // Act
+        var result = solution.ShortestWordDistance(
+            wordsDict,
+            word1,
+            word2);
+
+        // Assert
+        Assert.True(result == 1);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/ShortestWordDistanceIII/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Array.Medium.ShortestWordDistanceIII;
+
+//https://leetcode.com/problems/shortest-word-distance-iii/description/
+public class Solution
+{
+    public int ShortestWordDistance(string[] wordsDict, string word1, string word2)
+    {
+        int prevIndex = -1;
+        int length = int.MaxValue;
+        var isEqual = word1 == word2;
+
+        for (int i = 0; i < wordsDict.Length; i++)
+        {
+            var key = wordsDict[i];
+            if (key != word1 && key != word2) continue;
+
+            if (prevIndex != -1 && (wordsDict[prevIndex] != wordsDict[i] || isEqual))
+                length = System.Math.Min(length, System.Math.Abs(i - prevIndex));
+
+            prevIndex = i;
+        }
+
+        return length;
+    }
+}
