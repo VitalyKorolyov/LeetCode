30ae9035979a5a6cab45e461b5c25d6f1b3b31ab
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-10 22:14:01 +0200

    add solution of Serialize and Deserialize N-ary Tree;

---
+++ LeetCodeTests/Trees/Hard/SerializeAndDeserializeNAryTree/NodeTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Trees.Hard.SerializeAndDeserializeNAryTree;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Hard.SerializeAndDeserializeNAryTree;
+
+public class NodeTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var node = new Node(1 , new List<Node> { new Node(3, new List<Node> { new Node(5), new Node(6)}), 
+            new Node(2), new Node(4)});
+
+        // Act
+        var res = new Codec().serialize(node);
+
+        // Assert
+        Assert.True(res == "1[3] 3[2] 5 6 2 4");
+
+        var tree = new Codec().deserialize(res);
+    }
+}
---
+++ c#_LeetCode/Trees/Hard/SerializeAndDeserializeNAryTree/Codec.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Trees.Hard.SerializeAndDeserializeNAryTree;
+
+//https://leetcode.com/problems/serialize-and-deserialize-n-ary-tree/description/
+public class Node
+{
+    public int val;
+    public IList<Node> children;
+
+    public Node() { }
+
+    public Node(int _val)
+    {
+        val = _val;
+    }
+
+    public Node(int _val, IList<Node> _children)
+    {
+        val = _val;
+        children = _children;
+    }
+}
+
+public class Codec
+{
+    public string serialize(Node root)
+    {
+        if (root == null) return string.Empty;
+
+        if (root.children == null || !root.children.Any()) 
+            return $"{root.val}";
+
+        StringBuilder res = new($"{root.val}[{root.children.Count}] ");
+        foreach (var child in root.children)
+            res.Append(serialize(child) + " ");
+
+        res.Remove(res.Length - 1, 1);
+
+        return res.ToString();
+    }
+
+    public Node deserialize(string data)
+    {
+        if(data == null || string.IsNullOrEmpty(data)) return null;
+
+        return Deserialize(new Queue<char>(data));
+    }
+
+    private Node Deserialize(Queue<char> queue)
+    {
+        while(queue.Peek() == ' ') queue.Dequeue();
+
+        var value = GetInt(queue);
+        Node node = new(value, new List<Node>());
+
+        if(queue.Count > 0 && queue.Peek() == '[')
+        {
+            queue.Dequeue();
+            var size = GetInt(queue);
+            queue.Dequeue();
+
+            for (int i = 0; i < size; i++)
+                node.children.Add(Deserialize(queue));
+        }
+
+        return node;
+    }
+
+    private int GetInt(Queue<char> queue)
+    {
+        StringBuilder res = new();
+
+        while (queue.Count > 0 && char.IsDigit(queue.Peek()))
+            res.Append(queue.Dequeue());
+
+        return int.Parse(res.ToString());
+    }
+}
