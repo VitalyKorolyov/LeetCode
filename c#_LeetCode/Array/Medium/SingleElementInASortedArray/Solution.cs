6bad9b33d55478b88e84418810abb8727c34e91d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-10 23:15:26 +0200

    add solution of Single Element in a Sorted Array;

---
+++ c#_LeetCode/Array/Medium/SingleElementInASortedArray/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Array.Medium.SingleElementInASortedArray;
+
+//https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+public class Solution
+{
+    public int SingleNonDuplicate(int[] nums)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left < right)
+        {
+            int pivot = left + (right - left) / 2;
+
+            if (pivot % 2 == 1) pivot--;
+
+            if (nums[pivot] == nums[pivot+1])
+                left = pivot + 2;
+            else
+                right = pivot;
+        }
+
+        return nums[left];
+    }
+}
