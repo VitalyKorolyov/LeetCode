cd00767f7ddc4ff4e1832cb79b917d5b7fab41ab
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-14 01:03:36 +0200

    add solution of Gas Station

---
+++ LeetCodeTests/Array/Medium/GasStation/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.GasStation;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.GasStation;
+
+public class SolutionTests
+{
+    [Fact]
+    public void CanCompleteCircuit_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] gas = new int[] { 5, 8, 2, 8 };
+        int[] cost = new int[] { 6, 5, 6, 6 };
+
+        // Act
+        var result = solution.CanCompleteCircuit(gas, cost);
+
+        // Assert
+        Assert.True(result == 3);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/GasStation/Solution.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Array.Medium.GasStation;
+
+//https://leetcode.com/problems/gas-station/description/
+public class Solution
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        int wholeRoad = 0;
+        int startGasStation = 0;
+        int tank = 0;
+
+        for (int i = 0; i < gas.Length; i++)
+        {
+            var diff = gas[i] - cost[i];
+
+            wholeRoad += diff;
+            tank += diff;
+
+            if(tank < 0)
+            {
+                tank = 0;
+                startGasStation = i + 1;
+            }
+        }
+
+        return wholeRoad >= 0 ? startGasStation : -1;
+    }
+
+    //O(n^2)
+    //public int CanCompleteCircuit(int[] gas, int[] cost)
+    //{
+    //    int n = gas.Length;
+
+    //    for (int i = 0; i < n; i++)
+    //    {
+    //        int currentGas = 0;
+    //        int count = 0;
+    //        int j = i;
+
+    //        while(count < n && currentGas >= 0)
+    //        {
+    //            currentGas += gas[j % n] - cost[j % n];
+    //            count++;
+    //            j++;
+    //        }
+
+    //        if (count == n && currentGas >= 0) return i;
+    //    }
+
+    //    return -1;
+    //}
+}
