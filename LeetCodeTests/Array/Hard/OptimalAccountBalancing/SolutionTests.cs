ba9a0e8e60fb7d43bbb2908dfdc57b3a71397398
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-23 00:38:08 +0200

    add solution of Optimal Account Balancing

---
+++ LeetCodeTests/Array/Hard/OptimalAccountBalancing/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Hard.OptimalAccountBalancing;
+using Xunit;
+
+namespace LeetCodeTests.Array.Hard.OptimalAccountBalancing;
+
+public class SolutionTests
+{
+    [Fact]
+    public void MinTransfers_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[][] transactions = new int[][] { new int[] { 0, 1, 10 },
+                new int[] { 1, 0, 1 }, new int[] { 1, 2, 5 }, new int[] { 2, 0, 5 } };
+
+        // Act
+        var result = solution.MinTransfers(transactions);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Array/Hard/OptimalAccountBalancing/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array.Hard.OptimalAccountBalancing;
+
+//https://leetcode.com/problems/optimal-account-balancing/description/
+public class Solution
+{
+    public int MinTransfers(int[][] transactions)
+    {
+        Dictionary<int, int> map = new();
+        foreach (int[] trans in transactions)
+        {
+            map[trans[0]] = map.GetValueOrDefault(trans[0], 0) - trans[2];
+            map[trans[1]] = map.GetValueOrDefault(trans[1], 0) + trans[2];
+        }
+
+        return Dfs(0, map.Values.ToArray());
+    }
+
+    private int Dfs(int index, int[] balance)
+    {
+        if (index == balance.Length) return 0;
+        if (balance[index] == 0) return Dfs(index + 1, balance);  
+
+        int res = int.MaxValue;
+        int currDebt = balance[index];
+
+        for (int i = index + 1; i < balance.Length; i++)
+        {
+            if (balance[i] * currDebt >= 0) continue;
+
+            balance[i] += currDebt;
+            res = System.Math.Min(res, 1 + Dfs(index + 1, balance));
+            balance[i] -= currDebt;
+        }
+
+        return res;
+    }
+}
