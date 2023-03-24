8ca587b64f2116a8d0e92df20d1528b96f3c14d9
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-25 00:58:43 +0200

    add solution of Reverse Integer

---
+++ LeetCodeTests/Math/Medium/ReverseInteger/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Math.Medium.ReverseInteger;
+using Xunit;
+
+namespace LeetCodeTests.Math.Medium.ReverseInteger;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Reverse_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int x = 0;
+
+        // Act
+        var result = solution.Reverse(123);
+
+        // Assert
+        Assert.True(result == 321);
+    }
+}
---
+++ c#_LeetCode/Math/Medium/ReverseInteger/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Math.Medium.ReverseInteger;
+
+//https://leetcode.com/problems/reverse-integer/description/
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        int res = 0;
+
+        while (x != 0)
+        {
+            int value = x % 10;
+            x /= 10;
+
+            if (res > int.MaxValue / 10 || (res == int.MaxValue / 10 && value > 7)) return 0;
+            if (res < int.MinValue / 10 || (res == int.MinValue / 10 && value < -8)) return 0;
+
+            res = res * 10 + value;
+        }
+
+        return res;
+    }
+}
