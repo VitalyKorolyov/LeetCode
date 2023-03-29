aa972290c394dcf9c2d6631ba89ba816ae74468c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-03-29 22:34:54 +0300

    add solution of Evaluate Reverse Polish Notation

---
+++ LeetCodeTests/Stack/Medium/EvaluateReversePolishNotation/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Stack.Medium.EvaluateReversePolishNotation;
+using Xunit;
+
+namespace LeetCodeTests.Stack.Medium.EvaluateReversePolishNotation;
+
+public class SolutionTests
+{
+    [Fact]
+    public void EvalRPN_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string[] tokens = new string[] { "4", "3", "-" };
+
+        // Act
+        var result = solution.EvalRPN(tokens);
+
+        // Assert
+        Assert.True(result == 1);
+    }
+}
---
+++ c#_LeetCode/Stack/Medium/EvaluateReversePolishNotation/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Stack.Medium.EvaluateReversePolishNotation;
+
+//https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+public class Solution
+{
+    public int EvalRPN(string[] tokens)
+    {
+        Stack<int> stack = new();
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            if (char.IsDigit(tokens[i][0]) || tokens[i].Length > 1)
+                stack.Push(int.Parse(tokens[i]));
+            else
+            {
+                int first = stack.Pop();
+                int second = stack.Pop();
+
+                if (tokens[i] == "+")
+                    stack.Push(first + second);
+                if (tokens[i] == "-")
+                    stack.Push(second - first);
+                if (tokens[i] == "*")
+                    stack.Push(first * second);
+                if (tokens[i] == "/") 
+                    stack.Push(second / first);
+            }
+        }
+
+        return stack.Pop();
+    }
+}
