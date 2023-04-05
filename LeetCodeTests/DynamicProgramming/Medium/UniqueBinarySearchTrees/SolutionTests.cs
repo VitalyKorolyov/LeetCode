333260aa31c3ceaf6792f735c16d9f491fdec14e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-06 01:23:26 +0300

    add solution of Unique Binary Search Trees

---
+++ LeetCodeTests/DynamicProgramming/Medium/UniqueBinarySearchTrees/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.DynamicProgramming.Medium.UniqueBinarySearchTrees;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Medium.UniqueBinarySearchTrees;
+
+public class SolutionTests
+{
+    [Fact]
+    public void NumTrees_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int n = 3;
+
+        // Act
+        var result = solution.NumTrees(n);
+
+        // Assert
+        Assert.True(result == 5);
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Medium/UniqueBinarySearchTrees/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.DynamicProgramming.Medium.UniqueBinarySearchTrees;
+
+//https://leetcode.com/problems/unique-binary-search-trees/description/
+public class Solution
+{
+    public int NumTrees(int n)
+    {
+        int[] unique = new int[n + 1];
+        unique[0] = 1;
+        unique[1] = 1;
+
+        for (int i = 2; i <= n; ++i)
+            for (int j = 1; j <= i; ++j)
+                unique[i] += unique[j - 1] * unique[i - j];
+
+        return unique[n];
+    }
+}
