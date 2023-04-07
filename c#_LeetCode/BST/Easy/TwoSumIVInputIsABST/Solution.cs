2cf90c07392b65bd0a59a901c9e358ef336803cb
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-07 22:06:48 +0300

    add solution of Two Sum IV - Input is a BST

---
+++ c#_LeetCode/BST/Easy/TwoSumIVInputIsABST/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.BST.Easy.TwoSumIVInputIsABST;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+//https://leetcode.com/problems/two-sum-iv-input-is-a-bst/description/
+public class Solution
+{
+    public bool FindTarget(TreeNode root, int k)
+    {
+        return FindTarget(root, k, new());
+    }
+
+    private bool FindTarget(TreeNode node, int k, HashSet<int> values)
+    {
+        if(node == null) return false;
+        if(values.Contains(k - node.val)) return true;
+
+        values.Add(node.val);
+
+        return FindTarget(node.left, k, values) || FindTarget(node.right, k, values);
+    }
+}
