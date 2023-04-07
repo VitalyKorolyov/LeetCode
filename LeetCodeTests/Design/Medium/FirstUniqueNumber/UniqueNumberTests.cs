d9ee0b4b8026a13342b84ab1cf853bcf70b0485e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-08 01:08:17 +0300

    add solution of First Unique Number

---
+++ LeetCodeTests/Design/Medium/FirstUniqueNumber/UniqueNumberTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Design.Medium.FirstUniqueNumber;
+using Xunit;
+
+namespace LeetCodeTests.Design.Medium.FirstUniqueNumber;
+
+public class UniqueNumberTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var firstUnique = new FirstUnique(new int[] { 2, 3, 5 });
+
+        // Act
+        firstUnique.ShowFirstUnique();
+        firstUnique.Add(5);
+        firstUnique.ShowFirstUnique();
+
+        // Assert
+        Assert.True(false);
+    }
+}
---
+++ c#_LeetCode/Design/Medium/FirstUniqueNumber/FirstUnique.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Design.Medium.FirstUniqueNumber;
+
+//https://leetcode.com/problems/first-unique-number/description/
+
+public class UniqueNumber
+{
+    public int Number { get; set; }
+}
+
+public class FirstUnique
+{
+    private readonly HashSet<int> allNumbers;
+    private readonly Dictionary<int, UniqueNumber> numberToUniqueNumber;
+    private readonly LinkedList<UniqueNumber> uniqueNumbers;
+
+    public FirstUnique(int[] nums)
+    {
+        allNumbers = new();
+        numberToUniqueNumber = new();
+        uniqueNumbers = new();
+
+        for (int i = 0; i < nums.Length; i++)
+            Add(nums[i]);
+    }
+
+    public int ShowFirstUnique()
+    {
+        if (uniqueNumbers.Count == 0) return -1;
+
+        return uniqueNumbers.First.Value.Number;
+    }
+
+    public void Add(int value)
+    {
+        if (allNumbers.Contains(value) && numberToUniqueNumber.ContainsKey(value))
+        {
+            UniqueNumber numberToRemove = numberToUniqueNumber[value];
+            uniqueNumbers.Remove(numberToRemove);
+            numberToUniqueNumber.Remove(value);
+        }
+        else if(!allNumbers.Contains(value))
+        {
+            allNumbers.Add(value);
+            UniqueNumber uniqueNumberToAdd = new() { Number = value };
+            numberToUniqueNumber.Add(value, uniqueNumberToAdd);
+            uniqueNumbers.AddLast(uniqueNumberToAdd);
+        }
+    }
+}
