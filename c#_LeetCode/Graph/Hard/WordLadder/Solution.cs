49e9438ce3d2c390c5a6e9c816f424681afe2d20
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-09 01:30:09 +0300

    add solution of Word Ladder

---
+++ c#_LeetCode/Graph/Hard/WordLadder/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Graph.Hard.WordLadder;
+
+//https://leetcode.com/problems/word-ladder/description/
+public class Solution
+{
+    public int LadderLength(string beginWord, string endWord, IList<string> wordList)
+    {
+        HashSet<string> allWorlds = new(wordList);
+        if (!allWorlds.Contains(endWord)) return 0;
+
+        Queue<string> queue = new();
+        HashSet<string> visited = new();
+        queue.Enqueue(beginWord);
+        visited.Add(beginWord);
+
+        int changes = 1;
+
+        while (queue.Count > 0)
+        {
+            int size = queue.Count;
+
+            for (int i = 0; i < size; i++)
+            {
+                string word = queue.Dequeue();
+                if (word == endWord) return changes;
+
+                for (int j = 0; j < word.Length; j++)
+                {
+                    for (int k = 'a'; k <= 'z'; k++)
+                    {
+                        var arr = word.ToCharArray();
+                        arr[j] = (char)k;
+
+                        string str = new(arr);
+                        if (allWorlds.Contains(str) && !visited.Contains(str))
+                        {
+                            queue.Enqueue(str);
+                            visited.Add(str);
+                        }
+                    }
+                }
+            }
+
+            ++changes;
+        }
+
+        return 0;
+    }
+}
