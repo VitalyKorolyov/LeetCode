fe70082b5e6ef7006488ef6183d11342f53a44cc
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-09 16:39:38 +0300

    add solution of Construct Binary Search Tree from Preorder Traversal

---
+++ LeetCodeTests/BST/Medium/ConstructBinarySearchTreeFromPreorderTraversal/TreeNodeTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.BST.Medium.ConstructBinarySearchTreeFromPreorderTraversal;
+using Xunit;
+
+namespace LeetCodeTests.BST.Medium.ConstructBinarySearchTreeFromPreorderTraversal;
+
+public class TreeNodeTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var solution = new Solution();
+
+        // Act
+        var res = solution.BstFromPreorder(new int[] { 8, 5, 1, 7, 10, 12 });
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/BST/Medium/ConstructBinarySearchTreeFromPreorderTraversal/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.BST.Medium.ConstructBinarySearchTreeFromPreorderTraversal;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+//https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/description/
+public class Solution
+{
+    public TreeNode BstFromPreorder(int[] preorder)
+    {
+        return Bst(preorder, 0, preorder.Length - 1);
+    }
+
+    private TreeNode Bst(int[] preorder, int start, int end)
+    {
+        if (start > end) return null;
+
+        TreeNode node = new(preorder[start]);
+        if(start == end) return node;
+
+        int newEnd = start;
+        for (int i = newEnd + 1; i <= end && preorder[start] > preorder[i]; i++)
+            newEnd = i;
+
+        node.left = Bst(preorder, start + 1, newEnd);
+        node.right = Bst(preorder, newEnd + 1, end);
+
+        return node;
+    }
+}
