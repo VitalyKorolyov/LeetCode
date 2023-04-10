b58a61b9582490527266f3bfcae32626b956a817
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-10 21:53:05 +0300

    add solution of Difference Between Ones and Zeros in Row and Column

---
+++ LeetCodeTests/Matrix/Medium/DifferenceBetweenOnesAndZerosInRowAndColumn/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.Matrix.Medium.DifferenceBetweenOnesAndZerosInRowAndColumn;
+using Xunit;
+
+namespace LeetCodeTests.Matrix.Medium.DifferenceBetweenOnesAndZerosInRowAndColumn;
+
+public class SolutionTests
+{
+    [Fact]
+    public void OnesMinusZeros_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[][] grid = new int[3][];
+        grid[0] = new int[] { 0, 1, 1 };
+        grid[1] = new int[] { 1, 0, 1 };
+        grid[2] = new int[] { 0, 0, 1 };
+
+        // Act
+        var result = solution.OnesMinusZeros(grid);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Matrix/Medium/DifferenceBetweenOnesAndZerosInRowAndColumn/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Matrix.Medium.DifferenceBetweenOnesAndZerosInRowAndColumn;
+
+//https://leetcode.com/problems/difference-between-ones-and-zeros-in-row-and-column/description/
+public class Solution
+{
+    public int[][] OnesMinusZeros(int[][] grid)
+    {
+        var row = new int[grid.Length];
+        var col = new int[grid[0].Length];
+
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[0].Length; j++)
+            {
+                if (grid[i][j] == 1) row[i]++;
+                if (grid[i][j] == 0) row[i]--;
+            }
+        }
+
+        for (int i = 0; i < grid[0].Length; i++)
+        {
+            for (int j = 0; j < grid.Length; j++)
+            {
+                if (grid[j][i] == 1) col[i]++;
+                if (grid[j][i] == 0) col[i]--;
+            }
+        }
+
+        int[][] diff = new int[grid.Length][];
+        for (int i = 0; i < grid.Length; i++)
+        {
+            diff[i] = new int[grid[0].Length];
+            for (int j = 0; j < grid[0].Length; j++)
+                diff[i][j] = row[i] + col[j];
+        }
+
+        return diff;
+    }
+}
