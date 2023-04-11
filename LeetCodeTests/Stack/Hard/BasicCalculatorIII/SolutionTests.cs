0a640a6ed1ec7a759b67819f255ac0075102e797
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-11 23:48:46 +0300

    add solution of Basic Calculator III

---
+++ LeetCodeTests/Stack/Hard/BasicCalculatorIII/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Stack.Hard.BasicCalculatorIII;
+using Xunit;
+
+namespace LeetCodeTests.Stack.Hard.BasicCalculatorIII;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Calculate_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string s = "1*2-3/4+5*6-7*8+9/10";
+
+        // Act
+        var result = solution.Calculate(s);
+
+        // Assert
+        Assert.True(result == -24);
+    }
+}
---
+++ c#_LeetCode/Stack/Hard/BasicCalculatorIII/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Stack.Hard.BasicCalculatorIII;
+
+//https://leetcode.com/problems/basic-calculator-iii/description/
+public class Solution
+{
+    public int Calculate(string s)
+    {
+        Queue<char> queue = new();
+
+        foreach (var item in s.Where(x => x != ' '))
+            queue.Enqueue(item);
+
+        queue.Enqueue('+');
+
+        return Calculate(queue);
+    }
+
+    private int Calculate(Queue<char> queue)
+    {
+        char preOp = '+';
+        int number = 0;
+        Stack<int> numbers = new();
+
+        while (queue.Count > 0)
+        {
+            char item = queue.Dequeue();
+
+            if (char.IsDigit(item))
+                number = 10 * number + item - '0';
+            else if (item == '(')
+                number = Calculate(queue);
+            else
+            {
+                if (preOp == '+')
+                    numbers.Push(number);
+                if (preOp == '-')
+                    numbers.Push(-number);
+                if (preOp == '*')
+                    numbers.Push(numbers.Pop() * number);
+                if (preOp == '/')
+                    numbers.Push(numbers.Pop() / number);
+
+                number = 0;
+                preOp = item;
+
+                if (item == ')') break;
+            }
+        }
+
+        int result = 0;
+        while (numbers.Count > 0)
+            result += numbers.Pop();
+       
+        return result;
+    }
+}
