9a15273a16e9ac197865d9f00a2c06b4c9d9b6d5
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-11 23:38:37 +0300

    update solution of Basic Calculator

--- LeetCodeTests/Stack/Hard/BasicCalculator/SolutionTests.cs
+++ LeetCodeTests/Stack/Hard/BasicCalculator/SolutionTests.cs
@@ -1,30 +1,17 @@
 ﻿using LeetCode.Stack.Hard.BasicCalculator;
 using Xunit;
 
-namespace LeetCodeTests.Stack.Hard.BasicCalculator
+namespace LeetCodeTests.Stack.Hard.BasicCalculator;
+
+public class SolutionTests
 {
-    public class SolutionTests
+    [Fact]
+    public void Calculate_StateUnderTest_ExpectedBehavior()
     {
-        [Fact]
-        public void Calculate_StateUnderTest_ExpectedBehavior()
-        {
-            //Arrange
-            var solution = new Solution();
-            string s = "(1+(4+5+2)-3)+(6+8)";
+        var solution = new Solution();
 
-            //Act
-            var result = solution.Calculate(s);
+        var result = solution.Calculate(" (1+(4+5+2)-3)+(6+8)");
 
-            //Assert
-            Assert.True(2 == result);
-
-            s = " 2-1 + 2 ";
-
-            //Act
-            result = solution.Calculate(s);
-
-            //Assert
-            Assert.True(3 == result);
-        }
+        Assert.True(23 == result);
     }
 }
--- c#_LeetCode/Stack/Hard/BasicCalculator/Solution.cs
+++ c#_LeetCode/Stack/Hard/BasicCalculator/Solution.cs
@@ -1,81 +1,54 @@
 ﻿using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
-namespace LeetCode.Stack.Hard.BasicCalculator
+namespace LeetCode.Stack.Hard.BasicCalculator;
+
+//https://leetcode.com/problems/basic-calculator/
+public class Solution
 {
-    //https://leetcode.com/problems/basic-calculator/
-    public class Solution
+    public int Calculate(string s)
     {
-        public int Calculate(string s)
-        {
-            Queue<char> queue = new(s.Length);
-            foreach (char c in s)
-            {
-                if (c == ' ') continue;
-                queue.Enqueue(c);
-            }
+        Queue<char> queue = new(s.Length);
 
-            return Calculate(queue);
+        foreach (char c in s.Where(x => x != ' '))
+            queue.Enqueue(c);
+
+        queue.Enqueue('+');
+        return Calculate(queue);
+    }
+
+    private int Calculate(Queue<char> queue)
+    {
+        Stack<int> numbers = new();
+        int num = 0;
+        char preOp = '+';
+
+        while (queue.Count > 0)
+        {
+            char item = queue.Dequeue();
+
+            if (char.IsDigit(item))
+                num = num * 10 + item - '0';
+            else if (item == '(')
+                num = Calculate(queue);
+            else
+            {
+                if (preOp == '+')
+                    numbers.Push(num);
+                else if (preOp == '-')
+                    numbers.Push(-num);
+
+                preOp = item;
+                num = 0;
+
+                if (item == ')') break;
+            }
         }
 
-        private int Calculate(Queue<char> queue)
-        {
-            Stack<int> numbers = new();
-            Stack<char> operations = new();
+        int result = 0;
+        while (numbers.Count > 0)
+            result += numbers.Pop();
 
-            while(queue.Count > 0)
-            {
-                var item = queue.Peek();
-
-                if (char.IsDigit(item))
-                {
-                    var number = GetNumber(queue);
-                    numbers.Push(number);
-                    continue;
-                }
-                if(item == '(')
-                {
-                    queue.Dequeue();
-                    var number = Calculate(queue);
-                    numbers.Push(number);
-                    continue;
-                }
-                if(item == ')')
-                {
-                    queue.Dequeue();
-                    break;
-                }
-
-                operations.Push(queue.Dequeue());
-            }
-
-            int res = 0;
-            while (operations.Count > 0 && numbers.Count > 0)
-            {
-                var first = numbers.Pop();
-                res += operations.Pop() == '+' ? first : -first;
-            }
-
-            if(operations.Count > 0)
-                res *= operations.Pop() == '-' ? -1 : 1;
-            if(numbers.Count > 0)
-                res += numbers.Pop();
-
-            return res;
-        }
-
-        private int GetNumber(Queue<char> queue)
-        {
-            StringBuilder strBuilder = new();
-
-            while (queue.Count > 0)
-            {
-                if (char.IsDigit(queue.Peek()))
-                    strBuilder.Append(queue.Dequeue());
-                else break;
-            }
-
-            return int.Parse(strBuilder.ToString());
-        }
+        return result;
     }
 }
