6b816dd655fcab65a56f7a99735186da4f26c92d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-12 22:51:45 +0300

    add solution of Maximum Average Subtree

---
+++ c#_LeetCode/Trees/Medium/MaximumAverageSubtree/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Trees.Medium.MaximumAverageSubtree;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+//https://leetcode.com/problems/maximum-average-subtree/description/
+public class Solution
+{
+    private double maxAverage = int.MinValue;
+
+    public double MaximumAverageSubtree(TreeNode root)
+    {
+        Dfs(root);
+
+        return maxAverage;
+    }
+
+    private (int, int) Dfs(TreeNode node)
+    {
+        if (node == null) return (0, 0);
+
+        (int leftDescendants, int leftSum) = Dfs(node.left);
+        (int rightDescendants, int rightSum) = Dfs(node.right);
+
+        int currentDescendants = leftDescendants + rightDescendants + 1;
+        int currentSum = leftSum + rightSum + node.val;
+        maxAverage = System.Math.Max(maxAverage, (double)currentSum / (double)currentDescendants); ;
+
+        return (currentDescendants, currentSum);
+    }
+}
