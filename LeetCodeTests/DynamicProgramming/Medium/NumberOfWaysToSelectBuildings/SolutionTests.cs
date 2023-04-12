91d1623004bfa8c262446455bcb0ff40c5b1f5f0
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-12 21:16:49 +0300

    add solution of Number of Ways to Select Buildings

---
+++ LeetCodeTests/DynamicProgramming/Medium/NumberOfWaysToSelectBuildings/SolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.DynamicProgramming.Medium.NumberOfWaysToSelectBuildings;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Medium.NumberOfWaysToSelectBuildings;
+
+public class SolutionTests
+{
+    [Fact]
+    public void NumberOfWays_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+
+        // Act
+        var result = solution.NumberOfWays("001101");
+
+        // Assert
+        Assert.True(6 == result);
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Medium/NumberOfWaysToSelectBuildings/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.DynamicProgramming.Medium.NumberOfWaysToSelectBuildings;
+
+//https://leetcode.com/problems/number-of-ways-to-select-buildings/description/
+public class Solution
+{
+    public long NumberOfWays(string s)
+    {
+        long totalZeros = 0;
+        for (int i = 0; i < s.Length; i++)
+            if (s[i] == '0') totalZeros++;
+
+        long totalOnes = s.Length - totalZeros;
+
+        long leftOnes = s[0] == '1' ? 1 : 0;
+        long leftZeros = s[0] == '0' ? 1 : 0;
+
+        long result = 0;
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] == '1')
+            {
+                result += (totalZeros - leftZeros) * leftZeros;
+                leftOnes++;
+            }
+            else
+            {
+                result += (totalOnes - leftOnes) * leftOnes;
+                leftZeros++;
+            }
+        }
+
+        return result;
+    }
+}
