1d19e95a8a6a835a6a557ececba65ad3e3286519
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-13 20:49:13 +0300

    add solution of Append Characters to String to Make Subsequence

---
+++ c#_LeetCode/String/Medium/AppendCharactersToStringToMakeSubsequence/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.String.Medium.AppendCharactersToStringToMakeSubsequence;
+
+//https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/description/
+public class Solution
+{
+    public int AppendCharacters(string s, string t)
+    {
+        int j = 0;
+        for (int i = 0; i < s.Length && j < t.Length; i++)
+            if (s[i] == t[j]) j++;
+
+        return t.Length - j;
+    }
+}
