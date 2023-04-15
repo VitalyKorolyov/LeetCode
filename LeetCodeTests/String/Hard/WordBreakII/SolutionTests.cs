4373779b49b58f03c8b9a8d6bc37ab0f0a812e67
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-15 15:29:57 +0300

    add solution of Word Break II

---
+++ LeetCodeTests/String/Hard/WordBreakII/SolutionTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.String.Hard.WordBreakII;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCodeTests.String.Hard.WordBreakII;
+
+public class SolutionTests
+{
+    [Fact]
+    public void WordBreak_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string s = "catsanddog";
+        List<string> wordDict = new List<string> { "cat", "cats", "and", "sand", "dog" };
+
+        // Act
+        var result = solution.WordBreak(s, wordDict);
+
+        // Assert
+        Assert.True(result.Count == 2);
+        Assert.True(result[0] == "cat sand dog");
+        Assert.True(result[1] == "cats and dog");
+    }
+}
---
+++ c#_LeetCode/String/Hard/WordBreakII/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Hard.WordBreakII;
+
+//https://leetcode.com/problems/word-break-ii/description/
+public class Solution
+{
+    public IList<string> WordBreak(string s, IList<string> wordDict)
+    {
+        HashSet<string> words = new(wordDict);
+        List<string> result = new();
+
+        WordBreak(s, 0, words, result, string.Empty);
+
+        return result;
+    }
+
+    private void WordBreak(string s, int i, HashSet<string> words, List<string> result, string temp)
+    {
+        if(i == s.Length)
+        {
+            result.Add(temp);
+            return;
+        }
+
+        for (int j = i + 1; j <= s.Length; j++)
+        {
+            var sub = s.Substring(i, j - i);
+
+            if(words.Contains(sub))
+                WordBreak(s, j, words, result, temp == "" ? sub : temp + " " + sub);
+        }
+    }
+}
