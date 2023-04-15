ce9d115d09c0531c5b5c32d4f04bed3f43eb1387
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-16 00:18:01 +0300

    update solution of Sort Colors

--- c#_LeetCode/Array/Medium/SortColors/Solution.cs
+++ c#_LeetCode/Array/Medium/SortColors/Solution.cs
@@ -1,37 +1,33 @@
-﻿namespace LeetCode.Array.Medium.SortColors
+﻿namespace LeetCode.Array.Medium.SortColors;
+
+public class Solution
 {
-    public class Solution
+    public void SortColors(int[] nums)
     {
-        public void SortColors(int[] nums)
-        {
-            var left = 0;
-            var right = nums.Length - 1;
+        var left = 0;
+        var right = nums.Length - 1;
 
-            for (int i = 0; i <= right;)
+        for (int i = 0; i <= right;)
+        {
+            if (nums[i] == 0)
             {
-                if(nums[i] == 0)
-                {
-                    Swap(nums, i, left);
-                    left++;
-                    i++;
-                }
-                else if (nums[i] == 1)
-                {
-                    i++;
-                }
-                else if(nums[i] == 2)
-                {
-                    Swap(nums, i, right);
-                    right--;
-                }
+                Swap(nums, i, left);
+                left++;
+                i++;
             }
+            else if (nums[i] == 2)
+            {
+                Swap(nums, i, right);
+                right--;
+            }
+            else i++;
         }
+    }
 
-        private void Swap(int[] nums, int i, int j)
-        {
-            var saved = nums[j];
-            nums[j] = nums[i];
-            nums[i] = saved;
-        }
+    private void Swap(int[] nums, int i, int j)
+    {
+        var saved = nums[j];
+        nums[j] = nums[i];
+        nums[i] = saved;
     }
 }
