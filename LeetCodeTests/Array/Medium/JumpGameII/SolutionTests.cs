0ea0311f70a62a84ad3694fbafe63fd7fe44c8aa
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-16 23:24:37 +0300

    add solution of Jump Game II

---
+++ LeetCodeTests/Array/Medium/JumpGameII/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Array.Medium.JumpGameII;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.JumpGameII;
+
+public class SolutionTests
+{
+    [Fact]
+    public void Jump_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] nums = new int[] { 2, 3, 0, 1, 4 };
+
+        // Act
+        var result = solution.Jump(nums);
+
+        // Assert
+        Assert.True(result == 2);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/JumpGameII/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.JumpGameII;
+
+public class Solution
+{
+    //https://leetcode.com/problems/jump-game-ii/description/
+    //O(n^2)
+    public int Jump(int[] nums)
+    {
+        Dictionary<int, int> indexToMinJump = new();
+        return Jump(nums, 0, indexToMinJump);
+    }
+
+    private int Jump(int[] nums, int index, Dictionary<int, int> indexToMinJump)
+    {
+        if(indexToMinJump.ContainsKey(index)) return indexToMinJump[index];
+        if(nums.Length - 1 <= index) return 0;
+
+        int jump = nums[index];
+
+        while(jump > 0)
+        {
+            var currentRes = Jump(nums, index + jump, indexToMinJump);
+            indexToMinJump[index] = System.Math.Min(currentRes + 1, indexToMinJump.GetValueOrDefault(index, 10001));
+            jump--;
+        }
+
+        return indexToMinJump.GetValueOrDefault(index, 10001);
+    }
+}
