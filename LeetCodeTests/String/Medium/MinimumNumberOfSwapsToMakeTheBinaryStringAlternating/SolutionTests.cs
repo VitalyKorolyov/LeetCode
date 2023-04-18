b65060626dbfdcaa287f4692381ca4a3520bdbc3
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-18 22:48:55 +0300

    add solution of Minimum Number of Swaps to Make the Binary String Alternating

---
+++ LeetCodeTests/String/Medium/MinimumNumberOfSwapsToMakeTheBinaryStringAlternating/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.String.Medium.MinimumNumberOfSwapsToMakeTheBinaryStringAlternating;
+using Xunit;
+
+namespace LeetCodeTests.String.Medium.MinimumNumberOfSwapsToMakeTheBinaryStringAlternating;
+
+public class SolutionTests
+{
+    [Fact]
+    public void MinSwaps_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string s = "111000";
+
+        // Act
+        var result = solution.MinSwaps(s);
+
+        // Assert
+        Assert.True(result == 1);
+    }
+}
---
+++ c#_LeetCode/String/Medium/MinimumNumberOfSwapsToMakeTheBinaryStringAlternating/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode.String.Medium.MinimumNumberOfSwapsToMakeTheBinaryStringAlternating;
+
+//https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-binary-string-alternating/description/
+public class Solution
+{
+    public int MinSwaps(string s)
+    {
+        int countOne = 0, countZero = 0;
+        foreach (char c in s)
+        {
+            if (c == '0') countZero++;
+            else countOne++;
+        }
+
+        if (System.Math.Abs(countOne - countZero) > 1) return -1;
+
+        if (countZero > countOne) return CountSwaps(s, '0');
+        if (countZero < countOne) return CountSwaps(s, '1');
+
+        return System.Math.Min(CountSwaps(s, '0'), CountSwaps(s, '1'));
+    }
+
+    private int CountSwaps(string s, char start)
+    {
+        int wrongPositions = 0;
+
+        foreach (char current in s)
+        {
+            if (current != start) wrongPositions++;
+            start = start == '1' ? '0' : '1';
+        }
+
+        return wrongPositions / 2;
+    }
+}
