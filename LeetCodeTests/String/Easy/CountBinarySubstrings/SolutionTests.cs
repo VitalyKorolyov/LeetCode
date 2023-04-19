3d77b02a8e510ba7bc38e117cc281d4f5b1e4d6d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-19 22:39:31 +0300

    add solution of Count Binary Substrings

---
+++ LeetCodeTests/String/Easy/CountBinarySubstrings/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.String.Easy.CountBinarySubstrings;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.CountBinarySubstrings;
+
+public class SolutionTests
+{
+    [Fact]
+    public void CountBinarySubstrings_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string s = "10101";
+
+        // Act
+        var result = solution.CountBinarySubstrings(s);
+
+        // Assert
+        Assert.True(result == 4);
+    }
+}
---
+++ c#_LeetCode/String/Easy/CountBinarySubstrings/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.String.Easy.CountBinarySubstrings;
+
+//https://leetcode.com/problems/count-binary-substrings/description/
+public class Solution
+{
+    public int CountBinarySubstrings(string s)
+    {
+        if(string.IsNullOrWhiteSpace(s)) return 0;
+
+        int count = 0;
+        char current = s[0];
+        int countCurrent = 0;
+        int countPrev = 0;
+
+        foreach(char c in s)
+        {
+            if (c == current) countCurrent++;
+            else
+            {
+                count += System.Math.Min(countCurrent, countPrev);
+                countPrev = countCurrent;
+                countCurrent = 1;
+                current = c;
+            }
+        }
+
+        return count + System.Math.Min(countCurrent, countPrev);
+    }
+}
