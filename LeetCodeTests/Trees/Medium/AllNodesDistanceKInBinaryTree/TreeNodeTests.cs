abe6cd23c5637e9fe47071312c9202d5588c4d55
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-21 21:38:29 +0300

    add solution of All Nodes Distance K in Binary Tree

---
+++ LeetCodeTests/Trees/Medium/AllNodesDistanceKInBinaryTree/TreeNodeTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Trees.Medium.AllNodesDistanceKInBinaryTree;
+using Xunit;
+
+namespace LeetCodeTests.Trees.Medium.AllNodesDistanceKInBinaryTree;
+
+public class TreeNodeTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var treeNode = new TreeNode(0, new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3, null, new TreeNode(4)))));
+
+        // Act
+        var res = new Solution().DistanceK(treeNode, new TreeNode(3), 0);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Trees/Medium/AllNodesDistanceKInBinaryTree/Solution.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees.Medium.AllNodesDistanceKInBinaryTree;
+
+//https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/description/
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x, TreeNode left = null, TreeNode right = null) 
+    { 
+        val = x; 
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    private readonly Dictionary<TreeNode, TreeNode> parents = new();
+    private static readonly TreeNode nullNode = new TreeNode(0);
+
+    public IList<int> DistanceK(TreeNode root, TreeNode target, int k)
+    {
+        target = Dfs(root, nullNode, target);
+
+        HashSet<TreeNode> visited = new();
+        Queue<TreeNode> queue = new();
+        queue.Enqueue(target);
+        visited.Add(target);
+
+        while (queue.Count > 0 && k > 0)
+        {
+            int size = queue.Count;
+
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode current = queue.Dequeue();
+                AddToQueue(queue, current, visited);
+            }
+
+            k--;
+        }
+
+        List<int> result = new();
+        while (queue.Count > 0)
+            result.Add(queue.Dequeue().val);
+
+        return result;
+    }
+
+    private void AddToQueue(Queue<TreeNode> queue, TreeNode node, HashSet<TreeNode> visited)
+    {
+        if(node.left != null && !visited.Contains(node.left))
+        {
+            queue.Enqueue(node.left);
+            visited.Add(node.left);
+        }
+        if (node.right != null && !visited.Contains(node.right))
+        {
+            queue.Enqueue(node.right);
+            visited.Add(node.right);
+        }
+        var parent = parents[node];
+        if (parent != nullNode && !visited.Contains(parent))
+        {
+            queue.Enqueue(parent);
+            visited.Add(parent);
+        }
+    }
+
+    private TreeNode Dfs(TreeNode node, TreeNode parent, TreeNode target)
+    {
+        if(node == null) return null;
+
+        parents.Add(node, parent);
+
+        var left = Dfs(node.left, node, target);
+        var right = Dfs(node.right, node, target);
+
+        return node.val == target.val ? node : left ?? right;
+    }
+}
