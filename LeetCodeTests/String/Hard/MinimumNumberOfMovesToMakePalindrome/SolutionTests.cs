34da12377b92d939a06f6ad21d17ccb17edfb2d9
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-22 22:06:55 +0300

    add solution of Minimum Number of Moves to Make Palindrome

---
+++ LeetCodeTests/String/Hard/MinimumNumberOfMovesToMakePalindrome/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.String.Hard.MinimumNumberOfMovesToMakePalindrome;
+using Xunit;
+
+namespace LeetCodeTests.String.Hard.MinimumNumberOfMovesToMakePalindrome;
+
+public class SolutionTests
+{
+    [Fact]
+    public void MinMovesToMakePalindrome_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string s = "letelt";
+
+        // Act
+        var result = solution.MinMovesToMakePalindrome(s);
+
+        // Assert
+        Assert.True(result == 2);
+    }
+}
---
+++ c#_LeetCode/String/Hard/MinimumNumberOfMovesToMakePalindrome/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Hard.MinimumNumberOfMovesToMakePalindrome;
+
+//https://leetcode.com/problems/minimum-number-of-moves-to-make-palindrome/description/
+public class Solution
+{
+    public int MinMovesToMakePalindrome(string s)
+    {
+        int count = 0;
+        List<char> list = new(s);
+
+        while (list.Count > 2)
+        {
+            char ch1 = list[0];
+            char ch2 = list[list.Count - 1];
+
+            if(ch1 == ch2)
+            {
+                list.RemoveAt(list.Count - 1);
+                list.RemoveAt(0);
+            }
+            else
+            {
+                int id1 = list.LastIndexOf(ch1);
+                int id2 = list.IndexOf(ch2);
+
+                int step1 = list.Count - id1 - 1;
+                int step2 = id2;
+
+                if(step1 > step2)
+                {
+                    list.RemoveAt(id2);
+                    list.RemoveAt(list.Count - 1);
+                    count += step2;
+                }
+                else
+                {
+                    list.RemoveAt(id1);
+                    list.RemoveAt(0);
+                    count += step1;
+                }
+            }
+        }
+
+        return count;
+    }
+}
