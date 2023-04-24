7cd5aed482a3b3072064946f8039d6c0d43caca9
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-24 22:21:53 +0300

    add solution of Sqrt(x)

---
+++ c#_LeetCode/Math/Easy/Sqrt(x)/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Math.Easy.Sqrt_x_;
+
+//https://leetcode.com/problems/sqrtx/description/
+public class Solution
+{
+    public int MySqrt(int x)
+    {
+        if(x < 2) return x;
+
+        long left = 0;
+        long right = x;
+
+        while(left < right)
+        {
+            long mid = left + (right - left) / 2;
+
+            long num = mid * mid;
+            if (num > x)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return (int)left - 1;
+    }
+}
