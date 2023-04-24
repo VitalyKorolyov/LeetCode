ca966ceaa263d2ab53aa7f9efe4554cec0608731
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-25 00:04:08 +0300

    add solution of Time Based Key-Value Store

---
+++ LeetCodeTests/Design/Medium/TimeBasedKeyValueStore/TimeMapTests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Design.Medium.TimeBasedKeyValueStore;
+using Xunit;
+
+namespace LeetCodeTests.Design.Medium.TimeBasedKeyValueStore;
+
+public class TimeMapTests
+{
+    [Fact]
+    public void Set_StateUnderTest_ExpectedBehavior()
+    {
+        var timeMap = new TimeMap();
+
+        timeMap.Set("love", "high", 10);
+        timeMap.Set("love", "low", 20);
+
+        var res = timeMap.Get("love", 5);
+        Assert.True(res == "");
+
+        res = timeMap.Get("love", 10);
+        Assert.True(res == "high");
+
+        res = timeMap.Get("love", 15);
+        Assert.True(res == "high");
+
+        res = timeMap.Get("love", 20);
+        Assert.True(res == "low");
+
+        res = timeMap.Get("love", 25);
+        Assert.True(res == "low");
+    }
+}
---
+++ c#_LeetCode/Design/Medium/TimeBasedKeyValueStore/TimeMap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Design.Medium.TimeBasedKeyValueStore;
+
+//https://leetcode.com/problems/time-based-key-value-store/description/
+public class TimeMap
+{
+    private readonly Dictionary<string, List<(int timestamp, string value)>> keyToValues = new();
+
+    public void Set(string key, string value, int timestamp)
+    {
+        if (!keyToValues.ContainsKey(key))
+            keyToValues.Add(key, new List<(int timestamp, string value)>());
+
+        keyToValues[key].Add((timestamp, value));
+    }
+
+    public string Get(string key, int timestamp)
+    {
+        if(!keyToValues.ContainsKey(key)) return string.Empty;
+
+        var list = keyToValues[key];
+
+        int left = 0;
+        int right = list.Count - 1;
+
+        while(left < right) 
+        {
+            int mid = left + (right - left) / 2;
+
+            if (list[mid].timestamp >= timestamp)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        if(list[left].timestamp > timestamp)
+            return left > 0 ? list[left - 1].value : "";
+
+        return list[left].value;
+    }
+}
