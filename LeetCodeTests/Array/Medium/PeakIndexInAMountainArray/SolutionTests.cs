7ae31019b77d6a9471c2cf4bb6432633482478f8
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-25 21:19:47 +0300

    add solution of Peak Index in a Mountain Array

---
+++ LeetCodeTests/Array/Medium/PeakIndexInAMountainArray/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Array.Medium.PeakIndexInAMountainArray;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.PeakIndexInAMountainArray;
+
+public class SolutionTests
+{
+    [Fact]
+    public void PeakIndexInMountainArray_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] arr = new int[] { 1, 3, 5, 7, 3, 2 };
+
+        // Act
+        var result = solution.PeakIndexInMountainArray(arr);
+
+        // Assert
+        Assert.True(result == 3);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/PeakIndexInAMountainArray/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Array.Medium.PeakIndexInAMountainArray;
+
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+public class Solution
+{
+    public int PeakIndexInMountainArray(int[] arr)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid - 1] > arr[mid])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left - 1;
+    }
+}
