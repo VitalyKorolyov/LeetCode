554db13947100aaa1ffe0eb525b88322acf92faa
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-26 23:47:46 +0300

    add solution of Find K-th Smallest Pair Distance

---
+++ LeetCodeTests/Array/Hard/FindKthSmallestPairDistance/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Hard.FindKthSmallestPairDistance;
+using Xunit;
+
+namespace LeetCodeTests.Array.Hard.FindKthSmallestPairDistance;
+
+public class SolutionTests
+{
+    [Fact]
+    public void SplitArray_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] nums = { 1, 6, 1, 3 };
+        int k = 3;
+
+        // Act
+        var result = solution.SmallestDistancePair(nums, k);
+
+        // Assert
+        Assert.True(result == 2);
+    }
+}
---
+++ c#_LeetCode/Array/Hard/FindKthSmallestPairDistance/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Array.Hard.FindKthSmallestPairDistance;
+
+//https://leetcode.com/problems/find-k-th-smallest-pair-distance/description/
+public class Solution
+{
+    public int SmallestDistancePair(int[] nums, int k)
+    {
+        System.Array.Sort(nums);
+
+        int left = 0;
+        int right = nums[nums.Length - 1] - nums[0];
+
+        while(left < right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (Enough(mid, k, nums))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+
+    private bool Enough(int distance, int k, int[] nums)
+    {
+        int count = 0, i = 0, j = 0;
+
+        while (i < nums.Length || j < nums.Length)
+        {
+            while (j < nums.Length && nums[j] - nums[i] <= distance)
+                j += 1;
+
+            count += j - i - 1;
+            i += 1;
+        }
+
+        return count >= k;
+    }
+}
