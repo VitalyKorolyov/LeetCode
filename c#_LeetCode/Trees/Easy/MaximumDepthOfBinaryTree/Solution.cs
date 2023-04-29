de5320909df8b8aacfb1c2d92bd44a2dc2f13dd3
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-29 20:38:14 +0300

    add solution of Maximum Depth of Binary Tree

---
+++ c#_LeetCode/Trees/Easy/MaximumDepthOfBinaryTree/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Trees.Easy.MaximumDepthOfBinaryTree;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+public class Solution
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if(root == null) return 0;
+
+        int leftDepth = MaxDepth(root.left);
+        int rightDepth = MaxDepth(root.right);
+
+        return System.Math.Max(leftDepth, rightDepth) + 1;
+    }
+}
