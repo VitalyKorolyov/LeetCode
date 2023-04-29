b7f3000727c40683398e3e49e0835194324e6626
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-04-30 00:21:20 +0300

    add solution of Design Browser History

---
+++ c#_LeetCode/LinkedList/Medium/DesignBrowserHistory/BrowserHistory.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCode.LinkedList.Medium.DesignBrowserHistory;
+
+public class HistoryNode
+{
+    public string Page { get; set; }
+    public HistoryNode Next { get; set; }
+    public HistoryNode Previous { get; set;}
+}
+
+//https://leetcode.com/problems/design-browser-history/description/
+public class BrowserHistory
+{
+    private readonly HistoryNode head;
+    private HistoryNode current;
+
+    public BrowserHistory(string homepage)
+    {
+        var historyNode = new HistoryNode { Page = homepage };
+        head = historyNode;
+        current = historyNode;
+    }
+
+    public void Visit(string url)
+    {
+        var newNode = new HistoryNode { Page = url };
+
+        current.Next = newNode;
+        newNode.Previous = current;
+        current = newNode;
+    }
+
+    public string Back(int steps)
+    {
+        while(current.Previous != null && steps > 0)
+        {
+            current = current.Previous;
+            steps--;
+        }
+
+        return current.Page;
+    }
+
+    public string Forward(int steps)
+    {
+        while (current.Next != null && steps > 0)
+        {
+            current = current.Next;
+            steps--;
+        }
+
+        return current.Page;
+    }
+}
