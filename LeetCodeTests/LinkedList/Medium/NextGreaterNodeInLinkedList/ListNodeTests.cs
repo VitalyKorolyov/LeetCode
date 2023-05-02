3a099c3360b8f9d16fb234dcd738968aad3290d3
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-02 23:53:07 +0300

    add solution of Next Greater Node In Linked List

---
+++ LeetCodeTests/LinkedList/Medium/NextGreaterNodeInLinkedList/ListNodeTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.LinkedList.Medium.NextGreaterNodeInLinkedList;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Medium.NextGreaterNodeInLinkedList;
+
+public class ListNodeTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var listNode = new ListNode(2, new(7, new(4, new(3, new(5)))));
+
+        // Act
+        var res = new Solution().NextLargerNodes(listNode);
+
+        // Assert
+        Assert.Equal(new int[] { 7, 0, 5, 5, 0 }, res);
+    }
+}
---
+++ c#_LeetCode/LinkedList/Medium/NextGreaterNodeInLinkedList/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.LinkedList.Medium.NextGreaterNodeInLinkedList;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public int[] NextLargerNodes(ListNode head)
+    {
+        ListNode node = head;
+        List<int> array = new();
+
+        while (node != null)
+        {
+            array.Add(node.val);
+            node = node.next;
+        }
+
+        int[] result = new int[array.Count];
+        Stack<int> stack = new();
+        for (int i = array.Count - 1; i >= 0; i--)
+        {
+            while(stack.Count > 0 && stack.Peek() <= array[i])
+                stack.Pop();
+
+            if (stack.Count > 0)
+                result[i] = stack.Peek();
+            
+            stack.Push(array[i]);
+        }
+
+        return result;
+    }
+}
