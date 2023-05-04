f2597907081fdb6aefea8ad2ea67c34786de5a1c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-04 23:59:49 +0300

    add solution of Random Pick with Weight

---
+++ LeetCodeTests/Array/Medium/RandomPickWithWeight/SolutionTests.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Array.Medium.RandomPickWithWeight;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.RandomPickWithWeight;
+
+public class SolutionTests
+{
+    [Fact]
+    public void PickIndex_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution(new int[] { 1, 3 });
+
+        // Act
+        var result = solution.PickIndex();
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/RandomPickWithWeight/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LeetCode.Array.Medium.RandomPickWithWeight;
+
+//https://leetcode.com/problems/random-pick-with-weight/description/
+public class Solution
+{
+    private readonly int[] sum;
+    private readonly Random random;
+
+    public Solution(int[] w)
+    {
+        sum = new int[w.Length];
+
+        int prefixSum = 0;
+        for (int i = 0; i < w.Length; ++i)
+        {
+            prefixSum += w[i];
+            sum[i] = prefixSum;
+        }
+
+        random = new Random();
+    }
+
+    public int PickIndex()
+    {
+        double target = sum[sum.Length - 1] * random.NextDouble();
+
+        int left = 0, right = sum.Length - 1;
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (target < sum[mid])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+}
