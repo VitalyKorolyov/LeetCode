000d6746920008f618d70709cbfd4e6419f80685
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-07 00:41:45 +0300

    add solution of Car Fleet

---
+++ LeetCodeTests/Stack/Medium/CarFleet/SolutionTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Stack.Medium.CarFleet;
+using Xunit;
+
+namespace LeetCodeTests.Stack.Medium.CarFleet;
+
+public class SolutionTests
+{
+    [Fact]
+    public void CarFleet_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int target = 10;
+        int[] position = { 6, 8 };
+        int[] speed = { 3, 2 };
+
+        // Act
+        var result = solution.CarFleet(target, position, speed);
+
+        // Assert
+        Assert.True(result == 2);
+    }
+}
---
+++ c#_LeetCode/Stack/Medium/CarFleet/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Stack.Medium.CarFleet;
+
+//https://leetcode.com/problems/car-fleet/
+public class Solution
+{
+    private static readonly IComparer<(int, int)> comparer = 
+        Comparer<(int, int)>.Create((x, y) => y.Item1.CompareTo(x.Item1));
+
+    public int CarFleet(int target, int[] position, int[] speed)
+    {
+        List<(int, int)> list = new(position.Length);
+
+        foreach ((int p, int s) in position.Zip(speed))
+            list.Add((p, s));
+
+        list.Sort(comparer);
+
+        Stack<double> stack = new();
+        for (int i = 0; i < list.Count; i++)
+        {
+            double time = ((double)target - list[i].Item1) / (double)list[i].Item2;
+
+            if (stack.Count == 0)
+                stack.Push(time);
+            else if (stack.Peek() < time)
+                stack.Push(time);
+        }
+
+        return stack.Count;
+    }
+}
