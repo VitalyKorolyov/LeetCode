fe6e501363b0aacaee4087e6fd0830a30d68c794
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-07 00:39:54 +0300

    add solution of Design Linked List

---
+++ LeetCodeTests/LinkedList/Medium/DesignLinkedList/NodeTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.LinkedList.Medium.DesignLinkedList;
+using Xunit;
+
+namespace LeetCodeTests.LinkedList.Medium.DesignLinkedList;
+
+public class NodeTests
+{
+    [Fact]
+    public void TestMethod1()
+    {
+        // Arrange
+        var list = new MyLinkedList();
+
+        // Act
+        list.AddAtHead(1);
+        list.AddAtTail(2);
+        list.AddAtIndex(1, 2);
+        list.Get(1);
+        list.DeleteAtIndex(1);
+        list.Get(1);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/LinkedList/Medium/DesignLinkedList/MyLinkedList.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCode.LinkedList.Medium.DesignLinkedList;
+
+public class Node
+{
+    public int Value { get; set; }
+    public Node Next { get; set; }
+}
+
+//https://leetcode.com/problems/design-linked-list/description/
+public class MyLinkedList
+{
+    private Node head = new Node { Value = 0 };
+    private int count = 0;
+
+    public int Get(int index)
+    {
+        if (index < 0 || index >= count) return -1;
+
+        var node = head;
+        for (int i = 0; i < index + 1; i++)
+            node = node.Next;
+
+        return node.Value;
+    }
+
+    public void AddAtHead(int val)
+    {
+        AddAtIndex(0, val);
+    }
+
+    public void AddAtTail(int val)
+    {
+        AddAtIndex(count, val);
+    }
+
+    public void AddAtIndex(int index, int val)
+    {
+        if (index > count) return;
+        if (index < 0) index = 0;
+
+        count++;
+        var newNode = new Node { Value = val };
+        var node = head;
+
+        for (int i = 0; i < index; i++)
+            node = node.Next;
+
+        newNode.Next = node.Next;
+        node.Next = newNode;
+    }
+
+    public void DeleteAtIndex(int index)
+    {
+        if (index < 0 || index >= count) return;
+
+        count--;
+        var node = head;
+        for (int i = 0; i < index; i++)
+            node = node.Next;
+
+        node.Next = node.Next.Next;
+    }
+}
