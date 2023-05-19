3e281c9711138746c70e54f197ddb745f03ee7a6
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-19 22:48:35 +0300

    add solution of Remove Duplicates from Sorted Array II

---
+++ LeetCodeTests/Array/Medium/RemoveDuplicatesFromSortedArrayII/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Array.Medium.RemoveDuplicatesFromSortedArrayII;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.RemoveDuplicatesFromSortedArrayII;
+
+public class SolutionTests
+{
+    [Fact]
+    public void RemoveDuplicates_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] nums = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+
+        // Act
+        var result = solution.RemoveDuplicates(nums);
+
+        // Assert
+        Assert.True(result == 7);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/RemoveDuplicatesFromSortedArrayII/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Array.Medium.RemoveDuplicatesFromSortedArrayII;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        int duplicate = 1;
+        int indexToInsert = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i - 1] == nums[i]) 
+                duplicate++;
+            else
+            {
+                indexToInsert = Insert(nums, indexToInsert, i - 1, duplicate);
+                duplicate = 1;
+            }
+        }
+
+        indexToInsert = Insert(nums, indexToInsert, nums.Length - 1, duplicate);
+
+        return indexToInsert;
+    }
+
+    private int Insert(int[] nums, int indexToInsert, int prevIndex, int duplicate)
+    {
+        if (duplicate > 1)
+        {
+            nums[indexToInsert++] = nums[prevIndex];
+            nums[indexToInsert++] = nums[prevIndex];
+        }
+        else
+            nums[indexToInsert++] = nums[prevIndex];
+
+        return indexToInsert;
+    }
+}
