e1e723038a9c25d07eed3683826daa36915e390d
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-19 21:39:27 +0300

    add solution of Isomorphic Strings;

---
+++ LeetCodeTests/String/Easy/IsomorphicStrings/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.String.Easy.IsomorphicStrings;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.IsomorphicStrings;
+
+public class SolutionTests
+{
+    [Fact]
+    public void IsIsomorphic_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string s = "badc";
+        string t = "baba";
+
+        // Act
+        var result = solution.IsIsomorphic(s, t);
+
+        // Assert
+        Assert.False(result);
+    }
+}
---
+++ c#_LeetCode/String/Easy/IsomorphicStrings/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+//https://leetcode.com/problems/isomorphic-strings/description/
+
+namespace LeetCode.String.Easy.IsomorphicStrings;
+
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        if(s.Length != t.Length) return false;
+
+        Dictionary<char, char> mapToT = new(s.Length);
+        Dictionary<char, char> mapToS = new(s.Length);
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            char sChar = s[i];
+            char tChar = t[i];
+
+            if (!mapToT.ContainsKey(sChar) && mapToS.ContainsKey(tChar)) return false;
+
+            if (!mapToT.ContainsKey(sChar))
+            {
+                mapToT[sChar] = tChar;
+                mapToS[tChar] = sChar;
+            }
+            else
+                if (mapToT[sChar] != tChar) return false;
+        }
+
+        return true;
+    }
+}
