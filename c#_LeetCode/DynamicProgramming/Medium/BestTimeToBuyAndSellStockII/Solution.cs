7a57ee43e36cd3912224ed893173e3066747223b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-22 14:53:48 +0300

    add solution of Best Time to Buy and Sell Stock II

---
+++ c#_LeetCode/DynamicProgramming/Medium/BestTimeToBuyAndSellStockII/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.DynamicProgramming.Medium.BestTimeToBuyAndSellStockII;
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int maxProfit = 0;
+        if(prices == null || prices.Length <= 1) return maxProfit;
+
+        int minPrice = prices[0];
+
+        for(int i = 1; i < prices.Length; i++)
+        {
+            if(minPrice < prices[i])
+                maxProfit += prices[i] - minPrice;
+
+            minPrice = prices[i];
+        }
+
+        return maxProfit;
+    }
+}
