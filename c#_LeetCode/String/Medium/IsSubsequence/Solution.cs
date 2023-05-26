ca78e4370ddd587c7c32c9715e2f2b50fc59e98e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-26 22:54:30 +0300

    add solution of Is Subsequence

---
+++ c#_LeetCode/String/Medium/IsSubsequence/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.String.Medium.IsSubsequence;
+
+//https://leetcode.com/problems/is-subsequence/description
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int indexS = 0;
+
+        for (int indexT = 0; indexT < t.Length && indexS < s.Length; indexT++)
+        {
+            if (t[indexT] == s[indexS])
+                indexS++;
+        }
+
+        return indexS == s.Length ? true : false;
+    }
+}
