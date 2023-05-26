0bac1c5ef1f8d653a671be705753647de507fc22
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-26 22:34:29 +0300

    add solution of H-Index

---
+++ LeetCodeTests/Array/Medium/HIndex/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Array.Medium.HIndex;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.HIndex;
+
+public class SolutionTests
+{
+    [Fact]
+    public void HIndex_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] citations = { 1, 0 };
+
+        // Act
+        var result = solution.HIndex(citations);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/HIndex/Solution.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Array.Medium.HIndex;
+
+//https://leetcode.com/problems/h-index/description
+public class Solution
+{
+    public int HIndex(int[] citations)
+    {
+        Dictionary<int, int> count = new();
+
+        for (int i = 0; i < citations.Length; i++)
+        {
+            var currentCitation = citations[i] > citations.Length 
+                ? citations.Length 
+                : citations[i];
+
+            if (count.ContainsKey(currentCitation))
+                count[currentCitation]++;
+            else 
+                count[currentCitation] = 1;
+        }
+
+        int indexToInsert = 0;
+        for (int i = 0; i <= citations.Length; i++)
+        {
+            var currentCount = count.ContainsKey(i) ? count[i] : 0;
+
+            while(currentCount > 0)
+            {
+                citations[indexToInsert++] = i;
+                currentCount--;
+            }
+        }
+
+        int hIndex = 0;
+        for (int i = citations.Length - 1; i >= 0; i--)
+        {
+            if (hIndex + 1 <= citations[i]) hIndex++;
+            else break;
+        }
+
+        return hIndex;
+    }
+
+    public int HIndexNLogN(int[] citations)
+    {
+        System.Array.Sort(citations);
+
+        int hIndex = 0;
+        for (int i = citations.Length - 1; i >= 0; i--)
+        {
+            if (hIndex + 1 <= citations[i]) hIndex++;
+            else break;
+        }
+
+        return hIndex;
+    }
+
+    public int HIndexN2(int[] citations)
+    {
+        int n = citations.Length;
+
+        for (int hIndex = 1; hIndex <= n; hIndex++)
+        {
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (citations[i] >= hIndex)
+                    count++;
+            }
+
+            if(count < hIndex)
+                return hIndex - 1;
+        }
+
+        return n;
+    }
+}
