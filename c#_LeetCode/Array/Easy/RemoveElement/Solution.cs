d9c53f1dc7aee59a9d9322d83bb1a6ce0c384177
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-27 13:51:47 +0300

    update solution of Remove Element;

--- c#_LeetCode/Array/Easy/RemoveElement/Solution.cs
+++ c#_LeetCode/Array/Easy/RemoveElement/Solution.cs
@@ -1,24 +1,20 @@
-﻿using System.Linq;
+﻿namespace LeetCode.Array.Easy.RemoveElement;
 
-namespace LeetCode.Array.Easy.RemoveElement
+//https://leetcode.com/problems/remove-element
+public class Solution
 {
-    //https://leetcode.com/problems/remove-element
-    public class Solution
+    public static int RemoveElement(int[] nums, int val)
     {
-        public static int RemoveElement(int[] nums, int val)
+        if(nums == null || nums.Length == 0) return 0;
+
+        int indexToInsert = 0;
+
+        for(int i = 0; i < nums.Length; i++)
         {
-            if (!nums.Any()) return 0;
-
-            var index = 0;
-
-            for (var i = 0; i < nums.Length; i++)
-            {
-                if (nums[i] == val) continue;
-
-                nums[index++] = nums[i];
-            }
-
-            return index;
+            if (nums[i] != val)
+                nums[indexToInsert++] = nums[i];
         }
+
+        return indexToInsert;
     }
 }
