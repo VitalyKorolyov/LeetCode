43354f3ed9af4320195b55720fac27c175c73e29
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-27 15:05:55 +0300

    add solution of Two Sum Less Than K

---
+++ c#_LeetCode/Array/Easy/TwoSumLessThanK/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Array.Easy.TwoSumLessThanK;
+
+//https://leetcode.com/problems/two-sum-less-than-k/description/
+public class Solution
+{
+    public int TwoSumLessThanK(int[] nums, int k)
+    {
+        System.Array.Sort(nums);
+
+        int i = 0;
+        int j = nums.Length - 1;
+
+        int result = -1;
+
+        while (i < j)
+        {
+            int sum = nums[i] + nums[j];
+
+            if (sum < k)
+            {
+                result = System.Math.Max(result, sum);
+                i++;
+            }
+            else j--;
+        }
+
+        return result;
+    }
+}
