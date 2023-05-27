8a94349297fe0d8a1ef85a721be7fd0dbf218c66
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-27 12:49:08 +0300

    add solution of Summary Ranges

---
+++ LeetCodeTests/String/Easy/SummaryRanges/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.String.Easy.SummaryRanges;
+using Xunit;
+
+namespace LeetCodeTests.String.Easy.SummaryRanges;
+
+public class SolutionTests
+{
+    [Fact]
+    public void SummaryRanges_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] nums = { 0, 1, 2, 4, 5, 7 };
+
+        // Act
+        var result = solution.SummaryRanges(nums);
+
+        // Assert
+        Assert.Equal(result, new string[] { "0->2","4->5","7" });
+
+        // Arrange
+        nums = new int[] { 0,2,3,4,6,8,9 };
+
+        // Act
+        result = solution.SummaryRanges(nums);
+
+        // Assert
+        Assert.Equal(result, new string[] { "0", "2->4", "6", "8->9" });
+    }
+}
---
+++ c#_LeetCode/String/Easy/SummaryRanges/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.String.Easy.SummaryRanges;
+
+//https://leetcode.com/problems/summary-ranges/description
+public class Solution
+{
+    public IList<string> SummaryRanges(int[] nums)
+    {
+        List<string> result = new();
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int start = nums[i];
+
+            while (i + 1 < nums.Length && nums[i + 1] == nums[i] + 1)
+                i++;
+
+            if (nums[i] == start)
+                result.Add($"{start}");
+            else
+                result.Add($"{start}->{nums[i]}");
+        }
+
+        return result;
+    }
+}
