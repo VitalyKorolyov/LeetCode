2845e0f78639eec5ec0363bef3c63371d724774b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-27 13:58:17 +0300

    update solution of Valid Palindrome

--- c#_LeetCode/String/Easy/ValidPalindrome/Solution.cs
+++ c#_LeetCode/String/Easy/ValidPalindrome/Solution.cs
@@ -1,31 +1,31 @@
-﻿namespace LeetCode.String.Easy.ValidPalindrome
+﻿namespace LeetCode.String.Easy.ValidPalindrome;
+
+//https://leetcode.com/problems/valid-palindrome/
+public class Solution
 {
-    //https://leetcode.com/problems/valid-palindrome/
-    public class Solution
+    public bool IsPalindrome(string s)
     {
-        public bool IsPalindrome(string s)
+        if (s == null || s.Length <= 1) return true;
+
+        int i = 0, j = s.Length - 1;
+
+        while (i < j)
         {
-            if (s == null || s.Length <= 1) return true;
-
-            int i = 0, j = s.Length - 1;
-
-            while (i < j)
+            if (!char.IsLetterOrDigit(s[i]))
             {
-                if (!char.IsLetterOrDigit(s[i]))
-                {
-                    i++;
-                    continue;
-                }
-                if (!char.IsLetterOrDigit(s[j]))
-                {
-                    j--;
-                    continue;
-                }
-
-                if (char.ToLower(s[i++]) != char.ToLower(s[j--])) return false;
+                i++;
+                continue;
+            }
+            if (!char.IsLetterOrDigit(s[j]))
+            {
+                j--;
+                continue;
             }
 
-            return true;
+            if (char.ToLower(s[i++]) != char.ToLower(s[j--])) 
+                return false;
         }
+
+        return true;
     }
 }
