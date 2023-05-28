9610f13d1f9cd58af651ae0a7b4b82ad049a9acd
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-28 14:37:49 +0300

    add solution of Intersection of Two Linked Lists

---
+++ c#_LeetCode/LinkedList/Easy/IntersectionOfTwoLinkedLists/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.LinkedList.Easy.IntersectionOfTwoLinkedLists;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+//https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+public class Solution
+{
+    public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+    {
+        HashSet<ListNode> hash = new();
+
+        while (headA != null)
+        {
+            hash.Add(headA);
+            headA = headA.next;
+        }
+
+        while (headB != null)
+        {
+            if (hash.Contains(headB)) return headB;
+            headB = headB.next;
+        }
+
+        return null;
+    }
+}
