ed6b9e8d001e487ff48387ecbe706909880f874b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-28 14:48:50 +0300

    update solution of Container With Most Water

--- c#_LeetCode/Array/Medium/ContainerWithMostWater/Solution.cs
+++ c#_LeetCode/Array/Medium/ContainerWithMostWater/Solution.cs
@@ -1,24 +1,23 @@
-﻿namespace LeetCode.Array.Medium.ContainerWithMostWater
+﻿namespace LeetCode.Array.Medium.ContainerWithMostWater;
+
+//https://leetcode.com/problems/container-with-most-water/
+public class Solution
 {
-    //https://leetcode.com/problems/container-with-most-water/
-    public class Solution
+    public int MaxArea(int[] height)
     {
-        public int MaxArea(int[] height)
+        int l = 0;
+        int r = height.Length - 1;
+        int maxAreaOfWater = 0;
+
+        while (l < r)
         {
-            var left = 0;
-            var right = height.Length - 1;
-            var maxArea = int.MinValue;
+            var currentHeight = System.Math.Min(height[l], height[r]);
+            maxAreaOfWater = System.Math.Max(maxAreaOfWater, currentHeight * (r - l));
 
-            while (left != right)
-            {
-                var area = System.Math.Min(height[right], height[left]) * (right - left);
-                maxArea = System.Math.Max(area, maxArea);
-
-                if (height[left] > height[right]) right--;
-                else left++;
-            }
-
-            return maxArea;
+            if (height[l] > height[r]) r--;
+            else l++;
         }
+
+        return maxAreaOfWater;
     }
 }
