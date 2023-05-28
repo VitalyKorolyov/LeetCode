27cb8bb8b1a74adc033e92a1436b01ff3d3c571b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-28 17:09:20 +0300

    update solution of ransom note

--- c#_LeetCode/String/Easy/RansomNote/Solution.cs
+++ c#_LeetCode/String/Easy/RansomNote/Solution.cs
@@ -1,22 +1,21 @@
-﻿namespace LeetCode.String.Easy.RansomNote
+﻿namespace LeetCode.String.Easy.RansomNote;
+
+//https://leetcode.com/problems/ransom-note/description/
+public class Solution
 {
-    //https://leetcode.com/problems/ransom-note/description/
-    public class Solution
+    public bool CanConstruct(string ransomNote, string magazine)
     {
-        public bool CanConstruct(string ransomNote, string magazine)
+        int[] counter = new int[26];
+
+        for (int i = 0; i < magazine.Length; i++)
+            counter[magazine[i] - 'a']++;
+
+        for (int i = 0; i < ransomNote.Length; i++)
         {
-            int[] counter = new int[26];
-
-            for (int i = 0; i < magazine.Length; i++)
-                counter[magazine[i] - 'a']++;
-
-            for (int i = 0; i < ransomNote.Length; i++)
-            {
-                if (counter[ransomNote[i] - 'a'] == 0) return false;
-                counter[ransomNote[i] - 'a']--;
-            }
-
-            return true;
+            if (counter[ransomNote[i] - 'a'] == 0) return false;
+            counter[ransomNote[i] - 'a']--;
         }
+
+        return true;
     }
 }
