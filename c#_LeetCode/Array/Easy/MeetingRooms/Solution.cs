b4fb03db9997b366251c43e1ca28c5428f983bf9
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-30 00:04:21 +0300

    update solution of Meeting Rooms

--- c#_LeetCode/Array/Easy/MeetingRooms/Solution.cs
+++ c#_LeetCode/Array/Easy/MeetingRooms/Solution.cs
@@ -1,20 +1,15 @@
-﻿namespace LeetCode.Array.Easy.MeetingRooms
+﻿namespace LeetCode.Array.Easy.MeetingRooms;
+
+//https://leetcode.com/problems/meeting-rooms/
+public class Solution
 {
-    //https://leetcode.com/problems/meeting-rooms/
-    public class Solution
+    public bool CanAttendMeetings(int[][] intervals)
     {
-        public bool CanAttendMeetings(int[][] intervals)
-        {
-            if (intervals.Length == 0 || intervals.Length == 1) return true;
+        System.Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
 
-            System.Array.Sort(intervals, (x, y) => x[0].CompareTo(y[0]));
+        for (int i = 1; i < intervals.Length; i++)
+            if (intervals[i - 1][1] > intervals[i][0]) return false;
 
-            for (int i = 1; i < intervals.Length; i++)
-            {
-                if (intervals[i - 1][1] >= intervals[i][0]) return false;
-            }
-
-            return true;
-        }
+        return true;
     }
 }
