9138d5c5cba65569fe8e6a5496cf204259965a5f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-05-30 00:26:47 +0300

    update Meeting Rooms II solution;

--- LeetCodeTests/Array/Medium/MeetingRoomsII/SolutionTests.cs
+++ LeetCodeTests/Array/Medium/MeetingRoomsII/SolutionTests.cs
@@ -1,29 +1,28 @@
 ﻿using LeetCode.Array.Medium.MeetingRoomsII;
-using System;
 using Xunit;
 
-namespace LeetCodeTests.Array.Medium.MeetingRoomsII
+namespace LeetCodeTests.Array.Medium.MeetingRoomsII;
+
+public class SolutionTests
 {
-    public class SolutionTests
+    [Fact]
+    public void MinMeetingRooms_StateUnderTest_ExpectedBehavior()
     {
-        [Fact]
-        public void MinMeetingRooms_StateUnderTest_ExpectedBehavior()
-        {
-            // Arrange
-            // Arrange
-            var solution = new Solution();
-            int[][] intervals = { new[] { 1, 10 }, 
-                new[] { 2, 7 }, new[] { 3, 19 }, 
-                new[] { 8, 12 }, new[] { 10, 20 },
-                new[] { 11, 30 }
-            };
+        // Arrange
+        var solution = new Solution();
+        int[][] intervals = {
+            new[] { 1, 10 },
+            new[] { 2, 7 },
+            new[] { 3, 19 },
+            new[] { 8, 12 },
+            new[] { 10, 20 },
+            new[] { 11, 30 } 
+        };
 
-            // Act
-            var result = solution.MinMeetingRooms(
-                intervals);
+        // Act
+        var result = solution.MinMeetingRooms(intervals);
 
-            // Assert
-            Assert.Equal(4, result);
-        }
+        // Assert
+        Assert.Equal(4, result);
     }
 }
--- c#_LeetCode/Array/Medium/MeetingRoomsII/Solution.cs
+++ c#_LeetCode/Array/Medium/MeetingRoomsII/Solution.cs
@@ -1,33 +1,30 @@
-﻿namespace LeetCode.Array.Medium.MeetingRoomsII
+﻿namespace LeetCode.Array.Medium.MeetingRoomsII;
+
+//https://leetcode.com/problems/meeting-rooms-ii/
+public class Solution
 {
-    //https://leetcode.com/problems/meeting-rooms-ii/
-    public class Solution
+    public int MinMeetingRooms(int[][] intervals)
     {
-        public int MinMeetingRooms(int[][] intervals)
+        int[] starts = new int[intervals.Length];
+        int[] ends = new int[intervals.Length];
+
+        for (int i = 0; i < intervals.Length; i++)
         {
-            if (intervals == null || intervals.Length == 0) return 0;
-
-            var starts = new int[intervals.Length];
-            var ends = new int[intervals.Length];
-
-            for (var i = 0; i < intervals.Length; i++)
-            {
-                starts[i] = intervals[i][0];
-                ends[i] = intervals[i][1];
-            }
-
-            System.Array.Sort(starts);
-            System.Array.Sort(ends);
-
-            var result = 0;
-
-            for (int i = 0, j = 0; i < starts.Length; i++)
-            {
-                if (starts[i] < ends[j]) result++;
-                else j++;
-            }
-
-            return result;
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
         }
+
+        System.Array.Sort(starts);
+        System.Array.Sort(ends);
+
+        int count = 0;
+
+        for (int i = 0, j = 0; i < intervals.Length;)
+        {
+            if (starts[i++] < ends[j]) count++;
+            else j++;
+        }
+
+        return count;
     }
 }
