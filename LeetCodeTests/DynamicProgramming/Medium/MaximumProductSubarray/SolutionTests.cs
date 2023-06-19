561bd2a3de081665b000e27477ac5ed3be981bf1
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-06-19 23:42:23 +0300

    add solution of Maximum Product Subarray

---
+++ LeetCodeTests/DynamicProgramming/Medium/MaximumProductSubarray/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.DynamicProgramming.Medium.MaximumProductSubarray;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming.Medium.MaximumProductSubarray;
+
+public class SolutionTests
+{
+    [Fact]
+    public void MaxProduct_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] nums = new int[] { 2, 1, 1, -2 };
+
+        // Act
+        var result = solution.MaxProduct(nums);
+
+        // Assert
+        Assert.Equal(1, result);
+    }
+}
---
+++ c#_LeetCode/DynamicProgramming/Medium/MaximumProductSubarray/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.DynamicProgramming.Medium.MaximumProductSubarray;
+
+//https://leetcode.com/problems/maximum-product-subarray/description/
+public class Solution
+{
+    public int MaxProduct(int[] nums)
+    {
+        int left = 1, right = 1;
+        int maxProduct = nums[0];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            left = left == 0 ? 1 : left;
+            right = right == 0 ? 1 : right;
+
+            left *= nums[i];
+            right *= nums[nums.Length - 1 - i];
+
+            maxProduct = System.Math.Max(maxProduct, System.Math.Max(left, right));
+        }
+
+        return maxProduct;
+    }
+}
