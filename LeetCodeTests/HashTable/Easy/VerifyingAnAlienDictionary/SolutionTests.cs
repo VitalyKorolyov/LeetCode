30f038f4b926a8726f1e71aa80006a322e946c66
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-06-21 00:56:17 +0300

    add solution of Verifying an Alien Dictionary;

---
+++ LeetCodeTests/HashTable/Easy/VerifyingAnAlienDictionary/SolutionTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCode.HashTable.Easy.VerifyingAnAlienDictionary;
+using Xunit;
+
+namespace LeetCodeTests.HashTable.Easy.VerifyingAnAlienDictionary;
+
+public class SolutionTests
+{
+    [Fact]
+    public void IsAlienSorted_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string[] words = { "my", "f" };
+        string order = "gelyriwxzdupkjctbfnqmsavho";
+
+        // Act
+        var result = solution.IsAlienSorted(
+            words,
+            order);
+
+        // Assert
+        Assert.True(result);
+    }
+}
---
+++ c#_LeetCode/HashTable/Easy/VerifyingAnAlienDictionary/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.HashTable.Easy.VerifyingAnAlienDictionary;
+
+public class Solution
+{
+    public bool IsAlienSorted(string[] words, string order)
+    {
+        Dictionary<char, int> alienDictionary = new();
+        for (int i = 0; i < order.Length; i++)
+            alienDictionary[order[i]] = i;
+
+        for (int i = 0; i + 1 < words.Length; i++)
+        {
+            string first = words[i];
+            string second = words[i + 1];
+
+            int minLength = System.Math.Min(first.Length, second.Length);
+            for (int j = 0; j < minLength; j++)
+            {
+                var letterFromFirst = first[j];
+                var letterFromSecond = second[j];
+
+                if (alienDictionary[letterFromFirst] < alienDictionary[letterFromSecond]) break;
+                if (alienDictionary[letterFromFirst] > alienDictionary[letterFromSecond]) return false;
+
+                if (j == minLength - 1 && first.Length > second.Length)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
