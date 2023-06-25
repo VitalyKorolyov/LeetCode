0252f343a30b1fe1a897475bc17c48b667c8975b
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-06-25 23:10:04 +0300

    add solution of Reorder Routes to Make All Paths Lead to the City Zero;

---
+++ LeetCodeTests/Graph/Medium/ReorderRoutesToMakeAllPathsLeadToTheCityZero/SolutionTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Graph.Medium.ReorderRoutesToMakeAllPathsLeadToTheCityZero;
+using Xunit;
+
+namespace LeetCodeTests.Graph.Medium.ReorderRoutesToMakeAllPathsLeadToTheCityZero;
+
+public class SolutionTests
+{
+    [Fact]
+    public void MinReorder_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int n = 6;
+        int[][] connections = new int[][]{
+            new int[]{ 0, 1},
+            new int[] { 1, 3},
+            new int[] { 2, 3},
+            new int[] { 4, 0},
+            new int[] { 4, 5} };
+
+        // Act
+        var result = solution.MinReorder(n, connections);
+
+        // Assert
+        Assert.True(true);
+    }
+}
---
+++ c#_LeetCode/Graph/Medium/ReorderRoutesToMakeAllPathsLeadToTheCityZero/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Graph.Medium.ReorderRoutesToMakeAllPathsLeadToTheCityZero;
+
+//https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/description/
+public class Solution
+{
+    public int MinReorder(int n, int[][] connections)
+    {
+        Dictionary<int, HashSet<int>> graph = new();
+        Dictionary<int, HashSet<int>> neighborhoods = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = new HashSet<int>();
+            neighborhoods[i] = new HashSet<int>();
+        }
+
+        for (int i = 0; i < connections.Length; i++)
+        {
+            int parent = connections[i][0];
+            int child = connections[i][1];
+
+            graph[parent].Add(child);
+            neighborhoods[parent].Add(child);
+            neighborhoods[child].Add(parent);
+        }
+
+        Queue<int> queue = new();
+        HashSet<int> visited = new();
+        visited.Add(0);
+        queue.Enqueue(0);
+        int change = 0;
+
+        while(queue.Count > 0)
+        {
+            var city = queue.Dequeue();
+
+            foreach (var child in neighborhoods[city]) 
+            {
+                if (visited.Contains(child)) continue;
+
+                if (graph[city].Contains(child))
+                    change++;
+
+                visited.Add(child);
+                queue.Enqueue(child);
+            }
+        }
+
+        return change;
+    }
+}
