b7bd3a84b98874d04b4c620ce5a4a0207fe1cca1
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-06-28 00:14:04 +0300

    add solution of Coin Change

---
+++ LeetCodeTests/Array/Medium/CoinChange/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.CoinChange;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.CoinChange;
+
+public class SolutionTests
+{
+    [Fact]
+    public void CoinChange_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] coins = new int[] { 1,2,5 };
+        int amount = 100;
+
+        // Act
+        var result = solution.CoinChange(coins, amount);
+
+        // Assert
+        Assert.True(result == 20);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/CoinChange/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Array.Medium.CoinChange;
+
+//https://leetcode.com/problems/coin-change/description/
+public class Solution
+{
+    public int CoinChange(int[] coins, int amount)
+    {
+        var result = CoinChange(coins, amount, new Dictionary<int, int>());
+        return result == int.MaxValue ? -1 : result;
+    }
+
+    private int CoinChange(int[] coins, int amount, Dictionary<int, int> visited)
+    {
+        if (amount == 0) return 0;
+        if (amount < 0) return int.MaxValue;
+        if (visited.ContainsKey(amount)) return visited[amount];
+
+        int min = int.MaxValue;
+
+        for(int i = 0; i < coins.Length; i++)
+        {
+            var result = CoinChange(coins, amount - coins.ElementAt(i), visited);
+
+            if(result != int.MaxValue)
+                min = System.Math.Min(min, result + 1);
+        }
+
+        if (!visited.ContainsKey(amount))
+            visited[amount] = min;
+
+        return min;
+    }
+}
