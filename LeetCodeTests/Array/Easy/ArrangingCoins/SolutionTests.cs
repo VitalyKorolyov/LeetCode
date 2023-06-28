6680beac82c69b7a93b6cf737a3ddb14dfad4a8f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-06-28 21:41:38 +0300

    add solution of Arranging Coins;

---
+++ LeetCodeTests/Array/Easy/ArrangingCoins/SolutionTests.cs
@@ -0,0 +1,21 @@
+﻿using LeetCode.Array.Easy.ArrangingCoins;
+using Xunit;
+
+namespace LeetCodeTests.Array.Easy.ArrangingCoins;
+
+public class SolutionTests
+{
+    [Fact]
+    public void ArrangeCoins_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int n = 8;
+
+        // Act
+        var result = solution.ArrangeCoins(n);
+
+        // Assert
+        Assert.True(result == 3);
+    }
+}
---
+++ c#_LeetCode/Array/Easy/ArrangingCoins/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Array.Easy.ArrangingCoins;
+
+//https://leetcode.com/problems/arranging-coins/description/
+public class Solution
+{
+    public int ArrangeCoins(int n)
+    {
+        if (n == 1) return 1;
+
+        long left = 1;
+        long right = n;
+
+        while (left < right)
+        {
+            long mid = left + (right - left) / 2;
+            long midResult = mid * (1 + mid) / 2;
+
+            if (midResult > n)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return (int)left - 1;
+    }
+}
