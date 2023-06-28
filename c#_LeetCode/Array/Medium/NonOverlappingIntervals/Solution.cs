6be7195b147ad9a03a9d450502742f41c3779311
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-06-28 23:02:07 +0300

    add solution of Non-overlapping Intervals

---
+++ c#_LeetCode/Array/Medium/NonOverlappingIntervals/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Array.Medium.NonOverlappingIntervals;
+
+//https://leetcode.com/problems/non-overlapping-intervals/description/
+public class Solution
+{
+    public int EraseOverlapIntervals(int[][] intervals)
+    {
+        System.Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+
+        int overlapCount = 0;
+        int l = intervals[0][1];
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            if (IsOverlap(l, intervals[i]))
+            {
+                overlapCount++;
+                l = intervals[i][1] < l ? intervals[i][1] : l;
+            }
+            else
+                l = intervals[i][1];
+        }
+
+        return overlapCount;
+    }
+
+    private bool IsOverlap(int endOfFirst, int[] second)
+    {
+        return endOfFirst > second[0];
+    }
+}
