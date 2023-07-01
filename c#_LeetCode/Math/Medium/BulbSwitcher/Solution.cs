ce115cc228e5122defa85929ed98df3768fbb334
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-01 22:09:57 +0300

    add solution of Bulb Switcher

---
+++ c#_LeetCode/Math/Medium/BulbSwitcher/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Math.Medium.BulbSwitcher;
+
+//https://leetcode.com/problems/bulb-switcher/description/
+public class Solution
+{
+    public int BulbSwitch(int n)
+    {
+        return (int)System.Math.Sqrt(n);
+    }
+}
