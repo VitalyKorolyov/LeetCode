e45d8870a38c6713867e35e8e90b89410aa54a5f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-01 23:05:35 +0300

    add solution of Excel Sheet Column Number

---
+++ c#_LeetCode/Math/Easy/ExcelSheetColumnNumber/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Math.Easy.ExcelSheetColumnNumber;
+
+//https://leetcode.com/problems/excel-sheet-column-number/description/
+public class Solution
+{
+    public int TitleToNumber(string columnTitle)
+    {
+        int number = 0;
+
+        for (int i = 0; i < columnTitle.Length; i++)
+            number = 26 * number + (columnTitle[i] - 'A' + 1);
+
+        return number;
+    }
+}
