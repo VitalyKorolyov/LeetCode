7cbebc5cef44fc262c70263a672354dad05d641f
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-02 22:15:29 +0300

    add solution of Maximum Points You Can Obtain from Cards

---
+++ LeetCodeTests/Array/Medium/MaximumPointsYouCanObtainFromCards/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Array.Medium.MaximumPointsYouCanObtainFromCards;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.MaximumPointsYouCanObtainFromCards;
+
+public class SolutionTests
+{
+    [Fact]
+    public void MaxScore_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] cardPoints = new int[] { 1, 2, 3, 4, 5, 6, 1 };
+        int k = 3;
+
+        // Act
+        var result = solution.MaxScore(cardPoints, k);
+
+        // Assert
+        Assert.Equal(12, result);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/MaximumPointsYouCanObtainFromCards/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Array.Medium.MaximumPointsYouCanObtainFromCards;
+
+//https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/description/
+public class Solution
+{
+    public int MaxScore(int[] cardPoints, int k)
+    {
+        int currentScore = 0;
+        for (int i = cardPoints.Length - k; i < cardPoints.Length; i++)
+            currentScore += cardPoints[i];
+
+        int maxScore = currentScore;
+
+        int left = 0;
+        while(left < k)
+        {
+            currentScore -= cardPoints[cardPoints.Length - k + left];
+            currentScore += cardPoints[left];
+            left++;
+
+            maxScore = System.Math.Max(maxScore, currentScore);
+        }
+
+        return maxScore;
+    }
+}
