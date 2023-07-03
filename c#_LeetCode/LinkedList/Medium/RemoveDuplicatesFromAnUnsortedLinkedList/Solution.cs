bca0f3bf614006f259a2e153a0f265109f51574e
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-04 00:36:28 +0300

    add solution of Remove Duplicates From an Unsorted Linked List

---
+++ c#_LeetCode/LinkedList/Medium/RemoveDuplicatesFromAnUnsortedLinkedList/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.LinkedList.Medium.RemoveDuplicatesFromAnUnsortedLinkedList;
+
+//https://leetcode.com/problems/remove-duplicates-from-an-unsorted-linked-list/description/
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode DeleteDuplicatesUnsorted(ListNode head)
+    {
+        Dictionary<int, int> nodeCounter = new();
+        ListNode dummy = new(0, head);
+
+        while (head != null)
+        {
+            if (nodeCounter.ContainsKey(head.val))
+                nodeCounter[head.val]++;
+            else nodeCounter[head.val] = 1;
+
+            head = head.next;
+        }
+
+        ListNode prev = dummy;
+        head = dummy.next;
+
+        while(head != null)
+        {
+            if (nodeCounter[head.val] > 1)
+                prev.next = head.next;
+            else
+                prev = head;
+
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}
