60c15c8e175b03785d3188d9e785c077f296c365
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-06 01:15:46 +0300

    add solution of Remove All Adjacent Duplicates in String II

---
+++ LeetCodeTests/Stack/Medium/RemoveAllAdjacentDuplicatesInStringII/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Stack.Medium.RemoveAllAdjacentDuplicatesInStringII;
+using Xunit;
+
+namespace LeetCodeTests.Stack.Medium.RemoveAllAdjacentDuplicatesInStringII;
+
+public class SolutionTests
+{
+    [Fact]
+    public void RemoveDuplicates_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        string s = "deeedbbcccbdaa";
+        int k = 3;
+
+        // Act
+        var result = solution.RemoveDuplicates(s, k);
+
+        // Assert
+        Assert.Equal("aa", result);
+    }
+}
---
+++ c#_LeetCode/Stack/Medium/RemoveAllAdjacentDuplicatesInStringII/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Stack.Medium.RemoveAllAdjacentDuplicatesInStringII;
+
+//https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/description/
+public class Solution
+{
+    public string RemoveDuplicates(string s, int k)
+    {
+        Stack<(char letter, int count)> stack = new();
+
+        int count = 1;
+        foreach (char currentLetter in s)
+        {
+            if (stack.Count > 0)
+            {
+                var item = stack.Peek();
+
+                if (item.letter == currentLetter)
+                {
+                    if(item.count == k - 1)
+                    {
+                        while(stack.Count > 0 && stack.Peek().letter == currentLetter)
+                            stack.Pop();
+
+                        count = 1;
+                        continue;
+                    }
+                    else
+                        count = item.count + 1;
+                }
+                else
+                    count = 1;
+            }
+            
+            stack.Push((currentLetter, count++));
+        }
+
+        StringBuilder answer = new();
+
+        while(stack.Count > 0)
+            answer.Append(stack.Pop().letter);
+
+        int left = 0;
+        int right = answer.Length - 1;
+        while (left < right)
+        {
+            char saved = answer[left];
+            answer[left++] = answer[right];
+            answer[right--] = saved;
+        }
+
+        return answer.ToString();
+    }
+}
