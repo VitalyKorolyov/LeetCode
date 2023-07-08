7f8b507d432460fef3f9384c9b359b3c054a0b5c
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-08 23:10:57 +0300

    add solution of Number of Visible People in a Queue

---
+++ c#_LeetCode/Stack/Hard/NumberOfVisiblePeopleInAQueue/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Stack.Hard.NumberOfVisiblePeopleInAQueue;
+
+//https://leetcode.com/problems/number-of-visible-people-in-a-queue/description/
+public class Solution
+{
+    public int[] CanSeePersonsCount(int[] heights)
+    {
+        int[] result = new int[heights.Length];
+        Stack<int> stack = new();
+
+        for (int i = heights.Length - 1; i >= 0; i--)
+        {
+            while (stack.Count > 0 && stack.Peek() < heights[i])
+            {
+                result[i]++;
+                stack.Pop();
+            }
+
+            if(stack.Count > 0 && stack.Peek() > heights[i])
+                result[i]++;
+
+            stack.Push(heights[i]);
+        }
+
+        return result;
+    }
+}
