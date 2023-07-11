d2a96e719362cfc6d57e9f2330d2e7c626af25fe
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-11 22:20:19 +0300

    add solution of Number of Provinces

---
+++ c#_LeetCode/Graph/Medium/NumberOfProvinces/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Graph.Medium.NumberOfProvinces;
+
+//https://leetcode.com/problems/number-of-provinces/description/
+public class Solution
+{
+    public int FindCircleNum(int[][] isConnected)
+    {
+        HashSet<int> visited = new();
+        int numberOfProvince = 0;
+
+        for (int node = 0; node < isConnected.Length; node++)
+        {
+            if (visited.Contains(node)) continue;
+
+            Dfs(isConnected, visited, node);
+            numberOfProvince++;
+        }
+
+        return numberOfProvince;
+    }
+
+    private void Dfs(int[][] isConnected, HashSet<int> visited, int node)
+    {
+        visited.Add(node);
+
+        for (int i = 0; i < isConnected[node].Length; i++)
+            if (isConnected[node][i] == 1 && !visited.Contains(i))
+                Dfs(isConnected, visited, i);
+    }
+}
