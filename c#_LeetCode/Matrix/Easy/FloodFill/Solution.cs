0a1bf9e510637e61b330ccaf1c29032af778eaf1
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-12 22:02:07 +0300

    update solution of Flood Fill;

--- c#_LeetCode/Matrix/Easy/FloodFill/Solution.cs
+++ c#_LeetCode/Matrix/Easy/FloodFill/Solution.cs
@@ -1,29 +1,78 @@
-﻿namespace LeetCode.Matrix.Easy.FloodFill
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Matrix.Easy.FloodFill;
+
+//https://leetcode.com/problems/flood-fill/description/
+public class Solution
 {
-    //https://leetcode.com/problems/flood-fill/
-    public class Solution
+    private int[][] directions = new int[4][]
     {
-        public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+        new int[]{ -1, 0 },
+        new int[]{ 0, -1 },
+        new int[]{ 1, 0 },
+        new int[]{ 0, 1 }
+    };
+
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color)
+    {
+        int[][] newImage = (int[][])image.Clone();
+        if (image[sr][sc] == color) return newImage;
+
+        int origin = image[sr][sc];
+        Queue<(int i, int j)> queue = new();
+        HashSet<(int i, int j)> visited = new();
+
+        visited.Add((sr, sc));
+        queue.Enqueue((sr, sc));
+
+        while (queue.Count > 0)
         {
-            if (image[sr][sc] == color) return image;
+            (int i, int j) = queue.Dequeue();
+            newImage[i][j] = color;
 
-            PerformFloodFill(image, sr, sc, image[sr][sc], color);
+            foreach (var item in directions)
+            {
+                var newI = item[0] + i;
+                var newJ = item[1] + j;
 
-            return image;
+                var cellToVisite = (newI, newJ);
+
+                if (newI < 0 || newI >= newImage.Length) continue;
+                if (newJ < 0 || newJ >= newImage[0].Length) continue;
+                if (newImage[newI][newJ] != origin) continue;
+                if (visited.Contains(cellToVisite)) continue;
+
+                visited.Add(cellToVisite);
+                queue.Enqueue(cellToVisite);
+            }
         }
 
-        private void PerformFloodFill(int[][] image, int i, int j, int originalColor, int newColor)
-        {
-            if (i < 0 || i >= image.Length) return;
-            if (j < 0 || j >= image[i].Length) return;
-            if (image[i][j] != originalColor) return;
+        return newImage;
+    }
 
-            image[i][j] = newColor;
 
-            PerformFloodFill(image, i, j + 1, originalColor, newColor);
-            PerformFloodFill(image, i, j - 1, originalColor, newColor);
-            PerformFloodFill(image, i + 1, j, originalColor, newColor);
-            PerformFloodFill(image, i - 1, j, originalColor, newColor);
-        }
+    //DFS
+    public int[][] FloodFillDfs(int[][] image, int sr, int sc, int color)
+    {
+        int[][] newImage = (int[][])image.Clone();
+        if (image[sr][sc] == color) return newImage;
+
+        Dfs(newImage, sr, sc, newImage[sr][sc], color);
+
+        return newImage;
+    }
+
+    private void Dfs(int[][] image, int i, int j, int originColor, int newColor)
+    {
+        if (i < 0 || i >= image.Length) return;
+        if (j < 0 || j >= image[0].Length) return;
+        if (originColor != image[i][j]) return;
+
+        image[i][j] = newColor;
+
+        Dfs(image, i + 1, j, originColor, newColor);
+        Dfs(image, i, j + 1, originColor, newColor);
+        Dfs(image, i - 1, j, originColor, newColor);
+        Dfs(image, i, j - 1, originColor, newColor);
     }
 }
