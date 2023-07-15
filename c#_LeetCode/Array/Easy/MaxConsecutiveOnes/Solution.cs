7e4d574bd32bcd56d0ff64d419c18f3940c54690
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-16 00:12:00 +0300

    add solution of Max Consecutive Ones

---
+++ c#_LeetCode/Array/Easy/MaxConsecutiveOnes/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Array.Easy.MaxConsecutiveOnes;
+
+//https://leetcode.com/problems/max-consecutive-ones/description/
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        int left = 0;
+        int max = 0;
+
+        for(int right = 0; right < nums.Length; right++)
+        {
+            if (nums[right] == 0)
+                left = right + 1;
+
+            max = System.Math.Max(max, right - left + 1);
+        }
+
+        return max;
+    }
+}
