f3a526adfb68a961bf59db6fecf713d9371283ca
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-07-15 23:09:42 +0300

    update solution of Max Consecutive Ones III

--- c#_LeetCode/Array/Medium/MaxConsecutiveOnesIII/Solution.cs
+++ c#_LeetCode/Array/Medium/MaxConsecutiveOnesIII/Solution.cs
@@ -1,33 +1,26 @@
-﻿
-namespace LeetCode.Array.Medium.MaxConsecutiveOnesIII
+﻿namespace LeetCode.Array.Medium.MaxConsecutiveOnesIII;
+
+//https://leetcode.com/problems/max-consecutive-ones-iii/description/
+public class Solution
 {
-    //https://leetcode.com/problems/max-consecutive-ones-iii/
-    public class Solution
+    public int LongestOnes(int[] nums, int k)
     {
-        public int LongestOnes(int[] nums, int k)
+        int left = 0;
+        int counter = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < nums.Length; right++)
         {
-            int left = 0, right = 0;
-            int oneCounter = 0;
-            int maxLength = 0;
+            if (nums[right] == 0)
+                counter++;
 
-            while(right < nums.Length)
-            {
-                if (nums[right] == 1)
-                    oneCounter++;
+            while (counter > k)
+                if (nums[left++] == 0)
+                    counter--;
 
-                if (right - left + 1 - oneCounter > k)
-                {
-                    if (nums[left] == 1)
-                        oneCounter--;
-                    left++;
-                }
-
-                maxLength = System.Math.Max(maxLength, right - left + 1);
-
-                right++;
-            }
-
-            return maxLength;
+            maxLength = System.Math.Max(maxLength, right - left + 1);
         }
+
+        return maxLength;
     }
 }
