732d13fd742f2a88c2a6ef31b126d34173f1c631
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-08-06 22:20:06 +0300

    update solution of Merge k Sorted Lists

--- c#_LeetCode/LinkedList/Hard/MergeKSortedLists/Solution.cs
+++ c#_LeetCode/LinkedList/Hard/MergeKSortedLists/Solution.cs
@@ -1,70 +1,45 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
-namespace LeetCode.LinkedList.Hard.MergeKSortedLists
+namespace LeetCode.LinkedList.Hard.MergeKSortedLists;
+
+//https://leetcode.com/problems/merge-k-sorted-lists/
+public class ListNode
 {
-    //https://leetcode.com/problems/merge-k-sorted-lists/
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
+    public int val;
+    public ListNode next;
 
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
     }
+}
 
-    public class Solution
+public class Solution
+{
+    public ListNode MergeKLists(ListNode[] lists)
     {
-        public ListNode MergeKLists(ListNode[] lists)
+        if(lists == null || !lists.Any()) return null;
+
+        ListNode head = new(0);
+        PriorityQueue<ListNode, int> priorityQueue = new();
+
+        foreach(ListNode list in lists.Where(x => x != null))
+            priorityQueue.Enqueue(list, list.val);
+
+        ListNode node = head;
+        while(priorityQueue.Count > 0)
         {
-            if (!lists.Any()) return null;
+            var currentList = priorityQueue.Dequeue();
 
-            int interval = 1;
-            while (interval < lists.Length)
-            {
-                for (int i = 0; i + interval < lists.Length; i += interval * 2)
-                    lists[i] = Merge(lists[i], lists[i + interval]);
+            node.next = new ListNode(currentList.val);
+            node = node.next;
 
-                interval *= 2;
-            }
-
-            return lists[0];
+            if(currentList.next != null)
+                priorityQueue.Enqueue(currentList.next, currentList.next.val);
         }
 
-        private ListNode Merge(ListNode l1, ListNode l2)
-        {
-            var res = new ListNode();
-            var head = res;
-
-            while (l1 != null && l2 != null)
-            {
-                if (l1.val < l2.val)
-                {
-                    head.next = new ListNode(l1.val);
-                    l1 = l1.next;
-                }
-                else
-                {
-                    head.next = new ListNode(l2.val);
-                    l2 = l2.next;
-                }
-
-                head = head.next;
-            }
-
-            if (l1 != null)
-            {
-                head.next = l1;
-            }
-
-            if (l2 != null)
-            {
-                head.next = l2;
-            }
-
-            return res.next;
-        }
+        return head.next;
     }
 }
