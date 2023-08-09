9166cdc794b110d2a9fe71f253ffeb0333b6d3db
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-08-09 22:28:19 +0300

    update solution of Graph Valid Tree

--- LeetCodeTests/Graph/Medium/GraphValidTree/SolutionTests.cs
+++ LeetCodeTests/Graph/Medium/GraphValidTree/SolutionTests.cs
@@ -1,28 +1,26 @@
 ﻿using LeetCode.Graph.Medium.GraphValidTree;
-using System;
 using Xunit;
 
-namespace LeetCodeTests.Graph.Medium.GraphValidTree
+namespace LeetCodeTests.Graph.Medium.GraphValidTree;
+
+public class SolutionTests
 {
-    public class SolutionTests
+    [Fact]
+    public void ValidTree_StateUnderTest_ExpectedBehavior()
     {
-        [Fact]
-        public void ValidTree_StateUnderTest_ExpectedBehavior()
-        {
-            // Arrange
-            var solution = new Solution();
-            int n = 5;
-            int[][] edges = new int[4][];
-            edges[0] = new int[] {0, 1};
-            edges[1] = new int[] { 0, 2 };
-            edges[2] = new int[] { 0, 3 };
-            edges[3] = new int[] { 1, 4 };
+        // Arrange
+        var solution = new Solution();
+        int n = 5;
+        int[][] edges = new int[4][];
+        edges[0] = new int[] { 0, 1 };
+        edges[1] = new int[] { 0, 2 };
+        edges[2] = new int[] { 0, 3 };
+        edges[3] = new int[] { 1, 4 };
 
-            // Act
-            var result = solution.ValidTree(n, edges);
+        // Act
+        var result = solution.ValidTree(n, edges);
 
-            // Assert
-            Assert.True(result);
-        }
+        // Assert
+        Assert.True(result);
     }
 }
