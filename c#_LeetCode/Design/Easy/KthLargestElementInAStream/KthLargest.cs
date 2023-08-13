0afeec3a8478524a2b95154416c0bddd8e806d06
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-08-13 17:36:49 +0300

    update solution of Kth Largest Element in a Stream

--- c#_LeetCode/Design/Easy/KthLargestElementInAStream/KthLargest.cs
+++ c#_LeetCode/Design/Easy/KthLargestElementInAStream/KthLargest.cs
@@ -1,30 +1,28 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 
-namespace LeetCode.Design.Easy.KthLargestElementInAStream
+namespace LeetCode.Design.Easy.KthLargestElementInAStream;
+
+//https://leetcode.com/problems/kth-largest-element-in-a-stream/
+public class KthLargest
 {
-    //https://leetcode.com/problems/kth-largest-element-in-a-stream/
-    public class KthLargest
+    private readonly int k;
+    PriorityQueue<int, int> priority = new();
+
+    public KthLargest(int k, int[] nums)
     {
-        private readonly List<int> _list;
-        private readonly int _k;
+        this.k = k;
 
-        public KthLargest(int k, int[] nums)
-        {
-            _k = k;
-            _list = nums.OrderBy(x => x).ToList();
-        }
+        foreach (int num in nums)
+            Add(num);
+    }
 
-        public int Add(int val)
-        {
-            var index = _list.BinarySearch(val);
+    public int Add(int val)
+    {
+        priority.Enqueue(val, val);
 
-            if (index < 0)
-                _list.Insert(~index, val);
-            else 
-                _list.Insert(index, val);
+        if(priority.Count > k)
+            priority.Dequeue();
 
-            return _list[_list.Count - _k];
-        }
+        return priority.Peek();
     }
 }
