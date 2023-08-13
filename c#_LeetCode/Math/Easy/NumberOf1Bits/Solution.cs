1b488b355a9a55d51bc629bb82ea15d9488ff665
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-08-13 21:16:34 +0300

    add solution of Number of 1 Bits

---
+++ c#_LeetCode/Math/Easy/NumberOf1Bits/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Math.Easy.NumberOf1Bits;
+
+//https://leetcode.com/problems/number-of-1-bits/
+public class Solution
+{
+    public int HammingWeight(uint n)
+    {
+        int oneCount = 0;
+
+        while(n != 0)
+        {
+            if(n % 2 == 1)
+                oneCount++;
+
+            n /= 2;
+        }
+
+        return oneCount;
+    }
+}
