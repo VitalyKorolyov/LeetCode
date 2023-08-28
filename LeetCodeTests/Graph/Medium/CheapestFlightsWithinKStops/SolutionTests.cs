6a0120f8a13659578f702896967f734bcbdd9888
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-08-28 23:39:40 +0300

    add solution of Cheapest Flights Within K Stops

---
+++ LeetCodeTests/Graph/Medium/CheapestFlightsWithinKStops/SolutionTests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode.Graph.Medium.CheapestFlightsWithinKStops;
+using Xunit;
+
+namespace LeetCodeTests.Graph.Medium.CheapestFlightsWithinKStops;
+
+public class SolutionTests
+{
+    [Fact]
+    public void FindCheapestPrice_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int n = 3;
+        int[][] flights = new int[][]
+        {
+            new int[]{ 0,1,100 },
+            new int[]{ 1,2,100 },
+            new int[]{ 0,2,500 },
+        };
+        int src = 0;
+        int dst = 2;
+        int k = 1;
+
+        // Act
+        var result = solution.FindCheapestPrice(n, flights, src, dst, k);
+
+        // Assert
+        Assert.True(result == 200);
+    }
+}
---
+++ c#_LeetCode/Graph/Medium/CheapestFlightsWithinKStops/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Graph.Medium.CheapestFlightsWithinKStops;
+
+//https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
+public class Solution
+{
+    public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k)
+    {
+        Dictionary<int, List<(int city, int price)>> graph = new();
+        for (int i = 0; i < n; i++)
+            graph[i] = new List<(int city, int price)>();
+
+        foreach (int[] flight in flights)
+            graph[flight[0]].Add((flight[1], flight[2]));
+
+        int[] prices = new int[n];
+        System.Array.Fill(prices, int.MaxValue);
+
+        Queue<(int city, int price, int stops)> queue = new();
+        queue.Enqueue((src, 0, 0));
+
+        while (queue.Count > 0)
+        {
+            var (city, cost, stops) = queue.Dequeue();
+
+            if (stops > k) continue;
+
+            foreach (var (nextCity, currentCost) in graph[city])
+            {
+                int newCost = currentCost + cost;
+
+                if (prices[nextCity] < newCost) continue;
+
+                prices[nextCity] = newCost;
+                queue.Enqueue((nextCity, newCost, stops + 1));
+            }
+        }
+
+        return prices[dst] == int.MaxValue ? -1 : prices[dst];
+    }
+}
