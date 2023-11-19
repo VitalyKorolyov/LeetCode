a070e084012e3293c4a83f95a5bc507306c07cb5
Vitaly Korolev <vitaly.korolyov@gmail.com>
2023-11-19 18:31:24 +0200

    add solution of Frequency of the Most Frequent Element

---
+++ LeetCodeTests/Array/Medium/FrequencyOfTheMostFrequentElement/SolutionTests.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Array.Medium.FrequencyOfTheMostFrequentElement;
+using Xunit;
+
+namespace LeetCodeTests.Array.Medium.FrequencyOfTheMostFrequentElement;
+
+public class SolutionTests
+{
+    [Fact]
+    public void MaxFrequency_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+        var solution = new Solution();
+        int[] nums = { 1, 2, 4 };
+        int k = 5;
+
+        // Act
+        var result = solution.MaxFrequency(nums, k);
+
+        // Assert
+        Assert.Equal(3, result);
+
+        nums = new int[] { 1, 4, 8, 13 };
+
+        result = solution.MaxFrequency(nums, k);
+
+        // Assert
+        Assert.Equal(2, result);
+    }
+}
---
+++ c#_LeetCode/Array/Medium/FrequencyOfTheMostFrequentElement/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Array.Medium.FrequencyOfTheMostFrequentElement;
+
+//https://leetcode.com/problems/frequency-of-the-most-frequent-element/description/
+public class Solution
+{
+    public int MaxFrequency(int[] nums, int k)
+    {
+        System.Array.Sort(nums);
+
+        int answer = 1;
+        int l = 0;
+        int r = 1;
+
+        while(r < nums.Length)
+        {
+            int currentDiff = (nums[r] - nums[r - 1]) * (r - l);
+
+            if (currentDiff <= k)
+            {
+                k -= currentDiff;
+                r++;
+                answer = System.Math.Max(answer, r - l);
+            }
+            else
+            {
+                var moveDiff = nums[r - 1] - nums[l];
+                k += moveDiff;
+                l++;
+            }
+        }
+
+        return answer;
+    }
+}
